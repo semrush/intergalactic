70c05b7d24c6325ab89a8fbc0cf928fde3532d76
r.lysov <r.lysov@semrush.com>
2021-02-08 16:08:56 +0300

    [generator-component] Change default template.

---
+++ tools/generator-component/template-react/src/index.d.ts
@@ -0,0 +1,7 @@
+import { CProps } from '@semcore/core';
+
+export interface I{{ properCase name }}Props {}
+
+declare const {{ properCase name }}: (<T>(props: CProps<I{{ properCase name }}Props & T>) => ReturnEl);
+
+export default Input;
