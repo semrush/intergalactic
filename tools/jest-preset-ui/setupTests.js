bf46ffebc1a54fc35acd7896e859555477e175af
r.lysov <r.lysov@semrush.com>
2021-02-11 16:03:48 +0300

    [tests] Add fake method matchMedia for jsdom

--- tools/jest-preset-ui/setupTests.js
+++ tools/jest-preset-ui/setupTests.js
@@ -14,3 +14,16 @@ if (global.document) {
     },
   });
 }
+
+if (global.window) {
+  window.matchMedia = jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }));
+}
