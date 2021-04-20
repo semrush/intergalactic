4ab381a87695cd4be4bb01b5cff5075ba8eb0550
elena.krasnopolskaia <elena.krasnopolskaia@semrush.com>
2021-04-20 18:33:24 +0300

    [slider] add slider

--- website/docs/components/slider/examples/slider-with-input.js
+++ website/docs/components/slider/examples/slider-with-input.js
@@ -1,48 +1,23 @@
 import React, { useState } from 'react';
 import Slider from '@semcore/slider';
 import InputNumber from '@semcore/input-number';
-import Tooltip from '@semcore/tooltip';
-import { Box } from '@semcore/flex-box';
 
 const Demo = () => {
-  const [value, setValue] = useState(51);
-  const [error, setError] = useState('');
-  const min = 10;
-  const max = 100;
-
-  const handleChange = (value) => {
-    setValue(value);
-  };
-
-  const handleInput = (value) => {
-    if (value > max || value < min) {
-      setError(`Please enter a valid value`);
-    } else {
-      setError('');
-      setValue(value);
-    }
+  const [value, setValue] = useState(2);
+  const handleChange = (v) => {
+    setValue(v);
   };
 
   return (
     <>
-      <Box w={140}>
-        <Slider mb={3} value={value} onChange={handleChange} step={1} min={min} max={max}>
-          <Slider.Knob />
-          <Slider.Bar />
-        </Slider>
-      </Box>
-      <Tooltip
-        title={`Please enter a valid value within ${min} and ${max}.`}
-        visible={!!error}
-        interaction="click"
-        theme="warning"
-        placement="top-start"
-      >
-        <InputNumber mt={4} w={140} size="m" state={!!error ? 'invalid' : 'normal'}>
-          <InputNumber.Value step={1} value={value} onChange={handleInput} />
-          <InputNumber.Controls showControls />
-        </InputNumber>
-      </Tooltip>
+      <Slider mb={3} value={value} onChange={handleChange} step={1} min={1} max={3}>
+        <Slider.Knob color="#1d9c00" />
+        <Slider.Bar color="#8bc83570" />
+      </Slider>
+      <InputNumber mt={4} w={120} size="m" state="normal">
+        <InputNumber.Value max={100} min={0} step={1} value={value} />
+        <InputNumber.Controls />
+      </InputNumber>
     </>
   );
 };
--- website/docs/components/slider/slider-code.md
+++ website/docs/components/slider/slider-code.md
@@ -2,18 +2,6 @@
 title: Code
 ---
 
-@## Slider with value labels
-
 @example slider
 
-@## Slider with InputNumber
-
-The slider can be used in combination with InputNumber. Also, if you enter too large or too small value into the InputNumber, an error is shown
-
 @example slider-with-input
-
-@## Possibility of changing slider colours
-
-If required, you can assign your own colour to all parts of the slider
-
-@example slider-color
