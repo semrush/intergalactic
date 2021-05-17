955faf7b14ce6d43d4b9f1a928120d944060c112
r.lysov <r.lysov@semrush.com>
2021-05-17 11:47:57 +0300

    [@semcore/time-picker] upgrade to 2.4.0

--- semcore/time-picker/src/index.d.ts
+++ semcore/time-picker/src/index.d.ts
@@ -1,5 +1,5 @@
-import { ComponentProps } from 'react';
-import { CProps, PropGetter, ReturnEl } from '@semcore/core';
+import React, { ComponentProps } from 'react';
+import { CProps, PropGetterFn, ReturnEl } from '@semcore/core';
 import { IInputProps, IInputValueProps } from '@semcore/input';
 import { Box, IBoxProps } from '@semcore/flex-box';
 import { IKeyboardFocusProps } from '@semcore/utils/lib/enhances/keyboardFocusEnhance';
@@ -18,22 +18,21 @@ export interface ITimePickerProps extends Omit<IInputProps, 'size'> {
   is12Hour?: boolean;
 }
 
-export interface ITimePickerContext {
-  getHoursProps: PropGetter<TimePickerRoot['getHoursProps']>;
-  getFormatProps: PropGetter<TimePickerRoot['getFormatProps']>;
-  getMinutesProps: PropGetter<TimePickerRoot['getMinutesProps']>;
-}
-
 export interface ITimePickerItemProps extends IInputValueProps {
   /** Step for changing of the values in the dropdown list */
   step?: number;
-  /* @ignore */
-  $onValueChange?: (value: string, field?: string, event?: React.SyntheticEvent) => void;
 }
 
-export interface ITimePickerFormatProps extends IBoxProps, IKeyboardFocusProps {}
+export interface ITimePickerFormatProps extends IBoxProps, IKeyboardFocusProps {
+}
 
-declare const TimePicker: ((props: CProps<ITimePickerProps, ITimePickerContext>) => ReturnEl) & {
+export interface ITimePickerContext {
+  getHoursProps: PropGetterFn;
+  getFormatProps: PropGetterFn;
+  getMinutesProps: PropGetterFn;
+}
+
+declare const TimePicker: (<T>(props: CProps<ITimePickerProps & T, ITimePickerContext>) => ReturnEl) & {
   Hours: <T>(props: ITimePickerItemProps & T) => ReturnEl;
   Minutes: <T>(props: ITimePickerItemProps & T) => ReturnEl;
   Separator: <T>(props: ComponentProps<typeof Box> & T) => ReturnEl;
