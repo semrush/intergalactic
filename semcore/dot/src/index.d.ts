e7ed2ff5cb8b755422360da018a0a7bc31608e38
r.lysov <r.lysov@semrush.com>
2021-06-08 17:32:11 +0300

    [@semcore/dot] upgrade to 2.2.0

---
+++ semcore/dot/src/index.d.ts
@@ -0,0 +1,23 @@
+import { IBoxProps } from '@semcore/flex-box';
+import { ReturnEl } from '@semcore/core';
+
+export interface IDotProps extends IBoxProps {
+  /** Size of the dot
+   * @default m
+   */
+  size?: 'xl' | 'l' | 'm';
+  /** Property for placing the Dot in the upper right corner of the component
+   * @default false
+   * */
+  up?: boolean;
+  /** The property for Dot visibility control */
+  hidden?: boolean;
+  /**
+   * @ignore
+   *  */
+  invisible?: boolean;
+}
+
+declare const Dot: (<T>(props: IDotProps & T) => ReturnEl);
+
+export default Dot;
