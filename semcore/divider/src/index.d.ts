8d85683023db554c0e177f024347f78abb6c0696
r.lysov <r.lysov@semrush.com>
2021-06-08 17:41:10 +0300

    [@semcore/divider] upgrade to 2.2.0

---
+++ semcore/divider/src/index.d.ts
@@ -0,0 +1,23 @@
+import { IBoxProps } from '@semcore/flex-box';
+import { ReturnEl } from '@semcore/core';
+
+export interface IDividerProps extends IBoxProps {
+  /**
+   * Type of the divider
+   * @default primary
+   */
+  use?: 'primary' | 'secondary';
+  /**
+   * Theme of the divider
+   */
+  theme?: 'invert';
+  /**
+   * Orientation of the divider
+   * @default horizontal
+   */
+  orientation?: 'horizontal' | 'vertical';
+}
+
+declare const Divider: (<T>(props: IDividerProps & T) => ReturnEl);
+
+export default Divider;
