0cb7b0ccf5a88ae501179ebb2e8f5187ad8a54b6
s.kobets <s.kobets@semrush.com>
2021-07-27 17:15:31 +0300

    [Site] added example export to image from svg for d3-chart

--- website/docs/data-display/d3-chart/d3-chart.md
+++ website/docs/data-display/d3-chart/d3-chart.md
@@ -134,6 +134,16 @@ beta: true
 
 @example sync-charts
 
+@## Экспорт в изображение
+
+@example export-in-image
+
+@## Начальная загрузка
+
+Во время первоначальной загрузки чартов вы можете использовать вместо них [Skeleton](/components/skeleton/) c соответствующим типом чарта. Если у чарта есть заголовок, то он должен отображаться во время загрузки.
+
+Пример использования вы можете посмотреть [здесь](https://i.semrush.com/data-display/line-chart/line-chart-code/#ac26f2).
+
 @page line
 @page bar
 @page area
@@ -141,9 +151,3 @@ beta: true
 @page venn
 @page d3-chart-api
 @page d3-chart-changelog
-
-@## Начальная загрузка
-
-Во время первоначальной загрузки чартов вы можете использовать вместо них [Skeleton](/components/skeleton/) c соответствующим типом чарта. Если у чарта есть заголовок, то он должен отображаться во время загрузки.
-
-Пример использования вы можете посмотреть [здесь](https://i.semrush.com/data-display/line-chart/line-chart-code/#ac26f2).
---
+++ website/docs/data-display/d3-chart/examples/export-in-image.js
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from 'react';
+import { scaleLinear } from 'd3-scale';
+import { Line, minMax, Plot, XAxis, YAxis } from '@semcore/d3-chart';
+import { Flex } from '@semcore/flex-box';
+import DropdownMenu from '@semcore/dropdown-menu';
+import Button from '@semcore/button';
+import FileExportXS from '@semcore/icon/lib/FileExport/xs';
+
+const EXPORTS = ['PNG', 'JPG', 'WEBP'];
+
+export default () => {
+  const [visible, updateVisible] = useState(false);
+
+  const svg = React.createRef();
+  const download = React.createRef();
+  const MARGIN = 40;
+  const width = 500;
+  const height = 300;
+
+  const xScale = scaleLinear()
+    .range([MARGIN, width - MARGIN])
+    .domain(minMax(data, 'x'));
+
+  const yScale = scaleLinear()
+    .range([height - MARGIN, MARGIN])
+    .domain([0, 10]);
+
+  useEffect(() => {
+    const svgElement = svg.current;
+    const linkElements = Array.from(download.current.getElementsByTagName('a'));
+    const svgString = getSVGString(svgElement);
+    EXPORTS.forEach((name, ind) => {
+      const format = name.toLowerCase();
+      svgString2Image(svgString, 2 * width, 2 * height, format, save);
+      function save(image) {
+        const el = linkElements[ind];
+        el.download = `image.${format}`;
+        el.href = image;
+      }
+    });
+  }, []);
+  return (
+    <Flex>
+      <Plot ref={svg} data={data} scale={[xScale, yScale]} width={width} height={height}>
+        <YAxis ticks={yScale.ticks()}>
+          <YAxis.Ticks />
+          <YAxis.Grid />
+        </YAxis>
+        <XAxis ticks={xScale.ticks()}>
+          <XAxis.Ticks />
+        </XAxis>
+        <Line x="x" y="y">
+          <Line.Dots display />
+        </Line>
+      </Plot>
+      <DropdownMenu onVisibleChange={updateVisible}>
+        <DropdownMenu.Trigger tag={Button}>
+          <Button.Addon tag={FileExportXS} />
+          <Button.Text>Export</Button.Text>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Popper wMax="257px" visible style={{ display: visible ? 'block' : 'none' }}>
+          <DropdownMenu.List ref={download}>
+            {EXPORTS.map((name) => (
+              <DropdownMenu.Item tag="a" key={name}>
+                {name}
+              </DropdownMenu.Item>
+            ))}
+          </DropdownMenu.List>
+        </DropdownMenu.Popper>
+      </DropdownMenu>
+    </Flex>
+  );
+};
+
+const data = Array(20)
+  .fill({})
+  .map((d, i) => ({
+    x: i,
+    y: Math.random().toFixed(1) * 10,
+  }));
+
+function getSVGString(svgNode) {
+  svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
+  const cssStyleText = getCSSStyles(svgNode);
+  appendCSS(cssStyleText, svgNode);
+
+  const serializer = new XMLSerializer();
+  let svgString = serializer.serializeToString(svgNode);
+  svgString = svgString.replace(/(\w+)?:?xlink=/g, 'xmlns:xlink='); // Fix root xlink without namespace
+  svgString = svgString.replace(/NS\d+:href/g, 'xlink:href'); // Safari NS namespace fix
+
+  return svgString;
+
+  function getCSSStyles(parentElement) {
+    const selectorTextArr = [];
+
+    for (let c = 0; c < parentElement.classList.length; c++) {
+      if (!contains('.' + parentElement.classList[c], selectorTextArr))
+        selectorTextArr.push('.' + parentElement.classList[c]);
+    }
+
+    // Add Children element Ids and Classes to the list
+    const nodes = parentElement.getElementsByTagName('*');
+    for (let i = 0; i < nodes.length; i++) {
+      const id = nodes[i].id;
+      if (!contains('#' + id, selectorTextArr)) selectorTextArr.push('#' + id);
+
+      const classes = nodes[i].classList;
+      for (let c = 0; c < classes.length; c++)
+        if (!contains('.' + classes[c], selectorTextArr)) selectorTextArr.push('.' + classes[c]);
+    }
+
+    // Extract CSS Rules
+    let extractedCSSText = '';
+    for (let i = 0; i < document.styleSheets.length; i++) {
+      const s = document.styleSheets[i];
+
+      try {
+        if (!s.cssRules) continue;
+      } catch (e) {
+        if (e.name !== 'SecurityError') throw e; // for Firefox
+        continue;
+      }
+
+      const cssRules = s.cssRules;
+      for (let r = 0; r < cssRules.length; r++) {
+        if (
+          cssRules[r].selectorText &&
+          selectorTextArr.some((s) => cssRules[r].selectorText.includes(s))
+        )
+          extractedCSSText += cssRules[r].cssText;
+      }
+    }
+
+    return extractedCSSText;
+
+    function contains(str, arr) {
+      return arr.indexOf(str) === -1 ? false : true;
+    }
+  }
+
+  function appendCSS(cssText, element) {
+    const styleElement = document.createElement('style');
+    styleElement.setAttribute('type', 'text/css');
+    styleElement.innerHTML = cssText;
+    const refNode = element.hasChildNodes() ? element.children[0] : null;
+    element.insertBefore(styleElement, refNode);
+  }
+}
+
+function svgString2Image(svgString, width, height, format, callback) {
+  format = format ? format : 'png';
+  const imgsrc = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgString)));
+
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+
+  canvas.width = width;
+  canvas.height = height;
+
+  const image = new Image();
+  image.onload = function() {
+    context.clearRect(0, 0, width, height);
+    context.drawImage(image, 0, 0, width, height);
+
+    const img = canvas.toDataURL(`image/${format}`);
+    callback(img);
+  };
+
+  image.src = imgsrc;
+}
