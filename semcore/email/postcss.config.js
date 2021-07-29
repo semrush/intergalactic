d914f4abe8e8e5a1583ea38b83dd17d54dd33609
s.kobets <s.kobets@semrush.com>
2021-07-29 10:18:22 +0300

    [email] added typography, divider

--- semcore/email/postcss.config.js
+++ semcore/email/postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
     require('postcss-nested'),
     require('postcss-import'),
     require('postcss-css-variables')({ preserve: false }),
+    require('postcss-custom-media'),
     require('autoprefixer'),
     require('cssnano'),
   ],
