0ada7363abf17dd354ab1ca96a540a993be7f6e0
s.kobets <s.kobets@semrush.com>
2021-08-09 18:05:42 +0300

    [Email] update color name and package name

--- website/docs/product-emails/core-email/examples/utils.js
+++ website/docs/product-emails/core-email/examples/utils.js
@@ -1,4 +1,4 @@
 export const bg_100_400 = ['gray'];
-export const bg_100_300 = ['green', 'orange', 'red', 'purple', 'blue'];
-export const bg_100_200 = ['emerald'];
+export const bg_100_300 = ['lime', 'orange', 'red', 'purple', 'blue'];
+export const bg_100_200 = ['green'];
 export const bg = ['pink', 'black', 'white'];
