f2c43cf26bb156788d9377aef1319fc16a3bae29
elena.krasnopolskaia <elena.krasnopolskaia@semrush.com>
2021-11-26 13:57:08 +0300

    [Redesign] little fix

--- tools/babel-plugin-react-semcore/utils/vars.js
+++ tools/babel-plugin-react-semcore/utils/vars.js
@@ -2,17 +2,6 @@ const fs = require('fs');
 
 const CSS_VARS_REG = /--[\w+\-]+:\s+(#[\w\d]+|rgba?\(\d+,\s*\d+,\s*\d+(?:,\s*(\d+(?:\.\d+)?))?\))/g;
 
-// const cssVarsPath = path.resolve(__dirname, '../', 'style/var.css');
-//
-// const cssVarsContent = fs.readFileSync(cssVarsPath, 'utf8');
-//
-// const varsList = cssVarsContent.match(CSS_VARS_REG).map(cssVarDeclaration => {
-//   const [varName, varValue] = cssVarDeclaration.split(':');
-//   const colorName = varName.replace('--', '').trim();
-//   return { [colorName]: varValue.trim() };
-// });
-// module.exports = Object.assign({}, ...varsList);
-
 function getColorVars(cssPath) {
   const content = fs.readFileSync(cssPath, 'utf8');
   return content.match(CSS_VARS_REG).reduce((acc, match) => {
