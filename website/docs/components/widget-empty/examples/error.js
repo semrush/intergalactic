c699ec47302de44d43a3656084a8d42be52a84d5
elena.krasnopolskaia <elena.krasnopolskaia@semrush.com>
2021-12-14 11:14:01 +0300

    [website] update feedback-yes-no and widget-empty examples

--- website/docs/components/widget-empty/examples/error.js
+++ website/docs/components/widget-empty/examples/error.js
@@ -8,6 +8,7 @@ import { Text } from '@semcore/typography';
 import Button from '@semcore/button';
 import Link from '@semcore/link';
 import RepeatXS from '@semcore/icon/lib/Repeat/xs';
+import Divider from '@semcore/divider';
 
 const options = ['de', 'en', 'es', 'fr', 'it', 'ja', 'pt', 'ru', 'zh', 'ko', 'vi'].map((o) => ({
   value: o,
@@ -22,7 +23,10 @@ export default () => {
       Select lang: <Select options={options} value={lang} onChange={setLang} />
       <I18nProvider value={lang}>
         <Card my={5}>
-          <Text size={400}>Known error</Text>
+          <Text size={300} bold>
+            Known error
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <Error>
             <Box mt={4}>
               <Button>Try again</Button>
@@ -30,7 +34,10 @@ export default () => {
           </Error>
         </Card>
         <Card my={5}>
-          <Text size={400}>Don't known error</Text>
+          <Text size={300} bold>
+            Don't known error
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <Error
             description={
               <>
--- website/docs/components/widget-empty/examples/no-data.js
+++ website/docs/components/widget-empty/examples/no-data.js
@@ -4,6 +4,7 @@ import { I18nProvider } from '@semcore/utils/lib/enhances/WithI18n';
 import { NoData } from '@semcore/widget-empty';
 import Card from '@semcore/card';
 import { Text } from '@semcore/typography';
+import Divider from '@semcore/divider';
 
 const options = ['de', 'en', 'es', 'fr', 'it', 'ja', 'pt', 'ru', 'zh', 'ko', 'vi'].map((o) => ({
   value: o,
@@ -18,111 +19,192 @@ export default () => {
       Select lang: <Select options={options} value={lang} onChange={setLang} />
       <I18nProvider value={lang}>
         <Card my={5} pt={4}>
-          <Text size={400}>Line chart</Text>
+          <Text size={300} bold>
+            Line chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="line-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Area chart</Text>
+          <Text size={300} bold>
+            Area chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="area-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Stacked area chart</Text>
+          <Text size={300} bold>
+            Stacked area chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="stacked-area-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Funnel chart</Text>
+          <Text size={300} bold>
+            Funnel chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="funnel-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Pie chart</Text>
+          <Text size={300} bold>
+            Pie chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="pie-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Donut chart</Text>
+          <Text size={300} bold>
+            Donut chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="donut-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Vertical bar chart</Text>
+          <Text size={300} bold>
+            Vertical bar chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="vertical-bar-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Horizontal bar chart</Text>
+          <Text size={300} bold>
+            Horizontal bar chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="horizontal-bar-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Scatter plot chart</Text>
+          <Text size={300} bold>
+            Scatter plot chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="scatter-plot-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Venn chart</Text>
+          <Text size={300} bold>
+            Venn chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="venn-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Radar chart</Text>
+          <Text size={300} bold>
+            Radar chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="radar-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Lollipop chart</Text>
+          <Text size={300} bold>
+            Lollipop chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="lollipop-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Choropleth map chart</Text>
+          <Text size={300} bold>
+            Choropleth map chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="choropleth-map-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Combined chart</Text>
+          <Text size={300} bold>
+            Combined chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="combined-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Sankey chart</Text>
+          <Text size={300} bold>
+            Sankey chart
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="sankey-chart" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Table</Text>
+          <Text size={300} bold>
+            Table
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="table" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Text links etc</Text>
+          <Text size={300} bold>
+            Text links etc
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="text-links-etc" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Other data</Text>
+          <Text size={300} bold>
+            Other data
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="other-data" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Deleted page</Text>
+          <Text size={300} bold>
+            Deleted page
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="deleted-page" description="Some text about deleted page ¯\_(ツ)_/¯" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Congrats</Text>
+          <Text size={300} bold>
+            Congrats
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="congrats" description="Wow! You are doing great! Nothing to fix here." />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Good</Text>
+          <Text size={300} bold>
+            Good
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="good" description="Wow! You are doing great!" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Duplicates</Text>
+          <Text size={300} bold>
+            Duplicates
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="duplicates" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Nexttime</Text>
+          <Text size={300} bold>
+            Nexttime
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="nexttime" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Nothing found</Text>
+          <Text size={300} bold>
+            Nothing found
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="nothing-found" description="Try changing your filters." />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Processing</Text>
+          <Text size={300} bold>
+            Processing
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="processing" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Suggestions</Text>
+          <Text size={300} bold>
+            Suggestions
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="suggestions" />
         </Card>
         <Card my={5} pt={4}>
-          <Text size={400}>Tag cloud</Text>
+          <Text size={300} bold>
+            Tag cloud
+          </Text>
+          <Divider orientation="horizontal" m={'8px 0 12px -20px'} w={'calc(100% + 40px)'} />
           <NoData type="tag-cloud" />
         </Card>
       </I18nProvider>
