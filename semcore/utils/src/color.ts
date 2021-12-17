a3dcebcf41c569962437ab93e0e4db65c69b7418
elena.krasnopolskaia <elena.krasnopolskaia@semrush.com>
2021-12-17 11:06:10 +0300

    remove duplicate code

--- semcore/utils/src/color.ts
+++ semcore/utils/src/color.ts
@@ -108,13 +108,11 @@ function colorRGB(color) {
 }
 
 function hex2rgb(hex) {
-  if (hex.match(/^#?[a-f\d]{6}$/))
-    return hex
-      .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
-      .substring(1)
-      .match(/.{2}/g)
-      .map((x) => parseInt(x, 16));
-  else return '';
+  return hex
+    .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
+    .substring(1)
+    .match(/.{2}/g)
+    .map((x) => parseInt(x, 16));
 }
 
 export function opacity(color?: string, percent: number = 1) {
