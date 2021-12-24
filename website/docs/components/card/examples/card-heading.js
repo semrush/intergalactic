3dd638964e37575d7e4f8b4517c2c67dd746df0a
s.kobets <s.kobets@semrush.com>
2021-12-24 13:27:33 +0300

    [SITE] fixed example Card

--- website/docs/components/card/examples/card-heading.js
+++ website/docs/components/card/examples/card-heading.js
@@ -8,15 +8,13 @@ import Divider from '@semcore/divider';
 const TooltipContent = `Hey! Don't forget to place some useful info here 😏`;
 
 export default () => (
-  <Card wMax="400px">
+  <Card>
     <Flex mb={2} alignItems="center">
       <Card.Title hint={TooltipContent}>Card heading</Card.Title>
       <SettingsS color="stone" ml="auto" interactive />
     </Flex>
-    <Flex>
-      <Card.Description>This is card additional information or insights.</Card.Description>
-    </Flex>
-    <Divider orientation="horizontal" mb={5} ml={-5} w={440} />
+    <Card.Description>This is card additional information or insights.</Card.Description>
+    <Divider orientation="horizontal" mb={5} ml={-6} w={`calc(100% + 48px)`} />
     <Text size={100}>Your awesome card content ✨</Text>
   </Card>
 );
