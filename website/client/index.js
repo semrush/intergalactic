98c37ecfeb1b97aa1b18a272cbd78710a01fd2d0
r.lysov <r.lysov@semrush.com>
2021-12-27 13:24:54 +0300

    🎉 Add a holiday to our boring world! 🎉

--- website/client/index.js
+++ website/client/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './components/happyNewYear';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
