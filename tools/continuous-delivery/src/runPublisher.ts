1ac2386443c248b0f5588b72a87f37f334d7396c
Mikhail Sereniti <phytonmk@gmail.com>
2022-02-08 11:40:01 +0300

    handled different publisher roots in cd tool

--- tools/continuous-delivery/src/runPublisher.ts
+++ tools/continuous-delivery/src/runPublisher.ts
@@ -1,17 +1,30 @@
-import { execSync } from 'child_process';
 import { VersionPatch } from './makeVersionPatches';
 
 export const runPublisher = async (versionPatches: VersionPatch[]) => {
-  const packageArg = versionPatches
-    .map((patch) => {
-      const [, name] = patch.package.name.split('/');
-      return name.replace(/[^@\/\-\w_]/, '_');
-    })
-    .join(',');
+  const byRoot: { [root: string]: VersionPatch[] } = {};
 
-  const args = `--no-check-git --release current --no-check-changelog --package ${packageArg}`;
+  for (const patch of versionPatches) {
+    const pathParts = patch.package.path.split('/');
+    const root = pathParts[pathParts.length - 2];
+    byRoot[root] = byRoot[root] || [];
+    byRoot[root].push(patch);
+  }
 
-  execSync(`yarn pub ${args}`, {
-    stdio: 'inherit',
-  });
+  for (const root in byRoot) {
+    const packageArg = byRoot[root]
+      .map((patch) => {
+        const [, name] = patch.package.name.split('/');
+        return name.replace(/[^@\/\-\w_]/, '_');
+      })
+      .join(',');
+
+    const args = `--no-check-git --release current --no-check-changelog --root ./${root} --package ${packageArg}`;
+
+    // eslint-disable-next-line no-console
+    console.log(`Running publisher with following args: ${args}`);
+
+    execSync(`yarn pub ${args}`, {
+      stdio: 'inherit',
+    });
+  }
 };
