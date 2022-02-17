60b4b3e40a1178b471b1ad73333f14dbf5140d8c
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2022-02-17 18:09:51 +0300

    css hashes based on relative paths only

--- tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/purgeCSS/index.expected.js
+++ tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/purgeCSS/index.expected.js
@@ -7,12 +7,12 @@ var _ref =
     /*__reshadow_css_start__*/
     (__css__(
       /*__inner_css_start__*/
-      '.___SButton_wdxwg_gg_{padding:0 20px;background:purple}.___SButton_wdxwg_gg_._theme_default_wdxwg_gg_{background:#bada55}',
+      '.___SButton_xqfr3_gg_{padding:0 20px;background:purple}.___SButton_xqfr3_gg_._theme_default_xqfr3_gg_{background:#bada55}',
       '6pdq09_gg_',
     ),
     {
-      __SButton: `___SButton_wdxwg_gg_`,
-      _theme_default: `_theme_default_wdxwg_gg_`,
+      __SButton: `___SButton_xqfr3_gg_`,
+      _theme_default: `_theme_default_xqfr3_gg_`,
     }),
   styles = _extends({}, _ref);
 
