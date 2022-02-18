68cfb8fb244946ae21c14803fc0a544627051979
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2022-02-18 18:22:59 +0300

    updated snapshots hashes

--- tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/purgeCSS/index.expected.js
+++ tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/purgeCSS/index.expected.js
@@ -7,12 +7,12 @@ var _ref =
     /*__reshadow_css_start__*/
     (__css__(
       /*__inner_css_start__*/
-      '.___SButton_xqfr3_gg_{padding:0 20px;background:purple}.___SButton_xqfr3_gg_._theme_default_xqfr3_gg_{background:#bada55}',
+      '.___SButton_h1k9m_gg_{padding:0 20px;background:purple}.___SButton_h1k9m_gg_._theme_default_h1k9m_gg_{background:#bada55}',
       '6pdq09_gg_',
     ),
     {
-      __SButton: `___SButton_xqfr3_gg_`,
-      _theme_default: `_theme_default_xqfr3_gg_`,
+      __SButton: `___SButton_h1k9m_gg_`,
+      _theme_default: `_theme_default_h1k9m_gg_`,
     }),
   styles = _extends({}, _ref);
 
