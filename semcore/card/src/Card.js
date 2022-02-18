c6c01c6e3a98df5ef76f14d106a953382a1ce6e1
elena.krasnopolskaia <elena.krasnopolskaia@semrush.com>
2022-02-18 13:42:14 +0300

    [Card] Reverted to a previous state with styles in the card itself and without Header and Body

--- semcore/card/src/Card.js
+++ semcore/card/src/Card.js
@@ -6,30 +6,17 @@ import Tooltip from '@semcore/tooltip';
 import Info from '@semcore/icon/Info/m';
 
 import style from './style/card.shadow.css';
-import { isAdvanceMode } from '@semcore/utils/lib/findComponent';
 
-class CardRoot extends Component {
+class Card extends Component {
   static displayName = 'Card';
 
   static style = style;
 
   render() {
     const SCard = Root;
-    const { Children, styles } = this.asProps;
+    const { styles } = this.asProps;
 
-    const advanceMode = isAdvanceMode(Children, [Card.Header.displayName, Card.Body.displayName]);
-
-    return sstyled(styles)(
-      <SCard render={Box}>
-        {advanceMode ? (
-          <Children />
-        ) : (
-          <Card.Body>
-            <Children />
-          </Card.Body>
-        )}
-      </SCard>,
-    );
+    return sstyled(styles)(<SCard render={Box} />);
   }
 }
 
@@ -55,23 +42,7 @@ function Description(props) {
   return sstyled(styles)(<SDescription render={Text} tag="p" />);
 }
 
-function Header(props) {
-  const { styles } = props;
-  const SHeader = Root;
-  return sstyled(styles)(<SHeader render={Box} {...props} />);
-}
-
-function Body(props) {
-  const { styles } = props;
-  const SBody = Root;
-  return sstyled(styles)(<SBody render={Box} {...props} />);
-}
-
-const Card = createComponent(CardRoot, {
+export default createComponent(Card, {
   Title,
   Description,
-  Header,
-  Body,
 });
-
-export default Card;
