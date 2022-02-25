d95fe86b20272f62bbf122280b763362e9940dad
r.lysov <r.lysov@semrush.com>
2022-02-25 12:53:15 +0300

    Test CI

--- semcore/widget-empty/src/WidgetEmpty.js
+++ semcore/widget-empty/src/WidgetEmpty.js
@@ -10,7 +10,7 @@ const version = preval`
 `;
 
 export const getIconPath = (name) => {
-  return `https://static.semrush.com/ui-kit/widget-empty/${version}/${name}.svg`;
+  return `http://static.semrush.com/ui-kit/widget-empty/${version}/${name}.svg`;
 };
 
 class WidgetEmpty extends Component {
