293237f4585ec07cb544deaed7eab29cc032886d
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2022-03-03 13:59:31 +0300

    zero tolerance to failed screenshots

--- tools/jest-preset-ui/src/setupTests.js
+++ tools/jest-preset-ui/src/setupTests.js
@@ -7,7 +7,7 @@ const toMatchImageSnapshot = configureToMatchImageSnapshot({
   customDiffConfig: {
     ssim: 'fast',
   },
-  failureThreshold: 0.01,
+  failureThreshold: 0,
   failureThresholdType: 'percent',
 });
 
