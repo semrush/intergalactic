529629bf9051e083870c3677e93dd9b17364200a
r.lysov <r.lysov@semrush.com>
2022-03-09 15:33:11 +0300

    [website] Add ga event for sandbox

--- website/client/components/Sandbox.js
+++ website/client/components/Sandbox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Tooltip from '@semcore/tooltip';
 import { getParameters } from 'codesandbox/lib/api/define';
 
@@ -47,12 +47,24 @@ ${ExampleRawComponent}`,
     },
   });
 
+  const openHandler = useCallback(() => {
+    if (window.dataLayer) {
+      dataLayer.push({
+        event: 'semrush',
+        eventCategory: 'intergalactic:docs',
+        eventAction: 'click:sandbox',
+        eventLabel: path,
+      });
+    }
+  }, [path]);
+
   return (
     <Tooltip
       title="Open in CodeSandbox"
       tag="a"
       target="__blank"
       href={`https://codesandbox.io/api/v1/sandboxes/define?parameters=${parameters}`}
+      onClick={openHandler}
     >
       <svg viewBox="0 0 1024 1024" fill="currentColor">
         <path d="M755 140.3l0.5-0.3h0.3L512 0 268.3 140h-0.3l0.8 0.4L68.6 256v512L512 1024l443.4-256V256L755 140.3z m-30 506.4v171.2L548 920.1V534.7L883.4 341v215.7l-158.4 90z m-584.4-90.6V340.8L476 534.4v385.7L300 818.5V646.7l-159.4-90.6zM511.7 280l171.1-98.3 166.3 96-336.9 194.5-337-194.6 165.7-95.7L511.7 280z"></path>
