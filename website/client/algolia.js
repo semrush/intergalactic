9c080ab47817f88e810c4cc99e2cbc826b49f657
elena.krasnopolskaia <elena.krasnopolskaia@semrush.com>
2022-03-10 20:43:21 +0300

    [website] temporary revert updating Algolia app

--- website/client/algolia.js
+++ website/client/algolia.js
@@ -1,6 +1,6 @@
 module.exports = {
   ALGOLIA_APP: 'BJLIH701PI',
   ALGOLIA_OPEN_KEY: '55846807f9fc897dedb5ba02e79f48d6',
-  ALGOLIA_INDEX: 'intergalactics',
+  ALGOLIA_INDEX: 'intergalactic',
   ALGOLIA_INDEX_ICONS: 'icons',
 };
