12cd844a216d90e0d975125b7f3cf6c3431c7cbd
Mikhail Sereniti <phytonmk@gmail.com>
2022-03-17 00:30:19 +0300

    [inline-edit] introduced component

---
+++ semcore/inline-edit/__tests__/index.test.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { testing, snapshot } from '@semcore/jest-preset-ui';
+const { cleanup, axe, render } = testing;
+
+import InlineEdit from '../src';
+
+describe('InlineEdit', () => {
+  afterEach(cleanup);
+
+  test('renders view by default', async () => {
+    const component = (
+      <InlineEdit>
+        <InlineEdit.View>view</InlineEdit.View>
+        <InlineEdit.Edit>edit</InlineEdit.Edit>
+      </InlineEdit>
+    );
+    expect(await snapshot(component)).toMatchImageSnapshot();
+  });
+
+  test('supports edit render', async () => {
+    const component = (
+      <InlineEdit editable={true}>
+        <InlineEdit.View>view</InlineEdit.View>
+        <InlineEdit.Edit>edit</InlineEdit.Edit>
+      </InlineEdit>
+    );
+    expect(await snapshot(component)).toMatchImageSnapshot();
+  });
+
+  test('resizes edit to view', async () => {
+    const component = (
+      <InlineEdit editable={true}>
+        <InlineEdit.View>
+          <br />
+          <br />
+          <br />
+          <br />
+          view
+          <br />
+          <br />
+          <br />
+          <br />
+        </InlineEdit.View>
+        <InlineEdit.Edit>
+          <br />
+          <br />
+          <br />
+          edit
+        </InlineEdit.Edit>
+      </InlineEdit>
+    );
+    expect(await snapshot(component)).toMatchImageSnapshot();
+  });
+
+  test('a11y', async () => {
+    const { container } = render(
+      <InlineEdit>
+        <InlineEdit.View>view</InlineEdit.View>
+        <InlineEdit.Edit>edit</InlineEdit.Edit>
+      </InlineEdit>,
+    );
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
