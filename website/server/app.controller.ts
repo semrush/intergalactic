46a61728d2220bbd80e4b1ab0ceed2f2297de64f
r.lysov <r.lysov@semrush.com>
2022-04-02 19:56:05 +0300

    [website] Add host for test

--- website/server/app.controller.ts
+++ website/server/app.controller.ts
@@ -18,15 +18,16 @@ export class AppController {
   site(@Res() res: Response, @Next() next: NextFunction, @Req() req: Request) {
     if (req.path.includes('graphql')) return next();
 
-    if (req.header('host') === 'i.semrush.com') {
-      return {
-        url: 'https://developer.semrush.com/intergalactic' + req.path,
-        statusCode: 302,
-      };
-    }
+    // if (req.header('host') === 'i.semrush.com') {
+    //   return {
+    //     url: 'https://developer.semrush.com/intergalactic' + req.path,
+    //     statusCode: 302,
+    //   };
+    // }
 
     return res.render('index', {
       ROOT_PATH: this.configService.get('ROOT_PATH', '/'),
+      host: req.header('host'),
     });
   }
 }
