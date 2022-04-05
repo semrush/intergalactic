b812e114d7fcd7d6281be9c25292d19ad433f59c
s.kobets <s.kobets@semrush.com>
2022-04-05 08:16:16 +0300

    [flags] update tests

--- semcore/flags/__tests__/index.test.jsx
+++ semcore/flags/__tests__/index.test.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import { testing } from '@semcore/jest-preset-ui';
+import { testing, snapshot } from '@semcore/jest-preset-ui';
 const { render, cleanup } = testing;
-
-import { snapshot } from '@semcore/jest-preset-ui';
 import Flags from '../src';
 
 describe('Flags', () => {
   afterEach(cleanup);
 
-  test('should support className with name country without space', () => {
+  test('Should support className with name country without space', () => {
     const { getByTestId } = render(<Flags data-testid="flags" iso2="EH" />);
 
     expect(getByTestId('flags').classList[1]).toMatch('WesternSahara');
   });
 
-  test('should support className with name country and ,', () => {
+  test('Should support className with name country and ,', () => {
     const { getByTestId } = render(<Flags data-testid="flags" iso2="BQ" />);
 
     expect(getByTestId('flags').classList[1]).toMatch('BonaireSintEustatiusAndSaba');
   });
 
-  test('should support no name contry', async () => {
+  test('Should correctly render', () => {
+    const { getByTestId } = render(
+      <>
+        <Flags data-testid="flags" iso2="af" />
+        <Flags data-testid="flags2" iso3="afg" />
+      </>,
+    );
+
+    expect(getByTestId('flags').classList[1]).toMatch('Afghanistan');
+    expect(getByTestId('flags2').classList[1]).toMatch('Afghanistan');
+  });
+
+  test('Should set styles', () => {
+    render(<Flags data-testid="flags" iso2="af" />);
+
+    const link = document.querySelectorAll('[class*=_css-style-flags]')[0];
+
+    expect(link).toBeTruthy();
+    expect(link.tagName).toBe('LINK');
+  });
+
+  test('Should support no name country', async () => {
     const component = (
       <snapshot.ProxyProps style={{ margin: 5 }}>
         <Flags />
