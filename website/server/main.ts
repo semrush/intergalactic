5ab9f19d4f69252a732da227fa582aa575629e72
s.kobets <s.kobets@semrush.com>
2022-04-15 15:09:50 +0300

    [website] returned dependency rxjs and returned port 3000

--- website/server/main.ts
+++ website/server/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   hbs.registerPartials(join(process.cwd(), './server/views/partials'));
   app.setViewEngine('hbs');
 
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
