d56cc57a79658dea0433565e67260d8bca1b7ca9
s.kobets <s.kobets@semrush.com>
2022-04-15 12:01:00 +0300

    [website] fixed example list in typography

--- website/docs/style/typography/examples/list.jsx
+++ website/docs/style/typography/examples/list.jsx
@@ -6,7 +6,7 @@ export default () => (
   <div>
     <List size={300} marker={<CheckS color="green" mt={1} />}>
       <List.Item>I'm gonna make him an offer he can't refuse.</List.Item>
-      <List.Item marker={<CheckS />}>
+      <List.Item marker={<CheckS mt={1} />}>
         (Uncheck icon) Carpe diem. Seize the day, boys. Make your lives extraordinary.
       </List.Item>
       <List.Item>Listen to them. Children of the night. What music they make.</List.Item>
