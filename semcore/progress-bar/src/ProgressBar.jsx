3ed215540725846ab10dad500e2081860d51352e
r.lysov <r.lysov@semrush.com>
2022-04-25 19:06:40 +0300

    [progress-bar] Updated with restyling

--- semcore/progress-bar/src/ProgressBar.jsx
+++ semcore/progress-bar/src/ProgressBar.jsx
@@ -31,23 +31,16 @@ class ProgressBarRoot extends Component {
 
   render() {
     const SProgressBar = Root;
-    const { Children, styles, duration, theme, value, animation, ...other } = this.asProps;
+    const { Children, styles, duration, theme, value } = this.asProps;
     const useTheme = isCustomTheme(theme) ? 'custom' : theme;
-    const color = resolveColor(theme);
-
-    logger.warn(
-      animation !== undefined,
-      "The 'animation' property is deprecated, use 'value'",
-      other['data-ui-name'] || ProgressBarRoot.displayName,
-    );
 
     return sstyled(styles)(
       <SProgressBar
         render={Box}
         use:theme={useTheme}
-        use:animation={animation === undefined ? !value : animation}
+        use:animation={!value}
         use:duration={`${duration}ms`}
-        backgroundColor={color}
+        сolorBg={resolveColor(theme)}
       >
         <Children />
       </SProgressBar>,
@@ -59,14 +52,13 @@ function Value(props) {
   const SValue = Root;
   const { styles, value, theme, duration } = props;
   const width = `${value}%`;
-  const color = resolveColor(theme);
 
   return sstyled(styles)(
     <SValue
       render={Box}
       use:width={width}
       use:duration={`${duration}ms`}
-      backgroundColor={color}
+      сolorBg={resolveColor(theme)}
     />,
   );
 }
@@ -76,23 +68,3 @@ const ProgressBar = createComponent(ProgressBarRoot, {
 });
 
 export default ProgressBar;
-
-const Progress = React.forwardRef(function (props, ref) {
-  logger.warn(
-    true,
-    "Component '<Progress/>' is deprecated, use '<ProgressBar.Value/>'",
-    props['data-ui-name'] || ProgressBar.displayName,
-  );
-  return <ProgressBar.Value ref={ref} {...props} />;
-});
-
-const Bar = React.forwardRef(function (props, ref) {
-  logger.warn(
-    true,
-    "Component '<Bar/>' is deprecated, please use '<ProgressBar/>'",
-    props['data-ui-name'] || ProgressBar.displayName,
-  );
-  return <ProgressBar ref={ref} {...props} />;
-});
-
-export { Bar, Progress };
