e567b3930734fe5316b9d7a06bc3e33ba404fc3a
Mikhail Sereniti <phytonmk@gmail.com>
2022-04-26 11:02:18 +0300

    [flags-sprite] rewritten into ts, removed unsecure dependencies

--- tools/flags-sprite/bin/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const path = require('path');
-const generate = require('../index');
-
-const packagePath = path.resolve(process.cwd(), 'package.json');
-let { version } = require(packagePath);
-
-generate(version);
