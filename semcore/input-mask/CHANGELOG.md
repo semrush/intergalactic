9da74d82079e7cf3ac39e0bacf67713f6dd0a6c2
r.lysov <r.lysov@semrush.com>
2022-04-27 17:15:23 +0300

    [input-mask] Updated with restyling

--- semcore/input-mask/CHANGELOG.md
+++ semcore/input-mask/CHANGELOG.md
@@ -2,6 +2,13 @@
 
 CHANGELOG.md standards are inspired by [keepachangelog.com](https://keepachangelog.com/en/1.0.0/).
 
+## [3.0.0] - unreleased
+
+### BREAK
+
+- Restyling component.
+- Major dependency update Input.
+
 ## [3.2.3] - 2022-03-14
 
 ### Changed
--- semcore/input-mask/__tests__/index.test.tsx
+++ semcore/input-mask/__tests__/index.test.tsx
@@ -1,14 +1,25 @@
-import { testing, shared as testsShared } from '@semcore/jest-preset-ui';
+import React from 'react';
+import InputMask from '../src';
+
+import { testing, shared as testsShared, snapshot } from '@semcore/jest-preset-ui';
 const { cleanup } = testing;
 
 const { shouldSupportClassName, shouldSupportRef } = testsShared;
-import InputMask from '../src';
 
 describe('InputMask', () => {
   afterEach(cleanup);
 
   shouldSupportClassName(InputMask);
   shouldSupportRef(InputMask);
+
+  xtest('Should support placeholder', async () => {
+    const component = (
+      <InputMask size="l" mb={4}>
+        <InputMask.Value mask="9999 9999 9999 9999" placeholder="____ ____ ____ ____" value={999} />
+      </InputMask>
+    );
+    expect(await snapshot(component)).toMatchImageSnapshot();
+  });
 });
 
 describe('InputMask.Value', () => {
