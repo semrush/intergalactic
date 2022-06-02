20ca7cf5543fb10b072c7413df9924d53a28f6a8
Mikhail Sereniti <phytonmk@gmail.com>
2022-06-02 11:31:51 +0300

    [website] fixed mjs files resolving

--- tools/playground/esbuild-plugin-semcore.ts
+++ tools/playground/esbuild-plugin-semcore.ts
@@ -1,6 +1,6 @@
 import { Loader, Plugin } from 'esbuild';
 import babel from '@babel/core';
-import { readFile } from 'fs/promises';
+import { readFile, access } from 'fs/promises';
 import { dirname as resolveDirname } from 'path';
 import { makeCacheManager } from './cache-manager';
 import { extractSemcoreImplicitDependencies } from './semcore-implicit-dependncies-resolver';
@@ -26,10 +26,8 @@ const babelTransform = async (contents: string, path: string) => {
 };
 
 const supportedExtensions = ['ts', 'js', 'tsx', 'jsx'];
-const loaderOfExtension: { [key: string]: Loader } = {
-  md: 'text',
-  mjs: 'js',
-};
+const loaderOfExtension: { [key: string]: Loader } = { md: 'text', mjs: 'js' };
+const prioritizedExtensionFallback: { [key: string]: string } = { js: 'mjs' };
 
 const cacheManager = makeCacheManager('esbuild_plugin_semcore');
 await cacheManager.init();
@@ -42,6 +40,20 @@ export const esbuildPluginSemcore = (filter: RegExp, excludeFilter?: RegExp): Pl
   name: 'esbuild-plugin-semcore',
   setup(build) {
     build.onLoad({ filter }, async ({ path, namespace }) => {
+      {
+        const extension = path.split('.').pop()! as Loader;
+        if (prioritizedExtensionFallback[extension]) {
+          const fallbackPath =
+            path.split('.').slice(0, -1).join('.') + '.' + prioritizedExtensionFallback[extension];
+          try {
+            await access(fallbackPath);
+            path = fallbackPath;
+          } catch {
+            /* no file in fallback location */
+          }
+        }
+      }
+
       const sourceContents = await readFile(path, 'utf-8');
       const extension = path.split('.').pop()! as Loader;
       const loader = loaderOfExtension[extension] || extension;
