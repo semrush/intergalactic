2824a4291aaf40e88ebe0466132b10661b9b109b
Mikhail Sereniti <phytonmk@gmail.com>
2022-06-17 18:06:25 +0300

    [icon-transform-svg] cleaned up code (lite refactoring only)

--- tools/icon-transform-svg/__tests__/__fixtures__/path/m/index.d.ts
+++ tools/icon-transform-svg/__tests__/__fixtures__/path/m/index.d.ts
@@ -1,7 +1,5 @@
-
-        import React from 'react';
-        import { Merge } from '@semcore/core';
-        import { IIconProps } from '@semcore/icon';
-        declare const _default: import("@semcore/core").ComponentType<Merge<IIconProps, React.SVGAttributes<SVGElement>>, {}, {}>;
-        export default _default;
-      
+import React from 'react';
+import { Merge } from '@semcore/core';
+import { IIconProps } from '@semcore/icon';
+declare const _default: import("@semcore/core").ComponentType<Merge<IIconProps, React.SVGAttributes<SVGElement>>, {}, {}>;
+export default _default;
