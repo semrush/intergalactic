228fe9cfbea846f7ed7865e2aade62f15a4752a8
Mikhail Sereniti <phytonmk@gmail.com>
2022-07-05 19:22:42 +0300

    [icon-transform-svg] increased time limits for extremely slow test

--- tools/icon-transform-svg/__tests__/index.test.js
+++ tools/icon-transform-svg/__tests__/index.test.js
@@ -35,58 +35,62 @@ describe('Parsing svg', () => {
 });
 
 describe('Transform svg', () => {
-  test('Files should compile', async () => {
-    async function checkDistFiles(publicPath, familyIcons, opt, iconPath) {
-      try {
-        if (!iconPath) {
-          iconPath = glob.sync(`${publicPath}/${familyIcons}/**/*.svg`)[0];
-          iconPath = iconPath.replace('.svg', '').split('/').slice(opt.slice).join('/');
+  test(
+    'Files should compile',
+    async () => {
+      async function checkDistFiles(publicPath, familyIcons, opt, iconPath) {
+        try {
+          if (!iconPath) {
+            iconPath = glob.sync(`${publicPath}/${familyIcons}/**/*.svg`)[0];
+            iconPath = iconPath.replace('.svg', '').split('/').slice(opt.slice).join('/');
+          }
+          const isJSPathExists = await fs.pathExists(
+            `${opt.rootPath || rootPath}/${iconPath}/index.js`,
+          );
+          const isDTSPathExists = await fs.pathExists(
+            `${opt.rootPath || rootPath}/${iconPath}/index.d.ts`,
+          );
+
+          expect(isJSPathExists).toBeTruthy();
+          expect(isDTSPathExists).toBeTruthy();
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error(error);
+          throw Error(
+            `Failed to compile ${familyIcons} images from ${publicPath
+              .split('/')
+              .slice(-1)}/${familyIcons}`,
+          );
         }
-        const isJSPathExists = await fs.pathExists(
-          `${opt.rootPath || rootPath}/${iconPath}/index.js`,
-        );
-        const isDTSPathExists = await fs.pathExists(
-          `${opt.rootPath || rootPath}/${iconPath}/index.d.ts`,
-        );
-
-        expect(isJSPathExists).toBeTruthy();
-        expect(isDTSPathExists).toBeTruthy();
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        throw Error(
-          `Failed to compile ${familyIcons} images from ${publicPath
-            .split('/')
-            .slice(-1)}/${familyIcons}`,
-        );
       }
-    }
 
-    const publicSvgPath = `${rootPath}/svg`;
-    const publicSvgNewPath = `${rootPath}/svg-new`;
-    await cli(`${rootPath}/transformSvg --configFile=transform-svg-legacy.config.js`, rootPath);
+      const publicSvgPath = `${rootPath}/svg`;
+      const publicSvgNewPath = `${rootPath}/svg-new`;
+      await cli(`${rootPath}/transformSvg --configFile=transform-svg-legacy.config.js`, rootPath);
 
-    await checkDistFiles(publicSvgPath, 'icon', { slice: -2, rootPath: `${rootPath}/lib` });
-    await checkDistFiles(publicSvgPath, 'color', { slice: -3, rootPath: `${rootPath}/lib` });
+      await checkDistFiles(publicSvgPath, 'icon', { slice: -2, rootPath: `${rootPath}/lib` });
+      await checkDistFiles(publicSvgPath, 'color', { slice: -3, rootPath: `${rootPath}/lib` });
 
-    let iconPath = glob.sync(`${publicSvgPath}/external/**/*.svg`)[0];
-    iconPath = iconPath.replace('.svg', '').split('/').slice(-3);
-    iconPath = iconPath[0] + '/' + iconPath[1] + iconPath[2];
-    await checkDistFiles(publicSvgPath, 'external', { rootPath: `${rootPath}/lib` }, iconPath);
+      let iconPath = glob.sync(`${publicSvgPath}/external/**/*.svg`)[0];
+      iconPath = iconPath.replace('.svg', '').split('/').slice(-3);
+      iconPath = iconPath[0] + '/' + iconPath[1] + iconPath[2];
+      await checkDistFiles(publicSvgPath, 'external', { rootPath: `${rootPath}/lib` }, iconPath);
 
-    await checkDistFiles(publicSvgPath, 'pay', { slice: -2, rootPath: `${rootPath}/lib` });
+      await checkDistFiles(publicSvgPath, 'pay', { slice: -2, rootPath: `${rootPath}/lib` });
 
-    await cli(`${rootPath}/transformSvg --configFile=transform-svg.config.js`, rootPath);
-    await checkDistFiles(publicSvgNewPath, 'icon', { slice: -2 });
-    await checkDistFiles(publicSvgNewPath, 'color', { slice: -3 });
+      await cli(`${rootPath}/transformSvg --configFile=transform-svg.config.js`, rootPath);
+      await checkDistFiles(publicSvgNewPath, 'icon', { slice: -2 });
+      await checkDistFiles(publicSvgNewPath, 'color', { slice: -3 });
 
-    let iconPathExternal = glob.sync(`${publicSvgNewPath}/external/**/*.svg`)[0];
-    iconPathExternal = iconPathExternal.replace('.svg', '').split('/').slice(-3);
-    iconPathExternal = iconPathExternal[0] + '/' + iconPathExternal[1] + iconPathExternal[2];
-    await checkDistFiles(publicSvgNewPath, 'external', {}, iconPathExternal);
+      let iconPathExternal = glob.sync(`${publicSvgNewPath}/external/**/*.svg`)[0];
+      iconPathExternal = iconPathExternal.replace('.svg', '').split('/').slice(-3);
+      iconPathExternal = iconPathExternal[0] + '/' + iconPathExternal[1] + iconPathExternal[2];
+      await checkDistFiles(publicSvgNewPath, 'external', {}, iconPathExternal);
 
-    await checkDistFiles(publicSvgNewPath, 'pay', { slice: -3 });
-  });
+      await checkDistFiles(publicSvgNewPath, 'pay', { slice: -3 });
+    },
+    20 * 1000, // exteremly slow test
+  );
 });
 
 const familyNameIcons = ['color', 'external', 'pay', 'path', 'pathNew'];
