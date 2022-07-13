12f09803ae164130fe41b8fb56a4cb8c95ef15ea
r.lysov <r.lysov@semrush.com>
2022-07-13 23:33:43 +0300

    [babel-plugin-react-semcore] Fix fixture for tests
    Signed-off-by: r.lysov <r.lysov@semrush.com>

--- tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/purgeCSS/index.expected.js
+++ tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/purgeCSS/index.expected.js
@@ -7,12 +7,12 @@ var _ref =
     /*__reshadow_css_start__*/
     (__css__(
       /*__inner_css_start__*/
-      '.___SButton_13onc_gg_{padding:0 20px;background:purple}.___SButton_13onc_gg_._theme_default_13onc_gg_{background:#bada55}',
-      '1dlh6zt_gg_',
+      '.___SButton_glwee_gg_{padding:0 20px;background:purple}.___SButton_glwee_gg_._theme_default_glwee_gg_{background:#bada55}',
+      'glwee_gg_',
     ),
     {
-      __SButton: `___SButton_13onc_gg_`,
-      _theme_default: `_theme_default_13onc_gg_`,
+      __SButton: `___SButton_glwee_gg_`,
+      _theme_default: `_theme_default_glwee_gg_`,
     }),
   styles = _extends({}, _ref);
 
