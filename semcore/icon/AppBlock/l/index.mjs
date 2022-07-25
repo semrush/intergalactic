81ec6caceab03257ed15988cbaa3804af10b5fdd
Mikhail Sereniti <phytonmk@gmail.com>
2022-07-25 18:53:55 +0300

    [icon] added fallback for renamed AppBlock icon

---
+++ semcore/icon/AppBlock/l/index.mjs
@@ -0,0 +1,8 @@
+import AppsBlock from '../../AppsBlock/l';
+
+/**
+ * @deprecated Please, use `AppsBlock/l` instead. This icon will be removed in the next major release
+ *  */
+const AppBlock = AppsBlock;
+
+export default AppBlock;
---
+++ semcore/icon/AppBlock/m/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+
+var AppsBlock = require('../../AppsBlock/m');
+
+/**
+ * @deprecated Please, use `AppsBlock/m` instead. This icon will be removed in the next major release
+ *  */
+var AppBlock = AppsBlock;
+
+exports['default'] = AppBlock;
