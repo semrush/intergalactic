9bd59d33570b87b75d9de02f1ea7df1126d3f597
Mikhail Sereniti <phytonmk@gmail.com>
2022-07-25 21:00:27 +0300

    [d3-chart] refactored summarizer to fully follow philosophy of hints optionnality (especially for value sets and gropped values data)

--- semcore/d3-chart/src/types/Plot.d.ts
+++ semcore/d3-chart/src/types/Plot.d.ts
@@ -17,72 +17,74 @@ export interface IPlotProps extends IContext, IBoxProps {
    * */
   locale?: NavigatorLanguage['language'];
   /** Optional prop to tune up alt text generating for charts */
-  a11yAltTextConfig?: {
-    /** Disable */
-    disable?: boolean;
-    /** Disable automatic data summarization and use provided text */
-    overrite?: string;
-    /** Tuning up time series trends analyzing. Represents angle tangens of time series change of different strength */
-    trendTangens?: {
-      /** Angle tangens of time series that have meaningful trending change
-       * @default 1/15
-       */
-      static?: number;
-      /** Angle tangens of time series that have weak trending change
-       * @default 1/10
-       */
-      weak?: number;
-      /** Angle tangens of time series that have medium trending change
-       * @default 1/5
-       */
-      medium?: number;
-      /** Angle tangens of time series that have strong trending change
-       * @default Infinity
-       */
-      strong?: number;
-    };
-    /** Tuning up time series local trends detection based on moving averages */
-    movingAverage?: {
-      /** Frame size of long moving average
-       * @default Math.sqrt(data.length)
-       */
-      longSize?: number;
-      /** Frame size of short moving average
-       * @default Math.sqrt(data.length)/2
-       */
-      shortSize?: number;
-      /** Absolute difference between values on moving averages crossing to
-       * be handled as local trend change.
-       * @default StandardDeviation/10
-       */
-      notableDiff?: number;
-    };
-    /** The way data to be interpreted as. By default is based on used used Plot elements and data structure.
-     */
-    dataType?: 'time-series' | 'points-cloud' | 'values-set';
-    /** Grid size to split data of point clouds into clusters.
-     */
-    clustersGridSize?: number;
-    /** Limits some output lists in chart alt text
-     * @default 100
-     * **/
-    maxListSymbols?: number;
-    /** Always add time to dates in chart alt text **/
-    datesWithTime?: boolean;
-    /** Described clusters count before text "and X more"
-     * @default false
-     * **/
-    clustersLimit?: number;
-    /** Described values count before text "and X more"
-     * @default false
-     * **/
-    valuesLimit?: number;
-    /** Described grouped value groups count before text "and X more"
-     * @default false
-     * **/
-    groupsLimit?: number;
-  };
+  a11yAltTextConfig?: PlotSummarizerConfig;
 }
+
+export type PlotSummarizerConfig = {
+  /** Totally disable automatic data summarization */
+  disable?: boolean;
+  /** Disable automatic data summarization and use provided text */
+  override?: string;
+  /** Tuning up time series trends analyzing. Represents angle tangens of time series change of different strength */
+  trendTangens?: {
+    /** Angle tangens of time series that have meaningful trending change
+     * @default 1/15
+     */
+    static?: number;
+    /** Angle tangens of time series that have weak trending change
+     * @default 1/10
+     */
+    weak?: number;
+    /** Angle tangens of time series that have medium trending change
+     * @default 1/5
+     */
+    medium?: number;
+    /** Angle tangens of time series that have strong trending change
+     * @default Infinity
+     */
+    strong?: number;
+  };
+  /** Tuning up time series local trends detection based on moving averages */
+  movingAverage?: {
+    /** Frame size of long moving average
+     * @default Math.sqrt(data.length)
+     */
+    longSize?: number;
+    /** Frame size of short moving average
+     * @default Math.sqrt(data.length)/2
+     */
+    shortSize?: number;
+    /** Absolute difference between values on moving averages crossing to
+     * be handled as local trend change.
+     * @default StandardDeviation/10
+     */
+    notableDiff?: number;
+  };
+  /** The way data to be interpreted as. By default is based on used used Plot elements and data structure.
+   */
+  dataType?: 'time-series' | 'points-cloud' | 'values-set';
+  /** Grid size to split data of point clouds into clusters.
+   */
+  clustersGridSize?: number;
+  /** Limits some output lists in chart alt text
+   * @default 100
+   * **/
+  maxListSymbols?: number;
+  /** Always add time to dates in chart alt text **/
+  datesWithTime?: boolean;
+  /** Described clusters count before text "and X more"
+   * @default false
+   * **/
+  clustersLimit?: number;
+  /** Described values count before text "and X more"
+   * @default false
+   * **/
+  valuesLimit?: number;
+  /** Described grouped value groups count before text "and X more"
+   * @default false
+   * **/
+  groupsLimit?: number;
+};
 
 declare const Plot: <T>(props: CProps<IPlotProps & T>) => ReturnEl;
 
