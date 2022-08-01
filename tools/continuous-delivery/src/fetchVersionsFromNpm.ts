26a5c79c6bb571ee38b482c5d0f80bd09d4cdcbe
Mikhail Sereniti <phytonmk@gmail.com>
2022-08-02 01:06:51 +0300

    [continuous-delivery] enforced npm api to get published version for every package via lone request

--- tools/continuous-delivery/src/fetchVersionsFromNpm.ts
+++ tools/continuous-delivery/src/fetchVersionsFromNpm.ts
@@ -1,9 +1,25 @@
 import axios from 'axios';
+import pLimit from 'p-limit';
 
 export const fetchVersionsFromNpm = async () => {
   const { data: npmSearchResult } = await axios.get<
     {},
-    { data: { total: number; objects: { package: { name: string; version: string } }[] } }
+    {
+      data: {
+        total: number;
+        objects: {
+          package: {
+            name: string;
+            /**
+             * @deprecated
+             * NPM hadrly caches search results so version in search result
+             * may be outdated for several horus
+             */
+            version: string;
+          };
+        }[];
+      };
+    }
   >(`https://registry.npmjs.org/-/v1/search?text=@semcore/&size=250`);
 
   if (npmSearchResult.total !== npmSearchResult.objects.length) {
@@ -14,11 +30,18 @@ export const fetchVersionsFromNpm = async () => {
 
   const currentVersions: { [packageName: string]: string } = {};
 
-  for (const {
-    package: { name, version },
-  } of npmSearchResult.objects) {
-    currentVersions[name] = version;
-  }
+  const limit = pLimit(5);
+  await Promise.all(
+    npmSearchResult.objects.map(
+      async ({ package: { name } }) =>
+        await limit(async () => {
+          const npmResponse = await axios.get<{ 'dist-tags': { latest: string } }>(
+            `https://registry.npmjs.org/${name}`,
+          );
+          currentVersions[name] = npmResponse.data['dist-tags'].latest;
+        }),
+    ),
+  );
 
   return currentVersions;
 };
