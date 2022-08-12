761a5dd88946d5794c253c17773f1035be0287d3
r.lysov <r.lysov@semrush.com>
2022-08-12 15:17:55 +0300

    [badge,utils] Add support a11y & add util function `reactToText` to convert react component to text.
    Signed-off-by: r.lysov <r.lysov@semrush.com>

---
+++ semcore/utils/src/reactToText.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function reactToText(node: React.ReactNode): string {
+  if (typeof node === 'string' || typeof node === 'number' || typeof node === 'boolean') {
+    return node.toString();
+  }
+  if (!node) {
+    return '';
+  }
+  if (Array.isArray(node)) {
+    return node.map((entry) => reactToText(entry)).join('');
+  }
+
+  const props: { children?: React.ReactNode } = (node as any).props ? (node as any).props : {};
+
+  if (!props || !props.children) {
+    return '';
+  }
+
+  return reactToText(props.children);
+}
+
+export default reactToText;
