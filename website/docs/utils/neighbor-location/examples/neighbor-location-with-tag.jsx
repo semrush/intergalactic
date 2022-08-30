db1dbe9add384df9a9f7004cfc1abe06b96dba35
Julia Mnizhek <j.mnizhek@semrush.com>
2022-08-30 14:58:30 +0300

    [neighbor-location] update guide

--- website/docs/utils/neighbor-location/examples/neighbor-location-with-tag.jsx
+++ website/docs/utils/neighbor-location/examples/neighbor-location-with-tag.jsx
@@ -5,11 +5,18 @@ import NeighborLocation from '@semcore/neighbor-location';
 
 const Demo = () => {
   return (
-    <NeighborLocation tag={Flex}>
-      <Button>left</Button>
-      <Button>center</Button>
-      <Button>right</Button>
-    </NeighborLocation>
+    <>
+      <NeighborLocation tag={Flex} mb={4}>
+        <Button use="primary">left</Button>
+        <Button use="primary">center</Button>
+        <Button use="primary">right</Button>
+      </NeighborLocation>
+      <NeighborLocation tag={Flex}>
+        <Button>left</Button>
+        <Button>center</Button>
+        <Button>right</Button>
+      </NeighborLocation>
+    </>
   );
 };
 
