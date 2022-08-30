afb1639b3e50f70be060b62787d71952d7b0cc4a
Julia Mnizhek <j.mnizhek@semrush.com>
2022-08-30 14:58:30 +0300

    [neighbor-location] update guide

--- website/docs/utils/neighbor-location/examples/neighbor-location-input.jsx
+++ website/docs/utils/neighbor-location/examples/neighbor-location-input.jsx
@@ -1,16 +1,34 @@
+//https://github.com/semrush/intergalactic/tree/master/website/docs/utils/neighbor-location/examples/neighbor-location-input.jsx
 import React from 'react';
 import Input from '@semcore/input';
 import Button from '@semcore/button';
+import { Flex } from '@semcore/flex-box';
 import NeighborLocation from '@semcore/neighbor-location';
 
 const Demo = () => {
   return (
-    <NeighborLocation>
-      <Input w={200}>
-        <Input.Value placeholder="Placeholder" />
-      </Input>
-      <Button>Button</Button>
-    </NeighborLocation>
+    <>
+      <NeighborLocation tag={Flex} mb={4}>
+        <Input w={200}>
+          <Input.Value placeholder="Placeholder" />
+        </Input>
+        <Button>Button</Button>
+      </NeighborLocation>
+      <NeighborLocation tag={Flex} mb={4}>
+        <Input w={200}>
+          <Input.Value placeholder="Placeholder" />
+        </Input>
+        <Button use="primary">Button</Button>
+      </NeighborLocation>
+      <NeighborLocation tag={Flex}>
+        <Input w={200}>
+          <Input.Value placeholder="Placeholder" />
+        </Input>
+        <Button use="primary" theme="success">
+          Button
+        </Button>
+      </NeighborLocation>
+    </>
   );
 };
 
