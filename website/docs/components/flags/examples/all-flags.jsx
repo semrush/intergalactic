0ca41f7009a9a16284493ef503c6f9ffc9ec3d21
Ruslan Gaiazov <freeyoungstrong@gmail.com>
2022-09-12 23:32:38 +0200

    [flags] added acces to countries and union of countries without ISO code
    <!--- Commit was signed off by freeyoungstrong@gmail.com with GPG key ID EA2F630031F24EEF -->

--- website/docs/components/flags/examples/all-flags.jsx
+++ website/docs/components/flags/examples/all-flags.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Flags, { iso2Name } from '@semcore/flags';
+import Flags, { iso2Name, nameWithoutIso } from '@semcore/flags';
 import { Text } from '@semcore/typography';
 import { Flex } from '@semcore/flex-box';
 
+const allNames = { ...iso2Name, ...nameWithoutIso };
+
 const Demo = () => (
   <Flex flexWrap>
-    {Object.keys(iso2Name).map((iso2) => (
-      <Flex direction="column" alignItems="center" p={5} key={iso2}>
-        <Flags iso2={iso2} mb={2} />
+    {Object.keys(allNames).map((name) => (
+      <Flex direction="column" alignItems="center" p={5} key={name}>
+        <Flags name={name} mb={2} />
         <Text style={{ textAlign: 'center' }} noWrap w={160}>
-          {iso2Name[iso2]}
+          {allNames[name]}
         </Text>
       </Flex>
     ))}
