fcfa2feb99552c23f937d8bcad8d3eb2589839d5
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-10-06 14:01:43 +0200

    [website] updated to 1.8.14: much improved piping example of input-mask
    <!--- Commit was signed off by phytonmk@gmail.com with GPG key ID CF2DC815AE956C1F -->

--- website/docs/components/input-mask/examples/pipe.jsx
+++ website/docs/components/input-mask/examples/pipe.jsx
@@ -1,13 +1,14 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Box, Flex } from '@semcore/flex-box';
 import { Text } from '@semcore/typography';
 import InputMask from '@semcore/input-mask';
 
 export default () => {
-  const expireDateInput = useRef(null);
-  const handleSuccess = () => expireDateInput.current.focus();
+  const expireDateInput = React.useRef(null);
+  const handleSuccess = React.useCallback(() => expireDateInput.current.focus(), []);
+
   return (
-    <Box wMax={240} p={8} m="0 auto" style={{ borderRadius: '12px', background: '#dee3e5' }}>
+    <Box wMax={225} p={8} m="0 auto" style={{ borderRadius: '12px', background: '#dee3e5' }}>
       <Text
         tag="label"
         size={300}
@@ -23,21 +24,21 @@ export default () => {
           mask="9999 9999 9999 9999"
           placeholder="____ ____ ____ ____"
           onSuccess={handleSuccess}
-          title="16-digit number"
+          title="card number – 16-digits"
           id="card_number"
         />
       </InputMask>
       <Flex alignItems="center" justifyContent="flex-end">
-        <Text tag="label" my={0} size={100} wMax={40} htmlFor="expire_date">
+        <Text tag="label" mr={1} size={100} htmlFor="expire_date">
           Expire date
         </Text>
-        <InputMask size="l" wMax={65}>
+        <InputMask size="l" wMax={85}>
           <InputMask.Value
             ref={expireDateInput}
             mask="99/99"
-            placeholder="__/__"
-            pipe={datePipe}
-            title="4-digit number"
+            placeholder="MM/YY"
+            pipe={pipeExpireDate}
+            title="month and year of card expiration – 4 digits in total"
             id="expire_date"
           />
         </InputMask>
@@ -45,32 +46,23 @@ export default () => {
     </Box>
   );
 };
-function datePipe(value) {
-  const limits = [
-    [0, 12],
-    [19, 99],
-  ];
-  const valueArr = value.split('/');
-  const indexesOfPipedChars = [];
-  const month = parseInt(valueArr[0], 10);
 
-  // Add 0 for the month if the value is greater than 1
-  if (month < 10 && month !== 1) {
-    valueArr[0] = `0${month}`;
+const pipeExpireDate = (value) => {
+  const indexesOfPipedChars = [];
+  const firstMonthDigit = parseInt(value[0], 10);
+  if (firstMonthDigit > 1) {
+    value = `0${value[0]}/${value.split('/')[1]}`;
     indexesOfPipedChars.push(0);
   }
+  const [month, year] = value
+    .split('/')
+    .map((chunk) => (chunk.includes('_') ? undefined : parseInt(chunk, 10)));
+  const currentYear = new Date().getFullYear() % 100;
+  const currentMonth = new Date().getMonth() + 1;
 
-  const isInvalid = valueArr.some((value, index) => {
-    const intValue = parseInt(value, 10);
-    if (index === 1 && intValue < 10) return false;
-    if (intValue < limits[index][0] || intValue > limits[index][1]) return true;
-  });
+  if (month > 12) return false;
+  if (year === currentYear && month < currentMonth) return false;
+  if (year < currentYear) return false;
 
-  if (isInvalid) {
-    return false;
-  }
-  return {
-    value: valueArr.join('/'),
-    indexesOfPipedChars,
-  };
-}
+  return { value, indexesOfPipedChars };
+};
