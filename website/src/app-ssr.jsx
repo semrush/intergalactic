4347d98b9fa61491519dc425d5783fe2322ae0af
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-10-12 16:48:27 +0200

    [website] updated to 1.10.1: fixed building broken ssr
    <!--- Commit was signed off by phytonmk@gmail.com with GPG key ID CF2DC815AE956C1F -->

--- website/src/app-ssr.jsx
+++ website/src/app-ssr.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import * as ReactDOMServer from 'react-dom/server';
-import * as ReactDOMServer from 'react-dom/umd/react-dom-server.browser.production.min.js';
+import * as ReactDOMServer from 'react-dom/server';
 import { App } from './App';
 import { sstyled as semcoreSstyled } from '@semcore/core';
 
