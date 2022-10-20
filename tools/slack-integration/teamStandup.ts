aaf0f7e0d23553a3c7ea87c293399c27fc3f95b6
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-10-20 18:43:43 +0200

    [ci] added automatic notification about daily standup for the team
    <!--- Commit was signed off by phytonmk@gmail.com with GPG key ID CF2DC815AE956C1F -->

---
+++ tools/slack-integration/teamStandup.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { sendMessage } from './sendMessage';
+
+export const notifyTeamAboutStandup = async ({ dryRun }: { dryRun: boolean }) => {
+  const { data } = await axios.get('https://meme-api.herokuapp.com/gimme/ProgrammerHumor/50');
+  const { memes } = data;
+
+  memes.sort((memeA, memeB) => memeB.ups - memeA.ups);
+  const safeMemes = memes.filter((meme) => !meme.nsfw && !meme.spoiler);
+  const bestMemes = safeMemes.slice(0, 10);
+  const pickedOne = bestMemes[Math.floor(Math.random() * bestMemes.length)];
+
+  const thumb = pickedOne.preview[0];
+  const full = pickedOne.preview[pickedOne.preview.length - 1];
+
+  return await sendMessage({
+    title: 'Daily standup!',
+    body: `Write what you have done the previous working day and your plans for today in the thread :point-down:\n_${pickedOne.title}_`,
+    dryRun,
+    image: { thumb, full },
+  });
+};
