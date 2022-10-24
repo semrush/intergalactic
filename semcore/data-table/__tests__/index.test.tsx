74df3bcdfb235bc82ef83f0265ff8c89bdc7f371
r.lysov <r.lysov@semrush.com>
2022-10-24 12:54:51 +0300

    [data-table] Test fail test
    Signed-off-by: r.lysov <r.lysov@semrush.com>
    
    <!--- Commit was signed off by r.lysov@semrush.com with GPG key ID A04B4646C408DA52 -->

--- semcore/data-table/__tests__/index.test.tsx
+++ semcore/data-table/__tests__/index.test.tsx
@@ -61,7 +61,7 @@ describe('DataTable', () => {
   shouldSupportClassName(DataTable);
   shouldSupportRef(DataTable);
 
-  test('base render', async () => {
+  xtest('base render', async () => {
     const component = (
       <div style={{ width: 800 }}>
         <DataTable data={data}>
