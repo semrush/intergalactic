4232f28d03e144e0e940540a95a870f731798da5
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-10-28 02:23:57 +0200

    [inline-edit, inline-input] implememnted missing a11y
    <!--- Commit was signed off by phytonmk@gmail.com with GPG key ID CF2DC815AE956C1F -->
    
    <!--- Commit was signed off by j.mnizhek@semrush.com with GPG key ID 0B5264A4C3374D3E -->

--- semcore/inline-edit/CHANGELOG.md
+++ semcore/inline-edit/CHANGELOG.md
@@ -2,6 +2,12 @@
 
 CHANGELOG.md standards are inspired by [keepachangelog.com](https://keepachangelog.com/en/1.0.0/).
 
+## [2.1.4] - 2022-10-28
+
+### Fixed
+
+- Removed wrong aria role and added needed aria label.
+
 ## [2.1.3] - 2022-10-26
 
 ### Changed
---
+++ semcore/inline-edit/__tests__/inline-edit.vo-test.ts
@@ -0,0 +1,44 @@
+import { expect } from '@playwright/test';
+import { voTest as test } from '@guidepup/playwright';
+import { e2eStandToHtml } from '@semcore/jest-preset-ui/e2e-stand';
+import { resolve as resolvePath } from 'path';
+import { writeFile } from 'fs/promises';
+import { getReportHeader, makeVoiceOverReporter } from '@semcore/jest-preset-ui/vo-reporter';
+
+test('Users can interact with Slider via VoiceOver', async ({ page, voiceOver: pureVoiceOver }) => {
+  const standPath = resolvePath(
+    __dirname,
+    '../../../website/docs/components/inline-edit/examples/simple-text.jsx',
+  );
+  const reportPath = resolvePath(
+    __dirname,
+    '../../../website/docs/components/inline-edit/inline-edit-a11y-report.md',
+  );
+
+  const htmlContent = await e2eStandToHtml(standPath, 'en');
+  await page.reload();
+  await page.setContent(htmlContent);
+  const { voiceOver, getReport } = await makeVoiceOverReporter(pureVoiceOver);
+  await voiceOver.interact();
+
+  expect(await voiceOver.itemText()).toContain('Tap to edit');
+  await voiceOver.act();
+  expect(await voiceOver.lastSpokenPhrase()).toContain('edit text');
+  expect(await voiceOver.lastSpokenPhrase()).toContain(
+    'Press Enter to apply value, press Escape to discard changes',
+  );
+  for (let i = 0; i < 12; i++) {
+    await voiceOver.press('Backspace', { application: 'Playwright' });
+  }
+  await voiceOver.type('Algernon');
+  await voiceOver.press('Enter', { application: 'Playwright' });
+  expect(await voiceOver.lastSpokenPhrase()).toBe('Algernon');
+  await voiceOver.act();
+  await voiceOver.type('Hello world?');
+  await voiceOver.press('Escape', { application: 'Playwright' });
+  expect(await voiceOver.lastSpokenPhrase()).toBe('Algernon');
+
+  const report = (await getReportHeader()) + '\n\n' + (await getReport(standPath));
+
+  await writeFile(reportPath, report);
+});
