f4ab693913e19114ad4ee29c38f81f3421b38e82
Michael Sereniti <31261408+phytonmk@users.noreply.github.com>
2022-10-28 03:05:07 +0200

    [time-picker] introduced a11y (#423)
    Made time-picker accessible for Screen readers.
    
    ## Description
    
    I have hid combobox popups for screen readers and added several
    necessary labels. Also I have removed weird searching for sibling inputs
    to focus it and replaced with predictable refs.
    
    
    ## How has this been tested?
    
    I have added VO tests
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] My change requires a change to the documentation.
    - [x] I have updated the documentation accordingly.

---
+++ semcore/time-picker/__tests__/time-picker.vo-test.ts
@@ -0,0 +1,43 @@
+import { expect } from '@playwright/test';
+import { voTest as test } from '@guidepup/playwright';
+import { e2eStandToHtml } from '@semcore/jest-preset-ui/e2e-stand';
+import { resolve as resolvePath } from 'path';
+import { writeFile } from 'fs/promises';
+import { getReportHeader, makeVoiceOverReporter } from '@semcore/jest-preset-ui/vo-reporter';
+
+test('Users can interact with TimePicker via VoiceOver', async ({
+  page,
+  voiceOver: pureVoiceOver,
+}) => {
+  const standPath = resolvePath(
+    __dirname,
+    '../../../website/docs/components/time-picker/examples/expanded.jsx',
+  );
+  const reportPath = resolvePath(
+    __dirname,
+    '../../../website/docs/components/time-picker/time-picker-a11y-report.md',
+  );
+
+  const htmlContent = await e2eStandToHtml(standPath, 'en');
+  await page.reload();
+  await page.setContent(htmlContent);
+  const { voiceOver, getReport } = await makeVoiceOverReporter(pureVoiceOver);
+  await voiceOver.interact();
+  expect(await voiceOver.itemText()).toBe('Time input, no time entered group');
+  await voiceOver.interact();
+  expect(await voiceOver.lastSpokenPhrase()).toBe('hours field 00 edit text');
+  await voiceOver.interact();
+  await voiceOver.type('04', { application: 'Playwright' });
+  await voiceOver.stopInteracting();
+  await voiceOver.next();
+  await voiceOver.interact();
+  await voiceOver.type('20', { application: 'Playwright' });
+  await voiceOver.stopInteracting();
+  await voiceOver.previous();
+  await voiceOver.stopInteracting();
+  expect(await voiceOver.itemText()).toBe('Time input, entered time is 4:20 AM group');
+
+  const report = (await getReportHeader()) + '\n\n' + (await getReport(standPath));
+
+  await writeFile(reportPath, report);
+});
