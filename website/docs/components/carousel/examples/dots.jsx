7f3e28c550ec9883ccca12db334c38486b00e49c
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-10-31 17:09:06 +0100

    [carousel] fixed screen readers support
    <!--- Commit was signed off by phytonmk@gmail.com with GPG key ID CF2DC815AE956C1F -->
    
    <!--- Commit was signed off by j.mnizhek@semrush.com with GPG key ID 0B5264A4C3374D3E -->

--- website/docs/components/carousel/examples/dots.jsx
+++ website/docs/components/carousel/examples/dots.jsx
@@ -7,6 +7,11 @@ const images = [
   `https://picsum.photos/id/1024/600/400`,
   `https://picsum.photos/id/1025/600/400`,
 ];
+const altTexts = [
+  `A cyclist performing stunts in the forest`,
+  `A vulture flies with its wings spread wide`,
+  `A pug wrapped in a blanket sits on the road in the forest`,
+];
 const width = 600;
 const imageWidth = width - 75;
 
@@ -16,8 +21,8 @@ const Demo = () => (
       <Carousel.Prev />
       <Box style={{ overflow: 'hidden' }}>
         <Carousel.Container>
-          {images.map((url) => (
-            <Carousel.Item tag="img" key={url} src={url} w={imageWidth} />
+          {images.map((url, index) => (
+            <Carousel.Item tag="img" key={url} src={url} w={imageWidth} alt={altTexts[index]} />
           ))}
         </Carousel.Container>
       </Box>
