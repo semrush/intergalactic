90b781eb14a3117c0df5ade1af7e7dc92b46691a
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-11-07 18:16:06 +0100

    [base-trigger] fixed `FilterTrigger` accessability
    <!--- Commit was signed off by phytonmk@gmail.com with GPG key ID CF2DC815AE956C1F -->

---
+++ semcore/base-trigger/__tests__/filter-trigger.vo-test.ts
@@ -0,0 +1,48 @@
+import { expect } from '@playwright/test';
+import { voTest as test } from '@guidepup/playwright';
+import { e2eStandToHtml } from '@semcore/jest-preset-ui/e2e-stand';
+import { resolve as resolvePath } from 'path';
+import { getReportHeader, makeVoiceOverReporter } from '@semcore/jest-preset-ui/vo-reporter';
+import { writeFile } from 'fs/promises';
+
+test('Users can interact with FilterTrigger via VoiceOver', async ({
+  page,
+  voiceOver: pureVoiceOver,
+}) => {
+  const standPath = resolvePath(
+    __dirname,
+    '../../../website/docs/components/filter-trigger/examples/select.jsx',
+  );
+  const reportPath = resolvePath(
+    __dirname,
+    '../../../website/docs/components/filter-trigger/filter-trigger-a11y-report.md',
+  );
+
+  const htmlContent = await e2eStandToHtml(standPath, 'en');
+  await page.reload();
+  await page.setContent(htmlContent);
+  const { voiceOver, getReport } = await makeVoiceOverReporter(pureVoiceOver);
+  await voiceOver.interact();
+
+  expect(await voiceOver.itemText()).toContain('Select option');
+  await voiceOver.act();
+  await voiceOver.next();
+  await voiceOver.next();
+  await voiceOver.next();
+  await voiceOver.act();
+  await voiceOver.stopInteracting();
+  await voiceOver.interact();
+  expect(await voiceOver.itemText()).toBe('Select option Filter list box pop-up group');
+  await voiceOver.interact();
+  expect(await voiceOver.itemText()).toBe('Option 3');
+  await voiceOver.next();
+  expect(await voiceOver.itemText()).toBe('Clear button');
+  await voiceOver.act();
+  await voiceOver.stopInteracting();
+  await voiceOver.interact();
+  expect(await voiceOver.itemText()).toBe('Select option Filter list box pop-up group');
+
+  const report = (await getReportHeader()) + '\n\n' + (await getReport(standPath));
+
+  await writeFile(reportPath, report);
+});
