3d851b76d77c59173561c84f25d658933f134b00
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-12-01 19:15:10 +0100

    [chore] added missing crowdin entries

--- semcore/i18n-unplugin/src/index.ts
+++ semcore/i18n-unplugin/src/index.ts
@@ -8,35 +8,6 @@ type Options = {
   excludeLocales?: string[];
 };
 
-`import de from './de.json';
-import en from './en.json';
-import es from './es.json';
-import fr from './fr.json';
-import it from './it.json';
-import ja from './ja.json';
-import ko from './ko.json';
-import pt from './pt.json';
-import ru from './ru.json';
-import tr from './tr.json';
-import vi from './vi.json';
-import zh from './zh.json';
-
-export const localizedMessages = {
-  de,
-  en,
-  es,
-  fr,
-  it,
-  ja,
-  ko,
-  pt,
-  ru,
-  tr,
-  vi,
-  zh,
-};
-`;
-
 export const semcoreI18nUnplugin: UnpluginInstance<Options> = createUnplugin((options: Options) => {
   return {
     name: 'semcore-i18n-unplugin',
