b0847dc520c184a62f75d9c2afa1d8ae18e39a1b
Mikhail Karachev <m.karachev@semrush.com>
2022-12-05 15:18:45 +0100

    Removed unused interface in InputTags

--- semcore/input-tags/src/InputTags.tsx
+++ semcore/input-tags/src/InputTags.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from 'react';
+import React from 'react';
 import createComponent, { Component, sstyled, Root, PropGetterFn, CProps } from '@semcore/core';
 import Input, { IInputProps, IInputValueProps } from '@semcore/input';
 import ScrollArea, { IScrollAreaProps } from '@semcore/scroll-area';
