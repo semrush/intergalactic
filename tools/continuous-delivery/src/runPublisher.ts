ca551a7162df16eea461ca248d931d145c2fe0d8
Mikhail Sereniti <phytonmk@gmail.com>
2022-12-12 22:09:49 +0100

    [continuous-delivery] build semcore/ui only after successfull publish of all other packages

--- tools/continuous-delivery/src/runPublisher.ts
+++ tools/continuous-delivery/src/runPublisher.ts
@@ -18,13 +18,10 @@ export const runPublisher = async (versionPatches: VersionPatch[]) => {
   const pnpmOptions = process.argv.includes('--dry-run') ? '--dry-run' : '';
   const gitTags = versionPatches.map((patch) => `${patch.package.name}@${patch.to}`);
   const toPublish = versionPatches.filter((patch) => patch.needPublish);
-  const pnpmFilter = toPublish.map((patch) => `--filter ${patch.package.name}`).join(' ');
 
-  versionPatches.sort((a, b) => {
-    if (a.package.name === '@semcore/ui') return 1;
-    if (b.package.name === '@semcore/ui') return -1;
-    return 0;
-  });
+  const semcoreUiPatch = toPublish.find((patch) => patch.package.name === '@semcore/ui');
+  const nonSemcoreUiPatches = toPublish.filter((patch) => patch !== semcoreUiPatch);
+  const pnpmFilter = nonSemcoreUiPatches.map((patch) => `--filter ${patch.package.name}`).join(' ');
 
   if (status.files.length) {
     await git.add('.');
@@ -35,7 +32,7 @@ export const runPublisher = async (versionPatches: VersionPatch[]) => {
       }
     }
   }
-  if (toPublish.length !== 0) {
+  if (nonSemcoreUiPatches.length !== 0) {
     execSync(`pnpm ${pnpmFilter} run build`, {
       encoding: 'utf-8',
       stdio: ['inherit', 'inherit', 'inherit'],
@@ -49,11 +46,21 @@ export const runPublisher = async (versionPatches: VersionPatch[]) => {
       stdio: ['inherit', 'inherit', 'inherit'],
     });
   }
+  if (semcoreUiPatch) {
+    execSync(`pnpm --filter @semcore/ui run build`, {
+      encoding: 'utf-8',
+      stdio: ['inherit', 'inherit', 'inherit'],
+    });
+    execSync(`pnpm --filter @semcore/ui publish ${pnpmOptions}`, {
+      encoding: 'utf-8',
+      stdio: ['inherit', 'inherit', 'inherit'],
+    });
+  }
   if (!process.argv.includes('--dry-run')) {
     await git.pull('origin', 'master', { '--rebase': 'true' });
     await git.push('origin', 'master', { '--follow-tags': null });
 
-    if (toPublish.find((versionPatch) => versionPatch.package.name === '@semcore/ui')) {
+    if (semcoreUiPatch) {
       execSync(`bash -e ./tools/continuous-delivery/src/gh-release`, {
         encoding: 'utf-8',
         stdio: ['inherit', 'inherit', 'inherit'],
