c4d44a827c81e0e6a0b63fd5d458aaf35b515a0e
Mikhail Sereniti <phytonmk@gmail.com>
2022-12-16 00:37:57 +0100

    [website] updated to 1.18.20: fixed new year widget was hidden until window resize

--- website/src/components/HappyNewYear.tsx
+++ website/src/components/HappyNewYear.tsx
@@ -4,10 +4,11 @@ import styles from './HappyNewYear.module.css';
 export const HappyNewYear: React.FC = () => {
   const [windowWidth, setWindowWidth] = React.useState(undefined);
   const lightsCount = React.useMemo(
-    () => (windowWidth === undefined ? 0 : Math.floor(windowWidth / 60) + 2),
+    () => (windowWidth === undefined ? 0 : Math.floor(windowWidth / 60) + 3),
     [windowWidth],
   );
   React.useLayoutEffect(() => {
+    setWindowWidth(window.innerWidth);
     const eventHandler = () => setWindowWidth(window.innerWidth);
     window.addEventListener('resize', eventHandler);
     return () => window.removeEventListener('resize', eventHandler);
