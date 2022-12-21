6caee8c6f3f5bc5d083ac62ee70c479d0d6f8168
Ruslan Gaiazov <freeyoungstrong@gmail.com>
2022-12-21 11:24:57 +0100

    [data-table] updated example

--- website/docs/table-group/data-table/examples/expanding-column.jsx
+++ website/docs/table-group/data-table/examples/expanding-column.jsx
@@ -10,6 +10,7 @@ export default () => {
         <DataTable.Column name="kd" children="Difficulty Difficulty" sortable wMax={'85px'} />
         <DataTable.Column name="cpc" children="CPC" sortable />
         <DataTable.Column name="vol" children="Vol." sortable wMax={'300px'} />
+        <DataTable.Column name="md" children="Marketing SEO" sortable wMax={'90px'} />
       </DataTable.Head>
       <DataTable.Body />
     </DataTable>
@@ -22,29 +23,34 @@ const data = [
     kd: '77.8',
     cpc: '$1.25',
     vol: '32,500,000',
+    md: '221',
   },
   {
     keyword: 'www.ebay.com',
     kd: '11.2',
     cpc: '$3.4',
     vol: '65,457,920',
+    md: '221',
   },
   {
     keyword: 'www.ebay.com',
     kd: '10',
     cpc: '$0.65',
     vol: '47,354,640',
+    md: 'n/a',
   },
   {
     keyword: 'ebay buy',
     kd: '-',
     cpc: '$0',
     vol: 'n/a',
+    md: '221',
   },
   {
     keyword: 'ebay buy',
     kd: '75.89',
     cpc: '$0',
     vol: '21,644,290',
+    md: '221',
   },
 ];
