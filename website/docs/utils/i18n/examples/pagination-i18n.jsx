229678554a97330ddc8e6e2f9726ffc61eeef673
Mikhail Sereniti <phytonmk@gmail.com>
2023-01-09 17:48:58 +0100

    [docs] added i18n page

---
+++ website/docs/utils/i18n/examples/pagination-i18n.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, Flex } from '@semcore/ui/flex-box';
+import Pagination from '@semcore/ui/pagination';
+
+function Demo() {
+  return (
+    <Flex>
+      <Box>
+        <Pagination
+          locale="epo"
+          i18n={{
+            epo: {
+              prevPageLabel: 'Antaŭa',
+              nextPageLabel: 'Sekva',
+              pageInputLabel: 'Paĝo:',
+              totalPagesLabel: 'de',
+              pagination: 'Paĝonumerado',
+              firstPage: 'Unua paĝo',
+              currentPage: 'Aktuala paĝo',
+              confirm: 'Konfirmi paĝnumeron',
+              lastPage: 'Lasta paĝo #{lastPageNumber}',
+              goToPage: 'Paĝo {pageNumber}',
+            },
+          }}
+          totalPages={123}
+        />
+      </Box>
+    </Flex>
+  );
+}
+
+export default Demo;
