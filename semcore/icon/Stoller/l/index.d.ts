3dfaf1d6e0411a1ee793c6a0c432c69a33e886a2
r.lysov <r.lysov@semrush.com>
2023-01-10 16:05:49 +0200

    [icon] Add depricated warning for Stoller icon
    Signed-off-by: r.lysov <r.lysov@semrush.com>

---
+++ semcore/icon/Stoller/l/index.d.ts
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import { IIconProps } from '@semcore/icon';
+declare const _default: <T>(props: IIconProps & T) => React.ReactElement;
+/**
+ * @deprecated The "Stoller" icon is deprecated. Use the "Stroller".
+ */
+export default _default;
+      
