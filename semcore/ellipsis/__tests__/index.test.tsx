bc975be44e6117d857b686c5b04fd13047a0d250
Roman <r.lysov@semrush.com>
2023-01-27 16:52:40 +0200

    [ellipsis] Don't show tooltip if it's not needed  (#521)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    If the text is not truncated, then do not show the tooltip and duplicate
    the text
    
    ## Motivation and Context
    
    We missed this point in initial development 🤷‍♂️
    
    ## How has this been tested?
    
    I wrote tests. But you can also run in the playground
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ---------
    
    Signed-off-by: r.lysov <r.lysov@semrush.com>

--- semcore/ellipsis/__tests__/index.test.tsx
+++ semcore/ellipsis/__tests__/index.test.tsx
@@ -3,7 +3,40 @@ import { snapshot, testing } from '@semcore/jest-preset-ui';
 import Ellipsis from '../src';
 import { Box } from '@semcore/flex-box';
 
-const { render, axe, cleanup } = testing;
+const { render, axe, cleanup, fireEvent, act } = testing;
+
+function fakeTemporaryBlock(rect) {
+  const originalCreateElement = global.document.createElement;
+
+  global.document.createElement = (tag, ...other) => {
+    if (tag === 'temporary-block') {
+      const temporaryBlock = originalCreateElement.call(document, tag, ...other);
+      fakeBoundingClientRect(rect)(temporaryBlock);
+      return temporaryBlock;
+    }
+    return originalCreateElement.call(document, tag, ...other);
+  };
+
+  return () => {
+    global.document.createElement = originalCreateElement;
+  };
+}
+
+function fakeBoundingClientRect(rect) {
+  return (node) => {
+    if (!node) return;
+    node.getBoundingClientRect = () => ({
+      width: 0,
+      height: 0,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      ...rect,
+    });
+  };
+}
+
 
 describe('Ellipsis', () => {
   afterEach(cleanup);
@@ -35,39 +68,32 @@ describe('Ellipsis', () => {
   });
 
   test('Renders correctly with trim in the middle', async () => {
-    const initialCreateElement = document.createElement;
-
-    global.document.createElement = (tag, ...other) => {
-      if (tag === 'temporary-block') {
-        const temporaryBlock = initialCreateElement.call(document, tag, ...other);
-        temporaryBlock.getBoundingClientRect = () => ({
-          width: 10,
-          height: 10,
-          top: 0,
-          left: 0,
-          right: 10,
-          bottom: 10,
-        });
-        return temporaryBlock;
-      }
-      return initialCreateElement.call(document, tag, ...other);
-    };
+    const originalResizeObserver = global.ResizeObserver;
+    const unFake = fakeTemporaryBlock({ width: 10 });
 
     class ResizeObserver {
+      private cb: any;
+
       constructor(cb) {
         this.cb = cb;
       }
+
       observe() {
         this.cb([{ contentRect: { width: 200 } }]);
       }
-      unobserve() {}
-      disconnect() {}
+
+      unobserve() {
+      }
+
+      disconnect() {
+      }
     }
+
     global.ResizeObserver = ResizeObserver;
 
     const component = (
       <Box w={200}>
-        <Ellipsis trim="middle">
+        <Ellipsis trim='middle'>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque autem commodi,
           doloribus ex harum inventore modi praesentium quam ratione reprehenderit rerum tempore
           voluptas. Aliquam eos expedita illo quasi unde!
@@ -76,6 +102,57 @@ describe('Ellipsis', () => {
     );
 
     expect(await snapshot(component)).toMatchImageSnapshot();
+
+    unFake();
+    global.ResizeObserver = originalResizeObserver;
+  });
+
+  test('Show tooltip', async () => {
+    jest.useFakeTimers();
+
+    const unFake = fakeTemporaryBlock({ width: 400 });
+
+    const { getByTestId, baseElement } = render(
+      <Box w={200}>
+        <Ellipsis data-testid='text' ref={fakeBoundingClientRect({ width: 200 })}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque autem commodi,
+          doloribus ex harum inventore modi praesentium quam ratione reprehenderit rerum tempore
+          voluptas. Aliquam eos expedita illo quasi unde!
+        </Ellipsis>
+      </Box>,
+    );
+
+    const text = getByTestId('text');
+    fireEvent.mouseEnter(text);
+    act(() => jest.runAllTimers());
+
+    expect(baseElement.querySelector('[data-ui-name="Tooltip.Popper"]')).not.toBe(null);
+
+    jest.useRealTimers();
+    unFake();
+  });
+
+  test('Dont show tooltip', async () => {
+    jest.useFakeTimers();
+
+    const unFake = fakeTemporaryBlock({ width: 100 });
+
+    const { getByTestId, baseElement } = render(
+      <Box w={200}>
+        <Ellipsis data-testid='text' ref={fakeBoundingClientRect({ width: 100 })}>
+          Lorem ipsum dolor sit amet
+        </Ellipsis>
+      </Box>,
+    );
+
+    const text = getByTestId('text');
+    fireEvent.mouseEnter(text);
+    act(() => jest.runAllTimers());
+
+    expect(baseElement.querySelector('[data-ui-name="Tooltip.Popper"]')).toBe(null);
+
+    jest.useRealTimers();
+    unFake()
   });
 
   test('a11y', async () => {
