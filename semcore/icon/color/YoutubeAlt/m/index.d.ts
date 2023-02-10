472bc20c22acd9b9ee45ca41ced5d7f52f6f5d3a
Ruslan Gaiazov <44463016+freeyoungstrong@users.noreply.github.com>
2023-02-10 16:46:48 +0100

    [icon] changed YoutubeAlt and YoutubeRed icons to YoutubeColored and YoutubeInvert accordingly (#536)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    - Removed `YoutubeAlt` and `YoutubeRed` icons, added `YoutubeColored`
    and `YoutubeInvert` accordingly instead.
    - Added js script to remove icons befor build instead of command in
    package.json.
    
    ## Motivation and Context
    
    - Names of old icons were incorrect.
    - To save old icons in package we shouldn't remove it and it was
    impossible to do it adding names of icons to command in package.json.
    That's why was decided to use js script to remove icons with more
    flexibility.
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

---
+++ semcore/icon/color/YoutubeAlt/m/index.d.ts
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import { IIconProps } from '@semcore/icon';
+declare const _default: <T>(props: IIconProps & T) => React.ReactElement;
+/**
+ * @deprecated The "YoutubeAlt" icon is deprecated. Use the "YoutubeColored".
+ */
+export default _default;
+      
