3e28de0e30bb6311a2456e712e9a7b39743f5b41
Julia Mnizhek <j.mnizhek@semrush.com>
2023-02-13 18:30:27 +0100

    [website] fix typos

---
+++ website/src/utils/amplitude 3.ts
@@ -0,0 +1,17 @@
+import amplitudeHttp from './amplitude-client';
+
+export const initAmplitude = () => {
+  const apiKey = process.env.AMPLITUDE_API_KEY;
+
+  if (!apiKey) {
+    return;
+  }
+
+  amplitudeHttp.init(apiKey, undefined);
+};
+
+export const logEvent = (eventType: string, eventProperties: Record<string, any> = {}) => {
+  if (process.env.NODE_ENV === 'development') return;
+
+  amplitudeHttp.logEvent(eventType, { event_properties: eventProperties });
+};
