7d63b794c022788aeff998273e7f691eb8e48eb5
Michael Sereniti <31261408+phytonmk@users.noreply.github.com>
2023-02-22 17:44:58 +0100

    [docs] added motion page (#550)
    ## Description
    
    Documentation by @tatana-I with visual easing widget. 
    
    ## Motivation and Context
    
    Recently an animation release was rolled out, we need a better
    documentation about our motion principles.
    
    ## Screenshots (if appropriate):
    
    <img width="300" alt="Screenshot 2023-02-22 at 16 58 20"
    src="https://user-images.githubusercontent.com/31261408/220681011-50710d9e-3587-4dcc-bfeb-06c2921280cb.png">
    <img width="300" alt="Screenshot 2023-02-22 at 16 58 02"
    src="https://user-images.githubusercontent.com/31261408/220680902-6707890e-0fba-41e0-aba2-4b650fc4fb28.png">
    <img width="300" alt="Screenshot 2023-02-22 at 16 57 50"
    src="https://user-images.githubusercontent.com/31261408/220680836-2601f8fe-6d6b-4884-b307-30fbcc167db4.png">
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [x] My change requires a change to the documentation.
    - [x] I have updated the documentation accordingly.

---
+++ website/docs/core-principles/motion-principles-guide/components/easing.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styles from './easing.module.css';
+import cx from 'classnames';
+
+// Example of usage: <Easing props={{
+//  "cssFunc": "cubic-bezier(0.37, 0, 0.63, 1)",
+//  "jsFunc": " -(Math.cos(Math.PI * x) - 1) / 2",
+//  "name": "ease-in-out-sine",
+//  "description": "Lorem ipsum"
+// }} />
+
+const chartSize = 200;
+
+const Easing = ({ cssFunc, jsFunc, name, description }) => {
+  const pathD = React.useMemo(() => {
+    const x = Array(chartSize)
+      .fill(0)
+      .map((_, index) => index);
+    const y = x
+      .map((x) => x / chartSize)
+      .map(jsFunc)
+      .map((y) => chartSize - y * chartSize + 1);
+    const points = x.map((x, i) => ({ x, y: y[i] }));
+    return 'M' + points.map(({ x, y }) => `${x} ${y}`).join(' L');
+  }, [jsFunc]);
+
+  return (
+    <div className={styles.container}>
+      <svg width={chartSize} height={chartSize + 2} className={styles.chart}>
+        <path d={pathD} strokeWidth={2} />
+      </svg>
+      <div className={styles.aside}>
+        <div className={styles.details}>
+          <div className={styles.title}>
+            animation-timing-function: <strong>{name}</strong>
+          </div>
+          <div className={styles.description}>
+            <div>{cssFunc}</div>
+            <div>{description}</div>
+          </div>
+        </div>
+        <div className={styles.slider}>
+          <div className={styles.sliderLine} />
+          <div className={styles.sliderThumb} style={{ transitionTimingFunction: name }} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Take easings from https://easings.net/
+const EasingsDemo = () => {
+  return (
+    <div aria-hidden="true" className={styles.demo}>
+      <Easing
+        cssFunc="cubic-bezier(0.5, 0, 0.75, 0)"
+        jsFunc={(x) => x * x * x * x}
+        name="ease-in"
+        description="moves from slow to fast."
+      />
+      <Easing
+        cssFunc="cubic-bezier(0.45, 0, 0.55, 1)"
+        jsFunc={(x) => (x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2)}
+        name="ease-in-out"
+        description="moves slowly on both ends."
+      />
+    </div>
+  );
+};
+
+export default EasingsDemo;
