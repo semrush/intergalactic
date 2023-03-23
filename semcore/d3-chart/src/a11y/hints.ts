cb1a72f7858a4195f279545755d8a1f931acf125
elena.krasnopolskaia <elena.krasnopolskaia@semrush.com>
2023-03-23 14:41:58 +0100

    [d3-chart] fixed case when additionalFields isn't provided

--- semcore/d3-chart/src/a11y/hints.ts
+++ semcore/d3-chart/src/a11y/hints.ts
@@ -189,5 +189,5 @@ export const makeDataSummarizationConfig = (
     notableDiff: undefined,
     ...(config?.movingAverage ?? {}),
   },
-  additionalFields: [...config.additionalFields]
+  additionalFields: [...config?.additionalFields ?? []]
 });
