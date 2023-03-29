e2e30a2e8de26824a0db1f557c9cc5e2eb4bc581
Mikhail Sereniti <phytonmk@gmail.com>
2023-03-29 16:14:35 +0200

    [chore] updated babel plugins fixtures after runtime update

--- tools/icon-transform-svg/__tests__/__fixtures__/pathNew/m/index.js
+++ tools/icon-transform-svg/__tests__/__fixtures__/pathNew/m/index.js
@@ -1,32 +1,24 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
-
 var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
-
 var _react = _interopRequireDefault(require("react"));
-
 var _core = require("@semcore/core");
-
 var _icon = _interopRequireDefault(require("@semcore/icon"));
-
 var _excluded = ["width", "height", "viewBox"];
-
 function pathNew(_ref, ref) {
   var _ref$width = _ref.width,
-      width = _ref$width === void 0 ? '16' : _ref$width,
-      _ref$height = _ref.height,
-      height = _ref$height === void 0 ? '16' : _ref$height,
-      _ref$viewBox = _ref.viewBox,
-      viewBox = _ref$viewBox === void 0 ? '0 0 16 16' : _ref$viewBox,
-      props = (0, _objectWithoutProperties2["default"])(_ref, _excluded);
+    width = _ref$width === void 0 ? '16' : _ref$width,
+    _ref$height = _ref.height,
+    height = _ref$height === void 0 ? '16' : _ref$height,
+    _ref$viewBox = _ref.viewBox,
+    viewBox = _ref$viewBox === void 0 ? '0 0 16 16' : _ref$viewBox,
+    props = (0, _objectWithoutProperties2["default"])(_ref, _excluded);
   return /*#__PURE__*/_react["default"].createElement(_icon["default"], (0, _extends2["default"])({
     ref: ref,
     "data-name": "pathNew",
@@ -41,9 +33,6 @@ function pathNew(_ref, ref) {
     shapeRendering: "geometricPrecision"
   }));
 }
-
 pathNew.displayName = 'pathNew';
-
 var _default = (0, _core.createBaseComponent)(pathNew);
-
 exports["default"] = _default;
