b581611ef9e1373ab1c4b2b65e5f25e573b96439
Mikhail Sereniti <phytonmk@gmail.com>
2023-03-29 16:14:35 +0200

    [chore] updated babel plugins fixtures after runtime update

--- tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/semcore-theme/color.expected.js
+++ tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/semcore-theme/color.expected.js
@@ -1,28 +1,23 @@
 'use strict';
 
 var _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault');
-
 Object.defineProperty(exports, '__esModule', {
   value: true,
 });
 exports['default'] = resolveColor;
-
 var _slicedToArray2 = _interopRequireDefault(require('@babel/runtime/helpers/slicedToArray'));
+
 /*__semcore-vars__:"./var.css"*/
 
 /* @ts-ignore */
-
 var COLORS = {
   green: `#4fae33`,
   cyan: `#00b0ed`,
 };
-
 function resolveColor(color) {
   if (!color) return '';
-
   if (color in COLORS) {
     return COLORS[color];
   }
-
   return color;
 }
