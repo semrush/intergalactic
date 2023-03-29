0203fa523eed8d739fa8f5c71ae0f1ca2bbd08fa
Mikhail Sereniti <phytonmk@gmail.com>
2023-03-29 16:14:35 +0200

    [chore] updated babel plugins fixtures after runtime update

--- tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/unchanged/index.expected.js
+++ tools/babel-plugin-react-semcore/__tests__/__fixtures__/theme/unchanged/index.expected.js
@@ -1,24 +1,20 @@
 import _extends from '@babel/runtime/helpers/extends';
 import React from 'react';
 import { styled, css, __css__, set, create, map } from '@semcore/core';
-/*__reshadow-styles__:"./button.shadow.css"*/
 
-var _ref =
-    /*__reshadow_css_start__*/
+/*__reshadow-styles__:"./button.shadow.css"*/
+var _ref /*__reshadow_css_start__*/ =
     (__css__(
       /*__inner_css_start__*/
-      '.___SButton_1pmdw_gg_ {\n  padding: 0 20px;\n  background: purple;\n}\n\n.___SButton_1pmdw_gg_.__theme_1pmdw_gg_.__theme_default_1pmdw_gg_ {\n  background: red;\n}\n',
-      /*__inner_css_end__*/
+      '.___SButton_1pmdw_gg_ {\n  padding: 0 20px;\n  background: purple;\n}\n\n.___SButton_1pmdw_gg_.__theme_1pmdw_gg_.__theme_default_1pmdw_gg_ {\n  background: red;\n}\n' /*__inner_css_end__*/,
       '1hjym_gg_',
-    ),
-    /*__reshadow_css_end__*/
+    ) /*__reshadow_css_end__*/,
     {
       __SButton: '___SButton_1pmdw_gg_',
       _theme: '__theme_1pmdw_gg_',
       _theme_default: '__theme_default_1pmdw_gg_',
     }),
   styles = _extends({}, _ref);
-
 var styled_c8 = create([styles]);
 export default function(props) {
   var SButton = 'div';
