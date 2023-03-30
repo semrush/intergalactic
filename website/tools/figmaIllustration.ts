07e0c3d7304c1a11568c8e321a442b0c31f355a1
elena.krasnopolskaia <elena.krasnopolskaia@semrush.com>
2023-03-30 11:49:47 +0200

    [website] added script for downloading illustration from figma (#593)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    Wanted to automate the change of illustrations in the documentation.
    Figma api has been used for this purpose
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] My change requires a change to the documentation.
    - [x] I have updated the documentation accordingly.
    
    ---------
    
    Co-authored-by: Mikhail Sereniti <phytonmk@gmail.com>

---
+++ website/tools/figmaIllustration.ts
@@ -0,0 +1,105 @@
+/* eslint-disable no-console */
+import fetch from 'node-fetch';
+import * as fs from 'fs';
+import sharp from 'sharp';
+import * as dotenv from 'dotenv';
+import pLimit from 'p-limit';
+
+const limit = pLimit(1);
+const FIGMA_PROJECT_ID = '74268036';
+
+dotenv.config();
+
+if (!process.env.FIGMA_API_KEY) {
+  throw new Error('Create .env file and insert FIGMA_API_KEY variable that can be generated on https://www.figma.com/developers/api#authentication');
+}
+
+const figmaKey = process.env.FIGMA_API_KEY;
+
+const downloadIllustrations = async () => {
+  const chosenPath = process.argv.slice(2);
+
+  const getIllustration = async (children, category, fileId) => {
+    const folderName = `./docs/${category}/${children.name
+      .toLowerCase()
+      .split(' ')
+      .join('-')}/static`;
+    console.log('page', children.name);
+
+    if (fs.existsSync(folderName)) {
+      for (const illustration of children.children) {
+        try {
+          const response = await fetch(
+            `https://api.figma.com/v1/images/${fileId}?ids=${illustration.id}&format=png`,
+            { headers: { 'X-Figma-Token': figmaKey } },
+          );
+
+          const data: { images?: { id: string; name: string } } = await response.json();
+          const imageUrl = data.images[illustration.id];
+
+          fetch(imageUrl)
+            .then((res) => res.arrayBuffer())
+            .then((arrayBuffer) => {
+              const buffer = Buffer.from(arrayBuffer);
+              sharp(buffer)
+                .png({ quality: 90 })
+                .toFile(folderName + `/${illustration.name}.png`);
+            });
+
+          const fileName = `${illustration.name}.png`;
+          console.log('illustration', fileName);
+        } catch (error) {
+          console.error(error.message);
+        }
+      }
+    }
+  };
+
+  const getIllustrationList = async (fileId) => {
+    const response = await fetch(`https://api.figma.com/v1/files/${fileId}`, {
+      headers: { 'X-Figma-Token': figmaKey },
+    });
+
+    if (response.ok) {
+      try {
+        const data: { name?: string; document?: { children: { name: string }[] } } =
+          await response.json();
+        const category = data.name.toLowerCase().split(' ').join('-');
+        console.log('category', category);
+        const downloadPromises = data.document.children
+          .filter(
+            (file) =>
+              chosenPath.length !== 2 || file.name.toLowerCase() === chosenPath[1].toLowerCase(),
+          )
+          .map((children) =>
+            limit(async () => {
+              await getIllustration(children, category, fileId);
+            }),
+          );
+        await Promise.all(downloadPromises);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  };
+
+  const response = await fetch(`https://api.figma.com/v1/projects/${FIGMA_PROJECT_ID}/files`, {
+    headers: { 'X-Figma-Token': figmaKey },
+  });
+
+  if (response.ok) {
+    try {
+      const data: { files?: { key: string; name: string }[] } = await response.json();
+      data.files
+        .filter((file) => !chosenPath.length || file.name.toLowerCase() === chosenPath[0])
+        .map((file) => getIllustrationList(file.key));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+downloadIllustrations().catch((error) => {
+  console.error('Loading error', error);
+});
+// When calling the script, the first argument can be a filename and the second argument a page name in the file
