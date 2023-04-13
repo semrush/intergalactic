e542e8287bb7c27230fedd16f23c2876bf8063c3
Mikhail Sereniti <phytonmk@gmail.com>
2023-04-13 16:21:46 +0200

    [utils] delayed focus return in the macrotask to prevent focus return trigger event passing to to the return focus target

--- website/docs/components/fullscreen-modal/examples/header.jsx
+++ website/docs/components/fullscreen-modal/examples/header.jsx
@@ -3,18 +3,14 @@ import FullscreenModal from '@semcore/ui/fullscreen-modal';
 import Button from '@semcore/ui/button';
 
 const Demo = () => {
-  const [visible, updateVisible] = useState(false);
-
-  const changeModalVisibility = () => {
-    updateVisible((visible) => !visible);
-  };
+  const [visible, setVisible] = useState(false);
 
   return (
     <>
-      <Button use="primary" onClick={changeModalVisibility}>
+      <Button use="primary" onClick={() => setVisible(true)}>
         Open fullscreen window
       </Button>
-      <FullscreenModal visible={visible} onClose={changeModalVisibility}>
+      <FullscreenModal visible={visible} onClose={() => setVisible(false)}>
         <FullscreenModal.Close />
         <FullscreenModal.Back>Go to Tool Name</FullscreenModal.Back>
         <FullscreenModal.Header title="Heading 2, 25px" description="Some additional information" />
