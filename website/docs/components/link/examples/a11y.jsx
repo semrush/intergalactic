92b4d8a52bc630557a28c4ba2a571b187c210dd6
Mikhail Sereniti <phytonmk@gmail.com>
2023-04-13 15:31:14 +0200

    [docs] add href attrs to examples with Link component

--- website/docs/components/link/examples/a11y.jsx
+++ website/docs/components/link/examples/a11y.jsx
@@ -6,8 +6,8 @@ import ArrowRightM from '@semcore/ui/icon/ArrowRight/m';
 export default function () {
   return (
     <>
-      <Link addonLeft={HomeM} aria-label="home page" />
-      <Link ml={2} aria-label="go to the next page">
+      <Link addonLeft={HomeM} aria-label="home page" href="#" />
+      <Link ml={2} aria-label="go to the next page" href="#">
         <Link.Addon>
           <ArrowRightM />
         </Link.Addon>
