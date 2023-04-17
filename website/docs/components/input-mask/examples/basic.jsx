62a4483e2ef58506cc7ce4476b3b2c6742cd1399
r.lysov <r.lysov@semrush.com>
2023-04-17 16:37:01 +0200

    [input-mask] a11y improve
    Signed-off-by: r.lysov <r.lysov@semrush.com>

--- website/docs/components/input-mask/examples/basic.jsx
+++ website/docs/components/input-mask/examples/basic.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import InputMask from '@semcore/ui/input-mask';
+import { Text } from '@semcore/ui/typography';
 
 export default () => {
   return (
-    <InputMask w={300}>
-      <InputMask.Value
-        mask="9999 9999 9999 9999"
-        placeholder="____ ____ ____ ____"
-        title="16-digit number"
-      />
-    </InputMask>
+    <>
+      <Text tag="label" htmlFor="basic-example" size="300">
+        Card number
+      </Text>
+      <InputMask w={300}>
+        <InputMask.Value
+          mask="9999 9999 9999 9999"
+          placeholder="____ ____ ____ ____"
+          title="16-digit number"
+          id="basic-example"
+        />
+      </InputMask>
+    </>
   );
 };
--- website/docs/components/input-phone/examples/basic.jsx
+++ website/docs/components/input-phone/examples/basic.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import Input from '@semcore/ui/input';
 import CloseXS from '@semcore/ui/icon/Close/m';
+import { Text } from '@semcore/ui/typography';
 
 const Demo = () => {
   const [value, updateValue] = useState('+');
   return (
-    <Input w={180}>
-      <Input.Value value={value} onChange={(v) => updateValue(v)} />
-      {value.length > 1 && (
-        <Input.Addon
-          tag={CloseXS}
-          interactive
-          aria-label="Clear field"
-          onClick={() => updateValue('+')}
-        />
-      )}
-    </Input>
+    <>
+      <Text tag='label' htmlFor='basic-example' size='300'>
+        Phone
+      </Text>
+      <Input w={180}>
+        <Input.Value id='basic-example' value={value} onChange={(v) => updateValue(v)} />
+        {value.length > 1 && (
+          <Input.Addon
+            tag={CloseXS}
+            interactive
+            aria-label='Clear field'
+            onClick={() => updateValue('+')}
+          />
+        )}
+      </Input>
+    </>
   );
 };
 
