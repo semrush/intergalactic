5b7564c3bb7c73f81f2b63274c0d58ed583edfd9
Mikhail Sereniti <phytonmk@gmail.com>
2023-04-24 18:24:45 +0200

    [animation] fixed collapse animations in Safari

--- semcore/animation/src/Collapse.jsx
+++ semcore/animation/src/Collapse.jsx
@@ -1,22 +1,40 @@
-import React, { useCallback, useState, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { createBaseComponent, sstyled } from '@semcore/core';
 import Animation from './Animation';
 import style from './style/keyframes.shadow.css';
+import { useForkRef } from '@semcore/utils/lib/ref';
 
 function Collapse({ onAnimationStart, onAnimationEnd, overflowHidden = true, ...props }, ref) {
   const SCollapse = Animation;
-  const [height, setHeightVar] = useState('auto');
   const overflowRef = useRef('initial');
+  const innerRef = useRef(null);
+  const forkedRef = useForkRef(innerRef, ref);
 
-  const handlerAnimationStart = useCallback((e) => {
-    if (e.currentTarget !== e.target) return;
-    if (onAnimationStart) onAnimationStart(e);
-    if (overflowHidden) {
-      overflowRef.current = window.getComputedStyle(e.currentTarget).overflow;
-      e.currentTarget.style.overflow = 'hidden';
-    }
-    setHeightVar(e.currentTarget.scrollHeight + 'px');
+  useEffect(() => {
+    if (!innerRef.current) return;
+    innerRef.current.style.height = 'auto';
   }, []);
+
+  const handlerAnimationStart = useCallback(
+    (e) => {
+      if (e.currentTarget !== e.target) return;
+      if (onAnimationStart) onAnimationStart(e);
+      if (overflowHidden) {
+        overflowRef.current = window.getComputedStyle(e.currentTarget).overflow;
+        e.currentTarget.style.overflow = 'hidden';
+      }
+
+      const el = e.currentTarget;
+
+      if (props.visible) el.style.height = 0 + 'px';
+      else el.style.height = el.scrollHeight + 'px';
+      setTimeout(() => {
+        if (props.visible) el.style.height = el.scrollHeight + 'px';
+        else el.style.height = 0 + 'px';
+      }, 0);
+    },
+    [props.visible],
+  );
 
   const handlerAnimationEnd = useCallback((e) => {
     if (e.currentTarget !== e.target) return;
@@ -30,16 +48,19 @@ function Collapse({ onAnimationStart, onAnimationEnd, overflowHidden = true, ...
         }
       }, 0);
     }
-    setHeightVar('auto');
+
+    const el = e.currentTarget;
+    setTimeout(() => {
+      el.style.height = 'auto';
+    }, 0);
   }, []);
 
   return sstyled(style)(
     <SCollapse
-      ref={ref}
+      ref={forkedRef}
       {...props}
       onAnimationStart={handlerAnimationStart}
       onAnimationEnd={handlerAnimationEnd}
-      height={height}
       keyframes={[style['@collapse-enter'], style['@collapse-exit']]}
     />,
   );
