3d7561f17f4b4c3bccc29909488378b00fa1e6ca
Mikhail Sereniti <phytonmk@gmail.com>
2023-05-02 16:35:12 +0200

    [docs] improved filter-group/filter-search examples

--- website/docs/filter-group/filter-search/examples/SearchByButton.jsx
+++ website/docs/filter-group/filter-search/examples/SearchByButton.jsx
@@ -4,6 +4,8 @@ import CloseXS from '@semcore/ui/icon/Close/m';
 import Search from '@semcore/ui/icon/Search/m';
 import Button from '@semcore/ui/button';
 import NeighborLocation from '@semcore/ui/neighbor-location';
+import { Text } from '@semcore/ui/typography';
+import { Flex, Box } from '@semcore/ui/flex-box';
 
 const Demo = () => {
   const [value, setValue] = useState('');
@@ -20,19 +22,36 @@ const Demo = () => {
   }, ['']);
 
   return (
-    <NeighborLocation>
-      <Input w={200}>
-        <Input.Value placeholder="Filter by keyword" value={value} onChange={handleChange} />
-        {value && (
-          <Input.Addon tag={CloseXS} interactive onClick={handleClick} aria-label="Clear filter" />
-        )}
-      </Input>
-      <Button aria-label="Search">
-        <Button.Addon>
-          <Search />
-        </Button.Addon>
-      </Button>
-    </NeighborLocation>
+    <Flex direction="column">
+      <Text tag="label" htmlFor="search-by-button-filter-by-keyword">
+        Filter by keyword
+      </Text>
+      <Box mt={2}>
+        <NeighborLocation>
+          <Input w={200}>
+            <Input.Value
+              value={value}
+              onChange={handleChange}
+              id="search-by-button-filter-by-keyword"
+              placeholder="Enter keyword here"
+            />
+            {value && (
+              <Input.Addon
+                tag={CloseXS}
+                interactive
+                onClick={handleClick}
+                aria-label="Clear filter"
+              />
+            )}
+          </Input>
+          <Button aria-label="Search">
+            <Button.Addon>
+              <Search />
+            </Button.Addon>
+          </Button>
+        </NeighborLocation>
+      </Box>
+    </Flex>
   );
 };
 
