7ba2298ce5be5204a889c06df5b6093c3da3d65f
Mikhail Sereniti <phytonmk@gmail.com>
2023-05-02 16:35:12 +0200

    [docs] improved filter-group/filter-search examples

--- website/docs/filter-group/filter-search/examples/DynamicSearch.jsx
+++ website/docs/filter-group/filter-search/examples/DynamicSearch.jsx
@@ -2,6 +2,8 @@ import React, { useCallback, useState } from 'react';
 import Input from '@semcore/ui/input';
 import CloseXS from '@semcore/ui/icon/Close/m';
 import Search from '@semcore/ui/icon/Search/m';
+import { Text } from '@semcore/ui/typography';
+import { Flex } from '@semcore/ui/flex-box';
 
 const Demo = () => {
   const [value, setValue] = useState('');
@@ -18,15 +20,25 @@ const Demo = () => {
   }, ['']);
 
   return (
-    <Input w={200} aria-live="polite">
-      <Input.Addon>
-        <Search />
-      </Input.Addon>
-      <Input.Value placeholder="Filter by keyword" value={value} onChange={handleChange} />
-      {value && (
-        <Input.Addon tag={CloseXS} interactive onClick={handleClick} aria-label="Clear filters" />
-      )}
-    </Input>
+    <Flex direction="column">
+      <Text tag="label" htmlFor="dynamic-search-filter-by-keyword">
+        Filter by keyword
+      </Text>
+      <Input w={200} mt={2} aria-live="polite">
+        <Input.Addon>
+          <Search />
+        </Input.Addon>
+        <Input.Value
+          value={value}
+          onChange={handleChange}
+          id="dynamic-search-filter-by-keyword"
+          placeholder="Enter keyword here"
+        />
+        {value && (
+          <Input.Addon tag={CloseXS} interactive onClick={handleClick} aria-label="Clear filters" />
+        )}
+      </Input>
+    </Flex>
   );
 };
 
