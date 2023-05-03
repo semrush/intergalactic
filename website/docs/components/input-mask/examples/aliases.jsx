6ec79d47c6184ef246f9e03f7a34b597e4ad8f4c
Mikhail Sereniti <phytonmk@gmail.com>
2023-05-03 16:39:36 +0200

    [docs] improved input mask examples a11y

--- website/docs/components/input-mask/examples/aliases.jsx
+++ website/docs/components/input-mask/examples/aliases.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 import InputMask from '@semcore/ui/input-mask';
+import { Text } from '@semcore/ui/typography';
+import { Flex } from '@semcore/ui/flex-box';
 
 export default () => {
   const [value, changeValue] = useState('99');
   return (
-    <InputMask w={300}>
-      <InputMask.Value
-        aliases={{ x: /[0-9]/ }}
-        mask="99xxxx"
-        value={value}
-        onChange={changeValue}
-        title="4-digit number"
-      />
-    </InputMask>
+    <Flex direction="column" gap={2} w={224}>
+      <Text tag="label" htmlFor="aliases-example" size="200">
+        Item index
+      </Text>
+      <InputMask w={224}>
+        <InputMask.Value
+          id="aliases-example"
+          aliases={{ x: /[0-9]/ }}
+          mask="99xxxx"
+          value={value}
+          onChange={changeValue}
+          aria-describedby="aliases-example-hint"
+          title="4-digit number"
+        />
+      </InputMask>
+      <Text size="100" color="#6C6E79" id="aliases-example-hint">
+        Write any 4 digits and after
+      </Text>
+    </Flex>
   );
 };
--- website/docs/components/input-mask/examples/basic.jsx
+++ website/docs/components/input-mask/examples/basic.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import InputMask from '@semcore/ui/input-mask';
 import { Text } from '@semcore/ui/typography';
+import { Flex } from '@semcore/ui/flex-box';
 
 export default () => {
   return (
-    <>
-      <Text tag="label" htmlFor="basic-example" size="300">
+    <Flex direction="column" gap={2} w={224}>
+      <Text tag="label" htmlFor="basic-example" size="200">
         Card number
       </Text>
-      <InputMask w={300}>
+      <InputMask w={224}>
         <InputMask.Value
           mask="9999 9999 9999 9999"
           placeholder="____ ____ ____ ____"
           title="16-digit number"
           id="basic-example"
+          aria-describedby="basic-example-hint"
         />
       </InputMask>
-    </>
+      <Text size="100" color="#6C6E79" id="basic-example-hint">
+        Please enter the 16-digit number of your debit card.
+      </Text>
+    </Flex>
   );
 };
