bfd8ba828b0c0a76f07b313d60db7b23e4e56f92
Mikhail Sereniti <phytonmk@gmail.com>
2023-06-22 16:19:08 +0200

    [chore] little tests enhancements

--- tools/testing-utils/vitest.ts
+++ tools/testing-utils/vitest.ts
@@ -4,7 +4,7 @@ export * from 'vitest';
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toMatchImageSnapshot(task: any): R;
+      toMatchImageSnapshot(task: any, options?: { maxPixelDiff?: number }): R;
       toHaveNoViolations(): R;
     }
   }
