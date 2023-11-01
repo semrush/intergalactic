5d8e03bfba99fc8e7f876e6f7314ce8908815cb1
ilia.brauer <ilia.brauer@semrush.com>
2023-11-01 13:19:07 +0100

    [base-trigger] Do not animate the width/height of a node if they are specified as a percentage

--- website/docs/components/select/examples/custom-trigger.tsx
+++ website/docs/components/select/examples/custom-trigger.tsx
@@ -9,7 +9,7 @@ export default () => {
   const [value, setValue] = React.useState(null);
 
   return (
-    <Flex>
+    <>
       <Select onChange={setValue} placeholder='Select country'>
         <Select.Trigger w={180}>
           <Select.Trigger.Addon>
@@ -26,6 +26,24 @@ export default () => {
           ))}
         </Select.Menu>
       </Select>
-    </Flex>
+      <br />
+      <br />
+      <Select onChange={setValue} placeholder='Select country'>
+        <Select.Trigger w={'100%'}>
+          <Select.Trigger.Addon>
+            <Flags iso2={value} />
+          </Select.Trigger.Addon>
+          <Select.Trigger.Text>{formatName(iso2Name[value])}</Select.Trigger.Text>
+        </Select.Trigger>
+        <Select.Menu hMax={180}>
+          {Object.keys(iso2Name).map((value) => (
+            <Select.Option key={value} value={value}>
+              <Flags iso2={value as keyof typeof iso2Name} mr={2} />
+              {formatName(iso2Name[value])}
+            </Select.Option>
+          ))}
+        </Select.Menu>
+      </Select>
+    </>
   );
 };
