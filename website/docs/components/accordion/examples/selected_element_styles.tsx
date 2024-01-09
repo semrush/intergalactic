a3beb58294e42ec11d2b3a5e872a2d061cfdee4b
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2024-01-09 16:27:37 +0100

    [website] updated to 1.30.12: much improved accordion elements styles example"

--- website/docs/components/accordion/examples/selected_element_styles.tsx
+++ website/docs/components/accordion/examples/selected_element_styles.tsx
@@ -3,33 +3,53 @@ import Accordion from '@semcore/ui/accordion';
 import { Text } from '@semcore/ui/typography';
 import { Box, Flex } from '@semcore/ui/flex-box';
 
+const cn = (...classes: any[]) => classes.filter(Boolean).join(' ');
+
 const Demo = () => {
   return (
-    <Accordion>
-      {[...new Array(3)].map((_, index) => (
-        <Accordion.Item value={index} key={index}>
-          {({ selected }) => (
-            <>
-              <Accordion.Item.Toggle
-                tag={Flex}
-                alignItems='center'
-                style={{
-                  backgroundColor: selected ? 'var(--intergalactic-bg-secondary-neutral)' : '',
-                  padding: '8px 12px',
-                  marginBottom: '1px',
-                }}
-              >
-                <Accordion.Item.Chevron mr={2} />
-                <Text size={300} tag='h3' my={0}>{`Section ${index + 1}`}</Text>
-              </Accordion.Item.Toggle>
-              <Accordion.Item.Collapse>
-                <Box p='12px 36px'>{`Hello Section ${index + 1}`}</Box>
-              </Accordion.Item.Collapse>
-            </>
-          )}
-        </Accordion.Item>
-      ))}
-    </Accordion>
+    <>
+      <style>
+        {`
+          .styled-accordion-item {
+            background-color: var(--intergalactic-bg-secondary-neutral);
+            padding: var(--intergalactic-spacing-2x) var(--intergalactic-spacing-3x);
+          }
+          .styled-accordion-item:first-of-type {
+            border-radius: var(--intergalactic-control-rounded) var(--intergalactic-control-rounded) 0 0;
+          }
+          .styled-accordion-item:last-of-type {
+            border-radius: 0 0 var(--intergalactic-control-rounded) var(--intergalactic-control-rounded);
+          }
+          .styled-accordion-item-selected {
+            background-color: var(--intergalactic-bg-secondary-neutral-hover);
+          }
+        `}
+      </style>
+      <Accordion>
+        {[...new Array(3)].map((_, index) => (
+          <Accordion.Item value={index} key={index}>
+            {({ selected }) => (
+              <>
+                <Accordion.Item.Toggle
+                  tag={Flex}
+                  alignItems='center'
+                  className={cn(
+                    'styled-accordion-item',
+                    selected && 'styled-accordion-item-selected',
+                  )}
+                >
+                  <Accordion.Item.Chevron mr={2} />
+                  <Text size={200} tag='h3' my={0}>{`Section ${index + 1}`}</Text>
+                </Accordion.Item.Toggle>
+                <Accordion.Item.Collapse>
+                  <Box p='12px 32px'>{`Hello Section ${index + 1}`}</Box>
+                </Accordion.Item.Collapse>
+              </>
+            )}
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </>
   );
 };
 
