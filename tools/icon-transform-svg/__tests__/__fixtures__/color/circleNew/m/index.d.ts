bc901580437a3feaf98025e5d541d71aaf779a2a
Michael Sereniti <31261408+msereniti@users.noreply.github.com>
2024-01-15 13:15:20 +0100

    [icon] fixed typings (#1014)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Icons typings are controlled by `icon-transform-svg` component. So
    during major typings update it was forgot to be updated. It's why
    `IIconProps` was used in component typings instead of `IconProps`.
    
    Also I have updated all use of `I*Props` in docs
    
    ## How has this been tested?
    
    All existing tests should pass
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [x] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

--- tools/icon-transform-svg/__tests__/__fixtures__/color/circleNew/m/index.d.ts
+++ tools/icon-transform-svg/__tests__/__fixtures__/color/circleNew/m/index.d.ts
@@ -1,5 +1,5 @@
 import React from 'react';
 import { Merge } from '@semcore/core';
-import { IIconProps } from '@semcore/icon';
-declare const _default: import("@semcore/core").ComponentType<Merge<IIconProps, React.SVGAttributes<SVGElement>>, {}, {}>;
+import { IconProps } from '@semcore/icon';
+declare const _default: import("@semcore/core").ComponentType<Merge<IconProps, React.SVGAttributes<SVGElement>>, {}, {}>;
 export default _default;
