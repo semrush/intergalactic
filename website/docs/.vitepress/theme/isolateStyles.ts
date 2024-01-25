748b045fd481943c8670d7e53af94ad621f88287
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2024-01-25 14:42:00 +0100

    [website] updated to 1.32.0: disabled styles isolation

--- website/docs/.vitepress/theme/isolateStyles.ts
+++ website/docs/.vitepress/theme/isolateStyles.ts
@@ -1,25 +1,31 @@
 export const isolateStyles = (node: HTMLElement) => {
-  if (!document.adoptedStyleSheets) {
-    return node;
-  }
+  return node;
 
-  node.attachShadow({ mode: 'open' });
-  const shadowRoot = node.shadowRoot!;
-  const element = document.createElement('div');
-  shadowRoot.appendChild(element);
-  const reshadowContainer = document.querySelector('#__reshadow__');
-  if (reshadowContainer) {
-    shadowRoot.adoptedStyleSheets.push(
-      ...[...reshadowContainer.children].map((node) => {
-        const sheet = new CSSStyleSheet();
-        const styleNode = node as HTMLStyleElement;
-        const cssRules = [...(styleNode.sheet?.cssRules ?? [])];
-        const cssText = cssRules.reduce((acc, rule) => acc + rule.cssText, '');
-        sheet.replaceSync(cssText);
-        return sheet;
-      }),
-    );
-  }
+  /**
+   * Styles isolation is disabled caused our components are not ready to work in shadow DOM.
+   */
 
-  return element;
+  // if (!document.adoptedStyleSheets) {
+  //   return node;
+  // }
+
+  // node.attachShadow({ mode: 'open' });
+  // const shadowRoot = node.shadowRoot!;
+  // const element = document.createElement('div');
+  // shadowRoot.appendChild(element);
+  // const reshadowContainer = document.querySelector('#__reshadow__');
+  // if (reshadowContainer) {
+  //   shadowRoot.adoptedStyleSheets.push(
+  //     ...[...reshadowContainer.children].map((node) => {
+  //       const sheet = new CSSStyleSheet();
+  //       const styleNode = node as HTMLStyleElement;
+  //       const cssRules = [...(styleNode.sheet?.cssRules ?? [])];
+  //       const cssText = cssRules.reduce((acc, rule) => acc + rule.cssText, '');
+  //       sheet.replaceSync(cssText);
+  //       return sheet;
+  //     }),
+  //   );
+  // }
+
+  // return element;
 };
