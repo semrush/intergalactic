c97fc91fbe9cf6262575d5b192a4139558774822
ilyabrower <ilia.brauer@semrush.com>
2024-02-01 14:40:51 +0100

    [global] new release system - publish both releases from one script (#1065)

--- tools/continuous-delivery/src/intg-release/publishRelease.ts
+++ tools/continuous-delivery/src/intg-release/publishRelease.ts
@@ -19,7 +19,7 @@ const git = Git();
 
 const dirname = path.resolve(process.cwd(), 'node_modules', 'intergalactic');
 
-const publishRelease = async () => {
+const publishIntergalacticRelease = async () => {
   const packageJsonFilePath = path.resolve(dirname, 'package.json');
   const packageJson = fs.readJSONSync(packageJsonFilePath);
   const deps = fs.readJSONSync(path.resolve(dirname, 'components.json'));
@@ -64,25 +64,82 @@ const publishRelease = async () => {
   fs.writeJsonSync(path.resolve(packagePath, 'package.json'), packageJson, { spaces: 2 });
   await publishTarball(packageJson.name, packagePath);
 
-  // 5.1) Update versions in package.json in all checked components
-  // TODO - For now, they updates in old release process. Uncomment after it will be removed.
-  // updateVersionInComponents(changelogs);
-
   // 6) Close tasks in clickup
   if (!process.argv.includes('--dry-run') && version) {
     await closeTasks(version);
   }
-
-  // 7) Commit changes in package.json and components.json
-  if (!process.argv.includes('--dry-run') && version) {
-    await commitPatch(version);
-  }
-
-  // 8) Release notes in slack channel
-  // todo Brauer Ilia: uncomment after removing the previous release system
-  // if (!process.argv.includes('--dry-run') && version) {
-  //   await publishReleaseNotes(version, changelogs.slice(0, 1));
-  // }
 };
 
-export { publishRelease };
+// todo Brauer Ilia: uncomment after removing the previous release system
+// /** use it in future, after only one release system */
+const publishRelease = async () => {};
+//   const packageJsonFilePath = path.resolve(dirname, 'package.json');
+//   const packageJson = fs.readJSONSync(packageJsonFilePath);
+//   const deps = fs.readJSONSync(path.resolve(dirname, 'components.json'));
+//   const packages = Object.keys(deps);
+//
+//   // 1) Update changelog
+//   const { changelogs, version } = await updateReleaseChangelog(packageJson, deps);
+//
+//   // 2) Update version in package.json
+//   packageJson.version = version;
+//   fs.writeJsonSync(packageJsonFilePath, packageJson, { spaces: 2 });
+//
+//   // 3) Check that all tests are passed and release is unlocked
+//   const unlockedRelease = await getUnlockedPrerelease(packageJsonFilePath, log);
+//   if (!unlockedRelease) {
+//     log('No unlocked prerelease found.');
+//     if (!process.argv.includes('--dry-run')) {
+//       return;
+//     }
+//   }
+//
+//   // 4) Get prerelease tarball
+//   const logs = await git.log({ maxCount: 10 });
+//   const filteredLogs = logs.all.filter((item) => item.author_name !== CI_AUTHOR_NAME);
+//   const hash = filteredLogs[0].hash;
+//   const shortHash = hash.slice(0, 8);
+//
+//   const npmResponse = await axios.get<ResponseNpmRegistry>(
+//     `https://registry.npmjs.org/intergalactic/${version}-prerelease-${shortHash}`,
+//   );
+//
+//   const tarballUrl = npmResponse.data.dist.tarball;
+//
+//   const tarballPaths = await downloadTarballs([tarballUrl]);
+//   const [packagePath] = await unpackTarballs(tarballPaths);
+//
+//   // 5) Update versions in components.json in both tarball nd current entry-point
+//   updateComponentsVersions(packages, path.resolve(packagePath, 'components.json'));
+//   updateComponentsVersions(packages, path.resolve(dirname, 'components.json'));
+//
+//   // 5) Publish package
+//   fs.writeJsonSync(path.resolve(packagePath, 'package.json'), packageJson, { spaces: 2 });
+//   await publishTarball(packageJson.name, packagePath);
+//
+//   // 5.1) Update versions in package.json in all checked components
+//   // TODO - For now, they updates in old release process. Uncomment after it will be removed.
+//   // updateVersionInComponents(changelogs);
+//
+//   // 6) Close tasks in clickup
+//   if (!process.argv.includes('--dry-run') && version) {
+//     await closeTasks(version);
+//   }
+//
+//   // 7) Commit changes in package.json and components.json
+//   if (!process.argv.includes('--dry-run') && version) {
+//     await commitPatch(version);
+//   }
+//
+//   // 8) Release notes in slack channel
+//   // todo Brauer Ilia: uncomment after removing the previous release system
+//   // if (!process.argv.includes('--dry-run') && version) {
+//   //   await publishReleaseNotes(version, changelogs.slice(0, 1));
+//   // }
+// };
+
+export {
+  // todo Brauer Ilia: uncomment after removing the previous release system
+  publishRelease,
+  publishIntergalacticRelease,
+};
