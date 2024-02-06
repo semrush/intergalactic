02cf56d195bc5326fe89e130be3e8f3d4b24737e
ilia.brauer <ilia.brauer@semrush.com>
2024-02-06 14:53:21 +0100

    [flex-box] renamed folder

--- semcore/flex-box/src/index.tsx
+++ semcore/flex-box/src/index.tsx
@@ -7,4 +7,4 @@ export { default as Flex } from './Flex';
 export { default as useFlex } from './Flex/useFlex';
 export * from './Flex/useFlex';
 
-export { InvalidStateBox } from './Invalid-state-box/InvalidStateBox';
+export { InvalidStateBox } from './invalid-state-box/InvalidStateBox';
