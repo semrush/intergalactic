6a1e6a11ffd4812c86a103a8d63b4a1b813fcc9b
ilia.brauer <ilia.brauer@semrush.com>
2024-02-22 11:09:54 +0100

    [website] refactord connection to the amplitude (and fixed identify payload)

--- website/docs/.vitepress/theme/amplitude/amplitude.ts
+++ website/docs/.vitepress/theme/amplitude/amplitude.ts
@@ -3,14 +3,8 @@ import amplitudeHttp from './amplitude-client';
 
 export const initAmplitude = () => {
   const apiKey = '1e1d36fa96573d0839c6c3ccaffb7f62';
-  const systemInfo = Bowser.parse(window.navigator.userAgent);
-  const language = window.navigator.language;
 
-  amplitudeHttp.init(apiKey).sendUserProperties({
-    OS: `${systemInfo.os.name} ${systemInfo.os.versionName} v${systemInfo.os.version}`,
-    platform: `${systemInfo.platform.vendor} ${systemInfo.platform.type}`,
-    language,
-  });
+  amplitudeHttp.init(apiKey);
 };
 
 const logEvent = (eventType: string, eventProperties: Record<string, any> = {}) => {
