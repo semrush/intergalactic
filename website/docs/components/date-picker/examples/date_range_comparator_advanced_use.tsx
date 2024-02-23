f06485ce01d14cebc4db4247ef2073fd4f2830ea
Julia Mnizhek <j.mnizhek@semrush.com>
2024-02-23 15:17:43 +0100

    [website] update DatePicker documentation according to last changes

--- website/docs/components/date-picker/examples/date_range_comparator_advanced_use.tsx
+++ website/docs/components/date-picker/examples/date_range_comparator_advanced_use.tsx
@@ -7,7 +7,7 @@ import { Text } from '@semcore/ui/typography';
 
 const Demo = () => {
   return (
-    <Flex gap={4} flexWrap>
+    <Flex flexWrap>
       <DateRangeComparator>
         <DateRangeComparator.Trigger />
         <DateRangeComparator.Popper>
@@ -30,7 +30,7 @@ const Demo = () => {
             </DateRangeComparator.Periods>
           </DateRangeComparator.Body>
           <DateRangeComparator.Footer>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Place for a hint, useful message or controls.
           </DateRangeComparator.Footer>
           <Notice theme='warning' style={{ borderTopLeftRadius: 0, borderTopRightRadius: 0 }}>
             <Notice.Content>
@@ -46,44 +46,6 @@ const Demo = () => {
           </Notice>
         </DateRangeComparator.Popper>
       </DateRangeComparator>
-      <MonthDateRangeComparator>
-        <MonthDateRangeComparator.Trigger />
-        <MonthDateRangeComparator.Popper>
-          <MonthDateRangeComparator.Header>
-            <MonthDateRangeComparator.ValueDateRange />
-            <MonthDateRangeComparator.CompareToggle />
-            <MonthDateRangeComparator.CompareDateRange />
-          </MonthDateRangeComparator.Header>
-          <MonthDateRangeComparator.Body>
-            <MonthDateRangeComparator.RangeCalendar />
-            <MonthDateRangeComparator.Periods>
-              <MonthDateRangeComparator.Periods.Divider />
-              <MonthDateRangeComparator.Periods.Column>
-                <MonthDateRangeComparator.Periods.Options />
-                <MonthDateRangeComparator.Periods.Controls>
-                  <MonthDateRangeComparator.Apply />
-                  <MonthDateRangeComparator.Reset />
-                </MonthDateRangeComparator.Periods.Controls>
-              </MonthDateRangeComparator.Periods.Column>
-            </MonthDateRangeComparator.Periods>
-          </MonthDateRangeComparator.Body>
-          <MonthDateRangeComparator.Footer>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </MonthDateRangeComparator.Footer>
-          <Notice theme='warning' style={{ borderTopLeftRadius: 0, borderTopRightRadius: 0 }}>
-            <Notice.Content>
-              <Flex mb={1}>
-                <Notice.Label>
-                  <WarningIcon />
-                </Notice.Label>
-                <Text bold>Notice heading</Text>
-              </Flex>
-              If you change your location, all previously collected data for this article will be
-              lost.
-            </Notice.Content>
-          </Notice>
-        </MonthDateRangeComparator.Popper>
-      </MonthDateRangeComparator>
     </Flex>
   );
 };
