6f2b0b86091611a949850acec89a6be2ed1320d4
ilyabrower <ilia.brauer@semrush.com>
2024-02-26 15:54:53 +0100

    [website] refactord connection to the amplitude (and fixed identify payload) (#1120)

--- website/docs/.vitepress/theme/amplitude/amplitude.ts
+++ website/docs/.vitepress/theme/amplitude/amplitude.ts
@@ -7,14 +7,8 @@ const clickedPlaygrounds = new Set<string>();
 
 export const initAmplitude = () => {
   const apiKey = '1e1d36fa96573d0839c6c3ccaffb7f62';
-  const systemInfo = Bowser.parse(window.navigator.userAgent);
-  const language = window.navigator.language;
 
-  amplitudeHttp.init(apiKey).sendUserProperties({
-    OS: `${systemInfo.os.name} ${systemInfo.os.versionName} v${systemInfo.os.version}`,
-    platform: `${systemInfo.platform.vendor} ${systemInfo.platform.type}`,
-    language,
-  });
+  amplitudeHttp.init(apiKey);
 };
 
 export const logEvent = (eventType: string, eventProperties: Record<string, any> = {}) => {
