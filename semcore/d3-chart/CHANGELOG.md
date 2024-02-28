340ca270a4c09ff80f58ac6452649aca3c3b7f2a
ilia.brauer <ilia.brauer@semrush.com>
2024-02-28 11:16:38 +0100

    [d3-chart] fixed view of Line.Area with nulls data

--- semcore/d3-chart/CHANGELOG.md
+++ semcore/d3-chart/CHANGELOG.md
@@ -2,6 +2,12 @@
 
 CHANGELOG.md standards are inspired by [keepachangelog.com](https://keepachangelog.com/en/1.0.0/).
 
+## [3.30.2] - 2024-02-28
+
+### Fixed
+
+- view of Line.Area with `null` in data.
+
 ## [3.30.1-prerelease.1] - 2024-02-26
 
 ### Fixed
--- semcore/d3-chart/__tests__/index.test.tsx
+++ semcore/d3-chart/__tests__/index.test.tsx
@@ -2051,6 +2051,53 @@ describe('Line', () => {
 
     await expect(await snapshot(<Component />)).toMatchImageSnapshot(task);
   });
+
+  test.concurrent('should render line with area if line has null values', async ({ task }) => {
+    const data = Array(20)
+      .fill({})
+      .map((d, i) => {
+        const y = i > 8 && i < 16 ? Math.abs(Math.sin(Math.exp(i))) * 10 : null;
+
+        return {
+          x: i,
+          y,
+          y0: y ? y - 2 : null,
+          y1: y ? y + 2 : null,
+        };
+      });
+
+    const Component: React.FC = () => {
+      const MARGIN = 40;
+      const width = 500;
+      const height = 300;
+
+      const xScale = scaleLinear()
+        .range([MARGIN, width - MARGIN])
+        .domain(minMax(data, 'x'));
+
+      const yScale = scaleLinear()
+        .range([height - MARGIN, MARGIN])
+        .domain([0, 10]);
+
+      return (
+        <Plot data={data} scale={[xScale, yScale]} width={width} height={height}>
+          <YAxis>
+            <YAxis.Ticks />
+            <YAxis.Grid />
+          </YAxis>
+          <XAxis>
+            <XAxis.Ticks />
+          </XAxis>
+          <Line x='x' y='y' duration={0}>
+            <Line.Area y0='y0' y1='y1' />
+            <Line.Null />
+          </Line>
+        </Plot>
+      );
+    };
+
+    await expect(await snapshot(<Component />)).toMatchImageSnapshot(task);
+  });
 });
 
 describe('ChartLegend', () => {
