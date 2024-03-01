b4451e9f04fa10e8b76f78c003dc39660bdabd89
ilia.brauer <ilia.brauer@semrush.com>
2024-03-01 11:36:43 +0100

    [global] fixed pull/push

--- tools/continuous-delivery/src/utils/gitUtils.ts
+++ tools/continuous-delivery/src/utils/gitUtils.ts
@@ -20,62 +20,6 @@ export class GitUtils {
     }
   }
 
-  public static async commitNewPrerelease(versionPatches: VersionPatch[]) {
-    let commitMessage = '[chore] bumped';
-    if (versionPatches.length === 1) {
-      commitMessage += ' version of ';
-    } else {
-      commitMessage += ' versions of ';
-    }
-    commitMessage += versionPatches.map((patch) => `${patch.package.name}@${patch.to}`).join(', ');
-
-    const status = await git.status();
-    if (status.files.length) {
-      log('Committing changes...');
-      if (!process.argv.includes('--dry-run')) {
-        await git.add('.');
-        await GitUtils.commitOrAmend(commitMessage);
-      }
-      log('Changes committed.');
-    }
-  }
-
-  public static async createPrereleaseTag(patch: VersionPatch) {
-    const prerelease = await GitUtils.getPrerelease();
-
-    let tag = `v${patch.to}-${prerelaseSuffix}.0`;
-
-    if (prerelease !== null) {
-      const currentItem = Number(prerelease.split('.')[1]);
-
-      if (!Number.isNaN(currentItem)) {
-        tag = `v${patch.to}-${prerelaseSuffix}.${currentItem + 1}`;
-      }
-    }
-
-    await git.tag(['-f', tag]);
-
-    return tag;
-  }
-
-  public static async push(branch: string, tag?: string) {
-    if (!process.argv.includes('--dry-run')) {
-      log('Rebasing on git origin...');
-      try {
-        await git.pull('origin', branch, { '--rebase': 'true' });
-      } catch (err) {
-        // biome-ignore lint/suspicious/noConsoleLog:
-        console.log(await git.status());
-        throw err;
-      }
-      log('Rebased on git origin.');
-      log('Pushing to git origin...');
-      await git.push('origin', tag);
-      await git.push('origin', branch, { '--follow-tags': null });
-      log('Pushed to git origin.');
-    }
-  }
-
   public static async getUpdatedPackages() {
     const diff = await git.diffSummary('HEAD^1');
     const components: string[] = [];
@@ -109,6 +53,65 @@ export class GitUtils {
     } else {
       await git.commit(message, []);
       GitUtils.alreadyHasCommit = true;
+    }
+  }
+
+  private static async commitNewPrerelease(versionPatches: VersionPatch[]) {
+    let commitMessage = '[chore] bumped';
+    if (versionPatches.length === 1) {
+      commitMessage += ' version of ';
+    } else {
+      commitMessage += ' versions of ';
+    }
+    commitMessage += versionPatches.map((patch) => `${patch.package.name}@${patch.to}`).join(', ');
+
+    const status = await git.status();
+    if (status.files.length) {
+      log('Committing changes...');
+      if (!process.argv.includes('--dry-run')) {
+        await git.add('.');
+        await GitUtils.commitOrAmend(commitMessage);
+      }
+      log('Changes committed.');
+    }
+  }
+
+  private static async createPrereleaseTag(patch: VersionPatch) {
+    const prerelease = await GitUtils.getPrerelease();
+
+    let tag = `v${patch.to}-${prerelaseSuffix}.0`;
+
+    if (prerelease !== null) {
+      const currentItem = Number(prerelease.split('.')[1]);
+
+      if (!Number.isNaN(currentItem)) {
+        tag = `v${patch.to}-${prerelaseSuffix}.${currentItem + 1}`;
+      }
+    }
+
+    await git.tag(['-f', tag]);
+
+    return tag;
+  }
+
+  private static async pull(branch: string) {
+    log('Rebasing on git origin...');
+    try {
+      await git.pull('origin', branch, { '--rebase': 'true' });
+    } catch (err) {
+      // biome-ignore lint/suspicious/noConsoleLog:
+      console.log(await git.status());
+      throw err;
+    }
+    log('Rebased on git origin.');
+  }
+
+  private static async push(branch: string, tag?: string) {
+    if (!process.argv.includes('--dry-run')) {
+      log('Pushing to git origin...');
+      await git.push('origin', tag);
+      await git.push('origin', branch, { '--follow-tags': null });
+      log('Pushed to git origin.');
     }
   }
 }
