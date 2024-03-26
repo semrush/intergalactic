a7f07f750be26ef4cf2700fe5d3dd38206a22303
Michael Sereniti <31261408+msereniti@users.noreply.github.com>
2024-03-26 15:04:22 +0100

    [86bxxhdzg][utils, popper] made focus lock to work in popper with disabled portal (#1218)
    ## Motivation and Context
    
    If developer disables popper portal, popper prevents blur event to
    bubble of it. It's critical for work of focus lock, so I've added
    synthetic events in focus lock and made popper to forward it when it
    prevents specific event from bubbling.
    
    ## How has this been tested?
    
    I have added focus lock browser tests and covered disabledPortal caase.
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [x] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [x] I have added new unit tests on added of fixed functionality.

---
+++ semcore/popper/__tests__/stands/dropdown.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// @ts-ignore
+import DropdownMenu from 'intergalactic/dropdown-menu';
+// @ts-ignore
+import { ButtonTrigger } from 'intergalactic/base-trigger';
+
+const Demo = () => {
+  return (
+    <div>
+      <DropdownMenu visible>
+        <DropdownMenu.Trigger tag={ButtonTrigger}>Enabled portal</DropdownMenu.Trigger>
+        <DropdownMenu.Popper p={5} data-testid='popper'>
+          <input data-testid='input-in-popper' />
+        </DropdownMenu.Popper>
+      </DropdownMenu>
+      <input />
+    </div>
+  );
+};
+
+export default Demo;
