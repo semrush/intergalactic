a38e694bdef6dc8be8043b4605b3a4d2c4051a84
Michael Sereniti <31261408+msereniti@users.noreply.github.com>
2024-03-27 17:41:39 +0100

    [website] added basic blog functionality (#973)
    Also need to write first posts :)
    
    ---------
    
    Co-authored-by: Julia Mnizhek <j.mnizhek@semrush.com>

---
+++ website/docs/.vitepress/theme/blog/BlogPosts.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import usePosts from './useBlogPosts'
+import VPFeatures from 'vitepress/dist/client/theme-default/components/VPFeatures.vue'
+
+const { allPosts: posts } = usePosts()
+
+</script>
+
+<template>
+  <VPFeatures
+    :features="posts.map(post => ({
+      icon: post.image ? { src: `/blog/${post.image}`, width: 400 } : undefined,
+      title: post.title,
+      details: post.excerpt + '<br /><br />' + post.date.string,
+      link: `/blog/${post.slug}/`,
+    }))"
+  />
+</template>
---
+++ website/docs/blog/index.md
@@ -0,0 +1,5 @@
+---
+title: Intergalactic blog
+---
+
+<BlogPosts />
