7f6477683aff924d2b631dcef12c997e7b937639
Michael Sereniti <31261408+msereniti@users.noreply.github.com>
2024-04-08 16:56:06 +0200

    [86bxyrm4u][d3-chart] a11y module values and titles formatting (#1252)
    ## Motivation and Context
    
    This PR enhances usages of a11y module by received feedback. See added
    example to see what api was added.
    
    ## How has this been tested?
    
    Manually by added example.
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [x] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [x] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

---
+++ semcore/d3-chart/src/a11y/DataAccessibilityTable.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { DataStructureHints, DataSummarizationConfig } from './hints';
+import { Intl } from './intl';
+import { defaultValueFormatter } from './serialize';
+
+export const DataAccessibilityTable: React.FC<{
+  id: string;
+  dataTableRef: React.RefObject<HTMLTableElement>;
+  data: Record<string | number, unknown>[];
+  hints: DataStructureHints;
+  config: DataSummarizationConfig;
+  intl: Intl;
+}> = ({ id, dataTableRef, data, hints, config, intl }) => {
+  const keys = React.useMemo(() => {
+    let fromHints: (string | number)[] = [];
+    fromHints.push(...hints.fields.verticalAxes);
+    fromHints.push(...hints.fields.horizontalAxes);
+    fromHints.push(...hints.fields.valueAxes);
+    fromHints.push(...config.additionalFields);
+    fromHints.push(...Object.keys(hints.fields.values));
+
+    fromHints = [...new Set(fromHints)].filter((key) => key in data[0]);
+
+    if (fromHints.length === 0) {
+      data.forEach((row) => fromHints.push(...Object.keys(row)));
+    }
+
+    return [...new Set(fromHints)];
+  }, [data, hints]);
+  const duplicatedBaseKeys = React.useMemo(() => {
+    const baseKeys = keys
+      .filter((key) => !hints.titles.valuesAxes[key])
+      .map((key) => {
+        if (hints.fields.verticalAxes.has(key) && hints.axesTitle.vertical)
+          return [key, hints.axesTitle.vertical];
+        if (hints.fields.horizontalAxes.has(key) && hints.axesTitle.horizontal)
+          return [key, hints.axesTitle.horizontal];
+      })
+      .filter((entry) => entry !== undefined)
+      .map((entry) => entry!);
+    const doublicated: Record<string, true> = {};
+    const handled: Record<string, string | number> = {};
+    for (const [key, label] of baseKeys) {
+      if (handled[label]) {
+        doublicated[key] = true;
+        doublicated[handled[label]] = true;
+      }
+      handled[label] = key;
+    }
+    return doublicated;
+  }, [keys, hints]);
+  const renderTitle = React.useCallback(
+    (dataKey: string | number) => {
+      const formatted = config.titlesFormatter?.(dataKey);
+      if (formatted !== undefined) return formatted;
+      if (hints.titles.valuesAxes[dataKey]) return hints.titles.valuesAxes[dataKey];
+      if (!duplicatedBaseKeys[dataKey]) {
+        if (hints.fields.verticalAxes.has(dataKey) && hints.axesTitle.vertical)
+          return hints.axesTitle.vertical;
+        if (hints.fields.horizontalAxes.has(dataKey) && hints.axesTitle.horizontal)
+          return hints.axesTitle.horizontal;
+      }
+
+      return dataKey;
+    },
+    [data, hints, duplicatedBaseKeys, config],
+  );
+  const formatValue = React.useCallback(
+    (value: unknown, row: string) => {
+      return config.valuesFormatter?.(value, row) ?? defaultValueFormatter(intl, value);
+    },
+    [intl, config],
+  );
+
+  return (
+    <table id={`${id}-data-table`} tabIndex={0} ref={dataTableRef}>
+      <thead>
+        <tr>
+          {keys.map((key, index) => (
+            <th key={`${key}-${index}`}>{renderTitle(key)}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => {
+          const key = `${Object.values(row)
+            .filter((item) => typeof item !== 'symbol')
+            .join('-')}-${index}`;
+          const cells = keys.map((key) =>
+            typeof row[key] !== 'symbol' ? [key, row[key]] : [key, ''],
+          );
+
+          return (
+            <tr key={key}>
+              {cells.map(([key, value], index) => (
+                <td key={`${key}-${index}`}>{formatValue(value, keys[index] as string)}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
--- semcore/d3-chart/src/a11y/PlotA11yView.tsx
+++ semcore/d3-chart/src/a11y/PlotA11yView.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { heavyFindNextFocusableElement } from './focus';
 import { makeDataSummarizationConfig } from './hints';
 import type { A11yViewProps } from './PlotA11yModule';
-import { formatValue } from './serialize';
 import { getIntl } from './intl';
 import { summarize } from './summarize';
 import { Root, sstyled } from '@semcore/core';
@@ -10,6 +9,7 @@ import styles from '../style/plotA11yView.shadow.css';
 import { Box } from '@semcore/flex-box';
 import { useAsyncI18nMessages } from '@semcore/utils/lib/enhances/i18nEnhance';
 import { localizedMessages } from './translations/view/__intergalactic-dynamic-locales';
+import { DataAccessibilityTable } from './DataAccessibilityTable';
 
 export const PlotA11yView: React.FC<A11yViewProps> = ({
   id,
@@ -30,58 +30,6 @@ export const PlotA11yView: React.FC<A11yViewProps> = ({
   const data = React.useMemo(
     () => (Array.isArray(providedData) ? providedData : [providedData]),
     [providedData],
-  );
-  const keys = React.useMemo(() => {
-    let fromHints: (string | number)[] = [];
-    fromHints.push(...hints.fields.verticalAxes);
-    fromHints.push(...hints.fields.horizontalAxes);
-    fromHints.push(...hints.fields.valueAxes);
-    fromHints.push(...config.additionalFields);
-    fromHints.push(...Object.keys(hints.fields.values));
-
-    fromHints = [...new Set(fromHints)].filter((key) => key in data[0]);
-
-    if (fromHints.length === 0) {
-      data.forEach((row) => fromHints.push(...Object.keys(row)));
-    }
-
-    return [...new Set(fromHints)];
-  }, [data, hints]);
-  const duplicatedBaseKeys = React.useMemo(() => {
-    const baseKeys = keys
-      .filter((key) => !hints.titles.valuesAxes[key])
-      .map((key) => {
-        if (hints.fields.verticalAxes.has(key) && hints.axesTitle.vertical)
-          return [key, hints.axesTitle.vertical];
-        if (hints.fields.horizontalAxes.has(key) && hints.axesTitle.horizontal)
-          return [key, hints.axesTitle.horizontal];
-      })
-      .filter((entry) => entry !== undefined)
-      .map((entry) => entry!);
-    const doublicated: Record<string, true> = {};
-    const handled: Record<string, string | number> = {};
-    for (const [key, label] of baseKeys) {
-      if (handled[label]) {
-        doublicated[key] = true;
-        doublicated[handled[label]] = true;
-      }
-      handled[label] = key;
-    }
-    return doublicated;
-  }, [keys, hints]);
-  const renderTitle = React.useCallback(
-    (dataKey: string | number) => {
-      if (hints.titles.valuesAxes[dataKey]) return hints.titles.valuesAxes[dataKey];
-      if (!duplicatedBaseKeys[dataKey]) {
-        if (hints.fields.verticalAxes.has(dataKey) && hints.axesTitle.vertical)
-          return hints.axesTitle.vertical;
-        if (hints.fields.horizontalAxes.has(dataKey) && hints.axesTitle.horizontal)
-          return hints.axesTitle.horizontal;
-      }
-
-      return dataKey;
-    },
-    [data, hints, duplicatedBaseKeys],
   );
 
   const [summary, setSummary] = React.useState<string | null>(null);
@@ -172,30 +120,14 @@ export const PlotA11yView: React.FC<A11yViewProps> = ({
       <strong>
         <label htmlFor={`${id}-data-table`}>{texts.table}</label>
       </strong>
-      <table id={`${id}-data-table`} tabIndex={0} ref={dataTableRef}>
-        <thead>
-          <tr>
-            {keys.map((key, index) => (
-              <th key={`${key}-${index}`}>{renderTitle(key)}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr
-              key={`${Object.values(row)
-                .filter((item) => typeof item !== 'symbol')
-                .join('-')}-${index}`}
-            >
-              {keys
-                .map((key) => (typeof row[key] !== 'symbol' ? [key, row[key]] : [key, '']))
-                .map(([key, value], index) => (
-                  <td key={`${key}-${index}`}>{formatValue(intl, value)}</td>
-                ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataAccessibilityTable
+        id={id}
+        dataTableRef={dataTableRef}
+        data={data}
+        hints={hints}
+        config={config}
+        intl={intl}
+      />
     </SPlotA11yView>,
   ) as React.ReactElement;
 };
