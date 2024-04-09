6363c15f3766531c7d40fac32322676e37b68649
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2024-04-09 18:37:23 +0200

    [ellipsis] fixed middle ellipsis use with links

--- tools/testing-utils/snapshot.tsx
+++ tools/testing-utils/snapshot.tsx
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { act } from './testing-library';
+import { promises as fs } from 'fs';
 
 import playwright from 'playwright';
 
@@ -90,6 +91,8 @@ export const snapshot = async (
             </div>
         </body>
     </html>`;
+
+  await fs.writeFile('index.html', html, 'utf8');
 
   await page.setContent(html);
   const mainElement = await page.$(options.selector! || 'body');
