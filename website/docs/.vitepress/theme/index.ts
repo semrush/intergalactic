91096b7f251f06bde3934a0453ed0e316db93549
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2024-04-17 12:11:12 +0200

    [chore] fixed linting

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -37,14 +37,14 @@ export default {
   setup() {
     const route = useRoute();
     const initZoom = () => {
-      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); 
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
     };
     onMounted(() => {
       initZoom();
     });
     watch(
       () => route.path,
-      () => nextTick(() => initZoom())
+      () => nextTick(() => initZoom()),
     );
   },
 };
