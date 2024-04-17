17abb1dc2578c075089a55b8f22867f78c1095d4
Michael Sereniti <31261408+msereniti@users.noreply.github.com>
2024-04-17 16:39:16 +0200

    [ci] enabled vo-tests on macos-13 (#941)
    VO tests were working unstably because some old runners on macos-12
    wasn't patched properly to run a11y tests. I hope that it will work much
    better on macos-13

--- semcore/data-table/__tests__/data-table-virtual-scroll.vo-test.ts
+++ semcore/data-table/__tests__/data-table-virtual-scroll.vo-test.ts
@@ -1,12 +1,13 @@
 import { expect } from '@semcore/testing-utils/playwright';
-import { voTest as test } from '@guidepup/playwright';
+import { voiceOverTest as test } from '@guidepup/playwright';
+
 import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 
 test('Users can interact with DataTable virtual scroll via VoiceOver', async ({
   page,
   voiceOver,
 }) => {
-  const standPath = 'website/docs/table-group/data-table/examples/virtual-scroll.tsx';
+  const standPath = 'website/docs/table-group/data-table/examples/virtual-scroll-in-table.tsx';
   const htmlContent = await e2eStandToHtml(standPath, 'en');
 
   await page.setContent(htmlContent);
@@ -17,5 +18,7 @@ test('Users can interact with DataTable virtual scroll via VoiceOver', async ({
   for (let i = 0; i < 5; i++) {
     await voiceOver.press('Control+Option+ArrowDown');
   }
-  expect((await voiceOver.lastSpokenPhrase()).replace(/[,.\s]/g, ' ')).toBe('row 6 of 10 000 #5');
+  expect((await voiceOver.lastSpokenPhrase()).replace(/[,.\s]/g, ' ')).toContain(
+    'row 6 of 10 000 #5',
+  );
 });
--- semcore/progress-bar/__tests__/progress-bar.vo-test.ts
+++ semcore/progress-bar/__tests__/progress-bar.vo-test.ts
@@ -1,9 +1,10 @@
 import { expect } from '@semcore/testing-utils/playwright';
-import { voTest as test } from '@guidepup/playwright';
+import { voiceOverTest as test } from '@guidepup/playwright';
+
 import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 
 test('Users can interact with ProgressBar via VoiceOver', async ({ page, voiceOver }) => {
-  const standPath = 'website/docs/components/progress-bar/examples/progress-bar.tsx';
+  const standPath = 'website/docs/components/progress-bar/examples/complex_usage_example.tsx';
   const htmlContent = await e2eStandToHtml(standPath, 'en');
 
   await page.setContent(htmlContent);
