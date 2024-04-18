62477483948074dbe9bc08911a1fb438aefa5350
Julia Mnizhek <j.mnizhek@semrush.com>
2024-04-18 12:33:25 +0200

    [website] update Slider example

--- website/docs/components/slider/examples/numeric_slider.tsx
+++ website/docs/components/slider/examples/numeric_slider.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'intergalactic/slider';
 import InputNumber from 'intergalactic/input-number';
 import Tooltip from 'intergalactic/tooltip';
-import { Box } from 'intergalactic/flex-box';
+import { Flex } from 'intergalactic/flex-box';
 
 const Demo = () => {
   const [value, setValue] = React.useState(51);
@@ -22,24 +22,24 @@ const Demo = () => {
 
   return (
     <>
-      <Box w={140}>
-        <Slider mb={3} value={value} onChange={setValue} step={1} min={min} max={max}>
+      <Flex direction="row" gap={4}>
+        <Slider mb={4} value={value} onChange={setValue} step={1} min={min} max={max}>
           <Slider.Bar />
           <Slider.Knob />
         </Slider>
-      </Box>
-      <Tooltip
-        title={`Please enter a valid value within ${min} and ${max}.`}
-        visible={!!error}
-        interaction='click'
-        theme='warning'
-        placement='right'
-      >
-        <InputNumber mt={4} w={140} size='m' state={error ? 'invalid' : 'normal'}>
-          <InputNumber.Value step={1} value={value.toString()} onChange={handleInput} />
-          <InputNumber.Controls showControls />
-        </InputNumber>
-      </Tooltip>
+        <Tooltip
+          title={`Please enter a valid value within ${min} and ${max}.`}
+          visible={!!error}
+          interaction='click'
+          theme='warning'
+          placement='right'
+        >
+          <InputNumber w={80} size='m' state={error ? 'invalid' : 'normal'}>
+            <InputNumber.Value step={1} value={value.toString()} onChange={handleInput} />
+            <InputNumber.Controls showControls />
+          </InputNumber>
+        </Tooltip>
+      </Flex>
     </>
   );
 };
