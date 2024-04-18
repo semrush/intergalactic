71a2538e71d0845c126e540815cd438c2c6d8880
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2024-04-18 10:57:08 +0200

    [accordion] fixed that Pressing `Space` on focused `Accordion.Item.Toggle` was causing the page to scroll

--- semcore/accordion/src/Accordion.jsx
+++ semcore/accordion/src/Accordion.jsx
@@ -115,12 +115,22 @@ class Toggle extends Component {
 
   toggleRef = React.createRef();
 
-  handleKeyDown = (e) => {
-    switch (e.key) {
+  handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      if (this.toggleRef.current === event.target) {
+        event.currentTarget.click();
+      }
+    } else if (event.key === ' ') {
+      event.preventDefault();
+      if (this.toggleRef.current === event.target) {
+        event.currentTarget.click();
+      }
+    }
+    switch (event.key) {
       case 'Enter':
       case ' ': {
-        if (this.toggleRef.current === e.target) {
-          e.currentTarget.click();
+        if (this.toggleRef.current === event.target) {
+          event.currentTarget.click();
         }
       }
     }
