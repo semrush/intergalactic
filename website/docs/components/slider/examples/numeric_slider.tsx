bed6cac8f2e18c5755713201083007ba3a0a696c
Julia Mnizhek <j.mnizhek@semrush.com>
2024-04-18 12:33:25 +0200

    [website] update Slider example

--- website/docs/components/slider/examples/numeric_slider.tsx
+++ website/docs/components/slider/examples/numeric_slider.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'intergalactic/slider';
 import InputNumber from 'intergalactic/input-number';
 import Tooltip from 'intergalactic/tooltip';
-import { Box } from 'intergalactic/flex-box';
+import { Box, Flex } from 'intergalactic/flex-box';
 import { Text } from 'intergalactic/typography';
 
 const Demo = () => {
@@ -22,7 +22,7 @@ const Demo = () => {
   };
 
   return (
-    <>
+    <Flex direction='column'>
       <Text tag='label' size={200} htmlFor='slider-represantation'>
         Slider representation
       </Text>
@@ -44,26 +44,24 @@ const Demo = () => {
       <Text tag='label' size={200} htmlFor='numeric-value-represantation'>
         Numeric value representation
       </Text>
-      <Box mt={2}>
-        <Tooltip
-          title={`Please enter a valid value within ${min} and ${max}.`}
-          visible={!!error}
-          interaction='click'
-          theme='warning'
-          placement='right'
-        >
-          <InputNumber mt={2} w={140} size='m' state={error ? 'invalid' : 'normal'}>
-            <InputNumber.Value
-              id='numeric-value-represantation'
-              step={1}
-              value={value.toString()}
-              onChange={handleInput}
-            />
-            <InputNumber.Controls showControls />
-          </InputNumber>
-        </Tooltip>
-      </Box>
-    </>
+      <Tooltip
+        title={`Please enter a valid value within ${min} and ${max}.`}
+        visible={!!error}
+        interaction='click'
+        theme='warning'
+        placement='right'
+      >
+        <InputNumber mt={2} w={80} size='m' state={error ? 'invalid' : 'normal'}>
+          <InputNumber.Value
+            id='numeric-value-represantation'
+            step={1}
+            value={value.toString()}
+            onChange={handleInput}
+          />
+          <InputNumber.Controls showControls />
+        </InputNumber>
+      </Tooltip>
+    </Flex>
   );
 };
 
