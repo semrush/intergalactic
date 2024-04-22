33d2b05681ba9d327ad0e7ddf06f026e569f73f8
ilia.brauer <ilia.brauer@semrush.com>
2024-04-22 16:12:18 +0200

    [global] separate publishing release notes to github and message to slack

---
+++ tools/continuous-delivery/src/sendMessageAboutRelease.ts
@@ -0,0 +1,23 @@
+import { log } from './utils';
+import { makeMessageFromChangelogs, sendMessage } from '@semcore/slack-integration';
+import { Changelog } from '@semcore/changelog-handler';
+
+export async function sendMessageAboutRelease(version: string, lastVersionChangelogs: Changelog[]) {
+  log('Sending message to internal Slack...');
+
+  try {
+    const title = `New release v${version} is here!`;
+    const body = makeMessageFromChangelogs(lastVersionChangelogs, false);
+
+    await sendMessage({
+      title,
+      body,
+      dryRun: false,
+    });
+    log('Sent message to internal Slack.');
+    log('Release note is published.');
+  } catch (error) {
+    // biome-ignore lint/suspicious/noConsoleLog:
+    console.error(error);
+  }
+}
