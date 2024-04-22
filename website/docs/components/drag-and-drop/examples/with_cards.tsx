f8d0516344400ef39ddcf74a3f1cc9364ababbf1
Julia Mnizhek <j.mnizhek@semrush.com>
2024-04-22 16:35:33 +0200

    [website] update Cards with DnD example

--- website/docs/components/drag-and-drop/examples/with_cards.tsx
+++ website/docs/components/drag-and-drop/examples/with_cards.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import DnD from 'intergalactic/drag-and-drop';
 import Card from 'intergalactic/card';
 import { Flex } from 'intergalactic/flex-box';
-import Pills from 'intergalactic/pills';
-import TabLine from 'intergalactic/tab-line';
 import { Chart } from 'intergalactic/d3-chart';
 import MathPlusL from 'intergalactic/icon/MathPlus/l';
 import { Text } from 'intergalactic/typography';
@@ -19,38 +17,27 @@ const Widget: React.FC<{ title: string }> = ({ title }) => {
   const data = React.useMemo(() => {
     const random = stableRandom(title.length);
     const dateFormatter = new Intl.DateTimeFormat('en', { month: 'numeric', day: 'numeric' });
-    return Array(4)
+    return Array(2)
       .fill(0)
       .map((_, i) => ({
         date: dateFormatter.format(new Date(Date.now() - 1000 * 60 * 60 * 24 * 3 * i)),
-        value: Math.round(random() * 30),
+        value: Math.round(random() * 10),
       }));
   }, [title]);
 
   return (
-    <Card w={235}>
+    <Card w={240}>
       <Card.Header>
         <Card.Title>{title}</Card.Title>
       </Card.Header>
       <Card.Body>
-        <Flex justifyContent='space-between'>
-          <Pills defaultValue='a'>
-            <Pills.Item value={'a'}>A</Pills.Item>
-            <Pills.Item value={'b'}>B</Pills.Item>
-            <Pills.Item value={'c'}>C</Pills.Item>
-          </Pills>
-          <TabLine defaultValue='1m' w='auto'>
-            <TabLine.Item value='1m'>1M</TabLine.Item>
-            <TabLine.Item value='3m'>3M</TabLine.Item>
-          </TabLine>
-        </Flex>
         <Chart.Bar
           duration={0}
           mt={5}
           groupKey={'date'}
           data={data}
           plotWidth={200}
-          plotHeight={150}
+          plotHeight={200}
         />
       </Card.Body>
     </Card>
@@ -66,28 +53,8 @@ const widgetsSetup = [
     title: 'Backlinks',
     id: 'backlinks',
   },
-  {
-    title: 'Keyword Research',
-    id: 'keyword-research',
-  },
-  {
-    title: 'Organic traffic',
-    id: 'organic-traffic',
-  },
-  {
-    title: 'Paid traffic',
-    id: 'paid-traffic',
-  },
 ];
-const defaultWidgets = [
-  'market-traffic',
-  null,
-  'backlinks',
-  null,
-  'keyword-research',
-  'organic-traffic',
-  'paid-traffic',
-];
+const defaultWidgets = [null, 'market-traffic', 'backlinks', null];
 
 const Demo = () => {
   const [widgets, setWidgets] = React.useState(defaultWidgets);
@@ -111,15 +78,23 @@ const Demo = () => {
         if (!id) {
           return (
             <DnD.DropZone key={index}>
-              <Flex alignItems='center' justifyContent='center' w={235} h={278} direction='column'>
+              <Flex
+                alignItems='center'
+                gap={1}
+                justifyContent='center'
+                w={240}
+                h={278}
+                direction='column'
+                p={5}
+              >
                 <Text color='text-secondary'>
                   <MathPlusL />
                 </Text>
                 <Text color='text-secondary' bold size={200}>
-                  Put widget
+                  Place widget here
                 </Text>
-                <Text color='text-secondary' size={200}>
-                  You can move widget here
+                <Text color='text-secondary' textAlign='center' size={200}>
+                  Change the order of the widgets!
                 </Text>
               </Flex>
             </DnD.DropZone>
