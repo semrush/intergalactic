25d63cb2e83405838b1e72adf9eb5d7ad1ca990e
Mikhail Sereniti <mikhail.sereniti@semrush.com>
2024-05-22 17:23:57 +0200

    [chore] fixed feedback form tests

--- semcore/feedback-form/__tests__/five-star-form.browser-test.tsx
+++ semcore/feedback-form/__tests__/five-star-form.browser-test.tsx
@@ -1,9 +1,8 @@
 import { expect, test } from '@semcore/testing-utils/playwright';
 import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
-import { waitFor } from '@testing-library/react';
 
 test.describe('5-star Feedback form', () => {
-  test('simple use case', async ({ page }) => {
+  test('simple use case', async ({ page, browserName }) => {
     const standPath = 'website/docs/patterns/feedback-rating/examples/feedback-rating-example.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
 
@@ -20,6 +19,10 @@ test.describe('5-star Feedback form', () => {
     await expect(page).toHaveScreenshot();
 
     await stars[1].click();
+
+    const textarea = await page.locator('textarea');
+
+    await expect(textarea).toBeFocused();
 
     await expect(page).toHaveScreenshot();
 
