08ce52ccd270142790d9fc159d55016add52f832
Julia Mnizhek <j.mnizhek@semrush.com>
2024-05-24 14:45:46 +0200

    [website] update TabLine documentation and examples

---
+++ website/docs/components/tab-line/examples/tab_line_item_addons.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import TabLine from 'intergalactic/tab-line';
+import Tooltip from 'intergalactic/tooltip';
+import FacebookM from 'intergalactic/icon/Facebook/m';
+import InstagramM from 'intergalactic/icon/Instagram/m';
+import TwitterM from 'intergalactic/icon/Twitter/m';
+import LinkedInM from 'intergalactic/icon/LinkedIn/m';
+import Badge from 'intergalactic/badge';
+
+const Demo = () => {
+  const [value, onChange] = React.useState(1);
+  return (
+    <>
+      <TabLine value={value} onChange={onChange} aria-label='Social network reports'>
+        <TabLine.Item value={1} aria-controls='tab-panel-1'>
+          <TabLine.Item.Addon>
+            <FacebookM />
+          </TabLine.Item.Addon>
+          <TabLine.Item.Text>Facebook</TabLine.Item.Text>
+        </TabLine.Item>
+        <TabLine.Item value={2} aria-controls='tab-panel-2'>
+          <TabLine.Item.Addon>
+            <InstagramM />
+          </TabLine.Item.Addon>
+          <TabLine.Item.Text>Instagram</TabLine.Item.Text>
+          <TabLine.Item.Addon>
+            <Badge bg='green-400'>new</Badge>
+          </TabLine.Item.Addon>
+        </TabLine.Item>
+        <TabLine.Item value={3} aria-controls='tab-panel-3'>
+          <TabLine.Item.Addon>
+            <TwitterM />
+          </TabLine.Item.Addon>
+          <TabLine.Item.Text>Twitter</TabLine.Item.Text>
+        </TabLine.Item>
+        <Tooltip
+          title='You can disable the tab to indicate that data is either being collected or unavailable.'
+          placement='top'
+          role='tab'
+        >
+          <TabLine.Item value={4} disabled>
+            <TabLine.Item.Addon>
+              <LinkedInM />
+            </TabLine.Item.Addon>
+            <TabLine.Item.Text>LinkedIn</TabLine.Item.Text>
+          </TabLine.Item>
+        </Tooltip>
+      </TabLine>
+      {
+        [
+          <div id='tab-panel-1' role='tabpanel' aria-labelledby='tab-label-1' tabIndex={-1}>
+            <h3>Facebook</h3>
+            <p>
+              It's where your aunt's cat has more followers than you, and your high school nemesis
+              still thinks they're relevant.
+            </p>
+          </div>,
+          <div
+            id='tab-panel-2'
+            aria-hidden='true'
+            role='tabpanel'
+            aria-labelledby='tab-label-2'
+            tabIndex={-1}
+          >
+            <h3>Instagram</h3>
+            <p>
+              It's where people spend hours perfecting their avocado toast just for a 'like', and
+              your explore page thinks you need therapy based on your search history.
+            </p>
+          </div>,
+          <div
+            id='tab-panel-3'
+            aria-hidden='true'
+            role='tabpanel'
+            aria-labelledby='tab-label-3'
+            tabIndex={-1}
+          >
+            <h3>Twitter</h3>
+            <p>
+              It's where 280 characters can start a revolution or a feud over pineapple on pizza,
+              and your timeline is a mix of breaking news and cat memes.
+            </p>
+          </div>,
+        ][value - 1]
+      }
+    </>
+  );
+};
+
+export default Demo;
--- website/docs/components/tab-line/tab-line-code.md
+++ website/docs/components/tab-line/tab-line-code.md
@@ -4,9 +4,27 @@ fileSource: tab-line
 tabs: Design('tab-line'), A11y('tab-line-a11y'), API('tab-line-api'), Example('tab-line-code'), Changelog('tab-line-changelog')
 ---
 
+Try resizing the page to see how the tabs adjust. If the text in a tab is too long, it will be truncated with an `ellipsis`. You can also place the `TabLine.Item` within other components.
+
+::: info
+Make sure to provide a tooltip with full text for tabs with text truncated with an `ellipsis`.
+:::
+
+## TabLine item with addons
+
+You can add icons, badges, counters as addons to the left or right of the text in the item.
+
+::: sandbox
+
+<script lang="tsx">
+  export Demo from './examples/tab_line_item_addons.tsx';
+</script>
+
+:::
+
 ## Automatic tab activation
 
-Try changing the page size to observe how the tabs are compressed. If the text inside a tab is too long, it will be truncated with an `ellipsis`. You can also place the `<TabLine.Item />` within other components.
+By default, tabs are switched automatically when selected. You can also place the `<TabLine.Item />` within other components.
 
 ::: sandbox
 
@@ -18,6 +36,8 @@ Try changing the page size to observe how the tabs are compressed. If the text i
 
 ## Manual tab activation
 
+You can set `behavior='manual'` on TabLine to change the tab activation method to manual.
+
 ::: sandbox
 
 <script lang="tsx">
@@ -27,9 +47,9 @@ Try changing the page size to observe how the tabs are compressed. If the text i
 :::
 
 
-## Custom indents and occupying the entire space
+## Custom indents
 
-As you might have noticed, the TabLine component doesn't have default margins at the edges. To make the TabLine span the entire width of its parent block (which might have its own margins), you can set the desired `padding` and `margin` for the TabLine component.
+Since the TabLine component doesn't have default margins at the edges, to make it fill the entire width of its parent block (which may have its own margins), you can set the desired `padding` and `margin` for the TabLine component like this:
 
 ```typescript
 <Box p={5}>
