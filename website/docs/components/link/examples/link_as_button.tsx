fadabe84b7789f7262fe4c2cb6193f32cf3abd37
ilia.brauer <ilia.brauer@semrush.com>
2024-05-30 15:38:49 +0200

    [link] added example Link as a Button

--- website/docs/components/link/examples/link_as_button.tsx
+++ website/docs/components/link/examples/link_as_button.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Button from 'intergalactic/button';
-import Link from 'intergalactic/link';
 import PlusM from 'intergalactic/icon/MathPlus/m';
 
 const Demo = () => {
   return (
-    <Button tag={Link} href='#'>
-      <Button.Addon>
-        <PlusM />
-      </Button.Addon>
-      <Button.Text>Link as button</Button.Text>
-    </Button>
+    <>
+      <Button tag={'a'} href='#'>
+        <Button.Addon>
+          <PlusM />
+        </Button.Addon>
+        <Button.Text>Link as button</Button.Text>
+      </Button>{' '}
+      <Button tag={'a'} disabled>
+        <Button.Addon>
+          <PlusM />
+        </Button.Addon>
+        <Button.Text>Disabled link as button</Button.Text>
+      </Button>
+    </>
   );
 };
 
