19ecafc566d810d97d299dcf8492664e86554726
ilia.brauer <ilia.brauer@semrush.com>
2024-05-30 14:43:31 +0200

    [website,link] added Hint for links without text (addons only)

--- website/docs/components/link/examples/link_without_text.tsx
+++ website/docs/components/link/examples/link_without_text.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Link from 'intergalactic/link';
 import HomeM from 'intergalactic/icon/Home/m';
 import LinkExternalM from 'intergalactic/icon/LinkExternal/m';
+import { Hint } from 'intergalactic/tooltip';
 
 const Demo = () => {
   return (
     <>
       <Link addonLeft={HomeM} aria-label='home page' href='#' />
       <Link ml={4} aria-label='go to the next page' href='#'>
-        <Link.Addon>
+        <Link.Addon tag={Hint} title={'go to the next page'}>
           <LinkExternalM />
         </Link.Addon>
       </Link>
