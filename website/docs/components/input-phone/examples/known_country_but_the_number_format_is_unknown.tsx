921e053a822645d204171de1885372ec5eff2f51
Julia Mnizhek <j.mnizhek@semrush.com>
2024-07-01 17:48:53 +0200

    [website] fix formatting

--- website/docs/components/input-phone/examples/known_country_but_the_number_format_is_unknown.tsx
+++ website/docs/components/input-phone/examples/known_country_but_the_number_format_is_unknown.tsx
@@ -18,12 +18,7 @@ const Demo = () => {
         </Input.Addon>
         <Input.Value value={value} onChange={(v) => setValue(v)} id='phone-number' />
         {Number.parseInt(value, 10) > 2 && (
-          <Input.Addon
-            tag={CloseM}
-            interactive
-            aria-label='Clear'
-            onClick={() => setValue('+1')}
-          />
+          <Input.Addon tag={CloseM} interactive aria-label='Clear' onClick={() => setValue('+1')} />
         )}
       </Input>
     </Flex>
