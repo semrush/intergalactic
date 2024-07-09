b6cc7eb636a00188af04b7e89b88012d00f3e42e
ilia.brauer <ilia.brauer@semrush.com>
2024-07-09 12:45:54 +0200

    [global] added allure reports

--- tools/testing-utils/vitest.ts
+++ tools/testing-utils/vitest.ts
@@ -1,3 +1,22 @@
+import {beforeEach} from 'vitest';
+import { label, feature, story, parentSuite, suite, subSuite } from "allure-js-commons";
+
+beforeEach(async (context) => {
+  const suit = 'Unit';
+  const storyName = context.task.name;
+  const filePath = (context.task.file?.name ?? '').split('/');
+  const component = filePath[1] ?? '';
+  const componentName = component.charAt(0).toUpperCase() + component.slice(1);
+
+  await label('component', componentName);
+  await feature(suit);
+  await story(storyName);
+
+  await parentSuite(componentName);
+  await suite(suit);
+  await subSuite(storyName);
+});
+
 export * from 'vitest';
 
 declare global {
