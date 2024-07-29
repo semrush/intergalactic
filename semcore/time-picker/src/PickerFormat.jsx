620eb12b08fda3d9c8b280e9ec7bb9d7e56b59f7
ilia.brauer <ilia.brauer@semrush.com>
2024-07-29 10:18:12 +0300

    [time-picker] added role status with changed format notice

--- semcore/time-picker/src/PickerFormat.jsx
+++ semcore/time-picker/src/PickerFormat.jsx
@@ -8,45 +8,22 @@ import uniqueIDEnhancement from '@semcore/utils/lib/uniqueID';
 class TimePickerFormat extends Component {
   static enhance = [keyboardFocusEnhance(), uniqueIDEnhancement()];
 
-  state = {
-    ariaLabel: undefined,
-  };
-
-  handleClick = () => {
-    if (this.ariaLabelTimer) {
-      clearTimeout(this.ariaLabelTimer);
-    }
-
-    this.ariaLabelTimer = setTimeout(() => {
-      const { meridiem, getI18nText } = this.asProps;
-      this.setState({ ariaLabel: getI18nText('changedFormatNotice', { meridiem }) });
-    }, 0);
-  };
-
   render() {
     const SPickerFormat = Root;
     const { Children, meridiem, styles, getI18nText, uid } = this.asProps;
     const SPickerFormatText = 'span';
-    const describedby = getI18nText('formatToggler');
-    const { ariaLabel } = this.state;
 
     return sstyled(styles)(
       <>
-        <SPickerFormat
-          render={Box}
-          type='button'
-          tag='button'
-          aria-describedby={`${uid}_describe`}
-          onClick={this.handleClick}
-        >
-          <ScreenReaderOnly role={'status'} aria-live={'polite'}>
-            {ariaLabel}
-          </ScreenReaderOnly>
+        <SPickerFormat render={Box} type='button' tag='button' aria-describedby={`${uid}_describe`}>
           {Children.origin ? <Children /> : <SPickerFormatText>{meridiem}</SPickerFormatText>}
-          <ScreenReaderOnly aria-hidden={'true'} id={`${uid}_describe`}>
-            {describedby}
-          </ScreenReaderOnly>
         </SPickerFormat>
+        <ScreenReaderOnly role={'status'} aria-live={'polite'}>
+          {getI18nText('changedFormatNotice', { meridiem })}
+        </ScreenReaderOnly>
+        <ScreenReaderOnly aria-hidden={'true'} id={`${uid}_describe`}>
+          {getI18nText('formatToggler')}
+        </ScreenReaderOnly>
       </>,
     );
   }
