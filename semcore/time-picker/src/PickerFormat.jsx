becfa6af0e2970bbc72aef69494feb9cb91203e9
ilia.brauer <ilia.brauer@semrush.com>
2024-08-02 11:03:53 +0200

    [time-picker] fixed changedFormatNotice and some texts

--- semcore/time-picker/src/PickerFormat.jsx
+++ semcore/time-picker/src/PickerFormat.jsx
@@ -8,19 +8,43 @@ import uniqueIDEnhancement from '@semcore/utils/lib/uniqueID';
 class TimePickerFormat extends Component {
   static enhance = [keyboardFocusEnhance(), uniqueIDEnhancement()];
 
+  state = {
+    changedFormatNotice: '',
+  };
+
+  handleClick = () => {
+    setTimeout(() => {
+      const { meridiem, getI18nText } = this.asProps;
+      this.setState({ changedFormatNotice: getI18nText('changedFormatNotice', { meridiem }) });
+    }, 0);
+
+    setTimeout(() => {
+      this.setState({ changedFormatNotice: '' });
+    }, 2000);
+  };
+
   render() {
     const SPickerFormat = Root;
     const { Children, meridiem, styles, getI18nText, uid } = this.asProps;
+    const { changedFormatNotice } = this.state;
     const SPickerFormatText = 'span';
 
     return sstyled(styles)(
       <>
-        <SPickerFormat render={Box} type='button' tag='button' aria-describedby={`${uid}_describe`}>
+        <SPickerFormat
+          render={Box}
+          type='button'
+          tag='button'
+          onClick={this.handleClick}
+          aria-describedby={`${uid}_describe`}
+        >
           {Children.origin ? <Children /> : <SPickerFormatText>{meridiem}</SPickerFormatText>}
         </SPickerFormat>
-        <ScreenReaderOnly role={'status'} aria-live={'polite'}>
-          {getI18nText('changedFormatNotice', { meridiem })}
-        </ScreenReaderOnly>
+        {changedFormatNotice && (
+          <ScreenReaderOnly role={'status'} aria-live={'polite'}>
+            {changedFormatNotice}
+          </ScreenReaderOnly>
+        )}
         <ScreenReaderOnly aria-hidden={'true'} id={`${uid}_describe`}>
           {getI18nText('formatToggler')}
         </ScreenReaderOnly>
