57710522ebc619e4d01df3a7e6f39587af9aab69
faisne <166654065+sheila-semrush@users.noreply.github.com>
2024-08-13 20:37:11 +0200

    [docs] Fixed one of Dropdown examples

--- website/docs/components/dropdown/examples/basic_usage.tsx
+++ website/docs/components/dropdown/examples/basic_usage.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { ButtonTrigger } from 'intergalactic/base-trigger';
+import Button from 'intergalactic/button';
 import Dropdown from 'intergalactic/dropdown';
 import { Flex } from 'intergalactic/flex-box';
 import { Text } from 'intergalactic/typography';
 
 const Demo = () => (
   <Flex direction='column'>
-    <Text tag='label' size={200} htmlFor='dropdown-basic'>
-      Basic usage
-    </Text>
     <Dropdown>
-      <Dropdown.Trigger id='dropdown-basic' mt={2} mr='auto' tag={ButtonTrigger}>
-        Trigger
+      <Dropdown.Trigger id='dropdown-basic' mt={2} mr='auto' tag={Button}>
+        About export
       </Dropdown.Trigger>
-      <Dropdown.Popper p={4} aria-label='Dropdown popper description'>
-        Content
+      <Dropdown.Popper p={4} wMax={260} aria-labelledby='dropdown-basic'>
+        <Text size={200}>You can export up to 300 records in CSV or PDF format.</Text>
       </Dropdown.Popper>
     </Dropdown>
   </Flex>
