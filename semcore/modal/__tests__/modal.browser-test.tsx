80747ef97d9b12e516b39aa488b537be984b41d2
ilia.brauer <ilia.brauer@semrush.com>
2024-09-25 14:11:10 +0200

    [modal] added test for check working with iframes inside modal

---
+++ semcore/modal/__tests__/modal.browser-test.tsx
@@ -0,0 +1,25 @@
+import { expect, test } from '@semcore/testing-utils/playwright';
+import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
+
+test.describe('Modal', () => {
+  test('Modal with iframe inside', async ({ page, browserName }) => {
+    const standPath = 'semcore/modal/__tests__/stands/modal-with-iframe.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+
+    await page.setContent(htmlContent);
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await new Promise((resolve) => setTimeout(resolve, 50));
+
+    if (browserName !== 'chromium') {
+      await page.keyboard.press('Tab');
+    }
+
+    await page.keyboard.press('Tab');
+    await expect(page).toHaveScreenshot();
+
+    await page.keyboard.press('Tab');
+    await expect(page).toHaveScreenshot();
+  });
+});
---
+++ semcore/modal/__tests__/stands/modal-with-iframe.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from 'intergalactic/button';
+import Modal from 'intergalactic/modal';
+import { Text } from 'intergalactic/typography';
+
+const html = 'data:text/html,' + encodeURIComponent('<html><body><input /></body></html>');
+
+const Demo = () => {
+  const [visible, setVisible] = React.useState(false);
+  const handleOpen = React.useCallback(() => setVisible(true), []);
+  const handleClose = React.useCallback(() => setVisible(false), []);
+
+  return (
+    <React.Fragment>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal visible={visible} onClose={handleClose}>
+        <Modal.Title>Do you want to save your changes?</Modal.Title>
+        <Text size={200} mb={4} tag='p'>
+          Your changes will be lost if you don't save them.
+        </Text>
+        <iframe title={'test iframe in modal'} src={html} />
+        <Button use='primary' theme='success' size='l' onClick={handleClose}>
+          Save changes
+        </Button>
+        <Button size='l' ml={2} onClick={handleClose}>
+          Don't save
+        </Button>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+export default Demo;
+
+export const App = () => <Demo />;
