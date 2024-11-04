9a95d9b5db48897b959f21ef108191588d98f94e
ilia.brauer <ilia.brauer@semrush.com>
2024-11-04 11:33:03 +0100

    [button] fixed linter in stories

--- semcore/button/__stories/Button.stories.tsx
+++ semcore/button/__stories/Button.stories.tsx
@@ -26,92 +26,104 @@ export const SimpleButton: Story = {
   },
 };
 
-export const Addons: Story = () => {
-  return (
-    <>
-      <Button addonLeft={CheckM} use={'secondary'}>
-        Some button text
-      </Button>
-      <Button ml={2} use={'secondary'}>
-        <Button.Text>Some button text</Button.Text>
-        <Button.Addon>
-          <Badge bg='--intergalactic-control-primary-success'>new</Badge>
-        </Button.Addon>
-      </Button>
-    </>
-  );
+export const Addons: Story = {
+  render: () => {
+    return (
+      <>
+        <Button addonLeft={CheckM} use={'secondary'}>
+          Some button text
+        </Button>
+        <Button ml={2} use={'secondary'}>
+          <Button.Text>Some button text</Button.Text>
+          <Button.Addon>
+            <Badge bg='--intergalactic-control-primary-success'>new</Badge>
+          </Button.Addon>
+        </Button>
+      </>
+    );
+  },
 };
 
-export const Sizes: Story = () => {
-  return (
-    <>
-      <Button size={'m'}>Medium</Button>
-      <Button size={'l'} ml={2}>
-        Large
-      </Button>
-    </>
-  );
+export const Sizes: Story = {
+  render: () => {
+    return (
+      <>
+        <Button size={'m'}>Medium</Button>
+        <Button size={'l'} ml={2}>
+          Large
+        </Button>
+      </>
+    );
+  },
 };
 
-export const IconOnlyButton: Story = () => {
-  return (
-    <>
-      <Button title='Confirm'>
-        <Button.Addon>
-          <CheckM />
-        </Button.Addon>
-      </Button>
-    </>
-  );
-};
-
-export const ButtonLikeALink: Story = () => {
-  return (
-    <>
-      <Flex direction={'column'} gap={6} alignItems={'flex-start'}>
-        <ButtonLink addonLeft={CheckM}>Primary ButtonLink</ButtonLink>
-        <ButtonLink addonLeft={CheckM} color={'text-primary'}>
-          Colored primary ButtonLink
-        </ButtonLink>
-        <ButtonLink addonRight={CloseM} color={'text-critical'}>
-          Colored primary ButtonLink
-        </ButtonLink>
-        <ButtonLink use={'secondary'}>
-          <ButtonLink.Addon>
+export const IconOnlyButton: Story = {
+  render: () => {
+    return (
+      <>
+        <Button title='Confirm'>
+          <Button.Addon>
             <CheckM />
-          </ButtonLink.Addon>
-          <ButtonLink.Text>Secondary ButtonLink</ButtonLink.Text>
-        </ButtonLink>
-
-        <ButtonLink addonLeft={CheckM} aria-label={'Icon-only button'} />
-
-        <ButtonLink addonLeft={CheckL} size={500}>
-          ButtonLink with other text size
-        </ButtonLink>
-      </Flex>
-    </>
-  );
+          </Button.Addon>
+        </Button>
+      </>
+    );
+  },
 };
 
-export const ButtonWithNoVisibleText: Story = () => {
-  return (
-    <>
-      <Button addonLeft={CheckM} aria-label='Confirm action' mr={2} />
-      <Button addonLeft={CloseM} aria-label='Close notification' />
-    </>
-  );
+export const ButtonLikeALink: Story = {
+  render: () => {
+    return (
+      <>
+        <Flex direction={'column'} gap={6} alignItems={'flex-start'}>
+          <ButtonLink addonLeft={CheckM}>Primary ButtonLink</ButtonLink>
+          <ButtonLink addonLeft={CheckM} color={'text-primary'}>
+            Colored primary ButtonLink
+          </ButtonLink>
+          <ButtonLink addonRight={CloseM} color={'text-critical'}>
+            Colored primary ButtonLink
+          </ButtonLink>
+          <ButtonLink use={'secondary'}>
+            <ButtonLink.Addon>
+              <CheckM />
+            </ButtonLink.Addon>
+            <ButtonLink.Text>Secondary ButtonLink</ButtonLink.Text>
+          </ButtonLink>
+
+          <ButtonLink addonLeft={CheckM} aria-label={'Icon-only button'} />
+
+          <ButtonLink addonLeft={CheckL} size={500}>
+            ButtonLink with other text size
+          </ButtonLink>
+        </Flex>
+      </>
+    );
+  },
 };
 
-export const ButtonWithLoadingState: Story = () => {
-  return (
-    <>
-      <Button loading>Loading</Button>{' '}
-      <Button disabled>
-        <Button.Addon>
-          <Spin size='xs' />
-        </Button.Addon>
-        <Button.Text>Loading</Button.Text>
-      </Button>
-    </>
-  );
+export const ButtonWithNoVisibleText: Story = {
+  render: () => {
+    return (
+      <>
+        <Button addonLeft={CheckM} aria-label='Confirm action' mr={2} />
+        <Button addonLeft={CloseM} aria-label='Close notification' />
+      </>
+    );
+  },
+};
+
+export const ButtonWithLoadingState: Story = {
+  render: () => {
+    return (
+      <>
+        <Button loading>Loading</Button>{' '}
+        <Button disabled>
+          <Button.Addon>
+            <Spin size='xs' />
+          </Button.Addon>
+          <Button.Text>Loading</Button.Text>
+        </Button>
+      </>
+    );
+  },
 };
