31c4af70190f4c656e519da16f6dced53718c97d
ilia.brauer <ilia.brauer@semrush.com>
2024-11-04 12:59:56 +0100

    [modal] switched timeouts with waitFor selectors in tests

--- semcore/modal/__tests__/index.test.tsx
+++ semcore/modal/__tests__/index.test.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { snapshot } from '@semcore/testing-utils/snapshot';
 import * as sharedTests from '@semcore/testing-utils/shared-tests';
 import { expect, test, describe, beforeEach, vi } from '@semcore/testing-utils/vitest';
-import { cleanup, fireEvent, render, userEvent } from '@semcore/testing-utils/testing-library';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  userEvent,
+  waitFor,
+} from '@semcore/testing-utils/testing-library';
 import { axe } from '@semcore/testing-utils/axe';
 
 const { shouldSupportClassName, shouldSupportRef } = sharedTests;
@@ -340,11 +346,9 @@ describe('Modal', () => {
     expect(getByTestId('openModalButton')).toHaveFocus();
 
     await userEvent.keyboard('[Enter]');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    expect(getByText('Do you want to save your changes?')).toBeTruthy();
+    await waitFor(() => expect(getByText('Do you want to save your changes?')).toBeTruthy());
 
     await userEvent.keyboard('[Escape]');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    expect(getByTestId('openModalButton')).toHaveFocus();
+    await waitFor(() => expect(getByTestId('openModalButton')).toHaveFocus());
   });
 });
