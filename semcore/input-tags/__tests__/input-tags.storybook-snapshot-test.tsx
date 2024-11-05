238ce4e2ac3817d59b260d8ebcdd31d57e042853
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2024-11-05 19:53:12 +0400

    [input-tags] test updated and snaphot test added

---
+++ semcore/input-tags/__tests__/input-tags.storybook-snapshot-test.tsx
@@ -0,0 +1,24 @@
+import { expect, test } from '@semcore/testing-utils/playwright';
+
+test.describe('Input-Tags Render and Visible', () => {
+  //this is exmaple of screenshot test
+  test('Input Tag with Select - Empty Input ans expanded select', async ({ page , browserName}) => {
+    await page.goto('/?path=/story/components-inputtags--select-tag-for-filtering');
+    const frame = page.frameLocator('iframe[data-is-storybook="true"]');
+    await frame.locator('body').waitFor();
+    const element = page.locator('div.css-1beo138');
+    const x = 150;
+    const y = 30;
+    await element.click({ position: { x, y } });
+    await element.click({ position: { x, y } });
+    if(browserName === 'firefox'){
+        await page.keyboard.press('Tab');
+      }
+      else{
+        await page.keyboard.press('Tab');
+        await page.keyboard.press('Tab');
+      }  
+    await expect(element).toHaveScreenshot();
+  });
+
+});
