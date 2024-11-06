dfca0db51089871f81cc9a7d61faf24b7eea6d7b
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2024-11-06 15:12:32 +0400

    [input-tags] update test

--- semcore/input-tags/__tests__/input-tags.storybook-snapshot-test.tsx
+++ semcore/input-tags/__tests__/input-tags.storybook-snapshot-test.tsx
@@ -2,12 +2,12 @@ import { expect, test } from '@semcore/testing-utils/playwright';
 
 test.describe('Input-Tags Render and Visible', () => {
   test('Input Tag with Select - Empty Input ans expanded select', async ({ page, browserName }) => {
-    await page.goto('/?path=/story/components-inputtags--select-tag-for-filtering');
+    await page.goto('/?path=/story/components-inputtags-test--select-tag-for-filtering-test');
     const frame = page.frameLocator('iframe[data-is-storybook="true"]');
     await frame.locator('body').waitFor();
     const element = page.locator('div.css-1beo138');
-    const x = 150;
-    const y = 30;
+    const x = 400;
+    const y = 20;
     await element.click({ position: { x, y } });
     await element.click({ position: { x, y } });
     if (browserName === 'firefox') {
