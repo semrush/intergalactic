8fdbd6611bfbc23e7da799d8749193bb9636f1ea
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2024-11-07 17:35:54 +0400

    [input-tags] updated snaphot test and snapshots removed unneeded story

--- semcore/input-tags/__tests__/input-tags.storybook-snapshot-test.tsx
+++ semcore/input-tags/__tests__/input-tags.storybook-snapshot-test.tsx
@@ -2,21 +2,16 @@ import { expect, test } from '@semcore/testing-utils/playwright';
 
 test.describe('Input-Tags Render and Visible', () => {
   test('Input Tag with Select - Empty Input and expanded Select', async ({ page, browserName }) => {
-    await page.goto('/?path=/story/components-inputtags-test--select-tag-for-filtering-test');
+    await page.goto('/?path=/story/components-inputtags--select-tag-for-filtering');
     const frame = page.frameLocator('iframe[data-is-storybook="true"]');
     await frame.locator('body').waitFor();
     const element = page.locator('div.css-1beo138');
     await frame.locator('[data-ui-name="Select.Trigger"]').waitFor();
-    const x = 400;
-    const y = 20;
+    const x = 10;
+    const y = 10;
     await element.click({ position: { x, y } });
     await element.click({ position: { x, y } });
-    if (browserName === 'firefox') {
-      await page.keyboard.press('Tab');
-    } else {
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Tab');
-    }
+    await page.keyboard.press('Tab');
     await expect(element).toHaveScreenshot();
   });
 });
