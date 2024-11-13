091ff8194130bb21b59fa335eba8747064a5ee22
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2024-11-13 13:02:49 +0400

    [chore] upd test and added storybook run

--- semcore/button/__stories/Button.stories.tsx
+++ semcore/button/__stories/Button.stories.tsx
@@ -117,15 +117,15 @@ export const ButtonWithNoVisibleText: Story = {
     const button = canvas.getByRole('button', { name: 'Confirm action' });
     await userEvent.hover(button);
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    
+
     const hint = within(document.body).queryByText('Confirm action');
     if (!hint) throw new Error('Hint not found');
-    expect(hint).toBeVisible(); 
+    expect(hint).toBeVisible();
     const hintStyles = window.getComputedStyle(hint);
-    expect(hintStyles.padding).toBe("12px");
+    expect(hintStyles.padding).toBe('12px');
     expect(hintStyles.fontSize).toBe('14px');
     expect(hint.textContent).not.toBeNull();
-    expect(hintStyles.color).toBe('rgb(25, 27, 35)'); 
+    expect(hintStyles.color).toBe('rgb(25, 27, 35)');
     expect(hintStyles.backgroundColor).toBe('rgb(255, 255, 255)');
 
     await userEvent.unhover(button);
