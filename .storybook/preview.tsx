f40e8231f879efeb8300b3e7d733e003fbf8e54d
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2024-11-13 15:50:58 +0400

    [chore]  upd preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import type { Preview } from "@storybook/react";
-
 import { PortalProvider } from '@semcore/portal';
 
 const preview: Preview = {
@@ -14,11 +13,11 @@ const preview: Preview = {
     config: {
       rules: [
         { id: 'color-contrast', enabled: false },
-          { id: 'html-has-lang', enabled: false },
-          { id: 'html-lang-valid', enabled: false }, 
-         { id: 'meta-viewport', enabled: false },
+        { id: 'html-has-lang', enabled: false },
+        { id: 'html-lang-valid', enabled: false }, 
+        { id: 'meta-viewport', enabled: false },
         { id: 'video-caption', enabled: false },
-          { id: 'scope-attr-valid', enabled: true },
+        { id: 'scope-attr-valid', enabled: true },
       ],
     },
 
