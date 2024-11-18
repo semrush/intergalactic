71f3e59a65c5cb4d2641b3493e06f575d6b49d79
ilyabrower <ilia.brauer@semrush.com>
2024-11-18 12:23:09 +0100

    [UIK-2195][pagination] changed Input to InputNumber and fixed thousands separator (#1772)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    Manually + I've added unit test
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [X] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [X] My code follows the code style of this project.
    - [X] I have updated the documentation accordingly or it's not required.
    - [X] Unit tests are not broken.
    - [X] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

---
+++ stories/components/pagination/__stories__/index.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Pagination from '@semcore/pagination';
+
+import BasicUsageExample from './docs-examples/basic_usage';
+
+import TotalPageIs1Example from './components/total_pages_is_1';
+
+const meta: Meta<typeof Pagination> = {
+  title: 'Components/Pagination',
+  component: Pagination,
+};
+
+export default meta;
+type Story = StoryObj<typeof Pagination>;
+
+export const BasicUsage: Story = {
+  render: BasicUsageExample,
+};
+
+export const TotalPageIs1: Story = {
+  render: TotalPageIs1Example,
+};
