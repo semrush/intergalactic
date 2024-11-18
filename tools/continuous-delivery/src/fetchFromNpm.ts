2c2b8f4a95697688e9574a1136824bf013ad9c55
ilia.brauer <ilia.brauer@semrush.com>
2024-11-18 17:13:24 +0100

    [chore] turned back checker of packages count in prerelease job

--- tools/continuous-delivery/src/fetchFromNpm.ts
+++ tools/continuous-delivery/src/fetchFromNpm.ts
@@ -45,11 +45,11 @@ export const fetchFromNpm = async (filter?: string[]) => {
     }
   >('https://registry.npmjs.org/-/v1/search?text=@semcore/&size=250');
 
-  // if (npmSearchResult.total !== npmSearchResult.objects.length) {
-  //   throw new Error(
-  //     'Gosh, seems like our npm search unable to find all packages at once, need to update search',
-  //   );
-  // }
+  if (npmSearchResult.total !== npmSearchResult.objects.length) {
+    throw new Error(
+      'Gosh, seems like our npm search unable to find all packages at once, need to update search',
+    );
+  }
 
   const currentVersions: { [packageName: string]: { version: string } & PackageNpmRegistry } = {};
   const objectsToFetch = npmSearchResult.objects.filter(
