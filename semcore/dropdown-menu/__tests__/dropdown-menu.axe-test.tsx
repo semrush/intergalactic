dcdb5fbadb608aad4d60af9a176d53d4eaa62009
ilyabrower <ilia.brauer@semrush.com>
2024-11-19 10:26:35 +0100

    [chore] refactored folders structure in stories (#1776)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] I have updated the documentation accordingly or it's not required.
    - [ ] Unit tests are not broken.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- semcore/dropdown-menu/__tests__/dropdown-menu.axe-test.tsx
+++ semcore/dropdown-menu/__tests__/dropdown-menu.axe-test.tsx
@@ -3,7 +3,7 @@ import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 
 test.describe('Dropdown-menu', () => {
   test('Basic usage', async ({ page }) => {
-    const standPath = 'stories/components/dropdown-menu/__stories__/docs-examples/basic.tsx';
+    const standPath = 'stories/components/dropdown-menu/docs/examples/basic.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
 
     await page.setContent(htmlContent);
