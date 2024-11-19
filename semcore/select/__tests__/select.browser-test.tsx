99ea336e1da0b7a5a529f69c5e7a26562148455d
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2024-11-19 16:28:05 +0400

    [select]  added test

--- semcore/select/__tests__/select.browser-test.tsx
+++ semcore/select/__tests__/select.browser-test.tsx
@@ -63,7 +63,7 @@ test.describe('Select', () => {
 
     await expect(await triggerLocatior.textContent()).toBe('Banana');
   });
-  test('Show hint for clear input button', async ({ page }) => {
+  test('Show hint for clear input button by keyboard', async ({ page }) => {
     const standPath = 'stories/components/select/docs/examples/options_filtering.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
 
@@ -80,5 +80,24 @@ test.describe('Select', () => {
     await new Promise((resolve) => setTimeout(resolve, 500));
 
     await expect(page).toHaveScreenshot();
+  });
+
+  test('Show hint for clear input button by mouse', async ({ page }) => {
+    const standPath = 'stories/components/select/docs/examples/options_filtering.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+
+    await page.setContent(htmlContent);
+    const trigger = await page.locator('[data-ui-name="Select.Trigger"]');
+    await trigger.click();
+
+    const input = await page.waitForSelector('input');
+    await input.click();
+    await page.keyboard.type('Test');
+
+    const close = await page.locator('[data-ui-name="InputSearch.Clear"]');
+    await close.hover();
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    await expect(page).toHaveScreenshot();  
   });
 });
