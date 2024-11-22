66fae00fd510088e8d7ae5d0a29dc05e648ac03c
ilyabrower <ilia.brauer@semrush.com>
2024-11-22 14:31:09 +0100

    [UIK-2636][dropdown] fixed onMouseEnter bubbling (#1791)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    Manually
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [X] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [X] My code follows the code style of this project.
    - [X] I have updated the documentation accordingly or it's not required.
    - [X] Unit tests are not broken.
    - [X] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

---
+++ stories/components/dropdown/docs/dropdown.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Dropdown from '@semcore/dropdown';
+
+import BasicUsageExample from './examples/basic_usage';
+import FocusInteractionExample from './examples/focus_interaction';
+
+const meta: Meta<typeof Dropdown> = {
+  title: 'Components/Dropdown/Documentation',
+  component: Dropdown,
+};
+export default meta;
+
+type Story = StoryObj<typeof Dropdown>;
+
+export const BasicUsage: Story = {
+  render: BasicUsageExample,
+};
+
+export const FocusInteraction: Story = {
+    render: FocusInteractionExample,
+  };
