ea9d115c1848f3cc809b6c065ce9984d04d91d66
ilyabrower <ilia.brauer@semrush.com>
2024-11-22 07:35:41 +0100

    [UIK-2544][input-tags] a11y attrs (#1783)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [x] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>
    Co-authored-by: faisne <166654065+sheila-semrush@users.noreply.github.com>

--- semcore/utils/src/findComponent.ts
+++ semcore/utils/src/findComponent.ts
@@ -1,4 +1,4 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React, { ReactElement, ReactNode, MutableRefObject } from 'react';
 import { CHILDREN_COMPONENT, INHERITED_NAME } from './core/index';
 import getOriginChildren from './getOriginChildren';
 
@@ -36,8 +36,12 @@ export function isAdvanceMode(Children: any, name: string[], recursively?: boole
   return !!findComponent(children, name, recursively);
 }
 
-export function findAllComponents(Children: any, names: string[]): ReactElement[] {
-  const result: ReactElement[] = [];
+type IntergalacticComponent = ReactElement & {
+  ref?: MutableRefObject<HTMLElement>;
+};
+
+export function findAllComponents(Children: any, names: string[]): IntergalacticComponent[] {
+  const result: IntergalacticComponent[] = [];
 
   const findAllAndAdd = (Children: any) => {
     const children = Children[CHILDREN_COMPONENT] ? getOriginChildren(Children) : Children;
@@ -45,6 +49,12 @@ export function findAllComponents(Children: any, names: string[]): ReactElement[
       if (React.isValidElement(child)) {
         if (child.type === React.Fragment) {
           findAllAndAdd(child.props.children);
+        } else if (
+          typeof child.type === 'function' &&
+          CHILDREN_COMPONENT in child.type &&
+          child.type[CHILDREN_COMPONENT]
+        ) {
+          findAllAndAdd(child.type);
         } else {
           // @ts-ignore
           const inheritedNames = child.type[INHERITED_NAME] || [child.type.displayName];
