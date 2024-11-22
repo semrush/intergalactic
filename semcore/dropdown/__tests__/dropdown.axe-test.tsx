cdda16ac469dbc414bf588699431cf8ca74998e1
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2024-11-22 17:14:54 +0400

    [dropdown] added story

--- semcore/dropdown/__tests__/dropdown.axe-test.tsx
+++ semcore/dropdown/__tests__/dropdown.axe-test.tsx
@@ -3,7 +3,7 @@ import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 
 test.describe('Dropdown', () => {
   test('Basic usage', async ({ page }) => {
-    const standPath = 'website/docs/components/dropdown/examples/basic_usage.tsx';
+    const standPath = 'stories/components/dropdown/docs/examples/basic_usage.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
 
     await page.setContent(htmlContent);
