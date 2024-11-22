8d254b7bac8907d1db87d57b85b3dbd80a6f43fe
ilyabrower <ilia.brauer@semrush.com>
2024-11-22 12:11:33 +0100

    [UIK-2643][mini-chart] fixed cropping in SemiDonut (#1793)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    I fixed a little layout and calculation for SemiDonut chart to ensure it
    displays correctly
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    Manually and I updated snapshots
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    Before:
    
    ![image](https://github.com/user-attachments/assets/a95f7920-19c7-43e5-91d4-cd82d09911f8)
    
    After:
    <img width="74" alt="Screenshot 2024-11-22 at 11 11 06"
    src="https://github.com/user-attachments/assets/0b1f1f7e-9350-4c17-9495-36cb4a1a51a0">
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [X] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [X] My code follows the code style of this project.
    - [X] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [x] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

--- semcore/mini-chart/__tests__/stands/score-lines.tsx
+++ semcore/mini-chart/__tests__/stands/score-lines.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-// @ts-ignore
-import MiniChart from 'intergalactic/mini-chart';
-// @ts-ignore
-import { Flex, Box } from 'intergalactic/flex-box';
+
+import MiniChart from '../../src';
+import { Flex, Box } from '@semcore/flex-box';
 
 const Demo = () => {
   return (
