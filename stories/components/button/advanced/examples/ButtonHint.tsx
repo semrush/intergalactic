bab5ff55f99cc2244fcc7278b7b7c7a49828d614
Sheila <166654065+sheila-semrush@users.noreply.github.com>
2024-11-26 21:06:29 +0100

    [chore] Added stories to pnpm format path, formatted existing stories (#1807)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Thought this must be useful. 
    Maybe other pnpm commands need updating?
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly or it's not required.
    - [ ] Unit tests are not broken.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

--- stories/components/button/advanced/examples/ButtonHint.tsx
+++ stories/components/button/advanced/examples/ButtonHint.tsx
@@ -8,19 +8,28 @@ import { Flex } from 'intergalactic/flex-box';
 const Demo = () => {
   return (
     <Flex direction={'row'} gap={6} alignItems={'flex-start'}>
-      <Button addonLeft={SearchM} title='Button secondary Addon'/>  
+      <Button addonLeft={SearchM} title='Button secondary Addon' />
       <Hint tag={Button} addonLeft={SearchM} title='Hint Button secondary Addon' />
       <Tooltip tag={Button} addonLeft={SearchM} title='Tooltip Button secondary Addon' />
 
-      <Button addonLeft={SearchL} use={'primary'} title='Button primary Addon'/>  
+      <Button addonLeft={SearchL} use={'primary'} title='Button primary Addon' />
       <Hint tag={Button} addonLeft={SearchL} use={'primary'} title='Hint Button primary Addon' />
-      <Tooltip tag={Button} addonLeft={SearchM} use={'primary'} title='Tooltip Button secondary Addon' />
+      <Tooltip
+        tag={Button}
+        addonLeft={SearchM}
+        use={'primary'}
+        title='Tooltip Button secondary Addon'
+      />
 
-
-      <Button addonLeft={SearchM} use={'tertiary'} title='Button tertiary Addon'/>  
+      <Button addonLeft={SearchM} use={'tertiary'} title='Button tertiary Addon' />
       <Hint tag={Button} addonLeft={SearchM} use={'tertiary'} title='Hint Button tertiary Addon' />
-      <Tooltip tag={Button} addonLeft={SearchM} use={'tertiary'} title='Tooltip Button tertiary Addon' />
-  </Flex>
+      <Tooltip
+        tag={Button}
+        addonLeft={SearchM}
+        use={'tertiary'}
+        title='Tooltip Button tertiary Addon'
+      />
+    </Flex>
   );
 };
 
--- stories/components/dropdown/tests/examples/dd-with-tooltip.tsx
+++ stories/components/dropdown/tests/examples/dd-with-tooltip.tsx
@@ -6,19 +6,19 @@ import Tooltip from '@semcore/tooltip';
 import FileExportM from '@semcore/icon/FileExport/m';
 
 const Demo = () => (
-    <Dropdown>        
-    <Dropdown.Trigger id="dropdown-basic" tag={Button} ml={2}>
-    About export        
-    </Dropdown.Trigger>        
-    <Dropdown.Popper p={4} wMax={260} aria-labelledby="dropdown-basic">          
-   <Tooltip
-     title= "Default tooltip contains short text explaining something about the trigger"            
-     tag={Button}            
-     aria-label="Export to PDF"            
-     addonLeft={FileExportM}          
-     />        
-    </Dropdown.Popper>      
-    </Dropdown>
+  <Dropdown>
+    <Dropdown.Trigger id='dropdown-basic' tag={Button} ml={2}>
+      About export
+    </Dropdown.Trigger>
+    <Dropdown.Popper p={4} wMax={260} aria-labelledby='dropdown-basic'>
+      <Tooltip
+        title='Default tooltip contains short text explaining something about the trigger'
+        tag={Button}
+        aria-label='Export to PDF'
+        addonLeft={FileExportM}
+      />
+    </Dropdown.Popper>
+  </Dropdown>
 );
 
 export default Demo;
