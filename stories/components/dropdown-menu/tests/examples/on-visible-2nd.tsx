16bbc8cdaf13f5049f8128a35791b82871545a5e
ilyabrower <ilia.brauer@semrush.com>
2024-11-29 13:22:12 +0100

    [UIK 2603][Dropdown-menu] keyboard interaction (#1810)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    I fixed that handlers were not called for `Dropdown.Item` in a
    controlled `DropdownMenu` after it was closed and opened again.
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    Manually and I wrote a test.
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [X] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [X] My code follows the code style of this project.
    - [X] I have updated the documentation accordingly or it's not required.
    - [X] Unit tests are not broken.
    - [X] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- stories/components/dropdown-menu/tests/examples/on-visible-2nd.tsx
+++ stories/components/dropdown-menu/tests/examples/on-visible-2nd.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import DropdownMenu from "@semcore/ui/dropdown-menu";
-import Button from "@semcore/ui/button";
+import React from 'react';
+import DropdownMenu from '@semcore/dropdown-menu';
+import Button from '@semcore/button';
 
-const items = ["save", "rename", "delete"];
+const items = ['save', 'rename', 'delete'];
 const Demo = () => {
   const [visible, setVisible] = React.useState(false);
   return (
     <DropdownMenu
       visible={visible}
       onVisibleChange={(value) => {
-        console.log(value);
         setVisible(value);
       }}
     >
@@ -19,11 +18,10 @@ const Demo = () => {
           <DropdownMenu.Item
             key={name}
             onClick={() => {
-              console.log("clack");
+              console.log('click on ddMenu.Item');
 
               setVisible(false);
             }}
-            tabIndex={0}
           >
             {name}
           </DropdownMenu.Item>
