8eb3555738ae165c5ee02fc2c9de6db77d14d3d7
ilia.brauer <ilia.brauer@semrush.com>
2024-12-04 22:23:14 +0100

    [website] added some accessible labels to the playground

--- website/docs/.vitepress/theme/Sandbox.vue
+++ website/docs/.vitepress/theme/Sandbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :id="playgroundId" :class="{ 'playground-runtime': !hideCode, 'documentation-sandbox': true }">
+  <div :id="playgroundId" :class="{ 'playground-runtime': !hideCode, 'documentation-sandbox': true }" role="region" aria-label="Playground">
   </div>
   <div class="code-wrapper" v-if="!hideCode">
     <span v-html="htmlCode"></span>
