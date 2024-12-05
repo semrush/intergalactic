622da6556b0a1ae10da7082c1a2dcbf5c8772290
ilyabrower <ilia.brauer@semrush.com>
2024-12-05 15:05:32 +0100

    [UIK-2781][website][VPAT] fixed tab-line and tab-panel examples for vpat checks (#1836)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [X] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [X] My code follows the code style of this project.
    - [X] I have updated the documentation accordingly or it's not required.
    - [X] Unit tests are not broken.
    - [X] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

--- website/docs/components/tab-panel/examples/disabled_tab_panel_item.tsx
+++ website/docs/components/tab-panel/examples/disabled_tab_panel_item.tsx
@@ -7,7 +7,7 @@ const Demo = () => {
   return (
     <>
       <TabPanel value={value} onChange={setValue} aria-label='Social network reports'>
-        <TabPanel.Item value={1} aria-controls='tab-panel-1'>
+        <TabPanel.Item value={1} aria-controls='tab-panel-2-1' id='tab-label-2-1'>
           <TabPanel.Item.Text>Normal tab</TabPanel.Item.Text>
         </TabPanel.Item>
         <Hint
@@ -22,7 +22,7 @@ const Demo = () => {
       </TabPanel>
       {
         [
-          <div id='tab-panel-1' role='tabpanel' aria-labelledby='tab-label-1' tabIndex={-1}>
+          <div id='tab-panel-2-1' role='tabpanel' aria-labelledby='tab-label-2-1' tabIndex={-1}>
             <h3>Normal tab</h3>
             <p>
               Here you can place your content, or just leave it blank for that avant-garde
