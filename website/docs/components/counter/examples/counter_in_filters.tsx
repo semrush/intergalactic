fd333999e55e659dc7d113c257bd775618bab055
ilyabrower <ilia.brauer@semrush.com>
2024-12-05 15:54:36 +0100

    [UIK-2770][website][VPAT] fixed counter in FilterTrigger example (#1840)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [X] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [X] My code follows the code style of this project.
    - [X] I have updated the documentation accordingly or it's not required.
    - [X] Unit tests are not broken.
    - [X] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

--- website/docs/components/counter/examples/counter_in_filters.tsx
+++ website/docs/components/counter/examples/counter_in_filters.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import Counter, { AnimatedNumber } from 'intergalactic/counter';
-import { FilterTrigger } from 'intergalactic/base-trigger';
+import Counter, { AnimatedNumber } from '@semcore/counter';
+import { FilterTrigger } from '@semcore/base-trigger';
+import Dropdown from '@semcore/dropdown';
 
 const Demo = () => (
-  <FilterTrigger>
-    <FilterTrigger.Text>Link to website</FilterTrigger.Text>
-    <FilterTrigger.Addon>
-      <Counter theme='info'>
-        <AnimatedNumber value={500} delay={1000} formatValue={(x) => Math.round(x).toString()} />
-      </Counter>
-    </FilterTrigger.Addon>
-  </FilterTrigger>
+  <Dropdown>
+    <Dropdown.Trigger tag={FilterTrigger}>
+      <FilterTrigger.Text>Link to website</FilterTrigger.Text>
+      <FilterTrigger.Addon>
+        <Counter theme='info'>
+          <AnimatedNumber value={500} delay={1000} formatValue={(x) => Math.round(x).toString()} />
+        </Counter>
+      </FilterTrigger.Addon>
+    </Dropdown.Trigger>
+    <Dropdown.Popper aria-label='Filter content'>Filter content</Dropdown.Popper>
+  </Dropdown>
 );
 
 export default Demo;
