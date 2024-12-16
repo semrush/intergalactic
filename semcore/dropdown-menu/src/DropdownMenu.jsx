47bee797a90d2b971d393c2303ed3c6c4c33930c
ilia.brauer <ilia.brauer@semrush.com>
2024-12-16 22:34:16 +0100

    [dropdown-menu] VPAT fixed Certain ARIA roles must contain particular children

--- semcore/dropdown-menu/src/DropdownMenu.jsx
+++ semcore/dropdown-menu/src/DropdownMenu.jsx
@@ -244,7 +244,7 @@ function Menu(props) {
   };
   return (
     <ListBoxContextProvider>
-      <DropdownMenu.Popper {...popperProps} role={null}>
+      <DropdownMenu.Popper {...popperProps} role={null} use:tabIndex={null}>
         <Root render={DropdownMenu.List} />
       </DropdownMenu.Popper>
     </ListBoxContextProvider>
