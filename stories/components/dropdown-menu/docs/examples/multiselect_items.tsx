0cf1b06674bb4e4c5eedc65ca7f9f450dde6ec4b
ilia.brauer <ilia.brauer@semrush.com>
2024-12-24 14:16:36 +0100

    [dropdown-menu] added padding for checkbox

--- stories/components/dropdown-menu/docs/examples/multiselect_items.tsx
+++ stories/components/dropdown-menu/docs/examples/multiselect_items.tsx
@@ -8,30 +8,28 @@ const Demo = () => {
   const [selected, setSelected] = React.useState<number[]>([0, 1]);
 
   return (
-      <DropdownMenu selectable multiselect>
-        <DropdownMenu.Trigger tag={Button}>
-          Explore menu items
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Menu hMax={'180px'}>
-          <DropdownMenu.Group title={'List heading'} subTitle={'Subtitle'}>
-            {menuItems.map((_, index) => (
-              <DropdownMenu.Item
-                key={index}
-                selected={selected.includes(index)}
-                onClick={() => {
-                  if (!selected.includes(index)) {
-                    setSelected([...selected, index]);
-                  } else {
-                    setSelected(selected.filter((i) => i !== index));
-                  }
-                }}
-              >
-                Menu item {index + 1}
-              </DropdownMenu.Item>
-            ))}
-          </DropdownMenu.Group>
-        </DropdownMenu.Menu>
-      </DropdownMenu>
+    <DropdownMenu selectable multiselect>
+      <DropdownMenu.Trigger tag={Button}>Explore menu items</DropdownMenu.Trigger>
+      <DropdownMenu.Menu hMax={'180px'}>
+        <DropdownMenu.Group title={'List heading'} subTitle={'Subtitle'}>
+          {menuItems.map((_, index) => (
+            <DropdownMenu.Item
+              key={index}
+              selected={selected.includes(index)}
+              onClick={() => {
+                if (!selected.includes(index)) {
+                  setSelected([...selected, index]);
+                } else {
+                  setSelected(selected.filter((i) => i !== index));
+                }
+              }}
+            >
+              Menu item {index + 1}
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.Group>
+      </DropdownMenu.Menu>
+    </DropdownMenu>
   );
 };
 
