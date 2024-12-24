823da5d78f5f5a30637d3f99573483081b381af8
faisne <166654065+sheila-semrush@users.noreply.github.com>
2024-12-24 14:56:48 +0100

    [docs] Fixed hint placement in selectable menu item actions

--- stories/components/dropdown-menu/docs/examples/selectable_radio_items.tsx
+++ stories/components/dropdown-menu/docs/examples/selectable_radio_items.tsx
@@ -10,35 +10,38 @@ const Demo = () => {
   const [selected, setSelected] = React.useState<number>(0);
 
   return (
-      <DropdownMenu selectable>
-        <DropdownMenu.Trigger tag={Button}>
-          Explore menu items
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Menu hMax={'180px'}>
-          <DropdownMenu.Group title={'List heading'} subTitle={'Subtitle'}>
-            {menuItems.map((_, index) => (
-              <DropdownMenu.Item
-                key={index}
-                selected={index === selected}
-                onClick={() => {
-                  setSelected(index);
-                }}
-              >
-                <DropdownMenu inlineActions placement={'right'}>
-                  <Flex justifyContent='space-between'>
-                    <DropdownMenu.Item.Content tag={DropdownMenu.Trigger}>
-                      Menu item {index + 1}
-                    </DropdownMenu.Item.Content>
-                    <DropdownMenu.Actions>
-                      <DropdownMenu.Item tag={Button} addonLeft={Trash} title={'Delete item'} />
-                    </DropdownMenu.Actions>
-                  </Flex>
-                </DropdownMenu>
-              </DropdownMenu.Item>
-            ))}
-          </DropdownMenu.Group>
-        </DropdownMenu.Menu>
-      </DropdownMenu>
+    <DropdownMenu selectable>
+      <DropdownMenu.Trigger tag={Button}>Explore menu items</DropdownMenu.Trigger>
+      <DropdownMenu.Menu hMax={'180px'}>
+        <DropdownMenu.Group title={'List heading'} subTitle={'Subtitle'}>
+          {menuItems.map((_, index) => (
+            <DropdownMenu.Item
+              key={index}
+              selected={index === selected}
+              onClick={() => {
+                setSelected(index);
+              }}
+            >
+              <DropdownMenu inlineActions placement={'right'}>
+                <Flex justifyContent='space-between'>
+                  <DropdownMenu.Item.Content tag={DropdownMenu.Trigger}>
+                    Menu item {index + 1}
+                  </DropdownMenu.Item.Content>
+                  <DropdownMenu.Actions>
+                    <DropdownMenu.Item
+                      tag={Button}
+                      addonLeft={Trash}
+                      title={'Delete item'}
+                      hintPlacement='right'
+                    />
+                  </DropdownMenu.Actions>
+                </Flex>
+              </DropdownMenu>
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.Group>
+      </DropdownMenu.Menu>
+    </DropdownMenu>
   );
 };
 
