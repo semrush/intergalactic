4a089021d03fd0feace5c07ff718ea34242c3f87
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-01-17 19:29:57 +0400

    [stories] one  more test  example for ellipsis

--- stories/components/ellipsis/test/examples/text_cases.tsx
+++ stories/components/ellipsis/test/examples/text_cases.tsx
@@ -12,10 +12,10 @@ const Demo = () => {
         </Box>
       </p>
       <p>
-        <Box w={111}>
+        <Box w={111} >
           <Ellipsis trim="middle">
             Segment 2sfdsf
-            </Ellipsis>
+          </Ellipsis>
         </Box>
       </p>
       <p>
@@ -34,7 +34,7 @@ const Demo = () => {
       </p>
       <p>
         <Box w={220}>
-          <Ellipsis>
+          <Ellipsis maxLine={3}>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi hic nemo tenetur
             voluptatem! A aliquid assumenda dolore ducimus impedit numquam ratione recusandae sed ullam
             voluptate? Aperiam distinctio minus possimus quasi.
@@ -55,6 +55,17 @@ const Demo = () => {
           w={111}
           mb={3}>
           <Ellipsis trim="middle">Segment 2sfdsf</Ellipsis>
+        </Text>
+      </p>
+      <p>
+        <Text
+          display="block"
+          w={150}
+          h={150}
+          mb={3}
+          size={700} tag='p' mt={0}
+          style={{ outline: "1px solid red" }}>
+          <Ellipsis maxLine={2}>Word WordWor</Ellipsis>
         </Text>
       </p>
     </div>
