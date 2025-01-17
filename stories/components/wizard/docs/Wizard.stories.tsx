4af1f9b105d5dc10f96d253da94ede30bd744dc5
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-01-17 16:11:35 +0400

    [wizard] added story

--- stories/components/wizard/docs/Wizard.stories.tsx
+++ stories/components/wizard/docs/Wizard.stories.tsx
@@ -5,6 +5,7 @@ import Wizard from '@semcore/wizard';
 import BasicExample from './examples/basic_example';
 import CustomStepExample from './examples/custom_step';
 import CustomStepperExample from './examples/custom_stepper';
+import SidebarStatesExample from './examples/sidebar_states';
 
 const meta: Meta<typeof Wizard> = {
   title: 'Components/Wizard/Documentation',
@@ -25,4 +26,8 @@ export const CustomStep: Story = {
 
 export const CustomStepper: Story = {
   render: CustomStepperExample,
+};
+
+export const SidebarStates: Story = {
+    render: SidebarStatesExample,
 };
