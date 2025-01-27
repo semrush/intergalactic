a80822ba469a53ff348f93a16c462061600a402d
ilyabrower <ilia.brauer@semrush.com>
2025-01-27 16:26:14 +0100

    [UIK-3165][chore] prepend stories to display source code (#1930)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    Added ci tool to patch stories to show sources
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    Manually
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [X] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] I have updated the documentation accordingly or it's not required.
    - [ ] Unit tests are not broken.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- stories/components/base-trigger/docs/BaseTrigger.docs.stories.tsx
+++ stories/components/base-trigger/docs/BaseTrigger.docs.stories.tsx
@@ -1,15 +1,15 @@
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import LinkTriggerExample from './examples/linktrigger';
 
 import { LinkTriggerTest } from './__tests__/linktrigger.test';
 import { playWrapper } from '../../../utils/playWrapper';
 
-const meta = {
+const meta: Meta = {
   title: 'Components/BaseTrigger/Documentation',
 };
 
-export const LinkTrigger = {
+export const LinkTrigger : StoryObj = {
   render: LinkTriggerExample,
   play: playWrapper(LinkTriggerTest),
 };
