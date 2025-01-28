227318f0834085252923441a51a690a5213a9c6c
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-01-28 17:11:58 +0400

    [stories] removed feedback invalid story

--- stories/patterns/ux-patterns/feedback-rating/tests/FeedbackRating.stories.tsx
+++ stories/patterns/ux-patterns/feedback-rating/tests/FeedbackRating.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import FeedbackRatingWithTitleAndSubtitleExample from './examples/with-title-and-subtitle';
-import FeedbackRatingWithTitleExample from './examples/with_title';
+
 
 const meta: Meta = {
   title: 'Patterns/UX Patterns/FeedbackRating/Tests',
@@ -11,8 +11,5 @@ export const FeedbackRatingWithTitleAndSubtitle: StoryObj = {
   render: FeedbackRatingWithTitleAndSubtitleExample,
 };
 
-export const FeedbackRatingWithTitle: StoryObj = {
-    render: FeedbackRatingWithTitleExample,
-  };
 
 export default meta;
