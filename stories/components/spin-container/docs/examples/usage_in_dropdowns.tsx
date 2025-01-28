51fb40eff92db1524efc9cca441b83e15e65c9ba
faisne <166654065+sheila-semrush@users.noreply.github.com>
2025-01-28 15:44:39 +0100

    [docs] fixed SpinContainer in Dropdown example

--- stories/components/spin-container/docs/examples/usage_in_dropdowns.tsx
+++ stories/components/spin-container/docs/examples/usage_in_dropdowns.tsx
@@ -7,7 +7,6 @@ import Button from '@semcore/button';
 class Demo extends React.PureComponent {
   state = { loading: true, visible: false };
   timer: any;
-  textRef = React.createRef<HTMLDivElement>();
 
   componentWillUnmount() {
     clearTimeout(this.timer);
@@ -19,7 +18,7 @@ class Demo extends React.PureComponent {
       clearTimeout(this.timer);
       this.setState({ loading: true });
       this.timer = setTimeout(() => {
-        this.setState({ loading: false }, () => this.textRef.current?.focus());
+        this.setState({ loading: false });
       }, 1500);
     }
   };
@@ -32,14 +31,9 @@ class Demo extends React.PureComponent {
         <Dropdown.Trigger tag={Button} id='dropdown-trigger'>
           Dropdown with SpinContainer
         </Dropdown.Trigger>
-        <Dropdown.Popper
-          w={290}
-          p={4}
-          aria-labelledby='dropdown-trigger'
-          tabIndex={loading ? 0 : -1}
-        >
-          <SpinContainer loading={loading} size={'xl'}>
-            <Text size={200} ref={this.textRef} tabIndex={0} style={{ outline: '0' }}>
+        <Dropdown.Popper w={290} p={4} aria-labelledby='dropdown-trigger'>
+          <SpinContainer loading={loading} size={'xl'} aria-live='polite' role='status'>
+            <Text size={200}>
               The Intergalactic Design System uses two sets of design tokens: basic and semantic.
               Basic tokens set the main colors, while semantic tokens build on them. Changing the
               basic tokens lets you create new themes.
