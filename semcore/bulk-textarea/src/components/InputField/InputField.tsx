10c7296d2f9adf573f12874268ec6bb45a9f6348
ilia.brauer <ilia.brauer@semrush.com>
2025-01-28 13:28:24 +0100

    [bulk-textarea] UIK-3264 + UIK-3265

--- semcore/bulk-textarea/src/components/InputField/InputField.tsx
+++ semcore/bulk-textarea/src/components/InputField/InputField.tsx
@@ -160,7 +160,9 @@ class InputField extends Component<InputFieldProps, {}, State, typeof InputField
 
         setTimeout(() => {
           const newNode = this.textarea.childNodes.item(keyboardRowIndex);
-          this.setErrorIndex(newNode);
+          if (newNode) {
+            this.setErrorIndex(newNode);
+          }
         }, 0); // need this timeout to update errorIndex to the new (usually empty) line
       }
 
@@ -536,10 +538,6 @@ class InputField extends Component<InputFieldProps, {}, State, typeof InputField
 
     if (this.asProps.showErrors) {
       this.toggleErrorsPopperByKeyboard(150);
-      const focusedParagraph = document.getSelection()?.focusNode;
-      if (focusedParagraph instanceof HTMLParagraphElement) {
-        this.setErrorIndex(focusedParagraph);
-      }
     } else {
       this.toggleErrorsPopper('keyboardRowIndex', this.textarea);
     }
