bbb8a183bff3ddd4d0bf77cc926b2ac44dc21ef5
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-01-29 12:08:10 +0400

    [stories] update story name

---
+++ stories/components/bulk-textarea/docs/BulkTextarea.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import BulkTextarea from '@semcore/bulk-textarea';
+
+import BasicUsageExample from './examples/basic-usage';
+
+const meta: Meta<typeof BulkTextarea> = {
+  title: 'Components/BulkTextarea/Documentation',
+  component: BulkTextarea,
+};
+
+export default meta;
+
+export const BasicUsage: StoryObj<typeof BulkTextarea> = {
+  args: {
+    ofRows: 30,
+    size: 'm',
+    rowsDelimiters: [','],
+    readonly: false,
+    disabled: false,
+    placeholder: 'Placeholder',
+    minRows: 2,
+    maxRows: 10,
+    validateOn: [
+      // 'blur',
+      'blurRow',
+    ],
+    pasteProps: {
+      delimiter: '\n',
+      skipEmptyRows: true,
+      rowProcessing: (row) => {
+        return row.replace(/http:\/\//, '');
+      },
+    },
+    rowProcessing: (row) => {
+      return row.replace(/http:\/\//, '');
+    },
+  },
+  render: BasicUsageExample,
+};
