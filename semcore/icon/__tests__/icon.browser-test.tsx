ae13cf1ad72b3f07d78bea20dcf5ca9f59182dc3
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-02-04 15:55:50 +0400

    [illustration] added tests and test dev dependency

--- semcore/icon/__tests__/icon.browser-test.tsx
+++ semcore/icon/__tests__/icon.browser-test.tsx
@@ -2,7 +2,6 @@ import { expect, test } from '@semcore/testing-utils/playwright';
 import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 
 test.describe('Icons size rendering and attributes', () => {
-
   test('Icon in button', async ({ page }) => {
     const standPath = 'stories/components/icon/tests/examples/icon_in_buttons.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
@@ -13,7 +12,6 @@ test.describe('Icons size rendering and attributes', () => {
     const firstIcon = svgs.first();
     await expect(firstIcon).toHaveAttribute('aria-hidden', 'true');
     await expect(firstIcon).toHaveAttribute('tabindex', '-1');
-
   });
 
   test('Icons in buttonLink', async ({ page }) => {
@@ -21,7 +19,6 @@ test.describe('Icons size rendering and attributes', () => {
     const htmlContent = await e2eStandToHtml(standPath, 'en');
     await page.setContent(htmlContent);
     await expect(page).toHaveScreenshot();
-
   });
 
   test('Icon in buttonLink', async ({ page }) => {
@@ -29,7 +26,6 @@ test.describe('Icons size rendering and attributes', () => {
     const htmlContent = await e2eStandToHtml(standPath, 'en');
     await page.setContent(htmlContent);
     await expect(page).toHaveScreenshot();
-
   });
 
   test('Icon attributes in interactive', async ({ page }) => {
@@ -45,8 +41,5 @@ test.describe('Icons size rendering and attributes', () => {
     await expect(svgElement).toHaveAttribute('viewBox', '0 0 16 16');
     await expect(svgElement).toHaveAttribute('focusable', 'true');
     await expect(svgElement).toHaveAttribute('data-group', 'm');
-
   });
-
-
 });
