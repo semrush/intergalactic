91172dd213ccc0c9efe3559deb34ed323edfc533
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-02-07 12:34:08 +0400

    [neighbor-location] update tests

--- semcore/neighbor-location/__tests__/neighbor-location.browser-test.tsx
+++ semcore/neighbor-location/__tests__/neighbor-location.browser-test.tsx
@@ -2,42 +2,40 @@ import { expect, test } from '@semcore/testing-utils/playwright';
 import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 
 test.describe('Verify component added as wrapper', () => {
-  test('Navigation between wrapped components by keyboard', async ({ page }) => {
+  test('Verofy attributes and Navigation between wrapped components by keyboard', async ({ page }) => {
     const standPath =
-      'stories/components/heighbor-location/tests/examples/wrapping-test-combimation.tsx';
-    const htmlContent = await e2eStandToHtml(standPath, 'en');
-    await page.setContent(htmlContent);
-  });
-
-  test('Illustration with color', async ({ page }) => {
-    const standPath = 'stories/components/illustration/docs/examples/custom-color.tsx';
-    const htmlContent = await e2eStandToHtml(standPath, 'en');
-    await page.setContent(htmlContent);
-    await expect(page).toHaveScreenshot();
-  });
-
-  test('Illustration sizes', async ({ page }) => {
-    const standPath = 'stories/components/illustration/tests/examples/illustration_sizes.tsx';
+      'stories/components/neighbor-location/tests/examples/wraping-test-combination.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
     await page.setContent(htmlContent);
 
-    const expectedDimensions = [
-      { width: 40, height: 40 },
-      { width: 80, height: 80 },
-      { width: 140, height: 140 },
-    ];
+    // Access the first element of the group
+    const firstgroup = page.locator('[data-ui-name="NeighborLocation"]').first();
+    await expect(firstgroup).toHaveAttribute('role', 'group');
+    const buttons = await firstgroup.locator('button').all();
+    expect(buttons.length).toBe(3);  
 
-    const svgs = page.locator('svg');
-    const svgCount = await svgs.count();
+// Check if the first button contains the word 'first' in its class
+const firstButtonClass = await buttons[0].getAttribute('class');
+expect(firstButtonClass).toContain('right');  
+await page.keyboard.press('Tab');
+await expect(buttons[0]).toBeFocused();
 
-    for (let i = 0; i < svgCount; i++) {
-      const svg = svgs.nth(i);
+const secondButtonClass = await buttons[1].getAttribute('class');
+expect(secondButtonClass).toContain('both'); 
+await page.keyboard.press('Tab');
+await expect(buttons[1]).toBeFocused();
 
-      const width = await svg.getAttribute('width');
-      const height = await svg.getAttribute('height');
+const thirdButtonClass = await buttons[2].getAttribute('class');
+expect(thirdButtonClass).toContain('left'); 
+await page.keyboard.press('Tab');
+await expect(buttons[2]).toBeFocused();
 
-      expect(Number(width)).toBe(expectedDimensions[i]?.width);
-      expect(Number(height)).toBe(expectedDimensions[i]?.height);
-    }
+
+
+const secondgroup = page.locator('[data-ui-name="NeighborLocation"]').nth(1);
+    await expect(secondgroup).toHaveAttribute('role','group');
+
   });
+
+  
 });
