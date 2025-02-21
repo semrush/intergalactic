056554ac4e3d186084c8132c05a0f6f2b3bf4df1
Sheila <166654065+sheila-semrush@users.noreply.github.com>
2025-02-21 10:07:33 +0100

    [UIK-2538][website] Icons page a11y (#1949)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Improved a11y of the Icons page (full list is in the task).
    
    ## How has this been tested?
    
    Manually
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: ilyabrower <ilia.brauer@semrush.com>
    Co-authored-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- website/docs/style/icon/icon-group.jsx
+++ website/docs/style/icon/icon-group.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
-
-import Tooltip from 'intergalactic/tooltip';
-import { Col, Row } from 'intergalactic/grid';
+import Tooltip from '@semcore/tooltip';
+import { Flex } from '@semcore/flex-box';
 import Pills from '@semcore/pills';
-import OutsideClick from 'intergalactic/outside-click';
+import SidePanel from '@semcore/side-panel';
 import Copy from '@components/Copy';
 import styles from './styles.module.css';
 
-const iconLetterToNumericSize = { l: 24, m: 16 };
+const iconDimensions = {
+  common: { l: 24, m: 16 },
+  pay: { l: { w: 32, h: 24 }, m: { w: 21, h: 16 } },
+  external: { 20: 20 },
+};
 
-const DownloadIconButton = ({ size, name, action, dataIcons, icon: Icon }) => {
-  const iconSize = iconLetterToNumericSize[size] || '';
+const DownloadIconButton = ({ size, name, action, iconData, icon: Icon }) => {
   let nameSvg = `${name}/${size}`;
-
-  const filterIcons = dataIcons.icons.filter((icon) => icon.name === name)[0];
-  const groupName = filterIcons.group.toLowerCase();
+  const groupName = iconData.group.toLowerCase();
+  const iconSize = iconDimensions[groupName]
+    ? iconDimensions[groupName][size]
+    : iconDimensions.common[size];
   const haveGroupName = ['pay', 'external', 'color'].includes(groupName);
   let includeGroupName = haveGroupName ? `/${groupName}` : '';
 
   const getImportText = React.useCallback(() => {
     const lib = '@semcore/ui';
-    const haveSizeIcon = filterIcons.size.length > 1;
+    const haveSizeIcon = iconData.size.length > 1;
     const includeName = haveSizeIcon ? `${name}${size.toUpperCase()}` : name;
     const includeSize = haveSizeIcon ? `/${size}` : '';
     const importText = `import ${includeName} from '${lib}/icon${includeGroupName}/${name}${includeSize}'`;
 
     return importText;
-  }, [name, size, filterIcons.size]);
+  }, [name, size, iconData.size]);
 
   if (action === 'download') {
     includeGroupName = haveGroupName ? `${groupName}` : 'icon';
@@ -37,129 +40,136 @@ const DownloadIconButton = ({ size, name, action, dataIcons, icon: Icon }) => {
 
     const url = `semcore/icon/svg/${includeGroupName}/${nameSvg}.svg`;
     return (
-      <Tooltip title='Download!'>
-        <div className={styles.previewChangeIcon}>
-          <a
-            className={styles.areaLink}
-            rel='noopener noreferrer'
-            download={url}
-            target='_blank'
-            href={`https://github.com/semrush/intergalactic/raw/master/${url}?inline=false`}
-            data-container='body'
-            data-original-title='Download'
-          />
+      <Tooltip title='Download'>
+        <a
+          className={styles.previewChangeIcon}
+          rel='noopener noreferrer'
+          download={url}
+          target='_blank'
+          href={`https://github.com/semrush/intergalactic/raw/master/${url}?inline=false`}
+          data-container='body'
+          data-original-title='Download'
+        >
           <Icon width={20} height={20} />
           <span className={styles.iconSizes}>
             <span className={styles.iconSizeTitle}>{size.toUpperCase()}</span>
-            {` (${iconSize}x${iconSize}px)`}
+            {` (${iconSize.w || iconSize}x${iconSize.h || iconSize}px)`}
           </span>
-        </div>
+        </a>
       </Tooltip>
     );
   }
 
   return (
-    <Copy copiedToast='Copied!' toCopy={getImportText} trigger='click'>
-      <div className={styles.previewChangeIcon}>
+    <Copy toCopy={getImportText} trigger='click' title='Copy import'>
+      <button type='button' className={styles.previewChangeIcon}>
         <Icon width={20} height={20} />
         <span className={styles.iconSizes}>
           <span className={styles.iconSizeTitle}>{size.toUpperCase()}</span>
-          {` (${iconSize}x${iconSize}px)`}
+          {` (${iconSize.w || iconSize}x${iconSize.h || iconSize}px)`}
         </span>
-      </div>
+      </button>
     </Copy>
   );
 };
 
-const IconDetailsPanel = ({ name, json: dataIcons, icon: Icon, onClose }) => {
+export const IconDetailsPanel = ({ name, visible, onClose }) => {
   const [action, setAction] = React.useState('copy');
-  const ref = React.useRef(null);
+  const { json, icons } = React.useContext(Context);
+  const iconData = json.icons.filter((icon) => icon.name === name)[0];
 
   return (
-    <div className={styles.panelIcon} ref={ref}>
-      <OutsideClick onOutsideClick={onClose} excludeRefs={[ref]} />
-      <Row>
-        <Col style={{ display: 'flex', flexDirection: 'column', marginRight: 40 }}>
-          <b>{name}</b>
-          <Pills value={action} style={{ marginTop: 13 }} onChange={setAction}>
-            <Pills.Item value='copy'>Copy import</Pills.Item>
-            <Pills.Item value='download'>Download SVG</Pills.Item>
-          </Pills>
-        </Col>
-        <Col>
-          <div className={styles.panelIconList}>
-            {dataIcons.icons
-              .filter((icon) => icon.name === name)[0]
-              .size.map((size) => {
+    <SidePanel visible={visible} placement='bottom' onClose={onClose}>
+      <SidePanel.Panel
+        id={`${name}-dialog`}
+        aria-label={`Get ${name} icon`}
+        aria-modal={false}
+        zIndex='var(--intergalactic-z-index-modal)'
+      >
+        <Flex gap={10} justifyContent='center'>
+          <Flex direction='column'>
+            <b>{name}</b>
+            <Pills value={action} mt={3} onChange={setAction} aria-label={`Get ${name} icon`}>
+              <Pills.Item value='copy'>Copy import</Pills.Item>
+              <Pills.Item value='download'>Download SVG</Pills.Item>
+            </Pills>
+          </Flex>
+          <Flex gap={6}>
+            {visible &&
+              iconData.size.map((size) => {
                 return (
                   <DownloadIconButton
                     key={size}
                     size={size}
                     name={name}
                     action={action}
-                    dataIcons={dataIcons}
-                    icon={Icon}
+                    iconData={iconData}
+                    icon={icons[name]}
                   />
                 );
               })}
-          </div>
-        </Col>
-      </Row>
-    </div>
+          </Flex>
+        </Flex>
+      </SidePanel.Panel>
+    </SidePanel>
   );
 };
 
-export const ListIcons = ({ data, icons, json }) => {
-  const [showPanel, setShowPanel] = React.useState(null);
+export const ListIcons = ({ data, ...props }) => {
+  const { icons, selectedIcon, setSelectedIcon } = React.useContext(Context);
 
   return (
-    <div className={styles.list}>
-      {showPanel && (
-        <IconDetailsPanel
-          name={showPanel}
-          icon={icons[showPanel]}
-          json={json}
-          onClose={() => setShowPanel(null)}
-        />
-      )}
+    <ul
+      className={styles.list}
+      aria-labelledby={props['aria-labelledby']}
+      aria-label={props['aria-label']}
+    >
       {data.map((icon) => {
         const Icon = icons[icon.name];
         if (!Icon) {
-          throw new Error(`Icon ${icon.name} was not founded in import from @icons`);
+          throw new Error(`Icon ${icon.name} not found in import from @icons`);
         }
 
         return (
-          // biome-ignore lint/a11y/useKeyWithClickEvents:
-          <div
-            className={styles.previewIcon}
-            tabIndex={0}
-            key={icon.name}
-            data-name={icon.name}
-            onClick={() => setShowPanel(icon.name)}
-          >
-            <Icon width={20} height={20} />
-            <span>{icon.name}</span>
-          </div>
+          <li className={styles.previewIcon} key={icon.name}>
+            <button
+              type='button'
+              aria-haspopup='dialog'
+              aria-expanded={selectedIcon === icon.name}
+              aria-controls={selectedIcon === icon.name ? `${icon.name}-dialog` : undefined}
+              onClick={() => {
+                setSelectedIcon(icon.name);
+              }}
+              data-id={icon.name}
+              data-name='PanelTrigger'
+            >
+              <Icon width={20} height={20} />
+              {icon.name}
+            </button>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 };
 
 const Context = React.createContext();
 
-export const IconGroups = ({ children, ...props }) => {
-  return <Context.Provider value={props} children={children} />;
-};
+export const IconGroups = React.forwardRef(({ children, ...props }, forwardedRef) => (
+  <div className={styles.contextWrapper} ref={forwardedRef}>
+    <Context.Provider value={props} children={children} />
+  </div>
+));
+
 export default function ({ title }) {
-  const context = React.useContext(Context);
-  const dataIcons = context.json;
-  const filterIcons = dataIcons.icons.filter((icon) => icon.group === title);
+  const { json } = React.useContext(Context);
+  const filterIcons = json.icons.filter((icon) => icon.group === title);
+  const id = title.replace(' ', '-');
 
   return (
     <div className={styles.section}>
-      <h3>{title}</h3>
-      <ListIcons data={filterIcons} {...context} />
+      <h3 id={`${id}-heading`}>{title}</h3>
+      <ListIcons data={filterIcons} aria-labelledby={`${id}-heading`} />
     </div>
   );
 }
