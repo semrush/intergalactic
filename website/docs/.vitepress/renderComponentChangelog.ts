fb9e6641777ef71ddf5f66ea883a6a838eec8096
ilia.brauer <ilia.brauer@semrush.com>
2025-02-25 12:59:14 +0100

    [website] added nice view for changelogs

--- website/docs/.vitepress/renderComponentChangelog.ts
+++ website/docs/.vitepress/renderComponentChangelog.ts
@@ -23,8 +23,52 @@ export const renderComponentChangelog = (tokenList: any[], index: number) => {
         }
       }
       const changelogBody = changelogFile.substring(changelogFile.indexOf('##'));
+      const changelogItems = changelogBody.split('## [');
+      const changelogs = [];
+      const updateVersionChangelogs = [];
 
-      return markdownRenderer.render(changelogBody);
+      changelogItems.forEach((item) => {
+        if (item) {
+          const itemRows = item.split('\n');
+          const versionAndDate = itemRows[0]?.trim().replace(']', '').replace('[', '') ?? '';
+          const [version, date] = versionAndDate.split(' - ');
+          const niceDate = new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          }).format(new Date(date));
+
+          if (
+            item.includes('- Version minor update due to children dependencies') ||
+            item.includes('- Version patch update due to children dependencies') ||
+            item.includes('- Version preminor update due to children dependencies') ||
+            item.includes('- Version prepatch update due to children dependencies')
+          ) {
+            itemRows[0] = `### ${version} (${niceDate})`;
+            updateVersionChangelogs.push(itemRows.filter((row) => !row.includes('Changed')));
+          } else {
+            itemRows[0] = `## ${version} (${niceDate})`;
+
+            if (updateVersionChangelogs.length > 0) {
+              changelogs.push(`\n\n<div class="collapsed-versions">\n\n`);
+              changelogs.push(updateVersionChangelogs[0][0].replace('###', '##'));
+              changelogs.push(
+                `\n\n::: details ${updateVersionChangelogs.length} release${
+                  updateVersionChangelogs.length > 1 ? 's' : ''
+                } with dependency updates only`,
+              );
+              changelogs.push(updateVersionChangelogs.map((item) => item.join('\n')).join('\n'));
+              changelogs.push(':::\n</div>\n\n');
+
+              updateVersionChangelogs.length = 0;
+            }
+
+            changelogs.push(...itemRows);
+          }
+        }
+      });
+
+      return markdownRenderer.render(changelogs.join('\n'));
     }
     return [];
   };
--- website/docs/components/dot/dot-changelog.md
+++ website/docs/components/dot/dot-changelog.md
@@ -4,4 +4,26 @@ fileSource: dot
 tabs: Design('dot'), A11y('dot-a11y'), API('dot-api'), Example('dot-code'), Changelog('dot-changelog')
 ---
 
+<style>
+    .custom-block.details {
+        margin: 0;
+        background: transparent;
+        padding: 0;
+    }
+    .custom-block.details summary {
+        font-weight: 500;
+        margin: 0;
+        color: var(--vp-c-text-2);
+    }
+    .collapsed-versions {
+        background: var(--vp-custom-block-info-bg);
+        padding: 16px 16px;
+        border-radius: 6px;
+        margin: 32px 0;
+    }
+    .collapsed-versions h2 {
+        margin: 0 0 12px;
+    }
+</style>
+
 ::: changelog dot :::
