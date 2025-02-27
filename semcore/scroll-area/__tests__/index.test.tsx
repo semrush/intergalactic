912e541948bd7396007d1f72232fdf9a5ad3bcb4
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-02-27 20:49:30 +0400

    [scroll-area] upd lint error in test

--- semcore/scroll-area/__tests__/index.test.tsx
+++ semcore/scroll-area/__tests__/index.test.tsx
@@ -2,16 +2,10 @@ import React from 'react';
 import { snapshot } from '@semcore/testing-utils/snapshot';
 import * as sharedTests from '@semcore/testing-utils/shared-tests';
 import { expect, test, describe, beforeEach, vi } from '@semcore/testing-utils/vitest';
-import {
-  cleanup,
-  render,
-  fireEvent,
-  waitFor,
-} from '@semcore/testing-utils/testing-library';
-
+import { cleanup, render, fireEvent, waitFor } from '@semcore/testing-utils/testing-library';
 
 const { shouldSupportClassName, shouldSupportRef } = sharedTests;
-import ScrollArea, { eventCalculate }  from '../src';
+import ScrollArea, { eventCalculate } from '../src';
 
 describe('ScrollArea', () => {
   beforeEach(cleanup);
