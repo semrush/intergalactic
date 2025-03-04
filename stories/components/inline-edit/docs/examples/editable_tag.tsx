6369635b6de4f11f973cf0be96cb1619d9e07703
ilia.brauer <ilia.brauer@semrush.com>
2025-03-04 20:36:26 +0100

    [stories] fixed editable tag example

--- stories/components/inline-edit/docs/examples/editable_tag.tsx
+++ stories/components/inline-edit/docs/examples/editable_tag.tsx
@@ -6,6 +6,7 @@ import {TagContainer} from '@semcore/tag';
 const Demo = () => {
   const [value, setValue] = React.useState('Default tag');
   const [editable, setEditable] = React.useState(false);
+  const tagRef = React.useRef<HTMLElement | null>(null);
 
   const handleValue = (value: string) => {
     setEditable(false);
@@ -13,11 +14,17 @@ const Demo = () => {
   };
   const resetEditable = () => setEditable(false);
 
+  const handleTagContainerFocus = (e) => {
+    if (e.target === e.currentTarget) {
+      tagRef.current?.focus();
+    }
+  };
+
   return (
     <>
       <InlineEdit editable={editable} onEditableChange={setEditable}>
-        <InlineEdit.View pr={2} tag={TagContainer} interactive size='l' role={''} tabIndex={-1}>
-          <TagContainer.Tag>{value}</TagContainer.Tag>
+        <InlineEdit.View pr={2} tag={TagContainer} interactive size='l' role={''} tabIndex={-1} onFocus={handleTagContainerFocus}>
+          <TagContainer.Tag ref={tagRef}>{value}</TagContainer.Tag>
           <TagContainer.Close onClick={(e: React.SyntheticEvent) => e.stopPropagation()} />
         </InlineEdit.View>
         <InlineEdit.Edit>
