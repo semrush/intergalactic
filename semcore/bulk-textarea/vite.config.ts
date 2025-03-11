6c9bd84cc1977a0c145e2946fd71c019b592fbb2
ilia.brauer <ilia.brauer@semrush.com>
2025-03-11 12:33:13 +0100

    [chore] added vite-esm for all components

---
+++ semcore/bulk-textarea/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, mergeConfig } from 'vite';
+
+import viteConfig from '../../commonVite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    build: {
+      lib: {
+        entry: './src/index.ts',
+      },
+      rollupOptions: {
+        external: [
+          'react',
+          'react-dom',
+          'react/jsx-runtime',
+          /@babel\/runtime\/*/,
+          /@semcore\/*/,
+          'classnames',
+          'dompurify',
+        ],
+      },
+    },
+  }),
+);
