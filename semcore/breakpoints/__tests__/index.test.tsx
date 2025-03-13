2ad4c56abf22bb368585de07b6febac02a02d55b
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-03-13 17:19:00 +0400

    [breakpoints] upd deps test

--- semcore/breakpoints/__tests__/index.test.tsx
+++ semcore/breakpoints/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import { describe } from '@semcore/testing-utils/vitest';
 import { runDependencyCheckTests } from '@semcore/testing-utils/shared-tests';
 
-describe('breadcrumbs Dependency imports', () => {
-  runDependencyCheckTests('breadcrumbs');
+describe('Breakpoints Dependency imports', () => {
+  runDependencyCheckTests('breakpoints');
 });
