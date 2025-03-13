c9d0e3a0956c0fbfa9d1ead5189a49cf40bbd958
ilia.brauer <ilia.brauer@semrush.com>
2025-03-13 14:03:22 +0100

    [builder] fixed babel config for esm build for utils

--- tools/builder/mjs-imports-babelrc.js
+++ tools/builder/mjs-imports-babelrc.js
@@ -1,11 +1,12 @@
 const defaultConf = require('@semcore/babel-preset-ui/.babelrc');
 
 module.exports = function (babel, opts = {}) {
-  const defaultConfig = defaultConf(babel, {...opts, isEsm: true});
+  const defaultConfig = defaultConf(babel, { ...opts, isEsm: true });
 
   defaultConfig.plugins.push([
-    'replace-import-extension', { extMapping: { '.json': '.json', '': '.mjs', '.css': '.css' } }
-  ])
+    'replace-import-extension',
+    { extMapping: { '.json': '.json', '': '.mjs', '.css': '.css' } },
+  ]);
 
   return defaultConfig;
 };
