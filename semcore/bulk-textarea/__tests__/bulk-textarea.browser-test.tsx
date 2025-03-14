214789f8b2e67b398f4355fec6e5b11b1e803135
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-03-14 21:54:54 +0400

    [bulk-textarea] turn off one test for webkit because not stable

--- semcore/bulk-textarea/__tests__/bulk-textarea.browser-test.tsx
+++ semcore/bulk-textarea/__tests__/bulk-textarea.browser-test.tsx
@@ -789,7 +789,7 @@ test.describe('Common error On - Error tooltips', () => {
   });
 
   test('Verify tooltips when fixing errors', async ({ page, browserName }) => {
-    if (browserName==='webkit') return; //not stable for webkit
+    if (browserName === 'webkit') return; //not stable for webkit
     const standPath =
       'stories/components/bulk-textarea/tests/examples/validate-blur-base-example.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
