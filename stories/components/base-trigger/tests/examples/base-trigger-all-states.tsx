3c1d90df5e5bc48047f7735d7c98ef243875e292
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-03-20 17:53:09 +0400

    [base-trigger] upd tests

--- stories/components/base-trigger/tests/examples/base-trigger-all-states.tsx
+++ stories/components/base-trigger/tests/examples/base-trigger-all-states.tsx
@@ -54,6 +54,45 @@ const Demo = () => (
           </BaseTrigger>
         </Dropdown.Trigger>
       </Dropdown>
+
+
+      <DropdownMenu>
+      <DropdownMenu.Trigger tag={LinkTrigger}>
+        <LinkTrigger.Text w={150} tag={Text} size={400}>
+          <Ellipsis trim='middle'>Dat First List</Ellipsis>
+        </LinkTrigger.Text>
+      </DropdownMenu.Trigger>
+      <br />
+      <br />
+      <br />
+      <DropdownMenu.Trigger tag={LinkTrigger}>
+        <LinkTrigger.Text w={150} tag={Text} size={400}>
+          <Ellipsis trim='middle'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque autem
+            commodi, doloribus ex harum inventore modi praesentium quam ratione reprehenderit rerum
+            tempore voluptas. Aliquam eos expedita illo quasi unde!
+          </Ellipsis>
+        </LinkTrigger.Text>
+      </DropdownMenu.Trigger>
+      <br />
+      <br />
+      <br />
+      <DropdownMenu.Trigger tag={LinkTrigger}>
+        <LinkTrigger.Text w={150} tag={Text} size={400}>
+          <Ellipsis>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque autem
+            commodi, doloribus ex harum inventore modi praesentium quam ratione reprehenderit rerum
+            tempore voluptas. Aliquam eos expedita illo quasi unde!
+          </Ellipsis>
+        </LinkTrigger.Text>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Menu>
+        <DropdownMenu.Item>Save</DropdownMenu.Item>
+        <DropdownMenu.Item>Rename</DropdownMenu.Item>
+        <DropdownMenu.Item>Download</DropdownMenu.Item>
+        <DropdownMenu.Item>Delete</DropdownMenu.Item>
+      </DropdownMenu.Menu>
+    </DropdownMenu>
     </Flex>
   </Flex>
 );
