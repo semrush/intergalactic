8a8d8470e6a3cfc97082c4bac0f2400955cdbac9
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-04-22 14:11:59 +0400

    [spin] update tests

--- semcore/spin/__tests__/index.test.tsx
+++ semcore/spin/__tests__/index.test.tsx
@@ -1,66 +1,8 @@
 import * as React from 'react';
-import { snapshot } from '@semcore/testing-utils/snapshot';
-import { expect, test, describe, beforeEach } from '@semcore/testing-utils/vitest';
-import Spin from '../src';
-
-import { cleanup } from '@semcore/testing-utils/testing-library';
+import { describe } from '@semcore/testing-utils/vitest';
 
 import { runDependencyCheckTests } from '@semcore/testing-utils/shared-tests';
 
 describe('spin Dependency imports', () => {
   runDependencyCheckTests('spin');
-});
-
-describe('Spin', () => {
-  beforeEach(cleanup);
-
-  test.concurrent('Renders correctly', async ({ task }) => {
-    const component = <Spin />;
-    await expect(await snapshot(component)).toMatchImageSnapshot(task);
-  });
-
-  test.concurrent('Should support centered', async ({ task }) => {
-    const component = (
-      <div style={{ display: 'flex', width: 200, height: 200 }}>
-        <Spin centered />
-      </div>
-    );
-    await expect(await snapshot(component)).toMatchImageSnapshot(task);
-  });
-
-  test.concurrent('Should support theme', async ({ task }) => {
-    const component = (
-      <div style={{ background: '#979797' }}>
-        <Spin theme='dark' />
-        <Spin theme='invert' />
-      </div>
-    );
-    await expect(await snapshot(component)).toMatchImageSnapshot(task);
-  });
-
-  test.concurrent('Should support custom theme', async ({ task }) => {
-    const component = (
-      <>
-        <Spin theme='blanchedalmond' />
-        <Spin theme='#3eeb4c' />
-        <Spin theme='dark-violet' />
-      </>
-    );
-    await expect(await snapshot(component)).toMatchImageSnapshot(task);
-  });
-
-  test.concurrent('Should support size', async ({ task }) => {
-    const component = (
-      <>
-        <Spin size='xs' />
-        <Spin size='s' />
-        <Spin size='m' />
-        <Spin size='l' />
-        <Spin size='xl' />
-        <Spin size='xxl' />
-      </>
-    );
-
-    await expect(await snapshot(component)).toMatchImageSnapshot(task);
-  });
 });
