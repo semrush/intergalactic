edaf1e5fc372651ac0b869745d9cc68b4427291e
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-04-24 17:38:07 +0400

    [slider] fix example

--- stories/components/slider/docs/examples/numeric_slider.tsx
+++ stories/components/slider/docs/examples/numeric_slider.tsx
@@ -11,7 +11,7 @@ const Demo = () => {
   const min = 10;
   const max = 100;
 
-  const handleInput = (value) => {
+  const handleInput = (value:any) => {
     if (!!value && (value > max || value < min)) {
       setError('Please enter a valid value');
       setValue(value);
