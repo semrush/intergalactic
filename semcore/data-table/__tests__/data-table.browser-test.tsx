1971f3bf184b31c4863531dfa411b1a22665b259
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-05-07 13:32:10 +0400

    [data-table] add new perf test

--- semcore/data-table/__tests__/data-table.browser-test.tsx
+++ semcore/data-table/__tests__/data-table.browser-test.tsx
@@ -62,7 +62,7 @@ test.describe('DataTable', () => {
     const tooltipShownTime = await page.evaluate(() => performance.now());
     const duration = tooltipShownTime - renderTime;
 
-    console.log(`Tooltip render time: ${duration.toFixed(2)}ms`);
+    //console.log(`Tooltip render time: ${duration.toFixed(2)}ms`);
 
     // expect(duration).toBeGreaterThan(0);
     // expect(duration).toBeLessThan(100);
@@ -75,23 +75,21 @@ test.describe('DataTable', () => {
     await page.setContent(htmlContent);
 
     const start = await page.evaluate(() => performance.now());
-  
+
     const rows = page.locator('[role="row"]');
-  
-      const firstRowTime = await page.evaluate(() => performance.now());
-  
+
+    const firstRowTime = await page.evaluate(() => performance.now());
+
     const count = await rows.count();
-   // await expect(rows.nth(count - 1)).toBeVisible({ timeout: 2000 });
+    // await expect(rows.nth(count - 1)).toBeVisible({ timeout: 2000 });
     const lastRowTime = await page.evaluate(() => performance.now());
-  
+
     const firstRowDuration = firstRowTime - start;
     const fullRenderDuration = lastRowTime - start;
     const rangeDuration = lastRowTime - firstRowTime;
-  
+
     // console.log(`First row appeared in: ${firstRowDuration.toFixed(2)}ms`);
     // console.log(`Last row appeared in: ${fullRenderDuration.toFixed(2)}ms`);
-    console.log(`Time between first and last row: ${rangeDuration.toFixed(2)}ms`);
-  
+    //console.log(`Time between first and last row: ${rangeDuration.toFixed(2)}ms`);
   });
-  
 });
