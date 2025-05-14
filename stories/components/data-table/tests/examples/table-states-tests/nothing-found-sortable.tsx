a914070ed3dc01553cfb464df4d597e78bd47778
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-05-14 15:39:28 +0400

    [data-table] update test story

--- stories/components/data-table/tests/examples/table-states-tests/nothing-found-sortable.tsx
+++ stories/components/data-table/tests/examples/table-states-tests/nothing-found-sortable.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import { NoData } from '@semcore/widget-empty';
-import Button from '@semcore/button';
 import { DataTable, DataTableSort } from '@semcore/data-table';
 
-type SortableColumn = Exclude<keyof typeof data[0], 'keyword'>;
-const data = [
-    {
-      'keyword/kd/cpc/vol': '',
-    },
-  ];
+type DataRow = {
+  keyword: string;
+  kd: string;
+  cpc: string;
+  vol: string;
+};
+
+
+type SortableColumn = Exclude<keyof DataRow, never>; 
 
 const Demo = () => {
-  const [sort, setSort] = React.useState<DataTableSort<keyof typeof data[0]>>(['keyword/kd/cpc/vol','desc']);
+  const [sort, setSort] = React.useState<DataTableSort<keyof DataRow>>(['keyword', 'desc']);
 
   return (
-
-    <DataTable  data={[]} sort={sort}  aria-label={'Sorting'}
-    columns={[
-        {name: 'keyword', children: 'keyword', sortable: true},
-        {name: 'kd', children: 'KD,%', sortable: true},
-        {name: 'cpc', children: 'CPC', sortable: true},
-        {name: 'vol', children: 'Vol.'}
-    ]}
-    renderCell={() => (
-      <NoData type='nothing-found' my={7} mx='auto'>
-        <Button mt={4}>Clear filters</Button>
-      </NoData>
-    )}
-  />
+    <DataTable
+      data={[]}
+      sort={sort}
+     
+      aria-label="Sorting"
+      columns={[
+        { name: 'keyword', children: 'Keyword', sortable: true },
+        { name: 'kd', children: 'KD,%', sortable: true },
+        { name: 'cpc', children: 'CPC', sortable: true },
+        { name: 'vol', children: 'Vol.' },
+      ]}
+    />
   );
 };
 
