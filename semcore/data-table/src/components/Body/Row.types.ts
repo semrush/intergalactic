27660cfb4f7ad345cfb74421d8bd691a3ab4b47f
faisne <166654065+sheila-semrush@users.noreply.github.com>
2025-05-16 16:25:27 +0200

    [data-table] renamed sideIndents value from l to wide

--- semcore/data-table/src/components/Body/Row.types.ts
+++ semcore/data-table/src/components/Body/Row.types.ts
@@ -52,7 +52,7 @@ export type RowPropsInner = JSX.IntrinsicElements['div'] & {
 
   scrollAreaRef: React.RefObject<HTMLDivElement>;
   uid: string;
-  sideIndents?: 'l';
+  sideIndents?: 'wide';
   getFixedStyle: (
     cell: Pick<DTColumn, 'name' | 'fixed'>,
   ) => [side: 'left' | 'right', style: string | number] | [side: undefined, style: undefined];
