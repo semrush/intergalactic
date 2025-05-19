0aa619de1bc04df10ed7d0fccd4b1de25fa60462
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-05-19 14:58:41 +0400

    [stories] update data-table stories

--- stories/components/data-table/tests/examples/accordion-tests/accrodion-with-select.tsx
+++ stories/components/data-table/tests/examples/accordion-tests/accrodion-with-select.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { DataTable, ACCORDION, DataTableData } from '@semcore/data-table';
-import { ButtonLink } from '@semcore/button';
 import { NoData } from '@semcore/widget-empty';
-import Checkbox from '@semcore/checkbox';
-import { Flex } from '@semcore/flex-box';
-import Button from '@semcore/button';
-import Dropdown from '@semcore/ui/dropdown';
 import Select from '@semcore/select';
-import { Text } from '@semcore/typography';
-import { Hint } from '@semcore/tooltip';
-import InfoM from '@semcore/icon/Info/m';
 
 const options = Array(6)
     .fill('')
@@ -24,101 +16,101 @@ const stopPropagation = (event: React.SyntheticEvent) => {
 }
 
 const CustomSelect = () => {
-  const [isVisible, setIsVisible] = React.useState(false);
-  return (
-      <Select visible={isVisible} onVisibleChange={setIsVisible} mt={2} mr='auto' options={options} placeholder='Select option' id='basic-select'
-              onKeyDown={(e) => {
-                  if ((e.key === 'ArrowDown' || e.key === 'ArrowUp') && !isVisible) {
-                      return false
-                  }
-                  if (e.key.startsWith('Arrow') && isVisible) {
-                      e.stopPropagation();
-                  }
-              }}
-              onClick={stopPropagation}
-      />
-  );
+    const [isVisible, setIsVisible] = React.useState(false);
+    return (
+        <Select visible={isVisible} onVisibleChange={setIsVisible} mt={2} mr='auto' options={options} placeholder='Select option' id='basic-select'
+            onKeyDown={(e) => {
+                if ((e.key === 'ArrowDown' || e.key === 'ArrowUp') && !isVisible) {
+                    return false
+                }
+                if (e.key.startsWith('Arrow') && isVisible) {
+                    e.stopPropagation();
+                }
+            }}
+            onClick={stopPropagation}
+        />
+    );
 };
 
 const Demo = () => {
-  return (
-    <DataTable data={data} aria-label={'Accordion inside table'} h={'100%'}
-      columns={[
-        { name: 'keyword', children: 'Keyword', gtcWidth: 'minmax(60%, 80%)' },
-        {
-          children: 'Organic Sessions',
-          borders: 'both',
-          columns: [
-            { name: 'kd', children: 'KD,%' },
-            { name: 'cpc', children: 'CPC' },
-            { name: 'vol', children: 'Vol.' },
-          ]
-        }
-      ]}
+    return (
+        <DataTable data={data} aria-label={'Accordion inside table'} h={'100%'}
+            columns={[
+                { name: 'keyword', children: 'Keyword', gtcWidth: 'minmax(60%, 80%)' },
+                {
+                    children: 'Organic Sessions',
+                    borders: 'both',
+                    columns: [
+                        { name: 'kd', children: 'KD,%' },
+                        { name: 'cpc', children: 'CPC' },
+                        { name: 'vol', children: 'Vol.' },
+                    ]
+                }
+            ]}
 
-      renderCell={(props) => {
-        if (props.dataKey === 'keyword') {
-          return (<CustomSelect />);
-        }
+            renderCell={(props) => {
+                if (props.dataKey === 'keyword') {
+                    return (<CustomSelect />);
+                }
 
-        if (props.dataKey === 'kd') {
-          return {
-            'data-test-id': 'kd cell',
-          };
-        }
+                if (props.dataKey === 'kd') {
+                    return {
+                        'data-test-id': 'kd cell',
+                    };
+                }
 
-        return props.defaultRender();
-      }}
-    />
-  );
+                return props.defaultRender();
+            }}
+        />
+    );
 };
 
 const ChartExample = () => {
-  return (
-    <NoData type='nothing-found' my={7} mx='auto'>
+    return (
+        <NoData type='nothing-found' my={7} mx='auto'>
 
-    </NoData>
-  );
+        </NoData>
+    );
 };
 
 const data: DataTableData = [
-  {
-    keyword: 'ebay buy',
-    kd: '77.8',
-    cpc: '$1.25',
-    vol: '32,500,000',
-    [ACCORDION]: (<ChartExample />),
-  },
-  {
-    keyword: 'www.ebay.com',
-    kd: '11.2',
-    cpc: '$3.4',
-    vol: {
-      toString: () => '65,457,920',
-      [ACCORDION]: (<ChartExample />),
+    {
+        keyword: 'ebay buy',
+        kd: '77.8',
+        cpc: '$1.25',
+        vol: '32,500,000',
+        [ACCORDION]: (<ChartExample />),
     },
-  },
-  {
-    keyword: 'www.ebay.com',
-    kd: '10',
-    cpc: '$0.65',
-    vol: '47,354,640',
-    [ACCORDION]: (<ChartExample />),
-  },
-  {
-    keyword: 'ebay buy',
-    kd: '-',
-    cpc: '$0',
-    vol: 'n/a',
-    [ACCORDION]: (<ChartExample />),
-  },
-  {
-    keyword: 'ebay buy',
-    kd: '75.89',
-    cpc: '$0',
-    vol: '21,644,290',
-    [ACCORDION]: (<ChartExample />),
-  },
+    {
+        keyword: 'www.ebay.com',
+        kd: '11.2',
+        cpc: '$3.4',
+        vol: {
+            toString: () => '65,457,920',
+            [ACCORDION]: (<ChartExample />),
+        },
+    },
+    {
+        keyword: 'www.ebay.com',
+        kd: '10',
+        cpc: '$0.65',
+        vol: '47,354,640',
+        [ACCORDION]: (<ChartExample />),
+    },
+    {
+        keyword: 'ebay buy',
+        kd: '-',
+        cpc: '$0',
+        vol: 'n/a',
+        [ACCORDION]: (<ChartExample />),
+    },
+    {
+        keyword: 'ebay buy',
+        kd: '75.89',
+        cpc: '$0',
+        vol: '21,644,290',
+        [ACCORDION]: (<ChartExample />),
+    },
 ];
 
 export default Demo;
