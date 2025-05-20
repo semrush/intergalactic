43bb8cf16a631426c3193646d936109016a48e85
ilia.brauer <ilia.brauer@semrush.com>
2025-05-20 13:55:19 +0200

    [stories] added all icons and all illustrations stories

--- stories/components/illustration/tests/illustration.stories.tsx
+++ stories/components/illustration/tests/illustration.stories.tsx
@@ -1,16 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Icon from '@semcore/icon';
-
+import AllIllustrationGeneratedExample from './examples/all-illustration-generated';
 import SizesExample from './examples/illustration_sizes';
 
-const meta: Meta<typeof Icon> = {
+const meta: Meta = {
   title: 'Components/Illustration/Tests',
-  component: Icon,
 };
 
 export default meta;
-type Story = StoryObj<typeof Icon>;
+type Story = StoryObj;
+
+export const AllIllustrationGenerated: Story = {
+  render: AllIllustrationGeneratedExample,
+};
 
 export const Sizes: Story = {
   render: SizesExample,
