90066d0182e8019a477e149277cb6232a02ee912
ilia.brauer <ilia.brauer@semrush.com>
2025-05-21 17:31:01 +0200

    [stories] some type fixes

--- stories/components/data-table/tests/examples/cells-tests/one-merged-cell.tsx
+++ stories/components/data-table/tests/examples/cells-tests/one-merged-cell.tsx
@@ -1,38 +1,17 @@
 import React from 'react';
-import { DataTable, DataTableSort } from '@semcore/data-table';
+import { DataTable } from '@semcore/data-table';
 
 const data = [
     {
       'keyword/kd/cpc/vol': 'One merged ',
     },
   ];
-  type SortableColumn = Exclude<keyof typeof data[0], 'keyword'>;
-
   const Demo = () => {
-    const [sort, setSort] = React.useState<DataTableSort<keyof typeof data[0]>>(['keyword', 'desc']);
-    const sortedData = React.useMemo(
-      () =>
-        [...data].sort((aRow, bRow) => {
-          const [prop, sortDirection] = sort;
-          const a = aRow[prop as SortableColumn];
-          const b = bRow[prop as SortableColumn];
-          if (a === b) return 0;
-          if (sortDirection === 'asc') return a > b ? 1 : -1;
-          else return a > b ? -1 : 1;
-        }),
-      [sort],
-    );
-    const numberFormat = React.useMemo(() => new Intl.NumberFormat('en-US'), []);
-    const currencyFormat = React.useMemo(
-      () => new Intl.NumberFormat('en-US', { currency: 'USD', style: 'currency' }),
-      [],
-    );
-
     return (
       <DataTable
-        data={sortedData}
-        sort={sort}
-        onSortChange={setSort}
+        data={data}
+        // @ts-ignore
+        sort={['keyword', 'desc']}
         aria-label="Sorting"
         columns={[
           { name: 'keyword', children: 'Keyword', sortable: true },
