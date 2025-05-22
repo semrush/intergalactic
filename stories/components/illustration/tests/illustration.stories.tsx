c09ebab7be8b1165fc6264c4e56afa9dba599443
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-05-22 12:52:43 +0400

    [stories] added storybook play test

--- stories/components/illustration/tests/illustration.stories.tsx
+++ stories/components/illustration/tests/illustration.stories.tsx
@@ -3,6 +3,10 @@ import type { Meta, StoryObj } from '@storybook/react';
 import AllIllustrationGeneratedExample from './examples/all-illustration-generated';
 import SizesExample from './examples/illustration_sizes';
 
+import { CheckButtonsIllustrations } from './__tests__/all-allustrations-test.test';
+import { playWrapper } from '../../../utils/playWrapper';
+
+
 const meta: Meta = {
   title: 'Components/Illustration/Tests',
 };
@@ -12,6 +16,8 @@ type Story = StoryObj;
 
 export const AllIllustrationGenerated: Story = {
   render: AllIllustrationGeneratedExample,
+  play: playWrapper(CheckButtonsIllustrations),
+
 };
 
 export const Sizes: Story = {
