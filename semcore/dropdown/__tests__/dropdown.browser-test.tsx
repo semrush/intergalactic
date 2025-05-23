94d3e991fa2945dea851ff7c68d68b547f39f3bd
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-05-23 11:36:23 +0400

    [dropdown] upd tests

--- semcore/dropdown/__tests__/dropdown.browser-test.tsx
+++ semcore/dropdown/__tests__/dropdown.browser-test.tsx
@@ -1,7 +1,6 @@
 import { expect, test } from '@semcore/testing-utils/playwright';
 import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 
-
 test.describe('Dropdown', () => {
   test('Verify keyboard interactios with Basic usage', async ({ page }) => {
     const standPath = 'stories/components/dropdown/docs/examples/basic_usage.tsx';
@@ -12,35 +11,34 @@ test.describe('Dropdown', () => {
     const trigger = page.locator('[data-ui-name="Dropdown.Trigger"]');
     const popper = page.locator('[data-ui-name="Dropdown.Popper"]');
 
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('ArrowUp');
-      await page.keyboard.press('ArrowDown');
-      await expect(trigger).toBeFocused();
-      await expect(popper).toBeHidden();
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('ArrowUp');
+    await page.keyboard.press('ArrowDown');
+    await expect(trigger).toBeFocused();
+    await expect(popper).toBeHidden();
 
-      await page.keyboard.press('Enter');
-      await page.waitForTimeout(200);
-      await page.keyboard.press('ArrowUp');
-      await page.keyboard.press('ArrowDown');
-      await expect(trigger).not.toBeFocused();
-      await expect(popper).toBeFocused();
-      //snapshot
-    
-      await page.keyboard.press('Escape');
-      await page.waitForTimeout(200);
-      await expect(trigger).toBeFocused();
-      await expect(popper).toBeHidden();
+    await page.keyboard.press('Enter');
+    await page.waitForTimeout(200);
+    await page.keyboard.press('ArrowUp');
+    await page.keyboard.press('ArrowDown');
+    await expect(trigger).not.toBeFocused();
+    await expect(popper).toBeFocused();
+    //snapshot
 
-      await page.keyboard.press('Space');
-      await page.waitForTimeout(200);
-      await expect(trigger).not.toBeFocused();
-      await expect(popper).toBeFocused();
-    
-      await trigger.click();
-      await page.waitForTimeout(200);
-      await expect(trigger).not.toBeFocused();
-      await expect(popper).toBeHidden();
+    await page.keyboard.press('Escape');
+    await page.waitForTimeout(200);
+    await expect(trigger).toBeFocused();
+    await expect(popper).toBeHidden();
 
+    await page.keyboard.press('Space');
+    await page.waitForTimeout(200);
+    await expect(trigger).not.toBeFocused();
+    await expect(popper).toBeFocused();
+
+    await trigger.click();
+    await page.waitForTimeout(200);
+    await expect(trigger).not.toBeFocused();
+    await expect(popper).toBeHidden();
   });
 
   test('Verify mouse interactios with Basic usage', async ({ page }) => {
@@ -54,29 +52,27 @@ test.describe('Dropdown', () => {
 
     await trigger.click();
     await page.waitForTimeout(200);
-  
-      await expect(trigger).not.toBeFocused();
-      await expect(popper).not.toBeFocused();
-      await expect(popper).toBeVisible();
 
-      await trigger.click();
-      await page.waitForTimeout(200);
-      await expect(trigger).toBeFocused();
-      await expect(popper).toBeHidden();
+    await expect(trigger).not.toBeFocused();
+    await expect(popper).not.toBeFocused();
+    await expect(popper).toBeVisible();
 
-      await trigger.click();
-      await popper.click();
+    await trigger.click();
+    await page.waitForTimeout(200);
+    await expect(trigger).toBeFocused();
+    await expect(popper).toBeHidden();
 
-      await expect(trigger).not.toBeFocused();
-      await expect(popper).not.toBeFocused();
-      await expect(popper).toBeVisible();
-    
-      await page.keyboard.press('Escape');
-      await page.waitForTimeout(200);
-      await expect(trigger).toBeFocused();
-      await expect(popper).toBeHidden();
+    await trigger.click();
+    await popper.click();
 
+    await expect(trigger).not.toBeFocused();
+    await expect(popper).not.toBeFocused();
+    await expect(popper).toBeVisible();
 
+    await page.keyboard.press('Escape');
+    await page.waitForTimeout(200);
+    await expect(trigger).toBeFocused();
+    await expect(popper).toBeHidden();
   });
 
   test('Verifu keyboard interactio when Focus interaction enabled', async ({ page }) => {
@@ -87,7 +83,6 @@ test.describe('Dropdown', () => {
 
     const trigger = page.locator('[data-ui-name="Dropdown.Trigger"]');
     const popper = page.locator('[data-ui-name="Dropdown.Popper"]');
-
 
     await page.keyboard.press('Tab');
     await page.keyboard.press('ArrowUp');
@@ -109,12 +104,8 @@ test.describe('Dropdown', () => {
     await expect(trigger).toBeFocused();
     await expect(popper).not.toBeFocused();
 
-
     await page.keyboard.press('Tab');
     await expect(trigger).toBeFocused();
     await expect(popper).toBeHidden();
-
-
-  
   });
 });
