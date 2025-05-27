6c9256ddf49b32c844a880ec83116dc1c2934b1a
ilia.brauer <ilia.brauer@semrush.com>
2025-05-27 16:29:32 +0200

    [data-table] UIK-3730 fixed keyboard navigation

--- semcore/data-table/src/components/DataTable/DataTable.tsx
+++ semcore/data-table/src/components/DataTable/DataTable.tsx
@@ -413,9 +413,9 @@ class DataTableRoot<D extends DataTableData> extends Component<
 
     const row = this.getRow(newRow);
     const cell = row?.querySelector(
-      `:scope > [role=gridcell][aria-colindex="${
+      `:scope > div > [role=gridcell][aria-colindex="${
         newCol + 1
-      }"], :scope > [role=columnheader][aria-colindex="${
+      }"], :scope > div > [role=columnheader][aria-colindex="${
         newCol + 1
       }"], :scope > div > [role=columnheader][aria-colindex="${newCol + 1}"]`,
     );
