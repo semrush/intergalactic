a289e11721086410ded0613268c206f20c272e02
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-05-28 17:00:27 +0400

    [stories] update accordion test stories

--- stories/components/data-table/tests/accordion-tests.stories.tsx
+++ stories/components/data-table/tests/accordion-tests.stories.tsx
@@ -14,7 +14,9 @@ import TabeInTableFixedColumnExample from './examples/accordion-tests/table-in-t
 import HorizontalScrollExample from './examples/accordion-tests/accordion-with-horizontal-scroll';
 import TabeInTableSortableExample from './examples/accordion-tests/table-in-table-with-sorting';
 import ColoredAccordionExample from './examples/accordion-tests/colored-accordion';
-import TabeInTableAnimationTestExample from './examples/accordion-tests/table-in-table-with-sorting-animation-test';
+import TabeInTableAnimationTestExample from './examples/accordion-tests/table-in-table-animation-test';
+import AnimationDurationTestExample from './examples/accordion-tests/accordion-duration';
+
 
 const meta: Meta<typeof DataTable> = {
   title: 'Components/DataTable/Tests/AccordionTests',
@@ -26,6 +28,10 @@ type Story = StoryObj<typeof DataTable>;
 
 export const AccordionWithChartAndButton: Story = {
   render: AccordionWithChartAndButtonExample,
+};
+
+export const AnimationDurationTest: Story = {
+  render: AnimationDurationTestExample,
 };
 
 export const TabeInTableAnimationTest: Story = {
