e124891de09933c7da56aaea1883abdcf6827297
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-06-03 09:36:54 +0400

    [select] upd tests

--- semcore/select/__tests__/auto-suggest.axe-test.tsx
+++ semcore/select/__tests__/auto-suggest.axe-test.tsx
@@ -3,7 +3,7 @@ import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 
 test.describe('AutoSuggest', () => {
   test('Combobox', async ({ page }) => {
-    const standPath = 'stories/ux-patterns/auto-suggest/docs/examples/combobox_example.tsx';
+    const standPath = 'stories/patterns/ux-patterns/auto-suggest/docs/examples/combobox_example.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
 
     await page.setContent(htmlContent);
@@ -28,7 +28,7 @@ test.describe('AutoSuggest', () => {
   });
 
   test('AutoSuggest', async ({ page }) => {
-    const standPath = 'stories/ux-patterns/auto-suggest/docs/examples/autosuggest_example.tsx';
+    const standPath = 'stories/patterns/ux-patterns/auto-suggest/docs/examples/autosuggest_example.tsx';
     const htmlContent = await e2eStandToHtml(standPath, 'en');
 
     await page.setContent(htmlContent);
@@ -45,166 +45,6 @@ test.describe('AutoSuggest', () => {
       await page.keyboard.press('Tab');
       await page.keyboard.type('a');
       await page.waitForSelector('text=persian');
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-  });
-
-  test('Controlled and uncontrolled mode', async ({ page }) => {
-    const standPath =
-      'stories/components/select/docs/examples/controlled_and_uncontrolled_modes.tsx';
-    const htmlContent = await e2eStandToHtml(standPath, 'en');
-
-    await page.setContent(htmlContent);
-
-    // default check
-    {
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-
-    // open Controlled mode
-    {
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Enter');
-
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-
-    // select option in Controlled mode and open Uncontrolled mode
-    {
-      await page.keyboard.press('ArrowDown');
-      await page.keyboard.press('ArrowDown');
-      await page.keyboard.press('Enter');
-      await new Promise((resolve) => setTimeout(resolve, 100));
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Enter');
-
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-
-    // select option in Unontrolled
-    {
-      await page.keyboard.press('ArrowDown');
-      await page.keyboard.press('ArrowDown');
-      await page.keyboard.press('Enter');
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-  });
-
-  test('Trigger Customization', async ({ page }) => {
-    const standPath = 'stories/components/select/docs/examples/trigger_customization.tsx';
-    const htmlContent = await e2eStandToHtml(standPath, 'en');
-
-    await page.setContent(htmlContent);
-
-    // default check
-    {
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-
-    // open select from Button Link
-    {
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Enter');
-
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-  });
-
-  test('Trigger Customization Deep', async ({ page }) => {
-    const standPath = 'stories/components/select/docs/examples/trigger_customization_deep.tsx';
-    const htmlContent = await e2eStandToHtml(standPath, 'en');
-
-    await page.setContent(htmlContent);
-
-    // default check
-    {
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-
-    // open select
-    {
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Enter');
-
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-  });
-
-  test('Menu Customization With Notice', async ({ page }) => {
-    const standPath = 'stories/components/select/docs/examples/dropdownmenu_customization.tsx';
-    const htmlContent = await e2eStandToHtml(standPath, 'en');
-
-    await page.setContent(htmlContent);
-
-    // default check
-    {
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-
-    // open select
-    {
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Enter');
-
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-  });
-
-  test('Loading state', async ({ page }) => {
-    const standPath = 'stories/components/select/docs/examples/loading_state.tsx';
-    const htmlContent = await e2eStandToHtml(standPath, 'en');
-
-    await page.setContent(htmlContent);
-
-    // default check
-    {
-      const violations = await getAccessibilityViolations({ page });
-
-      expect(violations).toEqual([]);
-    }
-
-    // open select
-    {
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Enter');
-
       await new Promise((resolve) => setTimeout(resolve, 100));
 
       const violations = await getAccessibilityViolations({ page });
