e57a816ec4d9153d288be60e7efb68d92c8e977b
ilia.brauer <ilia.brauer@semrush.com>
2025-06-03 19:20:29 +0200

    [data-table] fixed tests

--- semcore/data-table/__tests__/data-table-accordion.browser-test.tsx
+++ semcore/data-table/__tests__/data-table-accordion.browser-test.tsx
@@ -230,7 +230,6 @@ test.describe('Accordion in table', () => {
     const firstArrow = await page.locator('[data-ui-name="ButtonLink"]').first();
 
     await test.step('Verify cell with accordion attributes', async () => {
-      await expect(firstArrow).toHaveAttribute('aria-controls');
       await expect(firstArrow).toHaveAttribute('aria-label', 'Show details');
 
       const row3 = page.locator('[data-ui-name="Body.Row"][aria-rowindex="2"]');
@@ -241,6 +240,7 @@ test.describe('Accordion in table', () => {
       await firstArrow.click();
       await page.waitForTimeout(250);
       await expect(firstArrow).toHaveAttribute('aria-expanded', 'true');
+      await expect(firstArrow).toHaveAttribute('aria-controls');
       await expect(cells.first()).toHaveAttribute('data-aria-level', '1');
     });
 
@@ -320,12 +320,12 @@ test.describe('Accordion in table', () => {
       const cells = row3.locator('[data-ui-name="Body.Cell"]');
 
       await expect(firstArrow).toHaveAttribute('aria-expanded', 'false');
-      await expect(firstArrow).toHaveAttribute('aria-controls');
 
       await expect(cells.nth(1)).not.toHaveAttribute('aria-expanded', 'false');
       await firstArrow.click();
       await page.waitForTimeout(250);
       await expect(firstArrow).toHaveAttribute('aria-expanded', 'true');
+      await expect(firstArrow).toHaveAttribute('aria-controls');
       await expect(cells.first()).toHaveAttribute('data-aria-level', '1');
     });
 
