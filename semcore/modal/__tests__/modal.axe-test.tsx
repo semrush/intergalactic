094f99043665d966ce757d9249e6b29478afd2cb
Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-06-06 17:19:14 +0400

    UIK-3717/modal-tests (#2200)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Signed-off-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>

---
+++ semcore/modal/__tests__/modal.axe-test.tsx
@@ -0,0 +1,119 @@
+import { expect, test, getAccessibilityViolations } from '@semcore/testing-utils/playwright';
+import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
+
+test.describe('Modal', () => {
+  test('Basic usage', async ({ page }) => {
+    const standPath = 'stories/components/modal/docs/examples/basic_modal_window_usage.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    {
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+
+    {
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('text=Do you want to save your changes?');
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+  });
+
+  test('Changing alignment', async ({ page }) => {
+    const standPath = 'stories/components/modal/docs/examples/changing_the_alignment.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    {
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+
+    {
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('text=Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+  });
+
+  test('Big height', async ({ page }) => {
+    const standPath =
+      'stories/components/modal/docs/examples/modal_window_height_is_bigger_than_the_browser_page.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    {
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+
+    {
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('text=Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+  });
+
+  test('Access to html nodes', async ({ page }) => {
+    const standPath = 'stories/components/modal/docs/examples/access_to_internal_html_nodes.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    {
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+
+    {
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('text=Lorem Title');
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+  });
+
+  test('Modal inside modal', async ({ page }) => {
+    const standPath =
+      'stories/components/modal/docs/examples/modal_window_inside_a_modal_window.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    {
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+
+    {
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('text=Open one more window');
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+
+    {
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('text=Save changes');
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+  });
+
+  test('Confirmation modal', async ({ page }) => {
+    const standPath =
+      'stories/patterns/ux-patterns/confirmation-modal-dialog/docs/examples/confirmation-modal-example.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+
+    {
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('text=Delete project?');
+      const violations = await getAccessibilityViolations({ page });
+      expect(violations).toEqual([]);
+    }
+  });
+});
