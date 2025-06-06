ff9e9105a33c552c7ce8f701578f3d981deba2e9
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-06-06 17:07:53 +0400

    [inline-input] move examples

---
+++ stories/components/inline-input/docs/inline-input.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import BasicUsageExample from './examples/basic_usage';
+import InheritingTextSizeExample from './examples/inheriting_text_size';
+import NumberOnlyInputExample from './examples/number-only_input';
+
+const meta: Meta = {
+  title: 'Components/Inline-Input/Documentation',
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const BasicUsage: Story = {
+  render: BasicUsageExample,
+};
+
+export const InheritingTextSize: Story = {
+  render: InheritingTextSizeExample,
+};
+
+export const NumberOnlyInput: Story = {
+  render: NumberOnlyInputExample,
+};
