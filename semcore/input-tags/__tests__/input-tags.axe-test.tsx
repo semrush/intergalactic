a32c69c7f92d5deecfd3fc74bb8c5ea783aa25c4
ilyabrower <ilia.brauer@semrush.com>
2025-06-09 14:37:07 +0200

    [UIK-3810][input-tags] added axe test (#2210)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] I have updated the documentation accordingly or it's not required.
    - [ ] Unit tests are not broken.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

---
+++ semcore/input-tags/__tests__/input-tags.axe-test.tsx
@@ -0,0 +1,50 @@
+import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
+import { expect, getAccessibilityViolations, test } from '@semcore/testing-utils/playwright';
+
+test.describe('Input tag', () => {
+  test('Select for tag filtering', async ({ page }) => {
+    const standPath = 'stories/components/input-tags/docs/examples/select_for_tag_filtering.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+
+    await test.step('Init state', async () => {
+      const violations = await getAccessibilityViolations({ page });
+
+      expect(violations).toEqual([]);
+    });
+
+    await test.step('Open select', async () => {
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+
+      const violations = await getAccessibilityViolations({ page });
+
+      expect(violations).toEqual([]);
+    });
+
+    await test.step('Select item', async () => {
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Enter');
+
+      const violations = await getAccessibilityViolations({ page });
+
+      expect(violations).toEqual([]);
+    });
+  });
+  test('Entering and editing tags', async ({ page }) => {
+    const standPath = 'stories/components/input-tags/docs/examples/entering_and_editing_tags.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+  test('Wrapping email in tag', async ({ page }) => {
+    const standPath = 'stories/components/input-tags/docs/examples/wrapping_email_in_tag.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+});
