a8627cf84e67a2d968f9877085c4affdbd685151
ilyabrower <ilia.brauer@semrush.com>
2025-06-09 14:41:04 +0200

    [UIK-3810][tag] added axe tests for all tag examples (#2208)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] I have updated the documentation accordingly or it's not required.
    - [ ] Unit tests are not broken.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

---
+++ semcore/tag/__tests__/tag.axe-test.tsx
@@ -0,0 +1,61 @@
+import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
+import { expect, getAccessibilityViolations, test } from '@semcore/testing-utils/playwright';
+
+test.describe('Tag', () => {
+  test('Adding tag', async ({ page }) => {
+    const standPath = 'stories/components/tag/docs/examples/adding_tag.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+  test('Custom tag color', async ({ page }) => {
+    const standPath = 'stories/components/tag/docs/examples/custom_tag_color.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+  test('Editing tag', async ({ page }) => {
+    const standPath = 'stories/components/tag/docs/examples/editing_tag.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+  test('Grouping tags less', async ({ page }) => {
+    const standPath = 'stories/components/tag/docs/examples/grouping_tags_less.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+  test('Grouping tags more', async ({ page }) => {
+    const standPath = 'stories/components/tag/docs/examples/grouping_tags_more.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+  test('Removing tag', async ({ page }) => {
+    const standPath = 'stories/components/tag/docs/examples/removing_tag.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+  test('Tag addon', async ({ page }) => {
+    const standPath = 'stories/components/tag/docs/examples/tag_addon.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+    const violations = await getAccessibilityViolations({ page });
+
+    expect(violations).toEqual([]);
+  });
+});
