b2b26360b9557d0f72c53f782e74ef9d9e2ef27a
ilia.brauer <ilia.brauer@semrush.com>
2025-06-09 12:13:28 +0200

    [carousel,input-mask,progress-bar] fixed path to tests

--- semcore/progress-bar/__tests__/progress-bar.vo-test.ts
+++ semcore/progress-bar/__tests__/progress-bar.vo-test.ts
@@ -2,7 +2,7 @@ import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
 import { expect, voiceOverTest as test } from '@semcore/testing-utils/playwright';
 
 test('Users can interact with ProgressBar via VoiceOver', async ({ page, voiceOver }) => {
-  const standPath = 'website/docs/components/progress-bar/examples/basic-usage.tsx';
+  const standPath = 'stories/components/progress-bar/docs/examples/basic-usage.tsx';
   const htmlContent = await e2eStandToHtml(standPath, 'en');
 
   await page.setContent(htmlContent);
