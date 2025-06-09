b32323d60921183837c6a6489755929764c413ff
ilia.brauer <ilia.brauer@semrush.com>
2025-06-09 12:47:52 +0200

    [notice] added axe test

--- stories/components/notice/docs/examples/basic_notice.tsx
+++ stories/components/notice/docs/examples/basic_notice.tsx
@@ -34,7 +34,7 @@ const Demo = () => (
       <Notice.Close />
     </Notice>
 
-    <Notice theme='muted' aria-label='New feature announcement' mb={2}>
+    <Notice theme='muted' aria-label='New muted feature announcement' mb={2}>
       <Notice.Label>
         <Question />
       </Notice.Label>
@@ -60,7 +60,7 @@ const Demo = () => (
       <Notice.Close />
     </Notice>
 
-    <Notice theme='warning' aria-label='New feature announcement' mb={2}>
+    <Notice theme='warning' aria-label='New warning feature announcement' mb={2}>
       <Notice.Label>
         <Question />
       </Notice.Label>
@@ -86,7 +86,7 @@ const Demo = () => (
       <Notice.Close />
     </Notice>
 
-    <Notice theme='danger' aria-label='New feature announcement' mb={2}>
+    <Notice theme='danger' aria-label='New danger feature announcement' mb={2}>
       <Notice.Label>
         <Warning />
       </Notice.Label>
@@ -112,7 +112,7 @@ const Demo = () => (
       <Notice.Close />
     </Notice>
 
-    <Notice theme='success' aria-label='New feature announcement' mb={2}>
+    <Notice theme='success' aria-label='New success feature announcement' mb={2}>
       <Notice.Label>
         <ThumbUpM />
       </Notice.Label>
