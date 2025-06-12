1cc48d1224ed8e27e28db83dc8ef66c12701e98d
ilia.brauer <ilia.brauer@semrush.com>
2025-06-12 12:45:30 +0200

    [dropdown-menu] open nested by pressing Space

--- semcore/dropdown-menu/src/DropdownMenu.jsx
+++ semcore/dropdown-menu/src/DropdownMenu.jsx
@@ -186,7 +186,7 @@ class DropdownMenuRoot extends AbstractDropdown {
       const show =
         (e.key === 'ArrowRight' && placement?.startsWith('right')) ||
         (e.key === 'ArrowLeft' && placement?.startsWith('left')) ||
-        ((e.key === 'Enter' || e.key === 'Space') && !inlineActions);
+        ((e.key === 'Enter' || e.key === ' ') && !inlineActions);
       const hide =
         (e.key === 'ArrowLeft' && placement?.startsWith('right')) ||
         (e.key === 'ArrowRight' && placement?.startsWith('left')) ||
