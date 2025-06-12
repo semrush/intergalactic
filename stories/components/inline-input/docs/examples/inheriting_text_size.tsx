7ddc0411ae8d581bfa7f653c3e96190541847b9e
Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-06-12 13:47:56 +0400

    UIK-3776/inline-input (#2209)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.

---
+++ stories/components/inline-input/docs/examples/inheriting_text_size.tsx
@@ -0,0 +1,50 @@
+import EditM from '@semcore/icon/Edit/m';
+import InlineEdit from '@semcore/inline-edit';
+import InlineInput from '@semcore/inline-input';
+import { Text } from '@semcore/typography';
+import React from 'react';
+
+const Example = () => {
+  const [title, setTitle] = React.useState('The Adventures of Intergalactic Whale');
+  const [editingTitle, setEditingTitle] = React.useState(false);
+  const [savingTitle, setSavingTitle] = React.useState(false);
+  const stopEditing = () => setEditingTitle(false);
+  const handleTitle = (title: any) => {
+    setSavingTitle(true);
+    /** Here we are doing some network activities */
+    setTimeout(() => {
+      setTitle(title);
+      setSavingTitle(false);
+      setEditingTitle(false);
+    }, 3000);
+  };
+
+  return (
+    <>
+      <Text tag='h3' mb={2}>
+        <InlineEdit editable={editingTitle} onEditableChange={setEditingTitle}>
+          <InlineEdit.View pr={5}>
+            {title}
+            {' '}
+            <EditM color='icon-secondary-neutral' />
+          </InlineEdit.View>
+          <InlineEdit.Edit>
+            <InlineInput onConfirm={handleTitle} onCancel={stopEditing} loading={savingTitle}>
+              <InlineInput.Value autoFocus defaultValue={title} aria-label='Article name' />
+              <InlineInput.ConfirmControl />
+              <InlineInput.CancelControl />
+            </InlineInput>
+          </InlineEdit.Edit>
+        </InlineEdit>
+      </Text>
+      Once upon a time in a distant galaxy far, far away, there existed a legendary creature known
+      as the Intergalactic Whale. This magnificent space-faring mammal was no ordinary whale. It had
+      the power to swim through the cosmic ocean, jumping from one star system to another with grace
+      and finesse.
+    </>
+  );
+};
+
+const Demo = Example;
+
+export default Demo;
