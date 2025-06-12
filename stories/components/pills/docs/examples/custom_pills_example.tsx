b8e4b94dfcbf436df8ab034990d94e45833d94a3
ilia.brauer <ilia.brauer@semrush.com>
2025-06-12 14:50:16 +0200

    [stories] simple fix in styles for custom pills

--- stories/components/pills/docs/examples/custom_pills_example.tsx
+++ stories/components/pills/docs/examples/custom_pills_example.tsx
@@ -10,21 +10,12 @@ const contentBLocks = [
   'Sleep is her full-time job. Occasionally wakes up just to sigh and go back to bed. Her alarm clock gave up and filed for early retirement.',
 ];
 
-const pillStyles: {
-  item: CSSProperties;
-  text: CSSProperties;
-} = {
-  item: {
-    height: 'fit-content',
-    alignItems: 'stretch',
-    textAlign: 'inherit' as const,
-    whiteSpace: 'normal' as const,
-  },
-  text: {
-    display: 'flex',
-    flexDirection: 'column' as const,
-  },
-};
+const pillStyles: CSSProperties = {
+  height: 'fit-content',
+  alignItems: 'stretch',
+  textAlign: 'inherit',
+  whiteSpace: 'normal',
+} as const;
 
 const Demo = () => {
   const [tab, setTab] = React.useState<number>(0);
@@ -35,20 +26,20 @@ const Demo = () => {
         Average sleep per day
       </Text>
       <Pills mb={4} behavior='manual' value={tab} onChange={setTab} aria-labelledby='king-pills'>
-        <Pills.Item value={0} style={pillStyles.item} p={5} id='pills-tab-0'>
-          <Pills.Item.Text style={pillStyles.text} m={0}>
+        <Pills.Item value={0} style={pillStyles} p={5} id='pills-tab-0'>
+          <Pills.Item.Text tag={Flex} direction='column' m={0}>
             <Text mb={1}>Wombat</Text>
             <Text size={500} bold>16 hours</Text>
           </Pills.Item.Text>
         </Pills.Item>
-        <Pills.Item value={1} style={pillStyles.item} p={5} id='pills-tab-1'>
-          <Pills.Item.Text style={pillStyles.text} m={0}>
+        <Pills.Item value={1} style={pillStyles} p={5} id='pills-tab-1'>
+          <Pills.Item.Text tag={Flex} direction='column' m={0}>
             <Text mb={1}>Bat</Text>
             <Text size={500} bold>20 hours</Text>
           </Pills.Item.Text>
         </Pills.Item>
-        <Pills.Item value={2} style={pillStyles.item} p={5} id='pills-tab-2'>
-          <Pills.Item.Text style={pillStyles.text} m={0}>
+        <Pills.Item value={2} style={pillStyles} p={5} id='pills-tab-2'>
+          <Pills.Item.Text tag={Flex} direction='column' m={0}>
             <Text mb={1}>Koala</Text>
             <Text size={500} bold>22 hours</Text>
           </Pills.Item.Text>
