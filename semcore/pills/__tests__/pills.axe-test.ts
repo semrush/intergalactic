e45cdefb9aa39cb574ae888aebece9b65c39e2a1
ilia.brauer <ilia.brauer@semrush.com>
2025-06-12 17:46:21 +0200

    [pills] added axe test

---
+++ semcore/pills/__tests__/pills.axe-test.ts
@@ -0,0 +1,21 @@
+import { e2eStandToHtml } from '@semcore/testing-utils/e2e-stand';
+import { expect, test, getAccessibilityViolations } from '@semcore/testing-utils/playwright';
+
+test.describe('Modal', () => {
+  test('Basic example', async ({ page }) => {
+    const standPath = 'stories/components/pills/docs/examples/basic_example.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+
+    const violations = await getAccessibilityViolations({ page });
+    expect(violations).toEqual([]);
+  });
+  test('Tabs example', async ({ page }) => {
+    const standPath = 'stories/components/pills/docs/examples/tabs_example.tsx';
+    const htmlContent = await e2eStandToHtml(standPath, 'en');
+    await page.setContent(htmlContent);
+
+    const violations = await getAccessibilityViolations({ page });
+    expect(violations).toEqual([]);
+  });
+});
