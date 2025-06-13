3c441c02303ce0e6f97aee71619f3a956831d0bf
ilia.brauer <ilia.brauer@semrush.com>
2025-06-13 15:06:42 +0200

    [stories] removed one example

--- stories/components/base-trigger/docs/examples/link-trigger.tsx
+++ stories/components/base-trigger/docs/examples/link-trigger.tsx
@@ -4,18 +4,7 @@ import Select from '@semcore/select';
 import { Text } from '@semcore/typography';
 import React from 'react';
 
-interface Project {
-  id: number;
-  name: string;
-}
-
 const Demo = () => {
-  const [selectedProject, setSelectedProject] = React.useState<Project | undefined>();
-
-  const handleSelect = (id: number) => {
-    setSelectedProject(() => projects.find((project) => project.id === id));
-  };
-
   return (
     <Flex gap={2}>
       <Text size={200} tag='label' htmlFor='device-link-select'>
@@ -29,36 +18,6 @@ const Demo = () => {
         placeholder='Select period'
         ml={4}
       />
-      <Select
-        value={selectedProject?.id}
-        onChange={handleSelect}
-        placeholder='Select Project'
-      >
-        {({ getTriggerProps }, actions) => {
-          return (
-            <>
-              <Select.Trigger tag={LinkTrigger}>
-                <LinkTrigger.Text
-                  size={400}
-                  tag={Text}
-                  fontWeight={400}
-                  noWrap
-                >
-                  {selectedProject?.name ??
-                    (getTriggerProps().placeholder as string)}
-                </LinkTrigger.Text>
-              </Select.Trigger>
-              <Select.Popper w={280} aria-label='Projects'>
-                {projects.map((project) => (
-                  <Select.Option key={project.id} value={project.id}>
-                    {project.name}
-                  </Select.Option>
-                ))}
-              </Select.Popper>
-            </>
-          );
-        }}
-      </Select>
     </Flex>
   );
 };
@@ -72,13 +31,5 @@ const periods = ['Last week', 'Last month', 'Last 6 months'].map((item) => ({
   value: item,
   children: item,
 }));
-
-const projects: Project[] = [
-  {
-    id: 1,
-    name: 'Semrush',
-  },
-  { id: 2, name: 'Google' },
-];
 
 export default Demo;
