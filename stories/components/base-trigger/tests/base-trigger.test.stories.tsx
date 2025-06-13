44bdeb5bc4f294687cd1efdd79d1c1e57f835c44
ilyabrower <ilia.brauer@semrush.com>
2025-06-13 15:44:46 +0200

    [UIK-3122][select] fixed render child with .Text or .Value in trigger (#2224)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] I have updated the documentation accordingly or it's not required.
    - [x] Unit tests are not broken.
    - [x] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new unit tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- stories/components/base-trigger/tests/base-trigger.test.stories.tsx
+++ stories/components/base-trigger/tests/base-trigger.test.stories.tsx
@@ -6,6 +6,7 @@ import ButtonTriggerLoadingExample from './examples/button-trigger-loading';
 import FilterTriggerExample from './examples/filter-trigger-all-states';
 import LinkTriggerExample from './examples/link-trigger-all-states';
 import LinkTriggerLoadingExample from './examples/link-trigger-loading';
+import LinkTriggerSelectExample from './examples/link-trigger-with-select';
 
 const meta: Meta = {
   title: 'Components/BaseTrigger/Test',
@@ -21,6 +22,10 @@ export const LinkTrigger: StoryObj = {
 
 export const LinkTriggerLoading: StoryObj = {
   render: LinkTriggerLoadingExample,
+};
+
+export const LinkTriggerSelect: StoryObj = {
+  render: LinkTriggerSelectExample,
 };
 
 export const ButtonTrigger: StoryObj = {
