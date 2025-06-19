5bb7841d55f55febea51030193b89bb65ec3441d
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-06-19 13:49:48 +0400

    [stories] added data table test stories

--- stories/components/data-table/tests/data-table-scroll.stories.tsx
+++ stories/components/data-table/tests/data-table-scroll.stories.tsx
@@ -5,9 +5,11 @@ import FixedColumnDiffWidthExample from './examples/scroll-tests/fixed-column-wi
 import FixedColumnWithoutVerticalExample from './examples/scroll-tests/fixed-column-without-vertical';
 import HorizontalScrollWithoutFixedExample from './examples/scroll-tests/horizontal-scroll';
 import FixedGroupExample from './examples/scroll-tests/horizontal-scroll-fixed-group';
+import MultiLevelHorizontalScrollExample from './examples/scroll-tests/milti-level-horizontal-scroll';
 import MultipleScrollsExample from './examples/scroll-tests/multiple-scrolls';
 import ScrollAndStickyExample from './examples/scroll-tests/scroll-in-table-sticky';
 import ScrollTopHeaderExample from './examples/scroll-tests/scroll-in-top-header';
+import CaseWithW100Example from './examples/scroll-tests/w-100';
 
 const meta: Meta<typeof DataTable> = {
   title: 'Components/DataTable/Tests/Scroll',
@@ -19,6 +21,10 @@ type Story = StoryObj<typeof DataTable>;
 
 export const FixedGroups: Story = {
   render: FixedGroupExample,
+};
+
+export const MultiLevelHorizontalScroll: Story = {
+  render: MultiLevelHorizontalScrollExample,
 };
 
 export const ScrollWithTopStickyProps: Story = {
@@ -43,4 +49,8 @@ export const HorizontalScrollWithFixedColumnAndDiffWidth: Story = {
 
 export const FixedColumnWithoutVertical: Story = {
   render: FixedColumnWithoutVerticalExample,
+};
+
+export const CaseWithW100: Story = {
+  render: CaseWithW100Example,
 };
