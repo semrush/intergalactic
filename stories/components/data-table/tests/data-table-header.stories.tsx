24cdd13082c05636af3dc997bc6a235be018f508
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-06-19 11:26:03 +0400

    [stories] update stories url

--- stories/components/data-table/tests/data-table-header.stories.tsx
+++ stories/components/data-table/tests/data-table-header.stories.tsx
@@ -8,6 +8,7 @@ import MultiLevelInteractiveExample from './examples/header-tests/multi-level-wi
 import SecondaryHeaderExample from './examples/header-tests/secondary-header';
 import SecondarySortingExample from './examples/header-tests/secondary-sorting';
 import SortingSizeExample from './examples/header-tests/sorting-changing-size';
+import SortingDefaultUndefinedExample from './examples/header-tests/sorting-default-undefined';
 import SortingInteractiveHeaderExample from './examples/header-tests/sorting-with-interactive';
 import InteractiveHeaderExample from './examples/header-tests/table-with-1tf-and diff-elements';
 
@@ -20,6 +21,10 @@ type Story = StoryObj;
 
 export const SecondaryHeader: Story = {
   render: SecondaryHeaderExample,
+};
+
+export const SortingDefaultUndefined: Story = {
+  render: SortingDefaultUndefinedExample,
 };
 
 export const SortingInteractiveHeader: Story = {
