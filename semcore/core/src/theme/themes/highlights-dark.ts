47b4a7322b617ff439d05f10c0c49dfff64f5719
Sheila Sheikh <166654065+sheila-semrush@users.noreply.github.com>
2025-07-07 18:44:00 +0200

    [core, feature-highlight, website] added missing redirects, added FH tokens to dark.json, removed unnecessary import from example

---
+++ semcore/core/src/theme/themes/highlights-dark.ts
@@ -0,0 +1,13 @@
+export default {
+  '--intergalactic-bg-primary-feature-highlight': 'rgba(30, 34, 49, 1)',
+  '--intergalactic-bg-primary-feature-highlight-hover-active': 'linear-gradient(90deg, #f9f2ff, #e9f7ff)',
+  '--intergalactic-bg-secondary-feature-highlight': 'linear-gradient(90deg, #f9f2ff, #e9f7ff)',
+  '--intergalactic-border-feature-highlight': 'linear-gradient(90deg, #c695ff, #2bb3ff)',
+  '--intergalactic-border-feature-highlight-active': 'linear-gradient(90deg, #ab6cfe, #008ff8)',
+  '--intergalactic-control-primary-feature-highlight': 'linear-gradient(90deg, #ab6cfe, #008ff8)',
+  '--intergalactic-control-primary-feature-highlight-hover': 'linear-gradient(90deg, #8649e1, #006dca)',
+  '--intergalactic-control-primary-feature-highlight-active': 'linear-gradient(90deg, #5925ab, #044792)',
+  '--intergalactic-icon-primary-feature-highlight': '#8649e1',
+  '--intergalactic-icon-primary-feature-highlight-hover-active': '#6B3AB4',
+  '--intergalactic-keyboard-focus-feature-highlight-outline': 'linear-gradient(90deg, rgba(171, 108, 254, 0.5), rgba(0, 143, 248, 0.5))',
+};
