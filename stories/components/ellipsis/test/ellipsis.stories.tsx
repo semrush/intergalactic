30002894fcd8fc249c65dd18a9a6cd7b704079f9
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-07-07 19:15:12 +0200

    [stories] added test story with specific text size

--- stories/components/ellipsis/test/ellipsis.stories.tsx
+++ stories/components/ellipsis/test/ellipsis.stories.tsx
@@ -1,13 +1,27 @@
 import Ellipsis from '@semcore/ellipsis';
 import type { Meta, StoryObj } from '@storybook/react-vite';
+export type ExampleEllipsisProps = EllipsisProps;
 
 import TableWithLinksExample from './examples/in_table_with_link';
 import OnChangePropsExample from './examples/on_change_props';
 import TextCasesExample from './examples/text_cases';
+import TrimWithTextSizeExample, { defaultProps as sizeEllipsisProps } from './examples/trim_with_special_text_size';
 
-const meta: Meta<typeof Ellipsis> = {
+const meta: Meta<ExampleEllipsisProps> = {
   title: 'Components/Ellipsis/Tests',
   component: Ellipsis,
+  argTypes: {
+    trim: {
+      control: { type: 'select' },
+      options: ['end', 'middle'],
+    },
+    tooltip: {
+      control: { type: 'boolean' },
+    },
+    maxLine: {
+      control: { type: 'number' },
+    },
+  },
 };
 
 export default meta;
@@ -23,4 +37,9 @@ export const TextCases: Story = {
 
 export const OnChangeProps: Story = {
   render: OnChangePropsExample,
+};
+
+export const TrimWithTextSize: Story = {
+  render: TrimWithTextSizeExample,
+  args: sizeEllipsisProps,
 };
---
+++ stories/components/ellipsis/test/examples/trim_with_special_text_size.tsx
@@ -0,0 +1,30 @@
+import Ellipsis from '@semcore/ellipsis';
+import type { EllipsisProps } from '@semcore/ellipsis';
+import { Text } from '@semcore/typography';
+import React from 'react';
+
+const Demo = (props: EllipsisProps) => {
+  return (
+    <Text
+      display='block'
+      w={100}
+      h={200}
+      mb={3}
+      size={400}
+      tag='p'
+      mt={0}
+      style={{ outline: '1px solid red' }}
+    >
+      <Ellipsis maxLine={props.maxLine} trim={props.trim} tooltip={props.tooltip}>WordWord WordWord</Ellipsis>
+    </Text>
+  );
+};
+export const defaultProps: ExampleEllipsisProps = {
+  trim: 'middle',
+  tooltip: true,
+  maxLine: 1,
+};
+
+Demo.defaultProps = defaultProps;
+
+export default Demo;
