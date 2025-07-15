cb9c5df4fb1bf2f7ba8a4966bdd953c9d9b3eb3b
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2025-07-15 10:39:29 +0200

    [UIK-3901][data-table] fixed row hover effect for empty state

--- semcore/data-table/src/components/Body/Row.types.ts
+++ semcore/data-table/src/components/Body/Row.types.ts
@@ -22,7 +22,7 @@ export type DataTableRowProps = {
   isAccordionRow?: DataTableCellProps['isAccordionRow'];
   animationExpand?: DataTableCellProps['animationExpand'];
   accordionRowIndex?: DataTableCellProps['accordionRowIndex'];
-  isEmptyRow?: boolean;
+  isNonInteractive?: boolean;
 };
 
 export type RowPropsInner = JSX.IntrinsicElements['div'] & {
