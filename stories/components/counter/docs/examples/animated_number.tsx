10d80d33e963344a102585a79b1655f499717c9d
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-07-17 18:24:37 +0200

    [counter] update tests

--- stories/components/counter/docs/examples/animated_number.tsx
+++ stories/components/counter/docs/examples/animated_number.tsx
@@ -2,7 +2,8 @@ import Button from '@semcore/button';
 import { AnimatedNumber } from '@semcore/counter';
 import React from 'react';
 
-const Demo = () => {
+type AnimatedNumberBaseProps = { duration: any; delay: any };
+const Demo = (props: AnimatedNumberBaseProps) => {
   const [value, setValue] = React.useState(20);
   const handleClick = () => {
     setValue(value + 20);
@@ -10,12 +11,19 @@ const Demo = () => {
 
   return (
     <>
-      <AnimatedNumber value={value} />
+      <AnimatedNumber value={value} duration={props.duration} delay={props.delay} />
       <Button onClick={handleClick} mt={2}>
         Rerender value
       </Button>
     </>
   );
 };
+
+export const defaultProps: AnimatedNumberBaseProps = {
+  duration: undefined,
+  delay: undefined,
+};
+
+Demo.defaultProps = defaultProps;
 
 export default Demo;
