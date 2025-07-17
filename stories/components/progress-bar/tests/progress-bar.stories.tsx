2476f298ac5cf4f5eec5d91404f04dd2fa14f8d7
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-07-17 20:34:45 +0200

    [progress-bar] update unit stories

---
+++ stories/components/progress-bar/tests/progress-bar.stories.tsx
@@ -0,0 +1,48 @@
+import ProgressBar from '@semcore/progress-bar';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import CustomizingTheBarExample1, { defaultProps as CustomizingTheBarProps } from './examples/customizing_the_bar1';
+import CustomizingTheValueExample, { defaultProps as CustomizingTheValueProps } from './examples/customizing_the_value';
+
+const meta: Meta<typeof ProgressBar> = {
+  title: 'Components/ProgressBar/Tests',
+  component: ProgressBar,
+};
+
+export default meta;
+
+export const CustomizingTheBar1: StoryObj<typeof CustomizingTheBarProps> = {
+  render: CustomizingTheBarExample1,
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: ['s', 'm', 'l'],
+    },
+    theme: {
+      control: { type: 'select' },
+      options: ['invert', 'dark', 'violet-100'],
+    },
+    value: {
+      control: { type: 'number' },
+    },
+  },
+  args: CustomizingTheBarProps,
+};
+
+export const CustomizingTheValue: StoryObj<typeof CustomizingTheValueProps> = {
+  render: CustomizingTheValueExample,
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: ['s', 'm', 'l'],
+    },
+    theme: {
+      control: { type: 'select' },
+      options: ['invert', 'dark', 'violet-500'],
+    },
+    value: {
+      control: { type: 'number' },
+    },
+  },
+  args: CustomizingTheValueProps,
+};
