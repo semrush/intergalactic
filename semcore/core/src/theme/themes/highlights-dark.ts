fd6df158387c14218a810d291edfb83f84f4d838
Julia Mnizhek <j.mnizhek@semrush.com>
2025-07-18 20:38:49 +0200

    [feature-highlight] update focus styles

--- semcore/core/src/theme/themes/highlights-dark.ts
+++ semcore/core/src/theme/themes/highlights-dark.ts
@@ -9,5 +9,5 @@ export default {
   '--intergalactic-control-primary-feature-highlight-active': 'linear-gradient(90deg, #5925ab, #044792)',
   '--intergalactic-icon-primary-feature-highlight': '#8649e1',
   '--intergalactic-icon-primary-feature-highlight-hover-active': '#6B3AB4',
-  '--intergalactic-keyboard-focus-feature-highlight-outline': 'linear-gradient(90deg, rgba(171, 108, 254, 0.5), rgba(0, 143, 248, 0.5))',
+  '--intergalactic-keyboard-focus-feature-highlight-outline': 'linear-gradient(90deg, #ab6cfe, #008ff8)',
 };
