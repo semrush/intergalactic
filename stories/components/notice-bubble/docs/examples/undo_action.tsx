bc77111175b2d173e4d7144a59cdf5aa2ad548bd
Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-07-22 11:08:34 +0200

    Update stories/components/notice-bubble/docs/examples/undo_action.tsx
    Co-authored-by: ilyabrower <ilia.brauer@semrush.com>
    Signed-off-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- stories/components/notice-bubble/docs/examples/undo_action.tsx
+++ stories/components/notice-bubble/docs/examples/undo_action.tsx
@@ -2,7 +2,7 @@ import Button from '@semcore/button';
 import Link from '@semcore/link';
 import { NoticeBubbleContainer, NoticeBubbleManager } from '@semcore/notice-bubble';
 import React from 'react';
-type UndoActionNoticeBubbleProps = { initialAnimation: any; duration: any; type: any; focusLock: any };
+type UndoActionNoticeBubbleProps = { initialAnimation: boolean; duration: number; type: 'info' | 'warning'; focusLock: boolean };
 
 const manager = new NoticeBubbleManager();
 
