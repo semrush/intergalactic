419af998ddceaf64bca0fc13a1fb5fda2a826962
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-07-22 18:38:13 +0200

    [data-table] added story for raw data

--- stories/components/data-table/tests/data-table-cells.stories.tsx
+++ stories/components/data-table/tests/data-table-cells.stories.tsx
@@ -9,6 +9,7 @@ import LongTextCellsExample from './examples/cells-tests/long-text-in-cells';
 import MiniChartsInCellsExample from './examples/cells-tests/mini-chart-inline-edit-in-cell';
 import MergedScrolledRowExample from './examples/cells-tests/one-big-merged-row-and-scroll';
 import OneMergedCellExample from './examples/cells-tests/one-merged-cell';
+import RenderCellRawValueExample from './examples/cells-tests/render-cell-raw-data';
 
 const meta: Meta<typeof DataTable> = {
   title: 'Components/DataTable/Tests/Cells',
@@ -24,6 +25,10 @@ export const CardFlagInCell: Story = {
 
 export const CheckBox: Story = {
   render: CheckBoxExample,
+};
+
+export const RenderCellRawValue: Story = {
+  render: RenderCellRawValueExample,
 };
 
 export const MiniChartsInCells: Story = {
