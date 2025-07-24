175a3e0d57d35340e6044bafaa53724a1f0cced5
Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-07-24 12:00:07 +0200

    UIK-3785/ notice-bubble  update tests (#2324)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new tests on added of fixed functionality.
    
    ---------
    
    Signed-off-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>
    Co-authored-by: ilyabrower <ilia.brauer@semrush.com>

--- semcore/notice-bubble/__tests__/index.test.tsx
+++ semcore/notice-bubble/__tests__/index.test.tsx
@@ -1,28 +1,32 @@
-import { runDependencyCheckTests } from '@semcore/testing-utils/shared-tests';
-import { snapshot } from '@semcore/testing-utils/snapshot';
-import { render, fireEvent, cleanup, act } from '@semcore/testing-utils/testing-library';
-import { expect, test, describe, beforeEach, vi } from '@semcore/testing-utils/vitest';
+import {
+  runDependencyCheckTests,
+  shouldSupportClassName,
+  shouldSupportRef,
+} from '@semcore/testing-utils/shared-tests';
+import {
+  render,
+  cleanup,
+} from '@semcore/testing-utils/testing-library';
+import { expect, test, describe, beforeEach } from '@semcore/testing-utils/vitest';
 import React from 'react';
 
 import {
   NoticeBubbleContainer,
-  NoticeBubbleManager,
-  NoticeBubble as NoticeBubbleImport,
-  NoticeBubbleWarning as NoticeBubbleWarningImport,
 } from '../src';
 
-const NoticeBubble = React.forwardRef((props: any, ref: React.Ref<HTMLElement>) => (
+const TestNoticeBubble = React.forwardRef((props: any, ref: React.Ref<HTMLElement>) => (
   <>
-    <NoticeBubbleContainer style={{ position: 'static', width: 'auto' }} disablePortal />
-    <NoticeBubbleImport ref={ref} style={{ marginBottom: 0 }} {...props} />
+    <NoticeBubbleContainer
+      disablePortal
+      style={{ position: 'static', width: 'auto' }}
+    />
+    <NoticeBubbleContainer
+      ref={ref}
+      style={{ marginBottom: 0 }}
+      {...props}
+    />
   </>
 ));
-
-// const NoticeBubbleWarning = React.forwardRef((props, ref) => (
-//   <NoticeBubbleContainer style={{ position: 'static', width: 'auto' }} disablePortal>
-//     <NoticeBubbleWarningImport ref={ref} style={{ marginBottom: 0 }} {...props} />
-//   </NoticeBubbleContainer>
-// ));
 
 describe('notice-bubble Dependency imports', () => {
   runDependencyCheckTests('notice-bubble');
@@ -31,197 +35,22 @@ describe('notice-bubble Dependency imports', () => {
 describe('NoticeBubbleContainer', () => {
   beforeEach(cleanup);
 
-  // shouldSupportClassName(NoticeBubble);
-  // shouldSupportRef(NoticeBubble);
+  shouldSupportClassName(TestNoticeBubble);
+  shouldSupportRef(TestNoticeBubble);
 
-  test('should support render outside dom', () => {
+  test('Verify supports rendering outside DOM', () => {
     const { queryByTestId } = render(
       <div data-testid='container'>
         <NoticeBubbleContainer data-testid='notice' />
       </div>,
     );
-    // Not render to container,
-    expect(queryByTestId('container')?.children.length).toEqual(0);
 
-    // but render to body
-    expect((document.body.lastChild as HTMLElement)?.dataset['testid']).toEqual('notice');
-  });
-});
+    // not renders in container
+    expect(queryByTestId('container')?.children.length).toBe(0);
 
-describe('NoticeBubble Timer', () => {
-  beforeEach(cleanup);
-  test.skip('should support pause timer at mouse enter', () => {
-    vi.useFakeTimers();
-    const spy = vi.fn();
-    const { getByTestId } = render(<NoticeBubble data-testid='notice' onClose={spy} />);
-    fireEvent.mouseEnter(getByTestId('notice'));
-    act(() => {
-      vi.runAllTimers();
-    });
-
-    expect(spy).not.toBeCalled();
-
-    fireEvent.mouseLeave(getByTestId('notice'));
-    act(() => {
-      vi.runAllTimers();
-    });
-
-    expect(spy).toBeCalled();
-    vi.useRealTimers();
-  });
-});
-
-describe('NoticeBubble', () => {
-  beforeEach(cleanup);
-
-  // shouldSupportClassName(NoticeBubble);
-  // shouldSupportRef(NoticeBubble);
-
-  test.skip('should support handler for close', () => {
-    const spy = vi.fn();
-    const { getByTitle } = render(<NoticeBubble onClose={spy} />);
-    fireEvent.click(getByTitle('Close'));
-    expect(spy).toBeCalled();
-  });
-
-  test.skip('should support closing after some time', () => {
-    vi.useFakeTimers();
-    const spy = vi.fn();
-    render(<NoticeBubble duration={300} onClose={spy} />);
-
-    act(() => {
-      vi.runAllTimers();
-    });
-
-    expect(spy).toBeCalled();
-    vi.useRealTimers();
-  });
-
-  test('should support the possibility of not closing', () => {
-    const manager = new NoticeBubbleManager();
-    vi.useFakeTimers();
-    const spy = vi.fn();
-    render(<NoticeBubble duration={0} onClose={spy} manager={manager} />);
-
-    act(() => {
-      vi.runAllTimers();
-    });
-
-    expect(spy).not.toBeCalled();
-    vi.useRealTimers();
-  });
-
-  test.concurrent('should support hover for icon close', async ({ task }) => {
-    const manager = new NoticeBubbleManager();
-    const component = (
-      <>
-        <NoticeBubbleContainer
-          style={{ position: 'static', width: 'auto', height: 'auto' }}
-          disablePortal
-          manager={manager}
-        />
-        <NoticeBubbleImport id='notice' manager={manager}>
-          Message
-        </NoticeBubbleImport>
-      </>
-    );
-    await expect(
-      await snapshot(component, { actions: { hover: '#notice [aria-label="Close"]' } }),
-    ).toMatchImageSnapshot(task);
-  });
-
-  test.concurrent('should support show more one notice', () => {
-    const manager = new NoticeBubbleManager();
-    render(
-      <React.Fragment>
-        <NoticeBubbleContainer data-testid='container' manager={manager} />
-        <NoticeBubbleImport data-testid='notice-1' visible manager={manager} />
-        <NoticeBubbleImport data-testid='notice-2' visible manager={manager} />
-      </React.Fragment>,
-    );
-
-    const container = document.body.querySelector('[data-testid="container"]');
-    expect(container?.querySelector('[data-testid="notice-1"]')).toBeTruthy();
-    expect(container?.querySelector('[data-testid="notice-2"]')).toBeTruthy();
-  });
-
-  test('should support render outside dom', () => {
-    const manager = new NoticeBubbleManager();
-    render(
-      <React.Fragment>
-        <NoticeBubbleContainer data-testid='container' manager={manager} />
-        <NoticeBubbleImport data-testid='notice' visible manager={manager} />
-      </React.Fragment>,
-    );
-
-    const container = document.body.querySelector('[data-testid="container"]');
-    expect(container?.querySelector('[data-testid="notice"]')).toBeTruthy();
-  });
-
-  test.concurrent('should render correctly', async ({ task }) => {
-    const manager = new NoticeBubbleManager();
-    const component = (
-      <>
-        <NoticeBubbleContainer
-          style={{ position: 'static', width: 'auto', height: 'auto' }}
-          disablePortal
-          manager={manager}
-        />
-        <NoticeBubbleWarningImport manager={manager}>Message</NoticeBubbleWarningImport>
-        <NoticeBubbleImport manager={manager}>Message</NoticeBubbleImport>
-      </>
-    );
-    await expect(await snapshot(component)).toMatchImageSnapshot(task);
-  });
-
-  test.concurrent('should support action node', async ({ task }) => {
-    const manager = new NoticeBubbleManager();
-    const component = (
-      <>
-        <NoticeBubbleContainer
-          style={{ position: 'static', width: 'auto', height: 'auto' }}
-          disablePortal
-          manager={manager}
-        />
-        <NoticeBubbleWarningImport
-          action={(
-            <button type='button' data-testid='action'>
-              Action
-            </button>
-          )}
-          manager={manager}
-        >
-          Message
-        </NoticeBubbleWarningImport>
-        <NoticeBubbleImport
-          action={(
-            <button type='button' data-testid='action'>
-              Action
-            </button>
-          )}
-          manager={manager}
-        >
-          Message
-        </NoticeBubbleImport>
-      </>
-    );
-    await expect(await snapshot(component)).toMatchImageSnapshot(task);
-  });
-
-  test.concurrent('Should render correctly for screen size 760px', async ({ task }) => {
-    const manager = new NoticeBubbleManager();
-    const component = (
-      <div style={{ width: '320px', height: '90px', position: 'relative' }}>
-        <NoticeBubbleContainer disablePortal manager={manager} />
-        <NoticeBubbleImport manager={manager}>Message</NoticeBubbleImport>
-      </div>
-    );
-    await expect(
-      await snapshot(component, {
-        selector: 'body',
-        width: 320,
-        height: 100,
-      }),
-    ).toMatchImageSnapshot(task);
+    // should be in body
+    expect(
+      Array.from(document.body.querySelectorAll('[data-testid="notice"]')).length,
+    ).toBeGreaterThan(0);
   });
 });
