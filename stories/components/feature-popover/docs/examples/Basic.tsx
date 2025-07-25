1ed989d9af97b172180af7f200e7121c61f4675b
Julia Mnizhek <77798437+j-mnizhek@users.noreply.github.com>
2025-07-25 10:19:03 +0200

    Update stories/components/feature-popover/docs/examples/Basic.tsx
    Co-authored-by: ilyabrower <ilia.brauer@semrush.com>
    Signed-off-by: Julia Mnizhek <77798437+j-mnizhek@users.noreply.github.com>

--- stories/components/feature-popover/docs/examples/Basic.tsx
+++ stories/components/feature-popover/docs/examples/Basic.tsx
@@ -25,7 +25,7 @@ const Demo = () => {
           </DropdownMenu>
           {visible && <FeaturePopover.Spot />}
         </FeaturePopover.Trigger>
-        <FeaturePopover.Popper closeIcon wMax={400} aria-label='New feature: Export' style={{ width: '400px' }}>
+        <FeaturePopover.Popper closeIcon w={400} aria-label='New feature: Export'>
           <Flex alignItems='start'>
             <Box
               w={40}
