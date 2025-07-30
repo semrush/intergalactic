18c65409410b9e3b79103f60b2d6435915d2c87d
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-07-30 17:54:13 +0200

    [stories] loading props in sticky accordion

--- stories/components/data-table/advanced/data-table.advanced.stories.tsx
+++ stories/components/data-table/advanced/data-table.advanced.stories.tsx
@@ -2,7 +2,7 @@ import { DataTable } from '@semcore/data-table';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import AccordionInMergedRowsExample from './examples/accordion_in_merged_rows';
-import AccordionWithStickyRowsExample from './examples/accordion_with_sticky_rows';
+import AccordionWithStickyRowsExample, { accordionStickyProps } from './examples/accordion_with_sticky_rows';
 import FakeMultiLineHeaderExample from './examples/fake-multi-level-header';
 import FixedColumnsWithDiffWidthsExample from './examples/fixed_columns_with_diff_widths';
 import LinkInTableExample from './examples/link_in_table';
@@ -22,8 +22,12 @@ export const AccordionInMergedRows: Story = {
   render: AccordionInMergedRowsExample,
 };
 
-export const AccordionWithStickyRows: Story = {
+export const AccordionWithStickyRows: StoryObj<typeof accordionStickyProps> = {
   render: AccordionWithStickyRowsExample,
+  argTypes: {
+    loading: { control: 'boolean' },
+  },
+  args: accordionStickyProps,
 };
 
 export const FixedColumnsWithDiffWidths: Story = {
