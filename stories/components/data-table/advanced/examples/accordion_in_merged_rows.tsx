60501e638ba94460e3f8fa3c42430cb2b9864b93
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-07-31 11:09:59 +0200

    [data-table] added some props to stories

--- stories/components/data-table/advanced/examples/accordion_in_merged_rows.tsx
+++ stories/components/data-table/advanced/examples/accordion_in_merged_rows.tsx
@@ -47,10 +47,13 @@ const data = [
   },
 ];
 
-export default function App() {
+type accordionMergedProps = { loading: boolean };
+const Demo = (props: accordionMergedProps) => {
   return (
     <DataTable
       data={data}
+      h={100}
+      loading={props.loading}
       aria-label='Rows grouping'
       columns={[
         { name: 'keyword', children: 'Keyword' },
@@ -60,4 +63,12 @@ export default function App() {
       ]}
     />
   );
-}
+};
+
+export const accordionMergedProps = {
+  loading: false,
+};
+
+Demo.defaultMergedProps = accordionMergedProps;
+
+export default Demo;
