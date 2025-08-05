1fdd1e099c3b20303bac227a6e503ed36965d75e
ilia.brauer <ilia.brauer@semrush.com>
2025-08-05 15:06:15 +0200

    [data-table] fixed types for Group in head

--- semcore/data-table/src/components/Head/Group.type.ts
+++ semcore/data-table/src/components/Head/Group.type.ts
@@ -23,5 +23,5 @@ export type GroupPropsInner = {
   getFixedStyle: (
     cell: Pick<DTColumn, 'name' | 'fixed'>,
   ) => [side: 'left' | 'right', style: string | number] | [side: undefined, style: undefined];
-  shadowVertical?: BodyPropsInner<UniqKeyType>['shadowVertical'];
+  shadowVertical?: BodyPropsInner<any>['shadowVertical'];
 };
