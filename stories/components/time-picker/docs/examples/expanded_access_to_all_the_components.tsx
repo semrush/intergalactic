c3ef36c913bce14b240dcc443aabb1f967306a89
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-08-13 17:48:00 +0200

    [time-picker] update tests

--- stories/components/time-picker/docs/examples/expanded_access_to_all_the_components.tsx
+++ stories/components/time-picker/docs/examples/expanded_access_to_all_the_components.tsx
@@ -1,12 +1,9 @@
 import { Box } from '@semcore/base-components';
 import TimePicker from '@semcore/time-picker';
-import type { TimePickerProps, TimePickerItemProps } from '@semcore/time-picker';
 import { Text } from '@semcore/typography';
 import React from 'react';
 
-type baseExampleProps = TimePickerProps & TimePickerItemProps;
-
-const Demo = (props: baseExampleProps) => {
+const Demo = () => {
   return (
     <>
       <Text tag='label' htmlFor='time-picker' size={200}>
