9b16100058ab66c065f90cd85aa0b92caffe27c5
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-08-18 12:24:13 +0200

    [wizard] added 2 new test stories

--- stories/components/wizard/tests/Wizard.stories.tsx
+++ stories/components/wizard/tests/Wizard.stories.tsx
@@ -2,6 +2,8 @@ import Wizard from '@semcore/wizard';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import SteppersAndButtonsStatesExample from './examples/steps_and_buttons_states';
+import WizardContentExample from './examples/wizard-content';
+import WizardSidebarRightExample from './examples/wizard-sidebar-right';
 
 const meta: Meta<typeof Wizard> = {
   title: 'Components/Wizard/Tests',
@@ -13,4 +15,12 @@ type Story = StoryObj<typeof Wizard>;
 
 export const SteppersAndButtonsStates: Story = {
   render: SteppersAndButtonsStatesExample,
+};
+
+export const WizardContent: Story = {
+  render: WizardContentExample,
+};
+
+export const WizardSidebarRight: Story = {
+  render: WizardSidebarRightExample,
 };
