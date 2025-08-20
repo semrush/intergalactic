436eecd2f4d99c6d3c6fb27a3e132b8401f28f98
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-08-20 13:45:53 +0200

    [wizard] remove not relevant example

--- stories/components/wizard/tests/Wizard.stories.tsx
+++ stories/components/wizard/tests/Wizard.stories.tsx
@@ -3,7 +3,6 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import SteppersAndButtonsStatesExample from './examples/steps_and_buttons_states';
 import WizardContentExample from './examples/wizard-content';
-import WizardSidebarRightExample from './examples/wizard-sidebar-right';
 
 const meta: Meta<typeof Wizard> = {
   title: 'Components/Wizard/Tests',
@@ -19,8 +18,4 @@ export const SteppersAndButtonsStates: Story = {
 
 export const WizardContent: Story = {
   render: WizardContentExample,
-};
-
-export const WizardSidebarRight: Story = {
-  render: WizardSidebarRightExample,
 };
