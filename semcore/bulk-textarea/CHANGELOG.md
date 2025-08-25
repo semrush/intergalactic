dae0d1d0e4f485fd9463bcaab2cd0505c2eeadb7
ilyabrower <ilia.brauer@semrush.com>
2025-08-25 15:24:50 +0200

    [UIK-3928][bulk-textarea] added controlled mode for errors (#2408)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- semcore/bulk-textarea/CHANGELOG.md
+++ semcore/bulk-textarea/CHANGELOG.md
@@ -2,6 +2,17 @@
 
 CHANGELOG.md standards are inspired by [keepachangelog.com](https://keepachangelog.com/en/1.0.0/).
 
+## [16.4.0] - 2025-08-29
+
+### Added
+
+- Controlled mode for errors.
+- `onImmediatelyChange` internal property.
+
+### Fixed
+
+- Incorrect paddings for line numbers starting from 1000 lines.
+
 ## [16.3.1] - 2025-08-29
 
 ### Changed
---
+++ stories/components/bulk-textarea/tests/examples/controlled-errors.tsx
@@ -0,0 +1,65 @@
+import BulkTextarea from '@semcore/bulk-textarea';
+import Button from '@semcore/button';
+import { Box, Flex } from '@semcore/flex-box';
+import { Text } from '@semcore/typography';
+import React from 'react';
+
+const Demo = () => {
+  const [index, setIndex] = React.useState(0);
+  const [value, setValue] = React.useState('');
+  const [errors, setErrors] = React.useState<any[]>([]);
+
+  const validate = React.useCallback(() => {
+    const error = {
+      lineIndex: index,
+      errorMessage: 'some error in row',
+    };
+    setIndex((prev) => prev + 1);
+    setErrors([error]);
+  }, [index, setErrors, setIndex]);
+
+  return (
+    <Box>
+      <BulkTextarea
+        w={400}
+        value={value}
+        onChange={setValue}
+        maxLines={10}
+        size='l'
+        linesDelimiters={[',']}
+        readonly={false}
+        disabled={false}
+        placeholder='Enter or paste a list using comma or Enter'
+        minRows={2}
+        maxRows={10}
+        errors={errors}
+        onErrorsChange={setErrors}
+        showErrors={true}
+        pasteProps={{
+          delimiter: '\n',
+          skipEmptyLines: true,
+        }}
+        onImmediatelyChange={(lines) => console.log(lines)}
+      >
+        <Flex alignItems='center' justifyContent='flex-start' mb={2} gap={1}>
+          <Text tag='label' size={300} id='keywords-label'>
+            Favourite movies
+          </Text>
+          <BulkTextarea.Counter />
+        </Flex>
+        <BulkTextarea.InputField
+          aria-labelledby='keywords-label'
+          commonErrorMessage=''
+          state='normal'
+        />
+        <Flex alignItems='center' justifyContent='space-between' mt={2}>
+          <BulkTextarea.ErrorsNavigation />
+          <BulkTextarea.ClearAll />
+        </Flex>
+      </BulkTextarea>
+      <Button onClick={validate}>Validate</Button>
+    </Box>
+  );
+};
+
+export default Demo;
