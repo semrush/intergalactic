759ebe21dd162ace38beb1445724e5c5179b5719
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-08-26 19:20:22 +0200

    [feature-highlight] added tests

--- semcore/feature-highlight/__tests__/index.test.tsx
+++ semcore/feature-highlight/__tests__/index.test.tsx
@@ -1,32 +1,19 @@
-import { render, cleanup } from '@semcore/testing-utils/testing-library';
-import { expect, test, it, describe, beforeEach } from '@semcore/testing-utils/vitest';
+import { render } from '@semcore/testing-utils/testing-library';
+import { expect, test, describe } from '@semcore/testing-utils/vitest';
 import React, { createRef } from 'react';
 
 import SvgSparkle from '../src/inner-components/sparkle/Sparkle';
 
 describe('SvgSparkle', () => {
-  it('renders an SVG element', () => {
+  test('Verify renders an SVG element', () => {
     const { container } = render(<SvgSparkle num={5} index={0} />);
     const svg = container.querySelector('svg');
     expect(svg).toBeTruthy();
   });
 
-  it('forwards ref to svg element', () => {
+  test('Verify forwards ref to svg element', () => {
     const ref = createRef<SVGSVGElement>();
     render(<SvgSparkle num={5} index={1} ref={ref} />);
     expect(ref.current).toBeInstanceOf(SVGElement);
-  });
-
-  it('applies aria-hidden', () => {
-    const { container } = render(<SvgSparkle num={5} index={2} />);
-    const svg = container.querySelector('svg');
-    expect(svg?.getAttribute('aria-hidden')).toBe('true');
-  });
-
-  it('renders <path> inside svg', () => {
-    const { container } = render(<SvgSparkle num={5} index={3} />);
-    const path = container.querySelector('path');
-    expect(path).toBeTruthy();
-    expect(path?.getAttribute('d')).toContain('M5.371');
   });
 });
