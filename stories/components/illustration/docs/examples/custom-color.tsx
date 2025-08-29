8e1346d88817a29ab94aa70ace8c14148af5e43a
Julia Mnizhek <j.mnizhek@semrush.com>
2025-08-29 14:10:08 +0200

    [core] naming updates

--- stories/components/illustration/docs/examples/custom-color.tsx
+++ stories/components/illustration/docs/examples/custom-color.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box } from '@semcore/base-components';
+import { Flex } from '@semcore/base-components';
 import Congrats from '@semcore/illustration/Congrats';
 import MailSent from '@semcore/illustration/MailSent';
 import React from 'react';
@@ -6,18 +6,18 @@ import React from 'react';
 const Demo = () => {
   return (
     <Flex gap={5} direction='column'>
-      <Box display='flex'>
+      <Flex gap={2}>
         <MailSent />
-        <MailSent primaryColor='--intergalactic-illustration-green' />
+        <MailSent primaryColor='--intergalactic-illustration-salad' />
         <MailSent primaryColor='--intergalactic-illustration-blue' />
         <MailSent primaryColor='--intergalactic-illustration-orange' />
-      </Box>
-      <Box display='flex'>
-        <Congrats primaryColor='--intergalactic-illustration-salmon' secondaryColor='#000' />
+      </Flex>
+      <Flex gap={2}>
+        <Congrats primaryColor='--intergalactic-illustration-red' secondaryColor='#000' />
         <Congrats primaryColor='--intergalactic-illustration-pink' secondaryColor='#000' />
         <Congrats primaryColor='--intergalactic-illustration-violet' secondaryColor='#000' />
         <Congrats primaryColor='--intergalactic-illustration-yellow' secondaryColor='#000' />
-      </Box>
+      </Flex>
     </Flex>
   );
 };
