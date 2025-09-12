0ee96d5bc5251ce1ddfd84399a8f443fc737e6c0
slizhevskyv-semrush <uladzislau.slizheuski@semrush.com>
2025-09-12 17:14:10 +0200

    [UIK-4147][website] playground - fixed accessible names for checkbox inputs and radiogroup elements (#2461)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Checkbox inputs and radiogroup elements render without accessible names.
    
    ## How has this been tested?
    
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new tests on added of fixed functionality.

--- playground/components/Controls/BooleanControl.tsx
+++ playground/components/Controls/BooleanControl.tsx
@@ -7,8 +7,15 @@ interface IBooleanControlProps extends BooleanControlType {
   onChange: (value: boolean) => void;
 }
 
-function BooleanControl({ value, onChange }: IBooleanControlProps) {
-  return <Checkbox mt={1} checked={value} onChange={onChange} />;
+function BooleanControl({ value, onChange, displayName }: IBooleanControlProps) {
+  return (
+    <Checkbox mt={1} checked={value} onChange={onChange} display='contents'>
+      <Checkbox.Text ml={0}>
+        {displayName}
+      </Checkbox.Text>
+      <Checkbox.Value />
+    </Checkbox>
+  );
 }
 
 export default BooleanControl;
