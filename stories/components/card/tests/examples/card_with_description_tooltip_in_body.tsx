8d1745adaebb5f0fbfc3d38d346f1b3d5046e6ec
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-09-23 10:26:20 +0200

    [stories] update card test story

--- stories/components/card/tests/examples/card_with_description_tooltip_in_body.tsx
+++ stories/components/card/tests/examples/card_with_description_tooltip_in_body.tsx
@@ -32,7 +32,7 @@ const Demo = () => (
         <DescriptionTooltip.Trigger tag={Button}>
           DescriptionTooltipTitle
         </DescriptionTooltip.Trigger>
-        <DescriptionTooltip.Popper aria-label='Hello'>Hello, stranger!</DescriptionTooltip.Popper>
+        <DescriptionTooltip.Popper aria-label='Hello2'>Hello, stranger!</DescriptionTooltip.Popper>
       </DescriptionTooltip>
       Market Traffic
     </Card.Title>
@@ -50,7 +50,7 @@ const Demo = () => (
         <DescriptionTooltip.Trigger tag={Button}>
           DescriptionTooltipDescription
         </DescriptionTooltip.Trigger>
-        <DescriptionTooltip.Popper aria-label='Hello'>Hello, stranger!</DescriptionTooltip.Popper>
+        <DescriptionTooltip.Popper aria-label='Hello3'>Hello, stranger!</DescriptionTooltip.Popper>
       </DescriptionTooltip>
       Info about data (optional)
     </Card.Description>
