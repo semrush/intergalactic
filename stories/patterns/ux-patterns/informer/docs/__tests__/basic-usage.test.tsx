e2b436a0ee7579ef30228fe6c31fe5d73359d718
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-09-29 18:28:41 +0200

    [feedback-form] update test

--- stories/patterns/ux-patterns/informer/docs/__tests__/basic-usage.test.tsx
+++ stories/patterns/ux-patterns/informer/docs/__tests__/basic-usage.test.tsx
@@ -3,4 +3,5 @@ import { expect, userEvent, within } from 'storybook/test';
 export async function BasicUsageTest({ canvasElement }: { canvasElement: HTMLElement }) {
   await userEvent.keyboard('{Tab}');
   await userEvent.keyboard('{Tab}');
+  await userEvent.keyboard('{Enter}');
 }
