b2520408253ee8be98b28cd332089b0dde1112db
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-09-29 17:14:11 +0200

    [mini-chart] update test

--- semcore/mini-chart/__tests__/mini-chart.browser-test.tsx
+++ semcore/mini-chart/__tests__/mini-chart.browser-test.tsx
@@ -35,40 +35,12 @@ test.describe('Visual-Score Donuts', () => {
       if (item.value === 0) {
         await test.step('Verify Donut', async () => {
           expect(donutCirclesCount).toEqual(1);
-
           expect(donutCircles.first()).toHaveAttribute('stroke-dashoffset', '0');
-
-          // there's no more nth(1) circle when value = 0
-
-          // expect(donutCircles.nth(1)).not.toHaveAttribute('stroke-dashoffset', '0');
-
-          // const firstAttr = await donutCircles.nth(0).getAttribute('stroke-dasharray');
-          // const secondAttr = await donutCircles.nth(1).getAttribute('stroke-dasharray');
-
-          // const firstValues = firstAttr?.split(' ').map(parseFloat) || [];
-          // const secondValues = secondAttr?.split(' ').map(parseFloat) || [];
-
-          // expect(secondValues[0]).toBe(0);
-
-          // expect(firstValues[0]).toBe(secondValues[1]);
         });
 
         await test.step('Verify SemiDonut', async () => {
           expect(semiDonutCirclesCount).toEqual(1);
-
-          // there's no more nth(1) circle when value = 0
-
-          // const firstAttr = await semiDonutCircles.nth(0).getAttribute('stroke-dasharray');
-          // const secondAttr = await semiDonutCircles.nth(1).getAttribute('stroke-dasharray');
-
-          // const firstValues = firstAttr?.split(' ').map(parseFloat) || [];
-          // const secondValues = secondAttr?.split(' ').map(parseFloat) || [];
-
-          // expect(secondValues[0]).toBe(0);
-
-          // expect(firstValues[0]).toBe(secondValues[1]);
           expect(semiDonutCircles.first()).toHaveAttribute('stroke-dashoffset', '0');
-          // expect(semiDonutCircles.nth(1)).not.toHaveAttribute('stroke-dashoffset', '0');
         });
       } else if (item.value > 0 && item.value < 100) {
         expect(donutCirclesCount).toEqual(2);
