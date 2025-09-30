504acbfee49482cfb9c0e61393807c812caab281
Julia Mnizhek <j.mnizhek@semrush.com>
2025-09-30 12:05:01 +0200

    [docs] update List mappings

--- figma/List.figma.jsx
+++ figma/List.figma.jsx
@@ -6,19 +6,19 @@ figma.connect(
   'https://www.figma.com/design/RLic9ruqNNm6qgARKFk5Ae/-Refactoring-WIP--%E2%9D%96-Core-Components?node-id=11735-134804',
   {
     props: {
-      size: figma.enum('font size', {
-        '300 (16px)': 300,
-        '200 (14px)': 200,
-        '100 (12px)': 100,
-      }),
+      // size: figma.enum('font size', {
+      //   '300 (16px)': 300,
+      //   '200 (14px)': 200,
+      //   '100 (12px)': 100,
+      // }),
       marker: figma.enum('list type', {
         'numbers (ol)': figma.textContent('number'),
         'custom marker (ul)': figma.instance('marker'),
       }),
       text: figma.string('↳ text'),
     },
-    example: ({ size, marker, text }) => (
-      <List.Item size={size} marker={marker}>
+    example: ({ marker, text }) => (
+      <List.Item marker={marker}>
         {text}
       </List.Item>
     ),
