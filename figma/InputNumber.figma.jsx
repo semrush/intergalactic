bc94cf0ecce11b29066e8f6e65ec84ef9c0e6826
Julia Mnizhek <j.mnizhek@semrush.com>
2025-10-03 12:34:30 +0200

    [docs] add react import to InputNumber

--- figma/InputNumber.figma.jsx
+++ figma/InputNumber.figma.jsx
@@ -1,6 +1,7 @@
 import figma from '@figma/code-connect';
 import { Flex } from '@semcore/ui/base-components';
 import InputNumber from '@semcore/ui/input-number';
+import React from 'react';
 
 figma.connect(
   InputNumber,
