35c2967737ac526329fafcf1bb6d82a7d2412e29
ilia.brauer <ilia.brauer@semrush.com>
2025-10-16 14:59:48 +0200

    [chore] added deprecate messages for some components

--- semcore/breakpoints/src/index.ts
+++ semcore/breakpoints/src/index.ts
@@ -5,6 +5,22 @@ import {
   createBreakpoints,
 } from '@semcore/base-components';
 
+/**
+ * @deprecated Use defaultBreakpoints from `@semcore/base-components` instead. Package `@semcore/breacpoints` will be removed in the next major release
+ */
 export default defaultBreakpoints;
 
-export { MediaList, DEFAULT_MEDIA, createBreakpoints };
+export {
+  /**
+   * @deprecated Use MediaList from `@semcore/base-components` instead. Package `@semcore/breacpoints` will be removed in the next major release
+   */
+  MediaList,
+  /**
+   * @deprecated Use DEFAULT_MEDIA from `@semcore/base-components` instead. Package `@semcore/breacpoints` will be removed in the next major release
+   */
+  DEFAULT_MEDIA,
+  /**
+   * @deprecated Use createBreakpoints from `@semcore/base-components` instead. Package `@semcore/breacpoints` will be removed in the next major release
+   */
+  createBreakpoints,
+};
