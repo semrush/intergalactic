c154d07d1617cf669d2ca06e82194f7e19cbaca3
ilia.brauer <ilia.brauer@semrush.com>
2025-10-16 09:54:25 +0200

    [data-table] fixed spinner height, depending on scrolling

--- semcore/data-table/src/components/Body/Body.tsx
+++ semcore/data-table/src/components/Body/Body.tsx
@@ -148,6 +148,22 @@ class BodyRoot<Data extends DataTableData, UniqKeyType> extends Component<DataTa
     };
   }
 
+  getSpinnerTopOffset = () => {
+    const { headerHeight: propsHeaderHeight, tableContainerRef } = this.asProps;
+
+    let headerHeight = propsHeaderHeight;
+
+    if (tableContainerRef.current) {
+      if (tableContainerRef.current.scrollTop > headerHeight) {
+        headerHeight = 0;
+      } else {
+        headerHeight = headerHeight - tableContainerRef.current.scrollTop;
+      }
+    }
+
+    return headerHeight;
+  };
+
   render() {
     const SBody = Root;
     const SRowGroup = Box;
@@ -155,7 +171,6 @@ class BodyRoot<Data extends DataTableData, UniqKeyType> extends Component<DataTa
     const {
       styles,
       loading,
-      headerHeight,
       spinnerRef,
       virtualScroll,
       scrollDirection,
@@ -169,8 +184,6 @@ class BodyRoot<Data extends DataTableData, UniqKeyType> extends Component<DataTa
     } = this.asProps;
 
     let rowsToRender = rows;
-    // let startIndex = -1;
-    // let lastIndex = -1;
 
     if (virtualScroll) {
       const rowsBuffer =
@@ -335,7 +348,7 @@ class BodyRoot<Data extends DataTableData, UniqKeyType> extends Component<DataTa
           <SSpinContainer
             innerOutline
             // @ts-ignore
-            headerHeight={`${headerHeight}px`}
+            headerHeight={`${this.getSpinnerTopOffset()}px`}
             tabIndex={-1}
             ref={spinnerRef}
             role='row'
