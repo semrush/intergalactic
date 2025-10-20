efdbf01e8d8ee3da5e1e6bac1cc91e7f7483b2a9
Sheila Sheikh <166654065+sheila-semrush@users.noreply.github.com>
2025-10-20 17:37:51 +0200

    [docs] connect TimePicker

---
+++ figma/TimePicker.figma.template.js
@@ -0,0 +1,26 @@
+// https://www.figma.com/design/RLic9ruqNNm6qgARKFk5Ae/-Refactoring-WIP--%E2%9D%96-Core-Components?node-id=10301-133764
+// import TimePicker from '@semcore/ui/time-picker'
+
+const figma = require('figma');
+const instance = figma.selectedInstance;
+
+const is12Hour = instance.getBoolean('12 hours')
+  ? 'is12Hour'
+  : undefined;
+
+const disabled = instance.getEnum('state', { disabled: 'disabled' });
+const state = instance.getEnum('state', { invalid: 'state="invalid"' });
+const size = instance.getEnum('size', { L: 'size="l"' });
+
+export default {
+  example: figma.tsx`
+<TimePicker
+  aria-label={/* text */}
+  ${is12Hour}
+  ${size}
+  ${disabled}
+  ${state}
+/>
+  `,
+  id: 'TimePicker',
+};
