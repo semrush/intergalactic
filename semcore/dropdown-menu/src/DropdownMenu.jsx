d744bf97dbad6892c59d2c590bb7f11cfc15c74d
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2025-10-22 16:50:39 +0200

    [UIK-3872][dropdown] fixed dd menu animation for keyboard interaction

--- semcore/dropdown-menu/src/DropdownMenu.jsx
+++ semcore/dropdown-menu/src/DropdownMenu.jsx
@@ -102,7 +102,7 @@ class DropdownMenuRoot extends AbstractDropdown {
   }
 
   afterOpenPopper() {
-    const { selected, options } = this.getMenuElements();
+    const { selected, options } = this.getMenuElements;
 
     // this case is handled slightly differently on line 63.
     if (selected && options && this.asProps.itemsCount === undefined) return;
