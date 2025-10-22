81f68e57f19b5eecd1b9fb65029ea5059211c686
ilyabrower <ilia.brauer@semrush.com>
2025-10-22 14:29:21 +0200

    [chore] added deprecate messages for some components (#2535)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new tests on added of fixed functionality.

--- semcore/outside-click/src/index.ts
+++ semcore/outside-click/src/index.ts
@@ -1,4 +1,16 @@
 import { OutsideClick } from '@semcore/base-components';
 import type { OutsideClickProps } from '@semcore/base-components';
-export { OutsideClick, type OutsideClickProps };
+export {
+  /**
+   * @deprecated Use OutsideClick from `@semcore/base-components` instead. Package `@semcore/outside-click` will be removed in the next major release
+   */
+  OutsideClick,
+  /**
+   * @deprecated Use OutsideClickProps from `@semcore/base-components` instead. Package `@semcore/outside-click` will be removed in the next major release
+   */
+  type OutsideClickProps,
+};
+/**
+ * @deprecated Use OutsideClick from `@semcore/base-components` instead. Package `@semcore/outside-click` will be removed in the next major release
+ */
 export default OutsideClick;
