cc1e9a055ebe7150af828fb90d3708e23e5a9ef0
Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-11-20 11:14:36 +0100

    [UIK-4378][dropdown] update and tag tests (#2578)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added changelog note to corresponding `CHANGELOG.md` file
    with planned publish date.
    - [ ] I have added new tests on added of fixed functionality.

---
+++ stories/components/dropdown/tests/examples/disable-enforce-focus-and-auto-focus.tsx
@@ -0,0 +1,58 @@
+import type { PopperProps, PopperTriggerProps, PopperPopperProps } from '@semcore/ui/base-components';
+import Button from '@semcore/ui/button';
+import Dropdown from '@semcore/ui/dropdown';
+import type { DropdownProps } from '@semcore/ui/dropdown';
+import FileExportM from '@semcore/ui/icon/FileExport/m';
+import Tooltip from '@semcore/ui/tooltip';
+import { Text } from '@semcore/ui/typography';
+import React from 'react';
+
+type DisableEnforceAndAutoFocusExampleProps = PopperPopperProps & PopperTriggerProps & PopperProps & DropdownProps;
+
+const Demo = (props: DisableEnforceAndAutoFocusExampleProps) => (
+  <>
+    <Dropdown>
+      <Dropdown.Trigger id='dropdown-basic' tag={Button} ml={2} disableEnforceFocus={props.disableEnforceFocus}>
+        disableEnforceFocus trigger
+      </Dropdown.Trigger>
+      <Dropdown.Popper p={4} wMax={260} aria-labelledby='dropdown-basic' autoFocus={props.autoFocus}>
+        <Text size={200}>
+          Hello there! I'm Dropdown's content
+        </Text>
+        <Tooltip
+          title='Default tooltip contains short text explaining something about the trigger'
+          tag={Button}
+          aria-label='Export to PDF'
+          addonLeft={FileExportM}
+        />
+      </Dropdown.Popper>
+    </Dropdown>
+
+    <Dropdown>
+      <Dropdown.Trigger id='dropdown-basic' tag={Button} ml={2}>
+        disableEnforceFocus popper
+      </Dropdown.Trigger>
+      <Dropdown.Popper p={4} wMax={260} aria-labelledby='dropdown-basic' disableEnforceFocus={props.disableEnforceFocus} autoFocus={props.autoFocus}>
+        <Text size={200}>
+          Hello there! I'm Dropdown's content
+        </Text>
+        <Tooltip
+          title='Default tooltip contains short text explaining something about the trigger'
+          tag={Button}
+          aria-label='Export to PDF'
+          addonLeft={FileExportM}
+        />
+      </Dropdown.Popper>
+    </Dropdown>
+  </>
+);
+
+export const defaultDisableEnforceAndAutoFocusExampleProps: DisableEnforceAndAutoFocusExampleProps = {
+  disableEnforceFocus: undefined,
+  autoFocus: undefined,
+
+};
+
+Demo.defaultProps = defaultDisableEnforceAndAutoFocusExampleProps;
+
+export default Demo;
