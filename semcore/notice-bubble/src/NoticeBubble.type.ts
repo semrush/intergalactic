0f3cf6a8f107094102607e8b5746f67cb2d496c1
ilyabrower <ilia.brauer@semrush.com>
2025-11-28 13:57:32 +0100

    [UIK-3307][notice-bubble] all notices in one container (#2590)
    ## Changelog
    
    ### @semcore/notice-bubble
    
    #### Added
    
    - `containerNode` property to set one common container for all notices
    on the page from different applications.
    
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    Manually
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [X] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [X] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added new tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>
    Co-authored-by: Sheila Sheikh <166654065+sheila-semrush@users.noreply.github.com>

---
+++ semcore/notice-bubble/src/NoticeBubble.type.ts
@@ -0,0 +1,141 @@
+import type { PortalProps, BoxProps } from '@semcore/base-components';
+import type { UnknownProperties, Intergalactic } from '@semcore/core';
+import type { useI18n } from '@semcore/core/lib/utils/enhances/WithI18n';
+import type { NodeByRef } from '@semcore/core/lib/utils/ref';
+import type { RefObject } from 'react';
+
+/** @deprecated */
+export interface INoticeBubbleContainerProps
+  extends NoticeBubbleContainerProps,
+  UnknownProperties {}
+
+/**
+ * @deprecated. Pass noticeBubbleContainer property from window.sm2.getNoticeBubbleContainer()
+ */
+export type NoticeBubbleContainerPortalProps = PortalProps;
+
+export type NoticeBubbleContainerProps = BoxProps &
+  NoticeBubbleContainerPortalProps & {
+    /** Ref or element to mount bubbles in. You should use element form window.sm2.getNoticeBubbleContainer() */
+    containerNode?: NodeByRef;
+    /** Manager copy */
+    manager?: NoticeBubbleManagerClass;
+    /** Specifies the locale for i18n support */
+    locale?: string;
+  };
+
+export type NoticeBubbleViewItemProps = NoticeBubbleProps & {
+  containerNode?: NodeByRef;
+  animationDuration: number;
+  getI18nText: ReturnType<typeof useI18n>;
+  styles: React.DetailedHTMLProps<React.StyleHTMLAttributes<HTMLStyleElement>, HTMLStyleElement> | undefined;
+};
+
+/** @deprecated */
+export interface INoticeBubbleProps extends NoticeBubbleProps, UnknownProperties {}
+export type NoticeBubbleProps = BoxProps & {
+  /** Notice type */
+  type?: 'info' | 'warning';
+  /**
+   * Notice display duration. Set to 0 to disable auto-close.
+   */
+  duration?: number;
+  /**
+   * Enables animation on first rendering.
+   * @default false
+   */
+  initialAnimation?: boolean;
+  /**
+   * Use it for complex notices with important controls.
+   * If enabled, browser focus will be locked in the notice
+   * until it's closed. After close focus should return to the element
+   * where it was placed before notice appear.
+   */
+  focusLock?: boolean;
+  /**
+   * Notice visibility.
+   */
+  visible?: boolean;
+  /**
+   * Control under the notice children.
+   * */
+  action?: React.ReactNode;
+  /**
+   * Callback triggered by "Close" icon click.
+   * */
+  onClose?: (e?: React.SyntheticEvent) => void;
+  /**
+   * Notice content.
+   * */
+  children?: React.ReactNode;
+  manager?: NoticeBubbleManagerClass;
+  icon?: React.ReactElement;
+};
+/** @deprecated */
+export interface INoticeBubbleInfoProps extends NoticeBubbleInfoProps, UnknownProperties {}
+export type NoticeBubbleInfoProps = NoticeBubbleProps & {
+  readonly type?: 'info';
+};
+
+/** @deprecated */
+export interface INoticeBubbleWarningProps extends NoticeBubbleWarningProps, UnknownProperties {}
+export type NoticeBubbleWarningProps = NoticeBubbleProps & {
+  readonly type?: 'warning';
+};
+
+export type AddedNoticeMeta = {
+  uid: number;
+  update: (props: NoticeBubbleInfoProps | NoticeBubbleWarningProps) => boolean;
+  remove: () => boolean;
+  ref: RefObject<HTMLElement>;
+  focus: () => void;
+};
+
+/** @deprecated */
+export interface INoticeBubbleManager extends NoticeBubbleManagerClass, UnknownProperties {}
+export type NoticeBubbleManagerClass = {
+  /**
+   * Creates and shows a notice.
+   * */
+  add: (props: NoticeBubbleInfoProps | NoticeBubbleWarningProps) => AddedNoticeMeta;
+  /**
+   * Updates notice by uid.
+   * @deprecated use replace instead.
+   */
+  update: (uid: number, props: Partial<NoticeBubbleInfoProps> | Partial<NoticeBubbleWarningProps>) => boolean;
+  /**
+   * Removes notice by uid.
+   * */
+  remove: (uid: number) => boolean;
+  /**
+   * Replace notice by uid.
+   */
+  replace: (uid: number, props: NoticeBubbleInfoProps | NoticeBubbleWarningProps) => void;
+  /**
+   * Replace last notice (if it is existing)
+   */
+  replaceLast: (props: NoticeBubbleInfoProps | NoticeBubbleWarningProps) => void;
+};
+
+/**
+ * @deprecated use `NoticeBubbleManager` instead.
+ * */
+export declare const NoticeBubble: Intergalactic.Component<'div', NoticeBubbleInfoProps>;
+/**
+ * @deprecated use `NoticeBubbleManager` instead.
+ * */
+export declare const NoticeBubbleWarning: Intergalactic.Component<'div', NoticeBubbleWarningProps>;
+export declare const NoticeBubbleContainer: Intergalactic.Component<
+  'div',
+  NoticeBubbleContainerProps
+> & {
+  /**
+   * @deprecated use `NoticeBubbleManager` instead.
+   * */
+  Info: typeof NoticeBubble;
+
+  /**
+   * @deprecated use `NoticeBubbleManager` instead.
+   * */
+  Warning: typeof NoticeBubbleWarning;
+};
