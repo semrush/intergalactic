10b8a34711ebb917a2ed6bb66f57a441b849dc8a
ilia.brauer <ilia.brauer@semrush.com>
2025-12-01 14:48:33 +0100

    [badge] removed deprecations

--- semcore/badge/src/index.d.ts
+++ semcore/badge/src/index.d.ts
@@ -1,8 +1,6 @@
 import type { UnknownProperties, Intergalactic } from '@semcore/core';
 import type { BoxProps } from '@semcore/flex-box';
 
-/** @deprecated */
-export interface IBadgeProps extends BadgeProps, UnknownProperties {}
 export type BadgeProps = BoxProps & {
   /** Fill color
    * @default gray-400
