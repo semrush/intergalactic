ce104f2b5db5c1fc7626564a45cfd04691136f6e
ilia.brauer <ilia.brauer@semrush.com>
2025-12-03 14:43:58 +0100

    [chore] cross-os path to test in browsers tests

--- tools/testing-utils/e2e-stand.ts
+++ tools/testing-utils/e2e-stand.ts
@@ -35,7 +35,7 @@ export const e2eStandToHtml = async (
             const contents = `
               import React from 'react';
               import ReactDOM from 'react-dom';
-              import App from '${resolvePath(...standFilePath.split('/'))}';
+              import App from '${resolvePath('.', ...standFilePath.split('/'))}';
               import { I18nProvider } from '@semcore/core/lib/utils/enhances/WithI18n';
 
               const props = { ${propsCode} };
