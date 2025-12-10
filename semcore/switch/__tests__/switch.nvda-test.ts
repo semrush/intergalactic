98994ffe56ccc2d78060887a544a5c22c6e9a6c2
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-12-10 15:40:47 +0100

    [switch] nvda tests

--- semcore/switch/__tests__/switch.nvda-test.ts
+++ semcore/switch/__tests__/switch.nvda-test.ts
@@ -3,13 +3,18 @@ import { loadPage } from '@semcore/testing-utils/shared/helpers';
 import { TAG } from '@semcore/testing-utils/shared/tags';
 
 test.describe(`@switch ${TAG.NVDA}`, () => {
-  test('Users can interact with Switch via NVDA', async ({ page, nvda }) => {
+  test('Users can read via NVDA', async ({ page, nvda }) => {
     await loadPage(page, 'stories/components/switch/docs/examples/basic_example.tsx', 'en');
 
     await nvda.next();
 
-    expect(await nvda.itemText()).toContain('check box, checked, Receive updates');
+    expect(await nvda.itemText()).toContain('clickable, Receive updates');
+  });
 
+  test('Users can interact with switch via NVDA', async ({ page, nvda }) => {
+    await loadPage(page, 'stories/components/switch/docs/examples/basic_example.tsx', 'en');
+
+    await page.keyboard.press('Tab');
     await page.keyboard.press('Enter');
     await page.waitForTimeout(300);
 
