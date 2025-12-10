d4a1b82af14c010a95751e82fb2766a127b6e0ed
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-12-10 13:49:52 +0100

    [slider] nvda test

---
+++ semcore/slider/__tests__/slider.nvda-test.ts
@@ -0,0 +1,27 @@
+import { expect, nvdaTest as test } from '@semcore/testing-utils/playwright';
+import { loadPage } from '@semcore/testing-utils/shared/helpers';
+import { TAG } from '@semcore/testing-utils/shared/tags';
+
+test.describe(`@slider ${TAG.NVDA}`, () => {
+  test('Users can interact with Slider via NVDA', async ({ page, nvda }) => {
+    await loadPage(page, 'stories/components/slider/docs/examples/slider_with_options.tsx', 'en');
+
+    await nvda.next();
+
+    expect(await nvda.itemText()).toContain('slider');
+    expect(await nvda.itemText()).toContain('Medium');
+
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('ArrowLeft');
+    await page.waitForTimeout(300);
+
+    expect(await nvda.itemText()).toContain('Small');
+
+    await page.keyboard.press('ArrowRight');
+    await page.waitForTimeout(300);
+    await page.keyboard.press('ArrowRight');
+    await page.waitForTimeout(300);
+
+    expect(await nvda.itemText()).toContain('Big');
+  });
+});
