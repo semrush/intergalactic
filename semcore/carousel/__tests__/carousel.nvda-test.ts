361f7e6cd3dee975bfc9bef5b68b5abc06af09b5
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2025-12-12 15:59:09 +0100

    [carousel] nvda test cleanup

--- semcore/carousel/__tests__/carousel.nvda-test.ts
+++ semcore/carousel/__tests__/carousel.nvda-test.ts
@@ -40,6 +40,9 @@ test.describe(`@carousel ${TAG.NVDA}`, () => {
 
     await nvda.interact();
     await nvda.press('Enter');
-    expect(await nvda.itemText()).toContain('tab, Slide 2');
+    expect(await nvda.itemText()).toContain('Modal window, dialog. Close, button. Modal window, dialog. button, Close');
+
+    await nvda.next();
+    expect(await nvda.itemText()).toBe('clickable');
   });
 });
