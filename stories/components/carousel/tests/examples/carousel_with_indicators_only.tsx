da43009ea0bb0f13dbc4e2333628bb3dfd53395e
Sheila <166654065+sheila-semrush@users.noreply.github.com>
2025-12-17 10:02:44 +0100

    [carousel] fixed indicator style (#2626)
    ## Changelog
    
    ### @semcore/carousel
    
    #### Fixed
    
    - Enabled `overflow-hidden` for `Indicator` for `border-radius` to apply
    correctly.
    - Moved default width and height for indicators with preview to CSS for
    more convenient styling.
    - Added `aria-roledescription="carousel"` to the root element.
    - Added translations for `aria-roledescription` in the root element and
    indicators with preview.
    
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Was:
    <img width="1096" height="456" alt="image"
    src="https://github.com/user-attachments/assets/810c7520-78ec-4bc6-9b5b-f6eb09aabf31"
    />
    
    Now:
    <img width="1194" height="356" alt="image"
    src="https://github.com/user-attachments/assets/70f4869b-07c4-43c9-955b-7433a056e589"
    />
    
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [ ] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added new tests on added of fixed functionality.
    
    ---------
    
    Signed-off-by: Sheila <166654065+sheila-semrush@users.noreply.github.com>
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- stories/components/carousel/tests/examples/carousel_with_indicators_only.tsx
+++ stories/components/carousel/tests/examples/carousel_with_indicators_only.tsx
@@ -3,9 +3,9 @@ import Carousel from '@semcore/ui/carousel';
 import React from 'react';
 
 const images = [
-  'https://placehold.co/600x400/png?text=Image+1',
-  'https://placehold.co/600x400/png?text=Image+2',
-  'https://placehold.co/600x400/png?text=Image+3',
+  'https://placehold.co/600x400/8000FF/FFFF00?text=Image+1',
+  'https://placehold.co/600x400/8000FF/FFFF00?text=Image+2',
+  'https://placehold.co/600x400/8000FF/FFFF00?text=Image+3',
 ];
 const altTexts = [
   'A cyclist performing stunts in the forest',
@@ -16,7 +16,7 @@ const width = 600;
 const imageWidth = width - 75;
 
 const Demo = () => (
-  <Carousel w={width} aria-roledescription='carousel' aria-label='Beauty of Nature'>
+  <Carousel w={width} aria-label='Beauty of Nature'>
     <>
       <Flex>
         <Carousel.ContentBox>
