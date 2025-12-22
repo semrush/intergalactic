c9b5d609edc7ec849b0f7001398d8389a5972f1f
ilia.brauer <ilia.brauer@semrush.com>
2025-12-22 12:12:05 +0100

    [errors] fixed example - added formatTags

--- semcore/errors/src/ProjectNotFound/index.jsx
+++ semcore/errors/src/ProjectNotFound/index.jsx
@@ -31,7 +31,7 @@ class RootProjectNotFound extends Component {
     return (
       <Root render={Error}>
         <Error.Title tag={titleTag}>{getI18nText('title')}</Error.Title>
-        <Error.Description tag={Text} size='l'>
+        <Error.Description tag={Text} formatTags={true} size='l'>
           {text}
         </Error.Description>
         <Children />
