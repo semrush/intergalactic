de24c760b6136caed02e5e4c15eb34d67106f4f1
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-01-13 14:20:41 +0100

    [UIK-4573][d3-chart] updated snapshots

--- playwright.browser.config.ts
+++ playwright.browser.config.ts
@@ -10,12 +10,10 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 2,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : 4,
   grep: TAG ? new RegExp(TAG) : testPlanFilter(),
-
-  updateSnapshots: 'all',
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'], ['allure-playwright']],
