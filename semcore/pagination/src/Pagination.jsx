b3a1d5e0d7a0fa68280c592c6e669b8cb00eaa67
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-01-20 15:54:32 +0100

    [UIK-3780][pagination] change margin for label 'of' for size l

--- semcore/pagination/src/Pagination.jsx
+++ semcore/pagination/src/Pagination.jsx
@@ -312,7 +312,7 @@ class TotalPages extends Component {
     const textSize = other.size === 'l' ? '300' : '200';
     return sstyled(styles)(
       <>
-        <STotalPagesLabel size={textSize}>{getI18nText('totalPagesLabel')}</STotalPagesLabel>
+        <STotalPagesLabel paginationSize={other.size} size={textSize}>{getI18nText('totalPagesLabel')}</STotalPagesLabel>
         {isLastOrSingle
           ? (
               <STotalLastPages
