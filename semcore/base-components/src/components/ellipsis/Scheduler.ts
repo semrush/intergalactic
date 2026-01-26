75ad7cb3b97b27a76edba4e888ee17e240f3c2a8
ilia.brauer <ilia.brauer@semrush.com>
2026-01-26 12:51:15 +0100

    [base-components] display of ellipsis and tooltips on pages with a small number of them has been accelerated.

--- semcore/base-components/src/components/ellipsis/Scheduler.ts
+++ semcore/base-components/src/components/ellipsis/Scheduler.ts
@@ -6,33 +6,44 @@ type Task = (...args: any[]) => void;
  * Util class for scheduling some work
  */
 export class Scheduler {
+  private allTasks: number = 0;
+  private cancelledTasks: number = 0;
   private idleId: number | null = null;
   private timeout: ReturnType<typeof setTimeout> | null = null;
   private readonly tasksTimeout: number = 200;
+  private readonly countNotToOptimise: number = 0;
 
-  constructor(tasksTimeout?: number) {
+  constructor(tasksTimeout?: number, countNotToOptimise?: number) {
     if (tasksTimeout !== undefined) {
       this.tasksTimeout = tasksTimeout;
+    }
+    if (countNotToOptimise !== undefined) {
+      this.countNotToOptimise = countNotToOptimise;
     }
   }
 
   public schedule(task: Task) {
+    this.allTasks++;
     if (canUseDOM() && 'requestIdleCallback' in window) {
       if (this.idleId !== null) {
+        this.cancelledTasks++;
         window.cancelIdleCallback(this.idleId);
       }
 
       this.idleId = window.requestIdleCallback(task);
     } else {
       if (this.timeout !== null) {
+        this.cancelledTasks++;
         clearTimeout(this.timeout);
       }
 
-      this.timeout = setTimeout(task, this.tasksTimeout);
+      const timeout = (this.allTasks - this.cancelledTasks) <= this.countNotToOptimise ? 0 : this.tasksTimeout;
+      this.timeout = setTimeout(task, timeout);
     }
   }
 
   public cancel() {
+    this.cancelledTasks++;
     if (this.idleId !== null) {
       window.cancelIdleCallback(this.idleId);
     } else if (this.timeout !== null) {
