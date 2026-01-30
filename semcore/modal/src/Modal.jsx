727a8cad626858e110ce6e1d5c3d18f119c7519c
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-01-30 12:56:52 +0100

    [UIK-3093][feedback-form] updated header component to use Modal.Title

--- semcore/modal/src/Modal.jsx
+++ semcore/modal/src/Modal.jsx
@@ -76,13 +76,12 @@ class ModalRoot extends Component {
   getWindowProps() {
     const { visible, closable, getI18nText, uid, duration, animationsDisabled } = this.asProps;
     const { hasTitle } = this.state;
-    const ariaLabelledBy = this.asProps['aria-labelledby'];
 
     return {
       visible,
       closable,
       'onKeyDown': this.handleKeyDown,
-      'aria-label': hasTitle || ariaLabelledBy ? undefined : getI18nText('title'),
+      'aria-label': hasTitle ? undefined : getI18nText('title'),
       'aria-labelledby': hasTitle ? `igc-${uid}-title` : undefined,
       duration,
       animationsDisabled,
