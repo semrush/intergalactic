8b59830c5c2f5a415f07263873502c654a1f4de9
Julia Mnizhek <j.mnizhek@semrush.com>
2026-02-02 14:01:30 +0100

    [typography] fix margins and update stories

--- stories/components/typography/docs/examples/list-with-custom-bullets.tsx
+++ stories/components/typography/docs/examples/list-with-custom-bullets.tsx
@@ -4,9 +4,21 @@ import React from 'react';
 
 const Demo = () => (
   <div>
-    <List size={300} marker={<CheckM color='icon-secondary-success' mt={1} />}>
+    <List mb={4} size={300} marker={<CheckM color='icon-secondary-success' mt={1} />}>
       <List.Item>List item with custom bullet.</List.Item>
       <List.Item marker={<CheckM color='icon-secondary-neutral' mt={1} />}>
+        List item with other custom bullet.
+      </List.Item>
+    </List>
+    <List mb={4} size={200} marker={<CheckM color='icon-secondary-success' />}>
+      <List.Item>List item with custom bullet.</List.Item>
+      <List.Item marker={<CheckM color='icon-secondary-neutral' />}>
+        List item with other custom bullet.
+      </List.Item>
+    </List>
+    <List mb={4} size={100} marker={<CheckM color='icon-secondary-success' />}>
+      <List.Item>List item with custom bullet.</List.Item>
+      <List.Item marker={<CheckM color='icon-secondary-neutral' />}>
         List item with other custom bullet.
       </List.Item>
     </List>
