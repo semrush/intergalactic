809f2ffd257f603362a57f996dc30d627412aecf
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2026-02-06 15:07:04 +0100

    [typography] update story

--- stories/components/typography/docs/examples/list-with-custom-bullets.tsx
+++ stories/components/typography/docs/examples/list-with-custom-bullets.tsx
@@ -5,25 +5,21 @@ import React from 'react';
 
 const Demo = () => (
   <div>
-<<<<<<< UIK-3931/typography-update-margins
-    <List mb={4} size={300} marker={<CheckM color='icon-secondary-success' mt={1} />}>
-=======
-    <List size={300} marker={<Box tag={CheckM} color='icon-secondary-success' mt={1} />}>
->>>>>>> release/v17
+    <List mb={4} size={300} marker={<Box tag={CheckM} color='icon-secondary-success' mt={1} />}>
       <List.Item>List item with custom bullet.</List.Item>
       <List.Item marker={<Box tag={CheckM} color='icon-secondary-neutral' mt={1} />}>
         List item with other custom bullet.
       </List.Item>
     </List>
-    <List mb={4} size={200} marker={<CheckM color='icon-secondary-success' />}>
+    <List mb={4} size={200} marker={<Box tag={CheckM} color='icon-secondary-success' />}>
       <List.Item>List item with custom bullet.</List.Item>
-      <List.Item marker={<CheckM color='icon-secondary-neutral' />}>
+      <List.Item marker={<Box tag={CheckM} color='icon-secondary-neutral' />}>
         List item with other custom bullet.
       </List.Item>
     </List>
-    <List mb={4} size={100} marker={<CheckM color='icon-secondary-success' />}>
+    <List mb={4} size={100} marker={<Box tag={CheckM} color='icon-secondary-success' />}>
       <List.Item>List item with custom bullet.</List.Item>
-      <List.Item marker={<CheckM color='icon-secondary-neutral' />}>
+      <List.Item marker={<Box tag={CheckM} color='icon-secondary-neutral' />}>
         List item with other custom bullet.
       </List.Item>
     </List>
