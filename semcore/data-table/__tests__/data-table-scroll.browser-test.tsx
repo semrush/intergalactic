fa4927af169665d3cf759a6f6e7c029d4fbaf851
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2026-02-20 16:45:34 +0100

    [chore] update browser and nvda

--- semcore/data-table/__tests__/data-table-scroll.browser-test.tsx
+++ semcore/data-table/__tests__/data-table-scroll.browser-test.tsx
@@ -130,11 +130,8 @@ test.describe(`${TAG.VISUAL}`, () => {
           await page.keyboard.press('ArrowRight');
         }
         await page.waitForTimeout(200);
-        if (browserName == 'webkit') {
-          await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
-        } else {
-          await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.0005 });
-        }
+
+        await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
       });
 
       await test.step('Verify vertical scroll', async () => {
