1a9c73e33ae859101eddf54e1a291a8c64977155
ilyabrower <ilia.brauer@semrush.com>
2026-02-20 11:54:06 +0100

    [UIK-4566][stories] fixed notice-bubble focus behavior (#2729)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    The focus return logic was in the stories. I fixed it.
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    Manually
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [X] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added new tests on added of fixed functionality.

--- stories/components/notice-bubble/docs/examples/notice_in_sm2.tsx
+++ stories/components/notice-bubble/docs/examples/notice_in_sm2.tsx
@@ -1,4 +1,5 @@
 import Button from '@semcore/ui/button';
+import { lastInteraction } from '@semcore/ui/core';
 import CheckM from '@semcore/ui/icon/Check/m';
 import Link from '@semcore/ui/link';
 import { NoticeBubbleContainer, NoticeBubbleManager } from '@semcore/ui/notice-bubble';
@@ -26,9 +27,11 @@ const Demo = (props: BaseNoticeBubbleProps) => {
       type: props.type,
       focusLock: props.focusLock,
       onClose: () => {
-        setTimeout(() => {
-          openButtonRef.current?.focus();
-        }, 300);
+        if (lastInteraction.isKeyboard()) {
+          setTimeout(() => {
+            openButtonRef.current?.focus();
+          }, 300);
+        }
       },
     });
   };
@@ -42,9 +45,11 @@ const Demo = (props: BaseNoticeBubbleProps) => {
       type: props.type,
       focusLock: props.focusLock,
       onClose: () => {
-        setTimeout(() => {
-          openButtonRef2.current?.focus();
-        }, 300);
+        if (lastInteraction.isKeyboard()) {
+          setTimeout(() => {
+            openButtonRef2.current?.focus();
+          }, 300);
+        }
       },
     });
   };
