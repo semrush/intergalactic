f799e32100b0fa67b9aa26456b2ab60a14e31f09
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-02-23 13:43:49 +0100

    [UIK-4738][bulk-textarea,website] udpate types for BulkTextarea.InputField

--- semcore/bulk-textarea/src/BulkTextarea.types.ts
+++ semcore/bulk-textarea/src/BulkTextarea.types.ts
@@ -5,6 +5,7 @@ import type { BoxProps } from '@semcore/flex-box';
 import type { CounterProps } from './components/Counter';
 import type { ErrorsNavigationProps } from './components/ErrorsNavigation';
 import type { InputFieldProps } from './components/InputField/InputField';
+import type { PasteProps } from './components/InputField/InputField.types';
 
 export type BulkTextareaProps<T extends string | string[]> = {
   /** The current value */
@@ -40,7 +41,7 @@ export type BulkTextareaProps<T extends string | string[]> = {
   /** List of errors */
   errors?: InputFieldProps<T>['errors'];
   /** Defines whether to show errors or not */
-  showErrors?: boolean;
+  showErrors?: InputFieldProps<T>['showErrors'];
   /** Internal */
   onErrorsChange?: InputFieldProps<T>['onErrorsChange'];
   /** Internal */
@@ -58,10 +59,91 @@ type BulkTextareaComponent = (<T extends string | string[]>(
 ) => Intergalactic.InternalTypings.ComponentRenderingResults) &
 Intergalactic.InternalTypings.ComponentAdditive<'div', 'div', {}>;
 
+export type BulkTextareaInputFieldProps<T extends string | string[] = string | string[]> = BoxProps & {
+  /**
+   * Unique id
+   */
+  id?: string;
+  /**
+   * Placeholder for field
+   */
+  placeholder?: string;
+  /**
+   * String to render in textarea. OnChanging value, it will go throw paste pipeline
+   */
+  value?: T;
+  /**
+   * This component doesn't have default onChange callback, because we think that you need only the result after every changes/fixes
+   */
+  onBlur?: (value: T, e: Event) => void;
+  /**
+   * Size of component
+   * @default m
+   */
+  size?: 'm' | 'l';
+  /**
+   * State for show errors or valid(green) borders
+   * @default normal
+   */
+  state?: 'normal' | 'valid' | 'invalid';
+  /**
+   * Flag for disabling field
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * Flag for readonly field
+   * @default false
+   */
+  readonly?: boolean;
+  /**
+   * Min rows
+   * @default 2
+   */
+  minRows?: number;
+  /**
+   * Max rows
+   * @default 10
+   */
+  maxRows?: number;
+  /**
+   * List of available points to validate value
+   * @default blur
+   */
+  validateOn?: ('blur' | 'blurLine' | 'paste')[];
+  /**
+   * Function to validate line
+   */
+  lineValidation?: (line: string, lines: string[]) => { isValid: boolean; errorMessage: string };
+  /**
+   * Message for display error about whole field, not only one line.
+   * If set empty string, field will not have invalid state.
+   */
+  commonErrorMessage: string;
+  /**
+   * Delimiters (event.key) for lines
+   * @default Enter
+   */
+  linesDelimiters?: string[];
+  /**
+   * Count of max lines in badge
+   * @default 100
+   */
+  maxLines?: number;
+  /**
+   * Paste props
+   */
+  pasteProps?: PasteProps;
+  /**
+   * Function for process line after it was blurred
+   */
+  lineProcessing?: (line: string, lines: string[]) => string;
+};
+
 export type BulkTextareaType<T extends string | string[]> = BulkTextareaComponent & {
   InputField: Intergalactic.Component<
     'div',
-    Pick<InputFieldProps<T>, 'commonErrorMessage' | 'id'> & Partial<BulkTextareaProps<T>> & BoxProps
+    BulkTextareaInputFieldProps<T>
   >;
   Counter: Intergalactic.Component<'div', Partial<CounterProps>>;
   ClearAll: typeof Button;
