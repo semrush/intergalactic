2400abd88969e34b341b28d61f91cc0a5818c1b3
Sheila Sheikh <166654065+sheila-semrush@users.noreply.github.com>
2026-03-03 13:39:55 +0100

    [website] updated semrush logo

--- website/docs/.vitepress/theme/DevportalLogo.vue
+++ website/docs/.vitepress/theme/DevportalLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <a href="https://developer.semrush.com/" class="devportal-logo" target="_blank" aria-label="Developer home page">
-    <img src="/devportal-logo.svg" width="30" height="30" class="devportal-logo" alt="Semrush Developer portal" />
+    <img src="/devportal-logo.svg" width="32" height="32" class="devportal-logo-img" alt="Semrush Developer portal" />
     Developer
   </a>
   <a :href="link" class="title intergalactic-logo" aria-label="Intergalactic Design System home page" @click.prevent="navigateAndReload">
