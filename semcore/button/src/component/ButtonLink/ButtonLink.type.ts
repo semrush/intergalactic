74cc6de2c1a4859f27afa2a47be3df4c0a7dd6fe
ilia.brauer <ilia.brauer@semrush.com>
2026-03-11 12:19:17 +0100

    [button] some refactoring, removed button props from ButtonLink

--- semcore/button/src/component/ButtonLink/ButtonLink.type.ts
+++ semcore/button/src/component/ButtonLink/ButtonLink.type.ts
@@ -1,30 +1,21 @@
+import type { BoxProps } from '@semcore/base-components';
 import type { Intergalactic } from '@semcore/core';
 import type { LinkProps } from '@semcore/link';
+import type { TextProps } from '@semcore/typography';
 
-import type {
-  AbstractButtonAddonProps,
-  AbstractButtonContext,
-  AbstractButtonTextProps,
-  AbstractButtonProps,
-} from '../AbstractButton/AbstractButton.type';
+import type { ButtonContext } from '../Button/Button.type';
 
-/**
- *  Button link size
- */
-export type ButtonLinkSize = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800;
-/**
- *  Button link type
- * @default primary
- */
-type Use = 'primary' | 'secondary';
+export type ButtonLinkProps = LinkProps & {
+  /**
+   *  Button link type
+   * @default primary
+   */
+  use: 'primary' | 'secondary';
+};
 
-export type ButtonLinkProps = AbstractButtonProps<ButtonLinkSize, Use, never> & LinkProps;
+export type ButtonLinkTextProps = TextProps;
 
-export type ButtonLinkTextProps = AbstractButtonTextProps<ButtonLinkSize>;
-
-export type ButtonLinkAddonProps = AbstractButtonAddonProps;
-
-export type ButtonLinkContext = AbstractButtonContext;
+export type ButtonLinkAddonProps = BoxProps;
 
 export type ButtonLinkChildren = {
   Text: Intergalactic.Component<'span', ButtonLinkTextProps>;
@@ -34,5 +25,5 @@ export type ButtonLinkChildren = {
 export type ButtonLinkComponent = Intergalactic.Component<
   'button',
   ButtonLinkProps,
-  ButtonLinkContext
+  ButtonContext
 > & ButtonLinkChildren;
