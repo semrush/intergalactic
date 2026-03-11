be97f2248dc8272727b47ec9543489fa583fe74a
ilia.brauer <ilia.brauer@semrush.com>
2026-03-11 18:28:03 +0100

    [website] logo for dark and light themes

--- website/docs/.vitepress/theme/DevportalLogo.vue
+++ website/docs/.vitepress/theme/DevportalLogo.vue
@@ -1,6 +1,8 @@
 <template>
   <a href="https://developer.semrush.com/" class="devportal-logo" target="_blank" aria-label="Developer home page">
-    <img src="/devportal-logo.svg" width="32" height="32" class="devportal-logo-img" alt="Semrush Developer portal" />
+    <svg width="32" height="32" viewBox="0 0 32 32" class="devportal-logo-img" aria-label="Semrush Developer portal" role="img" xmlns="http://www.w3.org/2000/svg">
+      <path d="M20.0964 8.00213C24.5215 8.00213 28.1418 11.6472 28.1418 16.002C28.1418 20.3568 24.6588 23.8877 20.3005 24H8.79908L15.7525 20.307H2L14.4915 13.678H7.6173L16.0069 9.22973C17.5118 8.4474 18.8604 8 20.0964 8V8.00213Z" />
+    </svg>
     Developer
   </a>
   <a :href="link" class="title intergalactic-logo" aria-label="Intergalactic Design System home page" @click.prevent="navigateAndReload">
@@ -31,3 +33,12 @@ const navigateAndReload = () => {
   window.location.reload();
 };
 </script>
+
+<style scoped>
+.devportal-logo-img {
+  fill: #181E15;
+}
+.dark .devportal-logo-img {
+  fill: #fff;
+}
+</style>
