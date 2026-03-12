9f3874602dd2804d387d6da6c35de9c82929d1f7
ilia.brauer <ilia.brauer@semrush.com>
2026-03-12 10:49:55 +0100

    [hint] fixed verticalCursorAnchoring calculation

--- semcore/base-components/src/components/hint/Middleware.ts
+++ semcore/base-components/src/components/hint/Middleware.ts
@@ -13,7 +13,11 @@ export class Middleware {
         const referenceLeft = elements.reference.getBoundingClientRect().left;
 
         return {
-          x: rects.floating.width > rects.reference.width ? x : mouseCursorPosition.x - referenceLeft + x - rects.floating.width / 2,
+          x: rects.floating.width > rects.reference.width
+            ? x
+            : referenceLeft > rects.reference.x
+              ? mouseCursorPosition.x - referenceLeft + rects.reference.x - rects.floating.width / 2
+              : mouseCursorPosition.x - rects.floating.width / 2,
         };
       },
     };
