16db60ef4531974a4e7284c2cb913f506b1b40ea
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-03-19 11:39:58 +0100

    [UIK-4955][base-components] add browser test for hoverCursor

--- semcore/base-components/__tests__/flex-box.browser-test.tsx
+++ semcore/base-components/__tests__/flex-box.browser-test.tsx
@@ -209,4 +209,26 @@ test.describe(`${TAG.VISUAL}`, () => {
     await loadPage(page, 'stories/components/base-components/flex-box/docs/examples/flex.tsx', 'en');
     await expect(page).toHaveScreenshot();
   });
+
+  test('Verify Flex hover cursor', {
+    tag: [TAG.PRIORITY_HIGH,
+      '@base-components',
+      '@flex-box'],
+  }, async ({ page }) => {
+    const hoverCursor = 'zoom-in';
+
+    await loadPage(page, 'stories/components/base-components/flex-box/tests/examples/box-all-props.tsx', 'en', { hoverCursor });
+
+    const box = page.locator('[data-ui-name="Box"]');
+
+    await box.hover();
+
+    const cursor = await box.evaluate((el) => {
+      const { cursor } = getComputedStyle(el);
+
+      return cursor;
+    });
+
+    expect(cursor).toBe(hoverCursor);
+  });
 });
