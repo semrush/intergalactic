9feaf33603042c28119f65806142f297b21b375b
ilia.brauer <ilia.brauer@semrush.com>
2026-03-25 23:07:20 +0100

    [chore] fixed tests

--- semcore/button/__tests__/button.browser-test.tsx
+++ semcore/button/__tests__/button.browser-test.tsx
@@ -605,9 +605,9 @@ test.describe(`${TAG.VISUAL} `, () => {
   });
 
   const ellipsisVariants = [
-    { ellipsis: { cropPosition: 'middle' }, size: 'l', description: 'cropPosition: middle size: l' },
-    { ellipsis: { cropPosition: 'end' }, description: 'cropPosition: end' },
-    { ellipsis: { cropPosition: 'middle', lastRequiredSymbols: 2 }, description: 'cropPosition: middle, , lastRequiredSymbols: 2' },
+    { ellipsis: { 'ellipsis:cropPosition': 'middle' }, size: 'l', description: 'cropPosition: middle size: l' },
+    { ellipsis: { 'ellipsis:cropPosition': 'end' }, description: 'cropPosition: end' },
+    { ellipsis: { 'ellipsis:cropPosition': 'middle', 'ellipsis:lastRequiredSymbols': 2 }, description: 'cropPosition: middle, , lastRequiredSymbols: 2' },
   ];
 
   ellipsisVariants.forEach((variant) => {
