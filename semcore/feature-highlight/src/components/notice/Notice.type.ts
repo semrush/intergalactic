fdd7c4c2101e3769c1db57e31585eacfd19075de
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-04-01 17:04:18 +0200

    [UIK-5054][feature-highlight] update prop type for NoticeFH

--- semcore/feature-highlight/src/components/notice/Notice.type.ts
+++ semcore/feature-highlight/src/components/notice/Notice.type.ts
@@ -1,4 +1,12 @@
 import type { NoticeSmart } from '@semcore/notice';
 import type Notice from '@semcore/notice';
+import type { Intergalactic } from '@semcore/ui/core/lib/index';
 
-export type HighlightedNoticeComponent = typeof Notice & typeof NoticeSmart;
+export type HighlightedNoticeComponent = Intergalactic.Component<typeof Notice & typeof NoticeSmart, { children: React.ReactNode }> & {
+  Label: typeof Notice['Label'];
+  Actions: typeof Notice['Actions'];
+  Content: typeof Notice['Content'];
+  Title: typeof Notice['Title'];
+  Text: typeof Notice['Text'];
+  Close: typeof Notice['Close'];
+};
