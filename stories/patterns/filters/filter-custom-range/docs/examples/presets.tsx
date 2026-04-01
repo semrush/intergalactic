53759b944cb2f54cce14eee9d88c8472d76db4ad
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2026-04-01 17:59:54 +0200

    [stories] update filter story

--- stories/patterns/filters/filter-custom-range/docs/examples/presets.tsx
+++ stories/patterns/filters/filter-custom-range/docs/examples/presets.tsx
@@ -90,9 +90,9 @@ const setTriggerText = ({ from, to }: ValueState): string | undefined => {
   if (from !== '') {
     if (to === '') return `${numberFormat.format(Number(from))}+`;
     if (from === to) return numberFormat.format(Number(from));
-    return `${numberFormat.format(Number(from))}-${numberFormat.format(Number(to))}`;
+    return `${numberFormat.format(Number(from))}–${numberFormat.format(Number(to))}`;
   } else if (to !== '') {
-    return `${numberFormat.format(minRange)}-${numberFormat.format(Number(to))}`;
+    return `${numberFormat.format(minRange)}–${numberFormat.format(Number(to))}`;
   }
   return undefined;
 };
