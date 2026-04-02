891fb138e72d442b57b1911e0423cb4fd4a29efc
ilia.brauer <ilia.brauer@semrush.com>
2026-04-02 14:53:12 +0200

    [website] fixed base path

--- website/docs/.vitepress/config.ts
+++ website/docs/.vitepress/config.ts
@@ -31,7 +31,7 @@ if (links.length > 0) {
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: viteConfig.base,
+  base: viteConfig.base?.replaceAll('_', '-'),
   outDir: resolvePath(__dirname, 'dist'),
   title: 'Intergalactic Design System',
   description: 'Design System',
