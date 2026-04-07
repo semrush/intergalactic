c28511f1dc0594482842debe736e07f19bb37ffb
slizhevskyv-semrush <uladzislau.slizheuski@semrush.com>
2026-04-07 13:01:29 +0200

    [UIK-5054][notice] remove children as a valid prop in NoticeSmart (#2831)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Remove the unused `children` prop from `NoticeSmart`. Keeping it may
    mislead developers into passing content that the component never
    renders.
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [x] I have added new tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: ilyabrower <ilia.brauer@semrush.com>
    Co-authored-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- semcore/feature-highlight/src/components/notice/Notice.type.ts
+++ semcore/feature-highlight/src/components/notice/Notice.type.ts
@@ -1,4 +1,12 @@
-import type { NoticeSmart } from '@semcore/notice';
+import type { Intergalactic } from '@semcore/core';
+import type { NoticeSmartProps } from '@semcore/notice';
 import type Notice from '@semcore/notice';
 
-export type HighlightedNoticeComponent = typeof Notice & typeof NoticeSmart;
+export type HighlightedNoticeComponent = Intergalactic.Component<'div', NoticeSmartProps> & {
+  Label: typeof Notice['Label'];
+  Actions: typeof Notice['Actions'];
+  Content: typeof Notice['Content'];
+  Title: typeof Notice['Title'];
+  Text: typeof Notice['Text'];
+  Close: typeof Notice['Close'];
+};
