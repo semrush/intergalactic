57cd474b18001e78ddbdd275cdf5020e4f4e16cf
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-04-08 17:17:17 +0200

    [radio] attempt to use ns instead of atomic types

--- semcore/feature-highlight/src/components/radio/Radio.type.ts
+++ semcore/feature-highlight/src/components/radio/Radio.type.ts
@@ -1,11 +1,11 @@
 import type { Intergalactic } from '@semcore/core';
-import type { RadioCtx, RadioProps, RadioValueProps } from '@semcore/radio';
+import type { NRadio } from '@semcore/radio';
 import type { Text } from '@semcore/typography';
 
 import type { AnimatedSparklesProps } from '../../inner-components/sparkle/AnimatedSparkles';
 
-export type HighlightedRadioComponent = Intergalactic.Component<'label', RadioProps, RadioCtx> & {
-  Value: Intergalactic.Component<'input', RadioValueProps>;
+export type HighlightedRadioComponent = Intergalactic.Component<'label', NRadio.Props, NRadio.Ctx> & {
+  Value: Intergalactic.Component<'input', NRadio.Value.Props>;
   Text: typeof Text;
   AnimatedSparkles: Intergalactic.Component<'div', AnimatedSparklesProps>;
 };
