8a897ded8702ca436ecde434096389fb3f82126a
ilyabrower <ilia.brauer@semrush.com>
2026-04-14 18:55:58 +0200

    [UIK-5072][base-trigger] added default width for LinkTrigger.Text (#2849)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    Manually, I've added a new story
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [X] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added new tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- stories/components/base-trigger/tests/examples/link-trigger/base.tsx
+++ stories/components/base-trigger/tests/examples/link-trigger/base.tsx
@@ -4,12 +4,13 @@ import Badge from '@semcore/ui/badge';
 import { LinkTrigger } from '@semcore/ui/base-trigger';
 import type { LinkTriggerProps } from '@semcore/ui/base-trigger';
 import Counter, { type CounterProps } from '@semcore/ui/counter';
-import Spin, { type SpinSize } from '@semcore/ui/spin';
+import Flags from '@semcore/ui/flags';
+import Tag, { type TagSize } from '@semcore/ui/tag';
 import type { TextEllipsisProps } from '@semcore/ui/typography';
 import { Text } from '@semcore/ui/typography';
 import React from 'react';
 
-type AddonType = 'icon' | 'badge' | 'counter' | 'spin';
+type AddonType = 'icon' | 'badge' | 'counter' | 'flag' | 'tag';
 
 type BasicLinktriggerProps = LinkTriggerProps & {
   text?: string;
@@ -47,18 +48,20 @@ const Demo = (props: BasicLinktriggerProps) => {
   const numSize = Number(size);
   const IconAddon = numSize < 600 ? MathPlusAltM : MathPlusAltL;
 
-  let spinSize: SpinSize = 'm';
-  if (numSize <= 200) {
-    spinSize = 'xs';
-  } else if (numSize <= 500) {
-    spinSize = 's';
-  }
-
   let counterSize: CounterProps['size'];
   if (numSize >= 600) {
     counterSize = 'l';
   } else if (numSize >= 300) {
     counterSize = 'm';
+  }
+
+  let tagSize: TagSize | undefined;
+  if (numSize >= 600) {
+    tagSize = 'xl';
+  } else if (numSize >= 300) {
+    tagSize = 'l';
+  } else {
+    tagSize = 'm';
   }
 
   const renderAddon = (show: boolean, type: AddonType) => {
@@ -69,8 +72,10 @@ const Demo = (props: BasicLinktriggerProps) => {
         return <LinkTrigger.Addon><Badge type='new' /></LinkTrigger.Addon>;
       case 'counter':
         return <LinkTrigger.Addon><Counter size={counterSize}>17</Counter></LinkTrigger.Addon>;
-      case 'spin':
-        return <LinkTrigger.Addon><Spin size={spinSize} /></LinkTrigger.Addon>;
+      case 'flag':
+        return <LinkTrigger.Addon><Flags name='US' /></LinkTrigger.Addon>;
+      case 'tag':
+        return <LinkTrigger.Addon><Tag size={tagSize}>Label</Tag></LinkTrigger.Addon>;
       case 'icon':
       default:
         if (merged) return <LinkTrigger.Addon tag={IconAddon} />;
