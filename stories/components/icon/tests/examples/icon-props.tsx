e53ef14c07432a7db30feada06deb67a640884d2
ilyabrower <ilia.brauer@semrush.com>
2026-04-20 19:13:07 +0200

    [UIK-5120][icon] fixed dts generation (#2880)
    ## Changelog
    
    ### @semcore/icon
    
    #### Fixed
    
    - Props in generated d.ts files.
    
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [X] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [X] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added new tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- stories/components/icon/tests/examples/icon-props.tsx
+++ stories/components/icon/tests/examples/icon-props.tsx
@@ -7,32 +7,13 @@ import { Flex, Box } from '@semcore/ui/base-components';
 import React from 'react';
 
 export type IconPropsAdvanced = IconProps & {
-  color?: string;
-  m?: number | string;
-  mt?: number | string;
-  mr?: number | string;
-  mb?: number | string;
-  ml?: number | string;
-  mx?: number | string;
-  my?: number | string;
-  scaleIndent?: number;
   iconSize?: 'm' | 'l';
 };
 
 const Demo = (props: IconPropsAdvanced) => {
   const {
-    color = 'icon-secondary-neutral',
-    m,
-    mt,
-    mr,
-    mb,
-    ml,
-    mx,
-    my,
     iconSize = 'm',
   } = props;
-
-  const marginProps = { m, mt, mr, mb, ml, mx, my };
 
   const ArrowsIcon = iconSize === 'l' ? ArrowsL : ArrowsM;
   const MailIcon = iconSize === 'l' ? MailFilledL : MailFilledM;
@@ -42,27 +23,20 @@ const Demo = (props: IconPropsAdvanced) => {
       <Box
         style={{ border: '1px dashed var(--intergalactic-border-primary)', display: 'inline-flex' }}
       >
-        <ArrowsIcon color={color} {...marginProps} />
+        <ArrowsIcon color={props.color} m={props.m} mb={props.mb} mt={props.mt} mr={props.mr} ml={props.ml} mx={props.mx} my={props.my} scaleIndent={props.scaleIndent} />
       </Box>
       <Box
         style={{ border: '1px dashed var(--intergalactic-border-primary)', display: 'inline-flex' }}
       >
-        <MailIcon color={color} {...marginProps} />
+        <MailIcon color={props.color} m={props.m} mb={props.mb} mt={props.mt} mr={props.mr} ml={props.ml} mx={props.mx} my={props.my} scaleIndent={props.scaleIndent} />
       </Box>
     </Flex>
   );
 };
 
 export const defaultProps: IconPropsAdvanced = {
-  color: 'icon-secondary-neutral',
-  m: undefined,
-  mt: undefined,
-  mr: undefined,
-  mb: undefined,
-  ml: undefined,
-  mx: undefined,
-  my: undefined,
   iconSize: 'm',
+  scaleIndent: undefined,
 };
 
 Demo.defaultProps = defaultProps;
