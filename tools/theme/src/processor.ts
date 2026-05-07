5d0493882d93e68dc1466206455e98d5c9860ca4
ilia.brauer <ilia.brauer@semrush.com>
2026-05-07 14:41:27 +0200

    [chore] added mixins to postprocessing css-variables

--- tools/theme/src/processor.ts
+++ tools/theme/src/processor.ts
@@ -86,7 +86,7 @@ export async function processStyles() {
   };
 
   const projectCssPaths = (
-    await glob(`./*/src/**/*.shadow.css`, {
+    await glob(['./*/src/**/*.shadow.css', './*/src/**/*mixin.css'], {
       ignore: ['node_modules', 'lib'],
       cwd: semcorePath,
     })
