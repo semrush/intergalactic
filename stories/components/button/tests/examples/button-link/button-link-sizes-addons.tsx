8ef29e9224c12596c2466f0e3e546bcd25e29f6f
slizhevskyv-semrush <uladzislau.slizheuski@semrush.com>
2026-05-27 17:05:00 +0200

    [UIK-5138][spin] rewrite component to TS (#2914)
    ## Changelog
    
    ### @semcore/spin
    
    #### Fixed
    
    - Rewrite component to TS. Deprecate atomic types. Atomic types are part
    of `NSSpin` namespace.
    
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Rewrite component to TS. Deprecate atomic types in favor of namespaces.
    Nothing changed in how component works.
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] I have updated the documentation accordingly.
    - [ ] I have added new tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- stories/components/button/tests/examples/button-link/button-link-sizes-addons.tsx
+++ stories/components/button/tests/examples/button-link/button-link-sizes-addons.tsx
@@ -4,7 +4,7 @@ import Badge from '@semcore/ui/badge';
 import { ButtonLink } from '@semcore/ui/button';
 import type { NSCounter } from '@semcore/ui/counter';
 import Counter from '@semcore/ui/counter';
-import type { SpinSize } from '@semcore/ui/spin';
+import type { NSSpin } from '@semcore/ui/spin';
 import Spin from '@semcore/ui/spin';
 import { Text } from '@semcore/ui/typography';
 import React from 'react';
@@ -25,7 +25,7 @@ const Demo = (props: ButtonLinkSizesProps) => {
   return (
     <>
       {sizes.map((size) => {
-        let spinSize: SpinSize = 'm';
+        let spinSize: NSSpin.Size = 'm';
         if (size <= 200) {
           spinSize = 'xs';
         } else if (size <= 500) {
