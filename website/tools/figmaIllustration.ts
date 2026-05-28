a94f189cb2113e93914e5b3ad5cb7b3328521f75
Sheila Sheikh <166654065+sheila-semrush@users.noreply.github.com>
2026-05-28 22:10:43 +0200

    [website] updated figma illustration script

--- website/tools/figmaIllustration.ts
+++ website/tools/figmaIllustration.ts
@@ -1,13 +1,12 @@
 import * as fs from 'fs';
 
+import type { CanvasNode, GetFileResponse, GetImagesResponse, GetProjectFilesResponse } from '@figma/rest-api-spec';
 import * as dotenv from 'dotenv';
 import fetch from 'node-fetch';
-import pLimit from 'p-limit';
 import sharp from 'sharp';
 
-const limit = pLimit(1);
-const FIGMA_PROJECT_ID = '74268036';
 const FIGMA_EXPORT_SCALE = 1.2;
+const MIN_FRAME_WIDTH_FOR_SCALE = 200;
 
 dotenv.config();
 
@@ -17,85 +16,108 @@ if (!process.env.FIGMA_API_KEY) {
   );
 }
 
+if (!process.env.FIGMA_PROJECT_ID) {
+  throw new Error(
+    'FIGMA_PROJECT_ID is missing in the .env file',
+  );
+}
+
 const figmaKey = process.env.FIGMA_API_KEY;
 
 const downloadIllustrations = async () => {
   const chosenPath = process.argv.slice(2);
 
-  const getIllustration = async (children, category, fileId) => {
-    const folderName = `./docs/${category}/${children.name
+  const getIllustration = async (page: CanvasNode, category: string, fileId: string) => {
+    const folderName = `./docs/${category}/${page.name
       .toLowerCase()
       .split(' ')
       .join('-')}/static`;
 
-    console.log('page', children.name);
+    console.log('page', page.name);
 
     if (fs.existsSync(folderName)) {
-      for (const illustration of children.children) {
-        try {
-          const response = await fetch(
-            `https://api.figma.com/v1/images/${fileId}?ids=${illustration.id}&format=png&scale=${FIGMA_EXPORT_SCALE}`,
-            { headers: { 'X-Figma-Token': figmaKey } },
-          );
+      const largeFrames = page.children.filter((frame) => 'absoluteRenderBounds' in frame && frame.absoluteRenderBounds && frame.absoluteRenderBounds.width > MIN_FRAME_WIDTH_FOR_SCALE);
+      const smallFrames = page.children.filter((frame) => 'absoluteRenderBounds' in frame && frame.absoluteRenderBounds && frame.absoluteRenderBounds.width <= MIN_FRAME_WIDTH_FOR_SCALE);
+      try {
+        // scale up only big images
+        const largeFrameIds = largeFrames.map((frame) => frame.id).join();
+        const responseLarge = await fetch(
+          `https://api.figma.com/v1/images/${fileId}?ids=${largeFrameIds}&format=png&scale=${FIGMA_EXPORT_SCALE}`,
+          { headers: { 'X-Figma-Token': figmaKey } },
+        );
 
-          const data: { images?: { id: string; name: string } } = await response.json();
-          const imageUrl = data.images[illustration.id];
+        const smallFrameIds = smallFrames.map((frame) => frame.id).join();
+        const responseSmall = await fetch(
+          `https://api.figma.com/v1/images/${fileId}?ids=${smallFrameIds}&format=png`,
+          { headers: { 'X-Figma-Token': figmaKey } },
+        );
 
-          fetch(imageUrl)
-            .then((res) => res.arrayBuffer())
-            .then((arrayBuffer) => {
-              const buffer = Buffer.from(arrayBuffer);
-              sharp(buffer)
-                .png({ compressionLevel: 2, quality: 98, adaptiveFiltering: true })
-                .toFile(`${folderName}/${illustration.name}.png`);
-            });
+        const dataLarge = await responseLarge.json() as GetImagesResponse;
+        const dataSmall = await responseSmall.json() as GetImagesResponse;
+        const imageResponses = { ...dataLarge.images, ...dataSmall.images };
 
-          const fileName = `${illustration.name}.png`;
+        page.children.forEach((frame) => {
+          const id = imageResponses[frame.id];
+          if (typeof id === 'string') {
+            fetch(id)
+              .then((res) => res.arrayBuffer())
+              .then((arrayBuffer) => {
+                const buffer = Buffer.from(arrayBuffer);
+                sharp(buffer)
+                  .png({ compressionLevel: 2, quality: 98, adaptiveFiltering: true })
+                  .toFile(`${folderName}/${frame.name}.png`);
+              });
+          }
 
+          const fileName = `${frame.name}.png`;
           console.log('illustration', fileName);
-        } catch (error) {
+        });
+      } catch (error) {
+        if (error instanceof Error) {
           console.error(error.message);
+        } else {
+          console.error(error);
         }
       }
     }
   };
 
-  const getIllustrationList = async (fileId) => {
+  const getIllustrationList = async (fileId: string) => {
     const response = await fetch(`https://api.figma.com/v1/files/${fileId}`, {
       headers: { 'X-Figma-Token': figmaKey },
     });
 
     if (response.ok) {
       try {
-        const data: { name?: string; document?: { children: { name: string }[] } } =
-          await response.json();
+        const data = await response.json() as GetFileResponse;
         const category = data.name.toLowerCase().split(' ').join('-');
 
         console.log('category', category);
-        const downloadPromises = data.document.children
+        data.document.children
           .filter(
-            (file) =>
-              chosenPath.length !== 2 || file.name.toLowerCase() === chosenPath[1].toLowerCase(),
+            (page) =>
+              chosenPath.length !== 2 || page.name.toLowerCase() === chosenPath[1].toLowerCase(),
           )
-          .map((children) =>
-            limit(async () => {
-              await getIllustration(children, category, fileId);
-            }),
+          .forEach(async (page) =>
+            await getIllustration(page, category, fileId),
           );
-        await Promise.all(downloadPromises);
       } catch (error) {
-        console.error(error.message);
+        if (error instanceof Error) {
+          console.error(error.message);
+        } else {
+          console.error(error);
+        }
       }
     }
   };
 
-  const response = await fetch(`https://api.figma.com/v1/projects/${FIGMA_PROJECT_ID}/files`, {
+  const response = await fetch(`https://api.figma.com/v1/projects/${process.env.FIGMA_PROJECT_ID}/files`, {
     headers: { 'X-Figma-Token': figmaKey },
   });
 
   if (response.ok) {
     try {
-      const data = await response.json() as { files?: { key: string; name: string }[] };
+      const data = await response.json() as GetProjectFilesResponse;
       data.files
         ?.filter((file) => !chosenPath.length || file.name.toLowerCase() === chosenPath[0])
         .map((file) => getIllustrationList(file.key));
