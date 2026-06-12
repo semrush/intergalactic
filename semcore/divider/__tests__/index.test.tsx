39452e9b7e9934164bed89165070396406a342ec
ilia.brauer <ilia.brauer@semrush.com>
2026-06-12 11:54:08 +0200

    [chore] added snapshots

--- semcore/divider/__tests__/index.test.tsx
+++ semcore/divider/__tests__/index.test.tsx
@@ -1,6 +1,24 @@
+import { extractUIName } from '@semcore/testing-utils/shared/extractUINameTree.ts';
 import { runDependencyCheckTests } from '@semcore/testing-utils/shared-tests';
-import { describe } from '@semcore/testing-utils/vitest';
+import { cleanup, render } from '@semcore/testing-utils/testing-library';
+import { beforeEach, describe, expect, test } from '@semcore/testing-utils/vitest';
+import React from 'react';
+
+import Divider from '../src';
 
 describe('Divider Dependency imports', () => {
   runDependencyCheckTests('divider');
+});
+
+describe('Divider', () => {
+  beforeEach(cleanup);
+
+  test('Verify data-ui-name', () => {
+    const divider = <Divider />;
+
+    const { container } = render(divider);
+    const result = extractUIName(container);
+
+    expect(result).toMatchSnapshot();
+  });
 });
