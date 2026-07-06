406a127255603a19781bc40860fdcd1c974f0e23
slizhevskyv-semrush <uladzislau.slizheuski@semrush.com>
2026-07-06 17:29:45 +0200

    [UIK-5379][breadcrumbs] rewrite component to TS (#3029)
    ## Changelog
    
    ### @semcore/breadcrumbs
    
    #### Fixed
    
    - Rewrite component to TS. Deprecate atomic types. Atomic types are part
    of `NSBreadcrumbs` namespace.
    
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Rewrite components to TS. Deprecated atomic types in favor namespaces.
    Nothing changed in how component works.
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] I have updated the documentation accordingly.
    - [ ] I have added new tests on added of fixed functionality.
    
    Co-authored-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- semcore/breadcrumbs/src/Breadcrumbs.tsx
+++ semcore/breadcrumbs/src/Breadcrumbs.tsx
@@ -1,23 +1,32 @@
 import { Box } from '@semcore/base-components';
+import type { Intergalactic } from '@semcore/core';
 import { createComponent, Component, sstyled, Root } from '@semcore/core';
+import type { WithI18nEnhanceProps } from '@semcore/core/lib/utils/enhances/i18nEnhance';
 import i18nEnhance from '@semcore/core/lib/utils/enhances/i18nEnhance';
 import SSeparator from '@semcore/icon/ChevronRight/m';
 import { Text } from '@semcore/typography';
 import React from 'react';
 
+import type { NSBreadcrumbs } from './Breadcrumbs.type';
 import style from './style/breadcrumbs.shadow.css';
 import { localizedMessages } from './translations/__intergalactic-dynamic-locales';
 
-class Breadcrumbs extends Component {
+class Breadcrumbs extends Component<
+  Intergalactic.InternalTypings.InferComponentProps<NSBreadcrumbs.Component>,
+  typeof Breadcrumbs.enhance,
+  {},
+  WithI18nEnhanceProps,
+  {},
+  NSBreadcrumbs.DefaultProps
+> {
   static displayName = 'Breadcrumbs';
   static style = style;
-  static enhance = [i18nEnhance(localizedMessages)];
+  static enhance = [i18nEnhance(localizedMessages)] as const;
   static defaultProps = {
     separator: <SSeparator />,
-    tag: 'nav',
     i18n: localizedMessages,
     locale: 'en',
-  };
+  } as const;
 
   getItemProps() {
     const { separator, locale } = this.asProps;
@@ -32,7 +41,7 @@ class Breadcrumbs extends Component {
     const SListContainer = 'ol';
     const { styles, getI18nText, Children } = this.asProps;
     return sstyled(styles)(
-      <SBreadcrumbs render={Box} aria-label={getI18nText('breadcrumbs')}>
+      <SBreadcrumbs tag='nav' render={Box} aria-label={getI18nText('breadcrumbs')}>
         <SListContainer>
           <Children />
         </SListContainer>
@@ -41,8 +50,13 @@ class Breadcrumbs extends Component {
   }
 }
 
-function Item(props) {
+function Item(
+  props: Intergalactic.InternalTypings.InferChildComponentProps<NSBreadcrumbs.Item.Component, typeof Breadcrumbs, 'Item'> & { tag?: Intergalactic.Tag },
+) {
   const SBreadcrumbsItem = Root;
+  // Used to tweak the root element when `disabled` is passed to the tag.
+  // The default tag is `a`, so `@ts-expect-error` is intentional.
+  // @ts-expect-error
   const { styles, separator, active, disabled, href, tabIndex, tag, ellipsis = true } = props;
   const SSeparator = 'div';
   const SListItem = 'li';
@@ -74,6 +88,9 @@ Item.defaultProps = {
  *
  * {@link https://developer.semrush.com/intergalactic/components/breadcrumbs/breadcrumbs-api/|API} | {@link https://developer.semrush.com/intergalactic/components/breadcrumbs/breadcrumbs-code/|Examples}
  */
-export default createComponent(Breadcrumbs, {
+export default createComponent<
+  NSBreadcrumbs.Component,
+  typeof Breadcrumbs
+>(Breadcrumbs, {
   Item,
 });
--- semcore/breadcrumbs/src/index.ts
+++ semcore/breadcrumbs/src/index.ts
@@ -1,1 +1,2 @@
 export { default } from './Breadcrumbs';
+export * from './Breadcrumbs.type';
