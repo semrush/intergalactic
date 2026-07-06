642d8368617000eb6c42a943d1ed180580e4c0d8
slizhevskyv-semrush <uladzislau.slizheuski@semrush.com>
2026-07-06 15:48:39 +0200

    [UIK-5340][notice] rewrite Notice/NoticeSmart to TS (#3027)
    ## Changelog
    
    ### @semcore/notice
    
    #### Fixed
    
    - Rewrite componenst to TS. Deprecate atomic types. Atomic types are
    part of `NSNotice/NSNoticeSmart` namespaces.
    
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Motivation and Context
    
    Rewrite components to TS. Deprecated atomic types in favor namespaces.
    Nothing changed in how component works.
    
    ## How has this been tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- For example: -->
    <!--- I have added unit tests -->
    <!--- I have added Voice Over tests -->
    <!--- Code cannot be tested automatically so I have tested it only
    manually -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected).
    - [x] Nice improve.
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] I have updated the documentation accordingly.
    - [ ] I have added new tests on added of fixed functionality.
    
    ---------
    
    Co-authored-by: Valeria-Zimnitskaya <valeryia.zimnitskaya@semrush.com>

--- semcore/notice/src/index.ts
+++ semcore/notice/src/index.ts
@@ -1,4 +1,6 @@
 import { default as Notice } from './Notice';
 
 export default Notice;
+export * from './Notice.type';
 export { default as NoticeSmart } from './NoticeSmart';
+export * from './NoticeSmart.type';
