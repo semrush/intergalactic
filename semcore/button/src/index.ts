bbf57f8dc6b80d4927bc4bb941bd23f66c93843d
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-07-14 11:38:53 +0200

    [UIK-5566][button] rewrite component to NS

--- semcore/button/src/index.ts
+++ semcore/button/src/index.ts
@@ -1,5 +1,4 @@
 export { default, MAP_USE_DEFAULT_THEME } from './component/Button/Button';
-export { BUTTON_USE_TO_DEFAULT_THEME } from './component/Button/mappers/BUTTON_USE_TO_DEFAULT_THEME';
 export * from './component/Button/Button.type';
 export * from './component/ButtonLink/ButtonLink';
 export * from './component/ButtonLink/ButtonLink.type';
