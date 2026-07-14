2e4d165fe9a51f97ce92e08767faa9c7cc09b9fc
ilia.brauer <ilia.brauer@semrush.com>
2026-07-14 17:13:19 +0200

    [bulk-textarea] removed role textbox

--- semcore/bulk-textarea/src/components/InputField/InputField.tsx
+++ semcore/bulk-textarea/src/components/InputField/InputField.tsx
@@ -297,7 +297,6 @@ class InputField<T extends string | string[]> extends Component<
   createContentEditableElement(props: Props<T>) {
     const textarea = document.createElement('div');
     textarea.setAttribute('contentEditable', props.disabled || props.readonly ? 'false' : 'true');
-    textarea.setAttribute('role', 'textbox');
     textarea.setAttribute('classname', 'editable');
 
     if (props.id) {
