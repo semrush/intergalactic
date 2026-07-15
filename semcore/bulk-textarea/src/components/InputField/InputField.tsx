a03bc2f0cdfb8ccf892805be14bee6e5a1653c05
ilia.brauer <ilia.brauer@semrush.com>
2026-07-15 23:43:18 +0200

    [bulk-textarea] added push to history for paste event

--- semcore/bulk-textarea/src/components/InputField/InputField.tsx
+++ semcore/bulk-textarea/src/components/InputField/InputField.tsx
@@ -405,6 +405,7 @@ class InputField<T extends string | string[]> extends Component<
   }
 
   handlePaste(event: ClipboardEvent) {
+    this.history.push(this.createHistoryState());
     this.insertFromPaste(event);
   }
 
