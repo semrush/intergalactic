940842c4f17d1b6b6e6f4d83f7fd00676a5e5e4e
Julia Mnizhek <j.mnizhek@semrush.com>
2026-07-17 21:22:34 +0200

    [input] update styles for a bunch of inputs

--- stories/components/input/docs/examples/input_with_an_icon_addon.tsx
+++ stories/components/input/docs/examples/input_with_an_icon_addon.tsx
@@ -7,25 +7,10 @@ import React from 'react';
 const Demo = () => {
   return (
     <>
-      <Text size={300} id='label-l' tag='label' htmlFor='permanent-placeholder-l-example'>
-        Input with permanent text, size L
+      <Text size={200} id='label-m' tag='label' htmlFor='permanent-placeholder-m-example'>
+        Input with icon, size M
       </Text>
       <Box mt={2} mb={4}>
-        <Input size='l' w={300}>
-          <Input.Addon>
-            <SearchM />
-          </Input.Addon>
-          <Input.Value
-            placeholder='Placeholder'
-            aria-labelledby='label-l prefix-l'
-            id='permanent-placeholder-l-example'
-          />
-        </Input>
-      </Box>
-      <Text size={200} id='label-m' tag='label' htmlFor='permanent-placeholder-m-example'>
-        Input with permanent text, size M
-      </Text>
-      <Box mt={2}>
         <Input size='m' w={300}>
           <Input.Addon>
             <SearchM />
@@ -34,6 +19,21 @@ const Demo = () => {
             placeholder='Placeholder'
             aria-labelledby='label-m prefix-m'
             id='permanent-placeholder-m-example'
+          />
+        </Input>
+      </Box>
+      <Text size={300} id='label-l' tag='label' htmlFor='permanent-placeholder-l-example'>
+        Input with icon, size L
+      </Text>
+      <Box mt={2}>
+        <Input size='l' w={300}>
+          <Input.Addon>
+            <SearchM />
+          </Input.Addon>
+          <Input.Value
+            placeholder='Placeholder'
+            aria-labelledby='label-l prefix-l'
+            id='permanent-placeholder-l-example'
           />
         </Input>
       </Box>
