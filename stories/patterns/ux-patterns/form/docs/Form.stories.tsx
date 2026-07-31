bf7fc14d177e67ace4420dab09475ca02e56117e
Julia Mnizhek <j.mnizhek@semrush.com>
2026-07-31 14:27:35 +0200

    [stories] update test Form stories

--- stories/patterns/ux-patterns/form/docs/Form.stories.tsx
+++ stories/patterns/ux-patterns/form/docs/Form.stories.tsx
@@ -3,10 +3,8 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { DefaultLogInFormTest } from './__tests__/DefaultLogInForm.test';
 import DatepickerAndTimepickerExample from './examples/datepicker-and-timepicker';
 import DefaultLogInFormExample from './examples/default-log-in-form';
-import HorizontalFormExample from './examples/horizontal-form';
 import InputTagsAndSelectExample from './examples/inputtags-and-select';
 import RadioAndSelectExample from './examples/radio-and-select';
-import VerticalFormExample from './examples/vertical-form';
 import { playWrapper } from '../../../../utils/playWrapper';
 
 const meta: Meta = {
@@ -28,14 +26,6 @@ export const DefaultLogInForm: StoryObj = {
 
 export const InputTagsAndSelect: StoryObj = {
   render: InputTagsAndSelectExample,
-};
-
-export const HorizontalForm: StoryObj = {
-  render: HorizontalFormExample,
-};
-
-export const VerticalForm: StoryObj = {
-  render: VerticalFormExample,
 };
 
 export default meta;
