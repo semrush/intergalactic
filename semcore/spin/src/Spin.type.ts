2952c3e48baa65e8c51c5d0ffe8c8a7cf03c69bc
ilia.brauer <ilia.brauer@semrush.com>
2026-08-18 12:52:37 +0200

    [spin] fixed texts in docs

--- semcore/spin/src/Spin.type.ts
+++ semcore/spin/src/Spin.type.ts
@@ -3,7 +3,7 @@ import type { Intergalactic } from '@semcore/core';
 
 declare namespace NSSpin {
   /**
-   * @deprecated
+   * @deprecated. Dark and custom themes are deprecated. Use default theme instead of dark.
    */
   type DeprecatedTheme = 'dark' | string;
   type Size = 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
@@ -12,10 +12,10 @@ declare namespace NSSpin {
      * @default m
      **/
     size?: NSSpin.Size;
-    /** Spinner theme. There are several default themes or you can use your own color
+    /** Spinner theme. Your own color is deprecated, use only `default` or `invert`.
      * @default default
      **/
-    theme?: DeprecatedTheme | 'default';
+    theme?: DeprecatedTheme | 'default' | 'invert';
     /** Whether the spinner should be in the center of the parent.
      * This works for a nested spinner in flex,
      * otherwise only horizontal alignment will occur.
