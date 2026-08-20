f2c2383514ed57d77d049328b7d24a79e7d14e97
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-08-20 12:57:46 +0200

    [UIK-5686][wizard] align checkmark

--- semcore/wizard/src/Wizard.tsx
+++ semcore/wizard/src/Wizard.tsx
@@ -1,4 +1,4 @@
-import { ScreenReaderOnly, Box } from '@semcore/base-components';
+import { ScreenReaderOnly, Box, Flex } from '@semcore/base-components';
 import Button from '@semcore/button';
 import { createComponent, Component, Root, sstyled } from '@semcore/core';
 import type { Intergalactic } from '@semcore/core';
@@ -247,8 +247,7 @@ function Stepper(
   } = props;
   const SStepper = Root;
   const SStepNumber = 'span';
-  const SStepDescription = 'span';
-  const SCompleted = CheckM;
+  const SStepDescription = Flex;
 
   const handlerClick = React.useCallback(
     (e: React.SyntheticEvent<HTMLElement>) => {
@@ -277,7 +276,7 @@ function Stepper(
 
   return sstyled(styles)(
     <SStepper
-      render={Box}
+      render={Flex}
       role='tab'
       tabIndex={disabled ? undefined : 0}
       onClick={handlerClick}
@@ -285,7 +284,7 @@ function Stepper(
     >
       {completed && <ScreenReaderOnly>{getI18nText('completedStep')}</ScreenReaderOnly>}
       <ScreenReaderOnly><Children /></ScreenReaderOnly>
-      <SStepNumber aria-hidden='true'>{completed ? <SCompleted /> : number}</SStepNumber>
+      <SStepNumber aria-hidden='true'>{completed ? <CheckM /> : number}</SStepNumber>
       <SStepDescription aria-hidden='true'>
         <Children />
       </SStepDescription>
