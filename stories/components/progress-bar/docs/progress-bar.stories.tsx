cbb72f2bc8667617a6789be2125e1ae2eddab0a2
Sheila Sheikh <166654065+sheila-semrush@users.noreply.github.com>
2026-08-27 13:10:42 +0200

    [stories] remove custom ProgressBar story

--- stories/components/progress-bar/docs/progress-bar.stories.tsx
+++ stories/components/progress-bar/docs/progress-bar.stories.tsx
@@ -2,7 +2,6 @@ import ProgressBar from '@semcore/ui/progress-bar';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import BasicUsageExample from './examples/basic-usage';
-import CustomizingTheBarExample, { defaultProps as CustomizingTheBarProps } from './examples/customizing_the_bar';
 
 const meta: Meta<typeof ProgressBar> = {
   title: 'Components/ProgressBar/Documentation',
@@ -14,22 +13,4 @@ type Story = StoryObj<typeof ProgressBar>;
 
 export const BasicUsage: Story = {
   render: BasicUsageExample,
-};
-
-export const CustomizingTheBar: StoryObj<typeof CustomizingTheBarProps> = {
-  render: CustomizingTheBarExample,
-  argTypes: {
-    size: {
-      control: { type: 'select' },
-      options: ['s', 'm', 'l'],
-    },
-    theme: {
-      control: { type: 'select' },
-      options: ['default', 'invert', 'violet-100'],
-    },
-    value: {
-      control: { type: 'number' },
-    },
-  },
-  args: CustomizingTheBarProps,
 };
