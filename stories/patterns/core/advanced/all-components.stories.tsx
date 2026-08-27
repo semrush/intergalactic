c0cb9e564c3d4eb14b826b1e186fba53232eb9f5
Julia Mnizhek <j.mnizhek@semrush.com>
2026-08-27 14:10:15 +0200

    [theme] update chart palette values

--- stories/patterns/core/advanced/all-components.stories.tsx
+++ stories/patterns/core/advanced/all-components.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import AllComponentsExample from './all-components';
+import ChartPaletteExample from './chart-palette';
 
 const meta: Meta = {
   title: 'Patterns/Core/Advanced',
@@ -15,4 +16,9 @@ export const AllComponents: Story = {
   parameters: {
     layout: 'fullscreen',
   },
+};
+
+export const ChartPalette: Story = {
+  name: 'Chart palette',
+  render: ChartPaletteExample,
 };
