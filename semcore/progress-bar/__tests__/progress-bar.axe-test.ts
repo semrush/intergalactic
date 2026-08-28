99ab23f63387dbcb11f28ea83e95922dc82e68ae
Valeryia Zimnitskaya <valeryia.zimnitskaya@semrush.com>
2026-08-28 16:34:07 +0200

    [progress-bar] update test

--- semcore/progress-bar/__tests__/progress-bar.axe-test.ts
+++ semcore/progress-bar/__tests__/progress-bar.axe-test.ts
@@ -10,13 +10,6 @@ test.describe(`@progress-bar ${TAG.ACCESSIBILITY}`, () => {
     expect(violations).toEqual([]);
   });
 
-  test('Customizing the bar', async ({ page }) => {
-    await loadPage(page, 'stories/components/progress-bar/docs/examples/customizing_the_bar.tsx', 'en');
-
-    const violations = await getAccessibilityViolations({ page });
-    expect(violations).toEqual([]);
-  });
-
   test('All themes on light and dark backgrounds', async ({ page }) => {
     await loadPage(page, 'stories/components/progress-bar/advanced/examples/all_themes.tsx', 'en');
 
