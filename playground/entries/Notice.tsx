b5e9ee2d7f67fd71e153a35db44bde9f8653e404
Sheila Sheikh <166654065+sheila-semrush@users.noreply.github.com>
2026-08-28 19:17:01 +0200

    [docs] changed button theme in playground, updated composition section

--- playground/entries/Notice.tsx
+++ playground/entries/Notice.tsx
@@ -32,7 +32,7 @@ function getJSX(props: NoticeJSXProps) {
         {props.title || props.label || props.closable ? <Notice.Text>{props.message}</Notice.Text> : props.message}
         {props.actions && (
           <Notice.Actions>
-            <Button use='primary' theme='success'>
+            <Button use='primary'>
               Wow, so cool!
             </Button>
             <Button use='tertiary'>
