4c01baf0c105d1e393853f629e6e76fbf34e65df
ilia.brauer <ilia.brauer@semrush.com>
2026-09-10 14:02:46 +0200

    [d3-chart] line redesign

--- stories/components/d3-chart/docs/examples/line-chart/basic-usage.tsx
+++ stories/components/d3-chart/docs/examples/line-chart/basic-usage.tsx
@@ -1,3 +1,4 @@
+import { Flex } from '@semcore/ui/base-components';
 import { Chart } from '@semcore/ui/d3-chart';
 import React from 'react';
 
@@ -5,14 +6,32 @@ import LineMockData from '../../../__mocks__/line';
 
 const Demo = () => {
   return (
-    <Chart.Line
-      data={data}
-      plotWidth={500}
-      plotHeight={200}
-      groupKey='x'
-      xTicksCount={data.length / 2}
-      aria-label='Line chart'
-    />
+    <Flex direction='column' gap={8}>
+      <Chart.Line
+        data={data}
+        plotWidth={500}
+        plotHeight={200}
+        groupKey='x'
+        xTicksCount={data.length / 2}
+        aria-label='Line chart'
+      />
+      <Chart.Line
+        data={LineMockData.Forecast}
+        plotWidth={500}
+        plotHeight={200}
+        groupKey='x'
+        xTicksCount={data.length / 2}
+        aria-label='Line chart'
+      />
+      <Chart.Line
+        data={LineMockData.Potential}
+        plotWidth={500}
+        plotHeight={200}
+        groupKey='x'
+        xTicksCount={data.length / 2}
+        aria-label='Line chart'
+      />
+    </Flex>
   );
 };
 
