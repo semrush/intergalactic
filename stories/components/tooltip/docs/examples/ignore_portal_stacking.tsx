a9fde334cd7b40a07b68cb6e6c2da742ae23e415
Slizhevsky Vladislav <uladzislau.slizheuski@semrush.com>
2026-09-17 12:24:32 +0200

    [UIK-5964][chore] extend ignorePortalsStacking story to have an example with select

--- stories/components/tooltip/docs/examples/ignore_portal_stacking.tsx
+++ stories/components/tooltip/docs/examples/ignore_portal_stacking.tsx
@@ -1,16 +1,21 @@
-import { Box } from '@semcore/ui/base-components';
+import { Box, Flex } from '@semcore/ui/base-components';
 import Button from '@semcore/ui/button';
 import Input from '@semcore/ui/input';
 import Modal from '@semcore/ui/modal';
+import Select from '@semcore/ui/select';
 import Tooltip from '@semcore/ui/tooltip';
 import { Text } from '@semcore/ui/typography';
 import React from 'react';
+
+const options = Array(2)
+  .fill('')
+  .map((_, index) => `Option ${index}`);
 
 const Demo = () => {
   const [visible, setVisible] = React.useState(false);
 
   return (
-    <React.Fragment>
+    <Flex gap={2} direction='column' alignItems='flex-start'>
       <Button onClick={() => setVisible(true)}>Open modal with tooltips</Button>
       <Modal visible={visible} onClose={() => setVisible(false)} w={536}>
         <Box mb={2}>
@@ -43,7 +48,27 @@ const Demo = () => {
           </Input>
         </Tooltip>
       </Modal>
-    </React.Fragment>
+      <Select>
+        <Select.Trigger placeholder='Select option' mt={2} id='select-with-tooltips' />
+        <Select.Menu>
+          <Tooltip w='100px' timeout={[0, 50]} ignorePortalsStacking placement='left'>
+            {options.map((option, index) => (
+              <Select.Option
+                value={option}
+                key={index}
+                tag={Tooltip.Trigger}
+              >
+                {option}
+              </Select.Option>
+            ))}
+            <Tooltip.Popper w={200}>
+              Tooltip Content
+            </Tooltip.Popper>
+          </Tooltip>
+        </Select.Menu>
+      </Select>
+
+    </Flex>
   );
 };
 
